1d6e9d7cc8facf7bce84ec30706da942003534d8
Emil Sjolander <emilsj@fb.com>
2017-02-20 03:33:02 -0800

    Hook up stetho dependencies
    Summary: Hook up dependencies to be able to register stetho descriptors. This needs to be in the components package to be able to expose all the internals of components (InternalNode and MountItems are needed for example)
    
    Reviewed By: passy
    
    Differential Revision: D4566452
    
    fbshipit-source-id: 8fbba3d4a16bad8469975b15d5c716cc386126e1

---
+++ src/main/java/com/facebook/components/ComponentsDescriptorProvider.java
@@ -0,0 +1,14 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.components;
+
+import com.facebook.stetho.inspector.elements.DescriptorProvider;
+import com.facebook.stetho.inspector.elements.DescriptorRegistrar;
+
+public final class ComponentsDescriptorProvider implements DescriptorProvider {
+
+  @Override
+  public void registerDescriptor(DescriptorRegistrar registrar) {
+
+  }
+}
