34ae6de84420f9ddcfeab03b24e26014cc05a5e6
Robert Spencer <rspencer@fb.com>
2017-03-02 09:25:55 -0800

    Move utils into libraries
    Summary: Moves the components utils into the libraries path for open sourcing
    
    Reviewed By: IanChilds
    
    Differential Revision: D4642493
    
    fbshipit-source-id: 827965bd556bc66309459dfea3aabbdb1c7eb799

---
+++ src/main/java/com/facebook/components/utils/IncrementalMountUtils.java
@@ -0,0 +1,106 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.components.utils;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.components.ComponentView;
+
+import static com.facebook.components.ThreadUtils.assertMainThread;
+
+/**
+ * Provides methods for enabling incremental mount.
+ */
+public class IncrementalMountUtils {
+
+  /**
+   * A view that wraps a child view and that provides a wrapped view to be incrementally mounted.
+   */
+  public interface WrapperView {
+
+    /**
+     * @return A child view that will be incrementally mounted.
+     */
+    View getWrappedView();
+  }
+
+  private static final Rect sTmpRect = new Rect();
+
+  /**
+   * Performs incremental mount on the children views of the given ViewGroup.
+   * @param scrollingViewParent ViewGroup container of views that will be incrementally mounted.
+   */
+  public static void performIncrementalMount(ViewGroup scrollingViewParent) {
+    assertMainThread();
+    final int viewGroupWidth = scrollingViewParent.getWidth();
+    final int viewGroupHeight = scrollingViewParent.getHeight();
+    for (int i = 0; i < scrollingViewParent.getChildCount(); i++) {
+      maybePerformIncrementalMountOnView(
+          viewGroupWidth,
+          viewGroupHeight,
+          scrollingViewParent.getChildAt(i));
+    }
+  }
+  /**
+   * Returns a scroll listener that performs incremental mount.  This is needed
+   * for feeds that do not use a ScrollingViewProxy, but rather directly use a
+   * RecyclerView.
+  */
+  public static RecyclerView.OnScrollListener createRecyclerViewListener() {
+    return new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+          performIncrementalMount(recyclerView);
+        }
+    };
+  }
+
+  private static void maybePerformIncrementalMountOnView(
+      int scrollingParentWidth,
+      int scrollingParentHeight,
+      View view) {
+    final View underlyingView = view instanceof WrapperView
+        ? ((WrapperView) view).getWrappedView()
+        : view;
+
+    if (!(underlyingView instanceof ComponentView)) {
+      return;
+    }
+
+    final ComponentView componentView = (ComponentView) underlyingView;
+    if (!componentView.isIncrementalMountEnabled()) {
+      return;
+    }
+
+    if (view != underlyingView && view.getHeight() != underlyingView.getHeight()) {
+      throw new IllegalStateException(
+          "ViewDiagnosticsWrapper must be the same height as the underlying view");
+    }
+
+    final int top = view.getTop();
+    final int bottom = view.getBottom();
+    final int left = view.getLeft();
+    final int right = view.getRight();
+
+    if (left >= 0 &&
+        top >= 0 &&
+        right <= scrollingParentWidth &&
+        bottom <= scrollingParentHeight &&
+        componentView.getPreviousMountBounds().width() == componentView.getWidth() &&
+        componentView.getPreviousMountBounds().height() == componentView.getHeight()) {
+      // View is fully visible, and has already been completely mounted
+      return;
+    }
+
+    sTmpRect.set(
+        Math.max(0, -left),
+        Math.max(0, -top),
+        Math.min(right, scrollingParentWidth) - left,
+        Math.min(bottom, scrollingParentHeight) - top);
+
+    componentView.performIncrementalMount(sTmpRect);
+  }
+}
