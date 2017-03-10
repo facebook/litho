b82ae818bd1e11f7a533fb916f76d24bd04a51fb
Robert Spencer <rspencer@fb.com>
2017-03-10 06:56:21 -0800

    Barebones tutorial 1
    Summary: This is the first of a set of 5 tutorials intended to get green developers up to speed with litho.
    
    Reviewed By: IanChilds
    
    Differential Revision: D4688717
    
    fbshipit-source-id: 9e6ff8f0bbee7c8535de16feacbc1e63be17e001

---
+++ sample-barebones/src/main/java/com/facebook/samples/lithobarebones/SampleActivity.java
@@ -0,0 +1,8 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.samples.lithobarebones;
+
+import android.app.Activity;
+
+public class SampleActivity extends Activity {
+}
