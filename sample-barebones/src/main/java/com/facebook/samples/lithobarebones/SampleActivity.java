84b73aab4e18a40c715356fbdd14d7c7aac91d18
Robert Spencer <rspencer@fb.com>
2017-03-10 06:56:23 -0800

    Barebones tutorial 2
    Summary: This is the second of a set of 5 tutorials intended to get green developers up to speed with litho.  It introduces a hello world text display
    
    Reviewed By: IanChilds
    
    Differential Revision: D4688792
    
    fbshipit-source-id: ca05495b2ebc1439688bde74ee7d85dd641198bf

--- sample-barebones/src/main/java/com/facebook/samples/lithobarebones/SampleActivity.java
+++ sample-barebones/src/main/java/com/facebook/samples/lithobarebones/SampleActivity.java
@@ -3,6 +3,30 @@
 package com.facebook.samples.lithobarebones;
 
 import android.app.Activity;
+import android.os.Bundle;
+import com.facebook.components.ComponentContext;
+import com.facebook.components.ComponentTree;
+import com.facebook.components.ComponentView;
+import com.facebook.components.widget.Text;
 
 public class SampleActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final ComponentView componentView = new ComponentView(this);
+        final ComponentContext context = new ComponentContext(this);
+
+        final ComponentTree componentTree = ComponentTree.create(
+            context,
+            Text.create(context)
+                        .text("Hello World")
+                        .textSizeDip(50)
+                        .build())
+                .build();
+
+        componentView.setComponent(componentTree);
+
+        setContentView(componentView);
+    }
 }
