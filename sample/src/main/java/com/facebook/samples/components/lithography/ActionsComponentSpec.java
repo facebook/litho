64e71251775d15d1c9b0966857ab5b8bf8d12a88
Robert Spencer <rspencer@fb.com>
2017-03-21 02:32:41 -0700

    Add animation to sample
    Summary: Adds some simple animation to the sample app
    
    Reviewed By: IanChilds
    
    Differential Revision: D4721586
    
    fbshipit-source-id: 38f5acdd0693ad57156c03daf7160edcf534cb23

---
+++ sample/src/main/java/com/facebook/samples/components/lithography/ActionsComponentSpec.java
@@ -0,0 +1,66 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.samples.components.lithography;
+
+import com.facebook.components.ClickEvent;
+import com.facebook.components.ComponentContext;
+import com.facebook.components.ComponentLayout;
+import com.facebook.components.Container;
+import com.facebook.components.StateValue;
+import com.facebook.components.Transition;
+import com.facebook.components.annotations.LayoutSpec;
+import com.facebook.components.annotations.OnCreateLayout;
+import com.facebook.components.annotations.OnEvent;
+import com.facebook.components.annotations.OnLayoutTransition;
+import com.facebook.components.annotations.OnUpdateState;
+import com.facebook.components.annotations.State;
+import com.facebook.yoga.YogaEdge;
+import com.facebook.yoga.YogaFlexDirection;
+import com.facebook.yoga.YogaPositionType;
+
+import static android.R.drawable.ic_media_ff;
+import static android.R.drawable.ic_media_rew;
+
+@LayoutSpec
+public class ActionsComponentSpec {
+
+  @OnCreateLayout
+  static ComponentLayout onCreateLayout(
+      ComponentContext c,
+      @State boolean snapToLeft) {
+    return Container.create(c)
+        .backgroundColor(0xDDFFFFFF)
+        .positionType(YogaPositionType.ABSOLUTE)
+        .positionDip(snapToLeft ? YogaEdge.LEFT : YogaEdge.RIGHT, 4)
+        .transitionKey("left_right_slide")
+        .positionDip(YogaEdge.TOP, 4)
+        .paddingDip(YogaEdge.ALL, 2)
+        .flexDirection(YogaFlexDirection.ROW)
+        .child(FavouriteButton.create(c))
+        .child(
+            Container.create(c)
+                .backgroundRes(snapToLeft ? ic_media_ff : ic_media_rew)
+                .widthDip(32)
+                .heightDip(32)
+                .clickHandler(ActionsComponent.onClick(c)))
+        .build();
+  }
+
+  @OnUpdateState
+  static void updateState(StateValue<Boolean> snapToLeft) {
+    snapToLeft.set(!snapToLeft.get());
+  }
+
+  @OnEvent(ClickEvent.class)
+  static void onClick(ComponentContext c) {
+    ActionsComponent.updateState(c);
+  }
+
+  @OnLayoutTransition
+  static Transition onLayoutTransition(ComponentContext c) {
+    return Transition.createSet(
+        Transition.create("left_right_slide")
+            .translationX()
+            .build());
+  }
+}
