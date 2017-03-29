e2b00d912c9aff487d60b5730b33d49871c4a7ad
marcocova <marcocova@fb.com>
2017-03-29 06:29:59 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MountState.java
+++ src/main/java/com/facebook/components/MountState.java
@@ -1332,3 +1332,4 @@ class MountState {
   private static void setFocusable(View view, @NodeInfo.FocusState short focusState) {
     if (focusState == NodeInfo.FOCUS_SET_TRUE) {
       view.setFocusable(true);
+    } else if (focusState == NodeInfo.FOCUS_SET_FALSE) {
