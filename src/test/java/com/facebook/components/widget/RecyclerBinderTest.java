861dcb52a9c450ffc43c43efe6750b437c5eee52
dnyy <dnyy@fb.com>
2017-03-29 02:44:46 -0700

    Lines authored by dnyy
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/widget/RecyclerBinderTest.java
+++ src/test/java/com/facebook/components/widget/RecyclerBinderTest.java
@@ -526,3 +526,4 @@ public class RecyclerBinderTest {
     TestComponentTreeHolder componentTreeHolder;
 
     for (int i = 0; i < components.size(); i++) {
+      componentTreeHolder = mHoldersForComponents.get(components.get(i).getComponent());
