216dc29df489dc66857fc4676e3125529c5946a9
tcm <tcm@fb.com>
2017-03-29 02:42:54 -0700

    Lines authored by tcm
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentTree.java
+++ src/main/java/com/facebook/components/ComponentTree.java
@@ -692,3 +692,8 @@ public class ComponentTree {
         null /*output */);
   }
 
+  /**
+   * Update the width/height spec. This is useful if you are currently detached and are responding
+   * to a configuration change. If you are currently attached then the HostView is the source of
+   * truth for width/height, so this call will be ignored.
+   */
