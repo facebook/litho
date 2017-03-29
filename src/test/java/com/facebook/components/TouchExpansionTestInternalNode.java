d24106fd01ce081121ec150caaa2cef416839d3e
mkillianey <mkillianey@fb.com>
2017-03-29 06:28:52 -0700

    Lines authored by mkillianey
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TouchExpansionTestInternalNode.java
+++ src/test/java/com/facebook/components/TouchExpansionTestInternalNode.java
@@ -11,3 +11,50 @@ package com.facebook.litho;
 
 /**
  * Used by the {@link com.facebook.litho.ViewNodeInfoTest}.
+ */
+public class TouchExpansionTestInternalNode extends InternalNode {
+  @Override
+  public int getX() {
+    return 10;
+  }
+
+  @Override
+  public int getY() {
+    return 10;
+  }
+
+  @Override
+  public int getHeight() {
+    return 10;
+  }
+
+  @Override
+  public int getWidth() {
+    return 10;
+  }
+
+  @Override
+  int getTouchExpansionLeft() {
+    return 1;
+  }
+
+  @Override
+  int getTouchExpansionTop() {
+    return 2;
+  }
+
+  @Override
+  int getTouchExpansionRight() {
+    return 3;
+  }
+
+  @Override
+  int getTouchExpansionBottom() {
+    return 4;
+  }
+
+  @Override
+  boolean hasTouchExpansion() {
+    return true;
+  }
+}
