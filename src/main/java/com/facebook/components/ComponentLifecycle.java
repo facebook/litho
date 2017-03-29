8ffcaeff4495c4b268f0a9e5f27a479e0a2cef1c
sriramramani <sriramramani@fb.com>
2017-03-29 02:44:41 -0700

    Lines authored by sriramramani
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -527,3 +527,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
 
   @Override
   public Object dispatchOnEvent(EventHandler eventHandler, Object eventState) {
+    // Do nothing by default.
