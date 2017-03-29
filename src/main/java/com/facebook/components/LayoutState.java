7cbc17cdae307a6282dc5c7d2d2cf42778fed5d0
realpassy <realpassy@fb.com>
2017-03-29 02:41:54 -0700

    Lines authored by realpassy
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -321,3 +321,23 @@ class LayoutState {
     return visibilityOutput;
   }
 
+  private static TestOutput createTestOutput(
+      InternalNode node,
+      LayoutState layoutState,
+      LayoutOutput layoutOutput) {
+    final int l = layoutState.mCurrentX + node.getX();
+    final int t = layoutState.mCurrentY + node.getY();
+    final int r = l + node.getWidth();
+    final int b = t + node.getHeight();
+
+    final TestOutput output = ComponentsPools.acquireTestOutput();
+    output.setTestKey(node.getTestKey());
+    output.setBounds(l, t, r, b);
+    output.setHostMarker(layoutState.mCurrentHostMarker);
+    if (layoutOutput != null) {
+      output.setLayoutOutputId(layoutOutput.getId());
+    }
+
+    return output;
+  }
+
