9b727feb2229697e6000bda3fd95afd1e8a898aa
realpassy <realpassy@fb.com>
2017-03-29 02:43:33 -0700

    Lines authored by realpassy
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentsPools.java
+++ src/main/java/com/facebook/components/ComponentsPools.java
@@ -521,3 +521,8 @@ public class ComponentsPools {
   }
 
   @ThreadSafe(enableChecks = false)
+  static void release(TestItem testItem) {
+    testItem.release();
+    sTestItemPool.release(testItem);
+  }
+
