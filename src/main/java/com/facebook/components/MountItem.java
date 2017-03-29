684f073dbc6733c6593dafe676f2a24d2a7d30c1
mng <mng@fb.com>
2017-03-29 06:27:17 -0700

    Lines authored by mng
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MountItem.java
+++ src/main/java/com/facebook/components/MountItem.java
@@ -61,3 +61,7 @@ class MountItem {
       Object content,
       LayoutOutput layoutOutput,
       DisplayListDrawable displayListDrawable) {
+    init(
+        component,
+        host,
+        content,
