4a15f97c8d29600fa5d2c3ba5699ba7a77071a90
realpassy <realpassy@fb.com>
2017-03-29 06:30:16 -0700

    Lines authored by realpassy
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MountState.java
+++ src/main/java/com/facebook/components/MountState.java
@@ -1964,3 +1964,17 @@ class MountState {
       }
     }
   }
+
+  /**
+   * @see ComponentViewTestHelper#findTestItems(ComponentView, String)
+   */
+  @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+  Deque<TestItem> findTestItems(String testKey) {
+    if (mTestItemMap == null) {
+      throw new UnsupportedOperationException("Trying to access TestItems while " +
+          "ComponentsConfiguration.isEndToEndTestRun is false.");
+    }
+
+    final Deque<TestItem> items = mTestItemMap.get(testKey);
+    return items == null ? new LinkedList<TestItem>() : items;
+  }
