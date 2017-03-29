e2b50cd53021bc591823d7c799a8635159017730
ishtiaq <ishtiaq@fb.com>
2017-03-29 06:29:14 -0700

    Lines authored by ishtiaq
    This commit forms part of the blame-preserving initial commit suite.

--- src/debug/java/com/facebook/components/testing/viewtree/LevenshteinDistance.java
+++ src/debug/java/com/facebook/components/testing/viewtree/LevenshteinDistance.java
@@ -4,3 +4,5 @@ package com.facebook.litho.testing.viewtree;
 
 public class LevenshteinDistance {
   /**
+   * Efficient version of Levenshtein Distance Algorithm. It saves CPU by returning early if
+   * the distance goes over maxAllowedEditDistance. See examples in LevenshteinDistanceTest class.
