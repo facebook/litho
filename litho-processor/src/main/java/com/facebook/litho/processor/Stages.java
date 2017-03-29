888fd1923dac5041acca83ab7cc2a5caae96df05
tcm <tcm@fb.com>
2017-03-29 02:43:05 -0700

    Lines authored by tcm
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -777,3 +777,6 @@ public class Stages {
   }
 
   /**
+   * Gather a list of parameters from the given element that are defined by the spec. That is, they
+   * aren't one of the parameters predefined for a given method. For example, OnCreateLayout has a
+   * predefined parameter of type LayoutContext. Spec-defined parameters are annotated with one of
