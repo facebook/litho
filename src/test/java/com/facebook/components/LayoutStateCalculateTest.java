c92cb25d766ec3bed52feea96cfd0e533e7965fb
emilsj <emilsj@fb.com>
2017-03-29 06:28:56 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -1789,3 +1789,5 @@ public class LayoutStateCalculateTest {
     final Component component = new InlineLayoutSpec() {
       @Override
       protected ComponentLayout onCreateLayout(ComponentContext c) {
+        return Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+            .paddingPx(YogaEdge.ALL, 2)
