e3c0c41873a4cc97626d09bef8b9a300ca8ec93d
mkillianey <mkillianey@fb.com>
2017-03-29 02:43:01 -0700

    Lines authored by mkillianey
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TouchExpansionTestInternalNode.java
+++ src/test/java/com/facebook/components/TouchExpansionTestInternalNode.java
@@ -6,3 +6,55 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+
+package com.facebook.components;
+
+/**
+ * Used by the {@link com.facebook.components.ViewNodeInfoTest}.
+ */
+public class TouchExpansionTestInternalNode extends InternalNode {
+  @Override
+  public int getX() {
+    return 10;
+  }
+
+  @Override
+  public int getY() {
+    return 10;
+  }
+
+  @Override
+  public int getHeight() {
+    return 10;
+  }
+
+  @Override
+  public int getWidth() {
+    return 10;
+  }
+
+  @Override
+  int getTouchExpansionLeft() {
+    return 1;
+  }
+
+  @Override
+  int getTouchExpansionTop() {
+    return 2;
+  }
+
+  @Override
+  int getTouchExpansionRight() {
+    return 3;
+  }
+
+  @Override
+  int getTouchExpansionBottom() {
+    return 4;
+  }
+
+  @Override
+  boolean hasTouchExpansion() {
+    return true;
+  }
+}
