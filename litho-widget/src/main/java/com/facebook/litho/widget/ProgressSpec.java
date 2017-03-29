97831ab2d0835ec50530bf5ebd7aab4c142706ec
emilsj <emilsj@fb.com>
2017-03-29 02:42:32 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/ProgressSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/ProgressSpec.java
@@ -21,3 +21,4 @@ import com.facebook.components.ComponentContext;
 import com.facebook.components.ComponentLayout;
 import com.facebook.components.Output;
 import com.facebook.components.Size;
+import com.facebook.components.SizeSpec;
