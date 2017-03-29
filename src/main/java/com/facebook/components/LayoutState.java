1d01e5b7e5e2a720154bfbdb847e44e0766c02d1
pasqualea <pasqualea@fb.com>
2017-03-29 02:44:22 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -1333,3 +1333,18 @@ class LayoutState {
   }
 
   boolean isCompatibleSpec(int widthSpec, int heightSpec) {
+    final boolean widthIsCompatible =
+        MeasureComparisonUtils.isMeasureSpecCompatible(
+            mWidthSpec,
+            widthSpec,
+            mWidth);
+
+    final boolean heightIsCompatible =
+        MeasureComparisonUtils.isMeasureSpecCompatible(
+            mHeightSpec,
+            heightSpec,
+            mHeight);
+
+    return widthIsCompatible && heightIsCompatible;
+  }
+
