2972b8fa2c572eaa20c18f5115ae6aa88b7faae5
rkhalil <rkhalil@fb.com>
2017-03-29 06:28:03 -0700

    Lines authored by rkhalil
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -189,3 +189,4 @@ public class Stages {
     mQualifiedClassName = qualifiedClassName;
     mStageAnnotations = stageAnnotations;
     mInterStagePropAnnotations = interStagePropAnnotations;
+    mClassTypeSpec = typeSpec;
