74a12df9b25be1378d4889b97c4c08abb5212e0f
ianc <ianc@fb.com>
2017-03-29 06:30:05 -0700

    Lines authored by ianc
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -1868,3 +1868,4 @@ public class Stages {
             .addModifiers(Modifier.PRIVATE)
             .superclass(
                 ParameterizedTypeName.get(
+                    SectionClassNames.SECTION,
