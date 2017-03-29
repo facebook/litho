790f40ff6aea2ea18d50a940886f3e7ee98d8b31
marcocova <marcocova@fb.com>
2017-03-29 06:29:28 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentsPools.java
+++ src/main/java/com/facebook/components/ComponentsPools.java
@@ -711,3 +711,6 @@ public class ComponentsPools {
     if (sMountContentPoolsByContext.containsKey(activity)) {
       throw new IllegalStateException("The MountContentPools has a reference to an activity" +
           "that has just been created");
+    }
+  }
+
