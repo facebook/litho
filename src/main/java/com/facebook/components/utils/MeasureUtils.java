3638e3973a7a9a84089c7122ee1df868c1dac94b
emilsj <emilsj@fb.com>
2017-03-29 06:28:06 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/utils/MeasureUtils.java
+++ src/main/java/com/facebook/components/utils/MeasureUtils.java
@@ -126,3 +126,62 @@ public final class MeasureUtils {
     outputSize.width = heightSize;
   }
 
+  /**
+   * Measure according to an aspect ratio an width and height constraints. This version
+   * of measureWithAspectRatio will respect the intrinsic size of the component being measured.
+   *
+   * @param widthSpec A SizeSpec for the width
+   * @param heightSpec A SizeSpec for the height
+   * @param intrinsicWidth A pixel value for the intrinsic width of the measured component
+   * @param intrinsicHeight A pixel value for the intrinsic height of the measured component
+   * @param aspectRatio The aspect ration size against
+   * @param outputSize The output size of this measurement
+   */
+  public static void measureWithAspectRatio(
+      int widthSpec,
+      int heightSpec,
+      int intrinsicWidth,
+      int intrinsicHeight,
+      float aspectRatio,
+      Size outputSize) {
+
+    if (SizeSpec.getMode(widthSpec) == AT_MOST &&
+        SizeSpec.getSize(widthSpec) > intrinsicWidth) {
+      widthSpec = SizeSpec.makeSizeSpec(intrinsicWidth, AT_MOST);
+    }
+
+    if (SizeSpec.getMode(heightSpec) == AT_MOST &&
+        SizeSpec.getSize(heightSpec) > intrinsicHeight) {
+      heightSpec = SizeSpec.makeSizeSpec(intrinsicHeight, AT_MOST);
+    }
+
+    measureWithAspectRatio(widthSpec, heightSpec, aspectRatio, outputSize);
+  }
+
+  /**
+   * Measure according to an aspect ratio an width and height constraints.
+   *
+   * @param widthSpec A SizeSpec for the width
+   * @param heightSpec A SizeSpec for the height
+   * @param aspectRatio The aspect ration size against
+   * @param outputSize The output size of this measurement
+   */
+  public static void measureWithAspectRatio(
+      int widthSpec,
+      int heightSpec,
+      float aspectRatio,
+      Size outputSize) {
+
+    final int widthMode = SizeSpec.getMode(widthSpec);
+    final int widthSize = SizeSpec.getSize(widthSpec);
+    final int heightMode = SizeSpec.getMode(heightSpec);
+    final int heightSize = SizeSpec.getSize(heightSpec);
+    final int widthBasedHeight = (int) Math.ceil(widthSize / aspectRatio);
+    final int heightBasedWidth = (int) Math.ceil(heightSize * aspectRatio);
+
+    if (widthMode == UNSPECIFIED && heightMode == UNSPECIFIED) {
+      outputSize.width = 0;
+      outputSize.height = 0;
+
+      if (ComponentsConfiguration.IS_INTERNAL_BUILD) {
+        Log.d(
