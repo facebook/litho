5090cecbdd32af807f525f9c4fd845628e53cb31
gkassabli <gkassabli@fb.com>
2017-03-29 06:29:52 -0700

    Lines authored by gkassabli
    This commit forms part of the blame-preserving initial commit suite.

--- lib/yogajni/yoga/Yoga.c
+++ lib/yogajni/yoga/Yoga.c
@@ -3362,3 +3362,6 @@ static void YGRoundToPixelGrid(const YGNodeRef node, const float pointScaleFacto
   }
   if (adjustedHeight - roundedHeight >= pointScaleFactor / 2.0f) {
     roundedHeight += pointScaleFactor;
+  }
+  node->layout.dimensions[YGDimensionWidth] = roundedWidth;
+  node->layout.dimensions[YGDimensionHeight] = roundedHeight;
