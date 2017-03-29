cc95e12bc1bebe0c7c7cc569458d90c7ac3106c9
ostrulovich <ostrulovich@fb.com>
2017-03-29 02:41:41 -0700

    Lines authored by ostrulovich
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/testing/viewtree/ViewPredicatesTest.java
+++ src/test/java/com/facebook/components/testing/viewtree/ViewPredicatesTest.java
@@ -28,3 +28,7 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests {@link ViewPredicates}
+ */
