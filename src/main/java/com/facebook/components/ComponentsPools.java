82f83cb0cd1427b3337eb0279f867ed314cb2e6c
aziz <aziz@fb.com>
2017-03-29 06:29:29 -0700

    Lines authored by aziz
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentsPools.java
+++ src/main/java/com/facebook/components/ComponentsPools.java
@@ -813,3 +813,16 @@ public class ComponentsPools {
     displayListDrawable.release();
     sDisplayListDrawablePool.release(displayListDrawable);
   }
+
+  public static BorderColorDrawable acquireBorderColorDrawable() {
+    if (sBorderColorDrawablePool == null) {
+      sBorderColorDrawablePool = new Pools.SynchronizedPool<>(10);
+    }
+    BorderColorDrawable drawable = sBorderColorDrawablePool.acquire();
+    if (drawable == null) {
+      drawable = new BorderColorDrawable();
+    }
+
+    return drawable;
+  }
+
