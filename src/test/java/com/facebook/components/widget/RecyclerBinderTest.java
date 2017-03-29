20ecb60752cc297db018678c17c25a7fc0fd7c38
denleybihari <denleybihari@fb.com>
2017-03-29 02:43:50 -0700

    Lines authored by denleybihari
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/widget/RecyclerBinderTest.java
+++ src/test/java/com/facebook/components/widget/RecyclerBinderTest.java
@@ -352,3 +352,4 @@ public class RecyclerBinderTest {
     int widthSpec = SizeSpec.makeSizeSpec(200, SizeSpec.EXACTLY);
     int heightSpec = SizeSpec.makeSizeSpec(200, SizeSpec.EXACTLY);
 
+    mRecyclerBinder.measure(size, widthSpec, heightSpec);
