19ac6243f6a7ac9d742d9b7c8ce89fc10c09b6ca
rratmansky <rratmansky@fb.com>
2017-03-29 02:44:40 -0700

    Lines authored by rratmansky
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -430,3 +430,6 @@ public abstract class ComponentLifecycle implements EventDispatcher {
   }
 
   protected void onBind(ComponentContext c, Object mountedContent, Component<?> component) {
+    // Do nothing by default.
+  }
+
