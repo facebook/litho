ee8b89ba2de27b46af45f0dfd83a16a5ed761c6f
pasqualea <pasqualea@fb.com>
2017-03-29 06:29:04 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentsPools.java
+++ src/main/java/com/facebook/components/ComponentsPools.java
@@ -267,3 +267,15 @@ public class ComponentsPools {
       }
     }
 
+    final Pools.SynchronizedPool<Object> pool;
+
+    synchronized (mountContentLock) {
+
+      if (sActivityCallbacks == null && !sIsManualCallbacks) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+          throw new RuntimeException(
+              "Activity callbacks must be invoked manually below ICS (API level 14)");
+        }
+        sActivityCallbacks = new PoolsActivityCallback();
+        ((Application) context.getApplicationContext())
+            .registerActivityLifecycleCallbacks(sActivityCallbacks);
