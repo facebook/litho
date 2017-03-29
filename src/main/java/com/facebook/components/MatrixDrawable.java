ae74460abd35a0539bc581d6c8967295085a7ec4
lrocha <lrocha@fb.com>
2017-03-29 02:43:05 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MatrixDrawable.java
+++ src/main/java/com/facebook/components/MatrixDrawable.java
@@ -83,3 +83,9 @@ public class MatrixDrawable<T extends Drawable> extends Drawable
 
   /**
    * Applies the given dimensions to the drawable.
+   * This method should be called in your component's @OnBind method.
+   *
+   * @param width The width of the drawable to be drawn.
+   * @param height The height of the drawable to be drawn.
+   */
+  public void bind(int width, int height) {
