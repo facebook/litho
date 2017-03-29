b6cb2e9ab9ac3d0e6014bea464534ac4646ca326
adamjernst <adamjernst@fb.com>
2017-03-29 06:30:23 -0700

    Lines authored by adamjernst
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -99,3 +99,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
       final int widthSpec;
       final int heightSpec;
 
+      ComponentsSystrace.beginSection("measure:" + component.getSimpleName());
