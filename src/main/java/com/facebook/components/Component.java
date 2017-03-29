a502b99d1ee377309c9047feef4c96061ae5e23d
lrocha <lrocha@fb.com>
2017-03-29 06:27:49 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/Component.java
+++ src/main/java/com/facebook/components/Component.java
@@ -259,3 +259,5 @@ public abstract class Component<L extends ComponentLifecycle> implements HasEven
 
   }
 
+  static boolean isHostSpec(Component<?> component) {
+    return (component != null && component.mLifecycle instanceof HostComponent);
