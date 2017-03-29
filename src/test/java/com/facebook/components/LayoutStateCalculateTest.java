a5fcda48db23b24bae8b2489a20cfcce6423a0c6
emilsj <emilsj@fb.com>
2017-03-29 06:27:45 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -644,3 +644,84 @@ public class LayoutStateCalculateTest {
 
   @Test
   public void testLayoutOutputStableIdsForMegaDeepComponent() {
+    final Component component1 = new InlineLayoutSpec() {
+      @Override
+      protected ComponentLayout onCreateLayout(ComponentContext c) {
+        return Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(TestDrawableComponent.create(c))
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(
+                        TestDrawableComponent.create(c)
+                            .withLayout().flexShrink(0)
+                            .wrapInView())
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestDrawableComponent.create(c))
+                            .wrapInView())
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestDrawableComponent.create(c)))
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestViewComponent.create(c)))
+                    .wrapInView())
+            .build();
+      }
+    };
+
+    final Component component2 = new InlineLayoutSpec() {
+      @Override
+      protected ComponentLayout onCreateLayout(ComponentContext c) {
+        return Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(TestDrawableComponent.create(c))
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(
+                        TestDrawableComponent.create(c)
+                            .withLayout().flexShrink(0)
+                            .wrapInView())
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestDrawableComponent.create(c))
+                            .wrapInView())
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestDrawableComponent.create(c)))
+                    .wrapInView())
+            .child(
+                Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                    .child(TestDrawableComponent.create(c))
+                    .child(
+                        Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+                            .child(TestDrawableComponent.create(c))
+                            .child(TestViewComponent.create(c)))
+                    .wrapInView())
+            .build();
+      }
+    };
