d01f05cfbeb70535c0d93805454b52e7159e6542
lrocha <lrocha@fb.com>
2017-03-29 06:28:41 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -1399,3 +1399,4 @@ public class LayoutStateCalculateTest {
                 TestDrawableComponent.create(c, true, true, true, true, false)
                     .withLayout().flexShrink(0)
                     .flex(1).flexBasisDip(0)
+                    .backgroundColor(Color.RED)
