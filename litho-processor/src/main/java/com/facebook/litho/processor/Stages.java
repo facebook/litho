ef48ad3c62eec8e325cc9d4f7695cd76690615dc
tcm <tcm@fb.com>
2017-03-29 06:28:15 -0700

    Lines authored by tcm
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -224,3 +224,5 @@ public class Stages {
     populateImplMembers();
     populateImplParameters();
     validateStyleOutputs();
+  }
+
--- litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
@@ -17,3 +17,4 @@ import java.util.List;
 import android.content.res.ColorStateList;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
