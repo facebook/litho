e14c01d3fb729a6b8936a661e6574e76b93dd326
marcocova <marcocova@fb.com>
2017-03-29 06:30:06 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -1198,3 +1198,4 @@ class LayoutState {
         nestedTree.getLastMeasuredHeight())) {
       if (nestedTree != null) {
         if (nestedTree != NULL_LAYOUT) {
+          releaseNodeTree(nestedTree, true /* isNestedTree */);
