e99cc122e643419c5bdb73e101445abc31137c12
weih <weih@fb.com>
2017-03-29 06:26:51 -0700

    Lines authored by weih
    This commit forms part of the blame-preserving initial commit suite.

--- litho-annotations/src/main/java/com/facebook/litho/annotations/OnCreateTreeProp.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/OnCreateTreeProp.java
@@ -16,3 +16,7 @@ import java.lang.annotation.RetentionPolicy;
  * An annotation for a Spec method that generates tree props.
  * These tree props will be passed silently to all of the Component's children.
  *
+ * Tree props are stored in a map keyed on their individual class object, meaning there will only be
+ * one entry for tree props of any given type. PLEASE DO NOT USE COMMON JAVA CLASSES, for example,
+ * String, Integer etc; creates a wrapper class instead.
+ *
