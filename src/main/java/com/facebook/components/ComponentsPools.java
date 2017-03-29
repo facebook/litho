b3039d34311eb1977fe5ab0d914124f898743b2a
lrocha <lrocha@fb.com>
2017-03-29 06:29:23 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentsPools.java
+++ src/main/java/com/facebook/components/ComponentsPools.java
@@ -644,3 +644,17 @@ public class ComponentsPools {
   }
 
   @ThreadSafe(enableChecks = false)
+  static void release(Rect rect) {
+    rect.setEmpty();
+    sRectPool.release(rect);
+  }
+
+  static Spacing acquireSpacing() {
+    Spacing spacing = sSpacingPool.acquire();
+    if (spacing == null) {
+      spacing = new Spacing();
+    }
+
+    return spacing;
+  }
+
