695c2fa2af98469a90c5e7dadd32c30b7bfeaa7e
lrocha <lrocha@fb.com>
2017-03-29 06:30:23 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -95,3 +95,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
         YogaMeasureMode heightMode) {
       final InternalNode node = (InternalNode) cssNode.getData();
       final DiffNode diffNode = node.areCachedMeasuresValid() ? node.getDiffNode() : null;
+      final Component<?> component = node.getComponent();
