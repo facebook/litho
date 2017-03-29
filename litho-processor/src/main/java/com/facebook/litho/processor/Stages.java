d9aa82a0299affe0da1f6dd18fd23c01a1b1b92a
tcm <tcm@fb.com>
2017-03-29 06:30:17 -0700

    Lines authored by tcm
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -2464,3 +2464,5 @@ public class Stages {
     methodBuilder.addParameter(eventHandlerClassName, "_eventHandler");
     for (VariableElement v : parameters) {
       methodBuilder.addParameter(ClassName.get(v.asType()), v.getSimpleName().toString());
+    }
+
