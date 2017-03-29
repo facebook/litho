5924c812589d27459cc9475be172034c778c95ae
pasqualea <pasqualea@fb.com>
2017-03-29 02:41:53 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TreeDiffingTest.java
+++ src/test/java/com/facebook/components/TreeDiffingTest.java
@@ -798,3 +798,8 @@ public class TreeDiffingTest {
         return Container.create(c)
             .alignItems(YogaAlign.FLEX_START)
             .child(Layout.create(c, component).heightPx(50))
+            .build();
+      }
+    };
+
+    LayoutState firstLayoutState = LayoutState.calculate(
