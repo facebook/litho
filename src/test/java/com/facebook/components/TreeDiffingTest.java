2957358e065d0221c30cf8c46475e703c22f900c
alextifrea <alextifrea@fb.com>
2017-03-29 06:28:11 -0700

    Lines authored by alextifrea
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TreeDiffingTest.java
+++ src/test/java/com/facebook/components/TreeDiffingTest.java
@@ -1112,3 +1112,4 @@ public class TreeDiffingTest {
 
     TestComponent nestedLeaf1 = (TestComponent) layoutState.getMountableOutputAt(2).getComponent();
     assertFalse(nestedLeaf1.wasMeasureCalled());
+    TestComponent nestedLeaf2 = (TestComponent) layoutState.getMountableOutputAt(3).getComponent();
