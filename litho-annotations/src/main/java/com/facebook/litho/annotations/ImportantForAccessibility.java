aee57dd0d7c121591456eca4dd35df9be565b5e1
Pascal Hartig <realpassy@fb.com>
2017-03-29 05:52:55 -0700

    Inline ImportantForAccessibility constants
    Reviewed By: IanChilds
    
    Differential Revision: D4794496
    
    fbshipit-source-id: 3f6f419c5f9e97cc1b0154ae37b8367e5afc6bba

--- litho-annotations/src/main/java/com/facebook/litho/annotations/ImportantForAccessibility.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/ImportantForAccessibility.java
@@ -14,13 +14,32 @@ import java.lang.annotation.RetentionPolicy;
 
 import android.support.annotation.IntDef;
 
-import android.support.v4.view.ViewCompat;
-
 @IntDef({
-    ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_AUTO,
-    ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_YES,
-    ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO,
-    ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
+    ImportantForAccessibility.IMPORTANT_FOR_ACCESSIBILITY_AUTO,
+    ImportantForAccessibility.IMPORTANT_FOR_ACCESSIBILITY_YES,
+    ImportantForAccessibility.IMPORTANT_FOR_ACCESSIBILITY_NO,
+    ImportantForAccessibility.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
 })
 @Retention(RetentionPolicy.SOURCE)
-public @interface ImportantForAccessibility {}
+public @interface ImportantForAccessibility {
+  /**
+   * Automatically determine whether a view is important for accessibility.
+   */
+  int IMPORTANT_FOR_ACCESSIBILITY_AUTO = 0x00000000;
+
+  /**
+   * The view is important for accessibility.
+   */
+  int IMPORTANT_FOR_ACCESSIBILITY_YES = 0x00000001;
+
+  /**
+   * The view is not important for accessibility.
+   */
+  int IMPORTANT_FOR_ACCESSIBILITY_NO = 0x00000002;
+
+  /**
+   * The view is not important for accessibility, nor are any of its
+   * descendant views.
+   */
+  int IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS = 0x00000004;
+}
