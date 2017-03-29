c65b2e3d6d8d87481fb9e8984b079928df60605a
mihaelao <mihaelao@fb.com>
2017-03-29 02:43:48 -0700

    Lines authored by mihaelao
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/RecyclerSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/RecyclerSpec.java
@@ -197,3 +197,4 @@ class RecyclerSpec {
     if (onScrollListener != null) {
       recyclerView.removeOnScrollListener(onScrollListener);
     }
+
--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -610,3 +610,4 @@ class LayoutState {
 
     // We must process the nodes in order so that the layout state output order is correct.
     for (int i = 0, size = node.getChildCount(); i < size; i++) {
+      collectResults(
