aac3041abd47deb4e349be5ab7acbe8e30848139
emilsj <emilsj@fb.com>
2017-03-29 02:41:48 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TreeDiffingTest.java
+++ src/test/java/com/facebook/components/TreeDiffingTest.java
@@ -721,3 +721,18 @@ public class TreeDiffingTest {
 
   @Test
   public void testLayoutOutputUpdateStateIdClash() {
+    final Component component1 = new InlineLayoutSpec() {
+      @Override
+      protected ComponentLayout onCreateLayout(ComponentContext c) {
+        return Container.create(c)
+            .child(
+                Container.create(c)
+                    .wrapInView()
+                    .child(TestDrawableComponent.create(c)))
+            .child(
+                Container.create(c)
+                    .wrapInView()
+                    .child(TestDrawableComponent.create(c)))
+            .build();
+      }
+    };
