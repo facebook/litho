62d949ff446ee04422e2b216d5289e2c02d2daa7
rkhalil <rkhalil@fb.com>
2017-03-29 06:27:50 -0700

    Lines authored by rkhalil
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
@@ -147,3 +147,5 @@ public class MountSpecHelper extends ComponentSpecHelper {
     }
 
     final MethodDescription methodDescription = new MethodDescription();
+    methodDescription.annotations = new Class[] { Override.class };
+    methodDescription.accessType = Modifier.PROTECTED;
