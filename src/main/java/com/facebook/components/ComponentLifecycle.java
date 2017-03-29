6b5e0af0323c027e7738029963feee29c9176674
ianc <ianc@fb.com>
2017-03-29 02:44:40 -0700

    Lines authored by ianc
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -489,3 +489,5 @@ public abstract class ComponentLifecycle implements EventDispatcher {
    * The number of extra accessibility nodes that this component wishes to provides to the
    * accessibility system.
    * @param component the {@link Component} for this component
+   * @return the number of extra nodes
+   */
