bf04b083c7c9c01c16bd3ef7c013e7d88b2dca24
mkonicek <mkonicek@fb.com>
2017-03-29 02:44:35 -0700

    Lines authored by mkonicek
    This commit forms part of the blame-preserving initial commit suite.

--- lib/fb/include/fb/log.h
+++ lib/fb/include/fb/log.h
@@ -319,3 +319,4 @@ FBEXPORT void setLogHandler(LogHandler logHandler);
 FBEXPORT int fb_printLog(int prio, const char* tag, const char* fmt, ...)
 #if defined(__GNUC__)
     __attribute__((format(printf, 3, 4)))
+#endif
