e256f5791a15478bad5310603a9dc492c1e5745f
alextifrea <alextifrea@fb.com>
2017-03-29 02:43:07 -0700

    Lines authored by alextifrea
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -1706,3 +1706,4 @@ public class LayoutStateCalculateTest {
     Rect mountBounds = new Rect();
     // Check host.
     assertTrue(isHostComponent(getComponentAt(layoutState, 0)));
+    layoutState.getMountableOutputAt(0).getMountBounds(mountBounds);
