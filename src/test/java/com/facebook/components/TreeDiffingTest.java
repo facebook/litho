1f418eba8a1311296a1dc8ded5482ad5ad89db33
emilsj <emilsj@fb.com>
2017-03-29 06:27:54 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TreeDiffingTest.java
+++ src/test/java/com/facebook/components/TreeDiffingTest.java
@@ -821,3 +821,7 @@ public class TreeDiffingTest {
 
     final Component secondLayoutComponent = new InlineLayoutSpec() {
       @Override
+      protected ComponentLayout onCreateLayout(ComponentContext c) {
+        return Container.create(c).flexDirection(YogaFlexDirection.COLUMN).flexShrink(0).alignContent(YogaAlign.FLEX_START)
+            .alignItems(YogaAlign.FLEX_START)
+            .child(Layout.create(c, secondComponent).flexShrink(0).heightPx(50))
