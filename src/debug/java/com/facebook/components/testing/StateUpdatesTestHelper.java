e52a4a0bac15972bbe947f672ab242dc1dfc8412
jsendros <jsendros@fb.com>
2017-03-29 06:28:06 -0700

    Lines authored by jsendros
    This commit forms part of the blame-preserving initial commit suite.

--- src/debug/java/com/facebook/components/testing/StateUpdatesTestHelper.java
+++ src/debug/java/com/facebook/components/testing/StateUpdatesTestHelper.java
@@ -62,3 +62,30 @@ public class StateUpdatesTestHelper {
       ComponentContext context,
       Component component,
       StateUpdater stateUpdater,
+      ShadowLooper layoutThreadShadowLooper,
+      boolean incrementalMountEnabled) throws Exception {
+    return getViewAfterStateUpdate(
+        context,
+        component,
+        stateUpdater,
+        new ShadowLooper[]{layoutThreadShadowLooper},
+        incrementalMountEnabled);
+  }
+
+  /**
+   *  Call a state update as specified in {@link StateUpdater#performStateUpdate(ComponentContext)}
+   *   on the component and return the updated view with the option to incrementally mount.
+   * @param context context
+   * @param component the component to update
+   * @param stateUpdater implementation of {@link StateUpdater} that triggers the state update
+   * @param loopers shadow loopers to post messages to the main thread, run in the same order they
+   * are specified
+   * @param incrementalMountEnabled whether or not to enable incremental mount for the component
+   * @return the updated ComponentView after the state update was applied
+   */
+  public static ComponentView getViewAfterStateUpdate(
+      ComponentContext context,
+      Component component,
+      StateUpdater stateUpdater,
+      ShadowLooper[] loopers,
+      boolean incrementalMountEnabled) throws Exception {
