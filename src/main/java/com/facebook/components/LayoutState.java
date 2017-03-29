88e8109cc560b40ba9fe0374f5954f13a9def6be
realpassy <realpassy@fb.com>
2017-03-29 06:30:16 -0700

    Lines authored by realpassy
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -1654,3 +1654,4 @@ class LayoutState {
    * 3) The old measureSpec is UNSPECIFIED, the new one is AT_MOST and the old measured size is
    *    smaller that the maximum size the new measureSpec will allow.
    * 4) Both measure specs are AT_MOST. The old measure spec allows a bigger size than the new and
+   *    the old measured size is smaller than the allowed max size for the new sizeSpec.
