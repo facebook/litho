c1273d07b81f807faabea54e408cee65e5e1f890
pasqualea <pasqualea@fb.com>
2017-03-29 02:44:16 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MountState.java
+++ src/main/java/com/facebook/components/MountState.java
@@ -1557,3 +1557,4 @@ class MountState {
     // This is the case when mountDiffing is enabled and unmountOrMoveOldItems() has a matching
     // sub tree. However, traversing the tree bottom-up, it needs to unmount a node holding that
     // sub tree, that will still have mounted items. (Different sequence number on LayoutOutput id)
+    if ((content instanceof ComponentHost) && !(content instanceof ComponentView)) {
