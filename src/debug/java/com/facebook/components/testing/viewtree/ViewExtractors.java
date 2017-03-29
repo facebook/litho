b4a1f0ddd6ffed521d7a01345cdf84a2508c831b
ostrulovich <ostrulovich@fb.com>
2017-03-29 02:40:39 -0700

    Lines authored by ostrulovich
    This commit forms part of the blame-preserving initial commit suite.

--- src/debug/java/com/facebook/components/testing/viewtree/ViewExtractors.java
+++ src/debug/java/com/facebook/components/testing/viewtree/ViewExtractors.java
@@ -1,3 +1,22 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 package com.facebook.components.testing.viewtree;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.components.ComponentHost;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+
+/**
+ * Function objects used for extracting specific information out of Android classes
+ */
--- src/test/java/com/facebook/components/testing/viewtree/ComponentQueriesTest.java
+++ src/test/java/com/facebook/components/testing/viewtree/ComponentQueriesTest.java
@@ -1,3 +1,4 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 package com.facebook.components.testing.viewtree;
+
