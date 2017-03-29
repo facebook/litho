95d18481d95ee545cfcbe82f2639eaf2eb1b6890
ianc <ianc@fb.com>
2017-03-29 02:44:41 -0700

    Lines authored by ianc
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -528,3 +528,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
   @Override
   public Object dispatchOnEvent(EventHandler eventHandler, Object eventState) {
     // Do nothing by default.
+    return null;
