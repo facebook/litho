620009ea61833e51c56701c3b258b77916f6ea11
emilsj <emilsj@fb.com>
2017-03-29 02:44:36 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -113,3 +113,5 @@ public abstract class ComponentLifecycle implements EventDispatcher {
       } else if (diffNode != null
           && diffNode.getLastWidthSpec() == widthSpec
           && diffNode.getLastHeightSpec() == heightSpec) {
+        outputWidth = (int) diffNode.getLastMeasuredWidth();
+        outputHeight = (int) diffNode.getLastMeasuredHeight();
