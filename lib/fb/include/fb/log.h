d1a0d706a817106da2d2c41aff2fe1b6d8142102
ritzau <ritzau@fb.com>
2017-03-29 06:30:21 -0700

    Lines authored by ritzau
    This commit forms part of the blame-preserving initial commit suite.

--- lib/fb/include/fb/log.h
+++ lib/fb/include/fb/log.h
@@ -54,3 +54,17 @@ extern "C" {
 // builds.
 // (they are borrowed from <android/log.h>)
 
+/*
+ * Android log priority values, in ascending priority order.
+ */
+typedef enum android_LogPriority {
+  ANDROID_LOG_UNKNOWN = 0,
+  ANDROID_LOG_DEFAULT, /* only for SetMinPriority() */
+  ANDROID_LOG_VERBOSE,
+  ANDROID_LOG_DEBUG,
+  ANDROID_LOG_INFO,
+  ANDROID_LOG_WARN,
+  ANDROID_LOG_ERROR,
+  ANDROID_LOG_FATAL,
+  ANDROID_LOG_SILENT, /* only for SetMinPriority(); must be last */
+} android_LogPriority;
