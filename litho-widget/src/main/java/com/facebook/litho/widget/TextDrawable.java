5f772614b74e3d80fe6c1856921e6f3bdeae3f0f
rratmansky <rratmansky@fb.com>
2017-03-29 02:42:53 -0700

    Lines authored by rratmansky
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
@@ -226,3 +226,13 @@ public class TextDrawable extends Drawable implements Touchable, TextContent {
     return mText != null ? Collections.singletonList(mText) : Collections.<CharSequence>emptyList();
   }
 
+  /**
+   * Get the clickable span that is at the exact coordinates
+   * @param x x-position of the click
+   * @param y y-position of the click
+   * @return a clickable span that's located where the click occurred,
+   *   or: {@code null} if no clickable span was located there
+   */
+  @Nullable
+  private ClickableSpan getClickableSpanInCoords(int x, int y) {
+    final int line = mLayout.getLineForVertical(y);
