2a486ea49cccf45c12533f38ac86d2abb6ef63bb
bjaeger <bjaeger@fb.com>
2017-03-29 02:44:38 -0700

    Lines authored by bjaeger
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -209,3 +209,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
     final InternalNode node;
     if (deferNestedTreeResolution) {
       node = ComponentsPools.acquireInternalNode(context, context.getResources());
+      node.markIsNestedTreeHolder(context.getTreeProps());
