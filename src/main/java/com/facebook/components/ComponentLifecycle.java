3767f15f30bf95d8a9c2c8d0016d5392ac6d6303
emilsj <emilsj@fb.com>
2017-03-29 02:44:39 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -349,3 +349,14 @@ public abstract class ComponentLifecycle implements EventDispatcher {
   }
 
   /**
+   * Called during layout calculation to determine the baseline of a component.
+   *
+   * @param c The {@link Context} used by this component.
+   * @param width The width of this component.
+   * @param height The height of this component.
+   */
+  protected int onMeasureBaseline(ComponentContext c, int width, int height) {
+    return height;
+  }
+
+  /**
