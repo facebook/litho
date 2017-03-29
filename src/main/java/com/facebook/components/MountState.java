70256f59f99c208dcca9f9d3dd23af7558505d45
realpassy <realpassy@fb.com>
2017-03-29 06:29:02 -0700

    Lines authored by realpassy
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/MountState.java
+++ src/main/java/com/facebook/components/MountState.java
@@ -323,3 +323,46 @@ class MountState {
         // The component is invisible now, but used to be visible.
         if (invisibleHandler != null) {
           EventDispatcherUtils.dispatchOnInvisible(invisibleHandler);
+        }
+
+        mVisibilityIdToItemMap.remove(visibilityOutputId);
+        ComponentsPools.release(visibilityItem);
+      }
+    }
+  }
+
+  /**
+   * Clears and re-populates the test item map if we are in e2e test mode.
+   */
+  private void processTestOutputs(LayoutState layoutState) {
+    if (mTestItemMap == null) {
+      return;
+    }
+
+    for (Collection<TestItem> items : mTestItemMap.values()) {
+      for (TestItem item : items) {
+        ComponentsPools.release(item);
+      }
+    }
+    mTestItemMap.clear();
+
+    for (int i = 0, size = layoutState.getTestOutputCount(); i < size; i++) {
+      final TestOutput testOutput = layoutState.getTestOutputAt(i);
+      final long hostMarker = testOutput.getHostMarker();
+      final long layoutOutputId = testOutput.getLayoutOutputId();
+      final MountItem mountItem =
+          layoutOutputId == -1 ? null : mIndexToItemMap.get(layoutOutputId);
+      final TestItem testItem = ComponentsPools.acquireTestItem();
+      testItem.setHost(hostMarker == -1 ? null : mHostsByMarker.get(hostMarker));
+      testItem.setBounds(testOutput.getBounds());
+      testItem.setTestKey(testOutput.getTestKey());
+      testItem.setContent(mountItem == null ? null : mountItem.getContent());
+
+      final Deque<TestItem> items = mTestItemMap.get(testOutput.getTestKey());
+      final Deque<TestItem> updatedItems =
+          items == null ? new LinkedList<TestItem>() : items;
+      updatedItems.add(testItem);
+      mTestItemMap.put(testOutput.getTestKey(), updatedItems);
+    }
+  }
+
