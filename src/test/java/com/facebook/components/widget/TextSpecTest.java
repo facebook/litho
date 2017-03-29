d01a0d8fd455400d367ca31c6b17b811f1081b21
oprisnik <oprisnik@fb.com>
2017-03-29 06:27:58 -0700

    Lines authored by oprisnik
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/widget/TextSpecTest.java
+++ src/test/java/com/facebook/components/widget/TextSpecTest.java
@@ -25,3 +25,5 @@ import org.junit.runner.RunWith;
 import org.robolectric.RuntimeEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
