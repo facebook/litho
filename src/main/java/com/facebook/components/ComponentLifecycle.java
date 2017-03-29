90389e2ac00c2e05790ae0a160b4110874aad884
marcocova <marcocova@fb.com>
2017-03-29 06:30:24 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -140,3 +140,5 @@ public abstract class ComponentLifecycle implements EventDispatcher {
           outputHeight = size.height;
 
           if (node.getDiffNode() != null) {
+            node.getDiffNode().setLastWidthSpec(widthSpec);
+            node.getDiffNode().setLastHeightSpec(heightSpec);
