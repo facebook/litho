1a7b1ddc55adf95acebd4e90aaa3d9709912ec8c
ianc <ianc@fb.com>
2017-03-29 02:42:43 -0700

    Lines authored by ianc
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -449,3 +449,5 @@ public class Stages {
                 interStagePropAnnotation.annotationType(), outputStage)) {
               throw new ComponentsProcessingException(
                   v,
+                  "Inter-stage prop declaration is incorrect, the same name and type must be " +
+                      "used in every method where the inter-stage prop is declared.");
