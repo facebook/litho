5ca5c564e150117d577c4be2f9688ba427ddbf18
adamjernst <adamjernst@fb.com>
2017-03-29 02:44:37 -0700

    Lines authored by adamjernst
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -205,3 +205,4 @@ public abstract class ComponentLifecycle implements EventDispatcher {
     populateTreeProps(component, parentTreeProps);
     context.setTreeProps(getTreePropsForChildren(context, component, parentTreeProps));
 
+    ComponentsSystrace.beginSection("createLayout:" + component.getSimpleName());
