795e14ca5496383ef364bf8e24ca65b21a11e1bf
rratmansky <rratmansky@fb.com>
2017-03-29 06:29:40 -0700

    Lines authored by rratmansky
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
@@ -363,3 +363,20 @@ public class TextDrawable extends Drawable implements Touchable, TextContent {
     setSelection(0, 0);
   }
 
+  private boolean isClickCloseToSpan(
+      ClickableSpan span,
+      Spanned buffer,
+      Layout layout,
+      Region touchAreaRegion,
+      Region clipBoundsRegion) {
+    final Region clickableSpanAreaRegion = new Region();
+    final Path clickableSpanAreaPath = new Path();
+
+    layout.getSelectionPath(
+        buffer.getSpanStart(span),
+        buffer.getSpanEnd(span),
+        clickableSpanAreaPath);
+    clickableSpanAreaRegion.setPath(clickableSpanAreaPath, clipBoundsRegion);
+
+    return clickableSpanAreaRegion.op(touchAreaRegion, Region.Op.INTERSECT);
+  }
