dbb4e1d1901c26f988bf82ea10e097a535d1a914
marcocova <marcocova@fb.com>
2017-03-29 06:28:02 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/InternalNode.java
+++ src/main/java/com/facebook/components/InternalNode.java
@@ -484,3 +484,4 @@ class InternalNode implements ComponentLayout, ComponentLayout.ContainerBuilder 
 
   @Override
   public InternalNode duplicateParentState(boolean duplicateParentState) {
+    mPrivateFlags |= PFLAG_DUPLICATE_PARENT_STATE_IS_SET;
