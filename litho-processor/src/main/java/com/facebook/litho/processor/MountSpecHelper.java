d143c4d8c133ecd1a31ec5ee6945af75acebabf0
tcm <tcm@fb.com>
2017-03-29 06:27:50 -0700

    Lines authored by tcm
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
@@ -149,3 +149,4 @@ public class MountSpecHelper extends ComponentSpecHelper {
     final MethodDescription methodDescription = new MethodDescription();
     methodDescription.annotations = new Class[] { Override.class };
     methodDescription.accessType = Modifier.PROTECTED;
+    methodDescription.name = "onMeasure";
