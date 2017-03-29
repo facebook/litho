c49b2857369551d45be34b72f4618e7b9f106a37
eyal <eyal@fb.com>
2017-03-29 06:28:23 -0700

    Lines authored by eyal
    This commit forms part of the blame-preserving initial commit suite.

--- src/debug/java/com/facebook/components/testing/viewtree/ComponentQueries.java
+++ src/debug/java/com/facebook/components/testing/viewtree/ComponentQueries.java
@@ -21,3 +21,4 @@ class ComponentQueries {
   /**
    * Checks whether the given {@link ComponentHost} has the given text as it's text. It does not
    * look at the host's children.
+   * @param host the component host
