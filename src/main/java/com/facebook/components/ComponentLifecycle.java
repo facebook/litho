d8aa8b8954b1dadf63669aefc83954e5ea2aadde
ianc <ianc@fb.com>
2017-03-29 02:44:41 -0700

    Lines authored by ianc
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -566,3 +566,17 @@ public abstract class ComponentLifecycle implements EventDispatcher {
     return true;
   }
 
+  /**
+   * Whether the component needs updating.
+   * <p>
+   * For layout components, the framework will verify that none of the children of the component
+   * need updating, and that both components have the same number of children. Therefore this
+   * method just needs to determine any changes to the top-level component that would cause it to
+   * need to be updated (for example, a click handler was added).
+   * <p>
+   * For mount specs, the framework does nothing extra and this method alone determines whether the
+   * component is updated or not.
+   * @param previous the previous component to compare against.
+   * @param next the component that is now in use.
+   * @return true if the component needs an update, false otherwise.
+   */
