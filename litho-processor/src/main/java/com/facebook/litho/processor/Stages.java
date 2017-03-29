0304c50803988d0ace16137e377f1c23d15ffeea
mihaelao <mihaelao@fb.com>
2017-03-29 06:30:06 -0700

    Lines authored by mihaelao
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -1878,3 +1878,4 @@ public class Stages {
 
     stateClassBuilder.addField(stateContainerImplClass, STATE_CONTAINER_IMPL_MEMBER);
     stateClassBuilder.addMethod(generateStateContainerGetter(SectionClassNames.STATE_CONTAINER_SECTION));
+
