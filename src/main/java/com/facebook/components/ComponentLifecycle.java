a01b75c15ac30567acc5e4e0ccd3fdbade5a2709
pasqualea <pasqualea@fb.com>
2017-03-29 02:44:37 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -169,3 +169,5 @@ public abstract class ComponentLifecycle implements EventDispatcher {
     return onCreateMountContent(c);
   }
 
+  void mount(ComponentContext c, Object convertContent, Component<?> component) {
+    onMount(c, convertContent, component);
