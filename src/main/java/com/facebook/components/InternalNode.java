3bea9ecec1cacbad89a5b7e97738d15cfb3d8c5f
marcocova <marcocova@fb.com>
2017-03-29 06:28:36 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/InternalNode.java
+++ src/main/java/com/facebook/components/InternalNode.java
@@ -1207,3 +1207,4 @@ class InternalNode implements ComponentLayout, ComponentLayout.ContainerBuilder 
 
   @Override
   public InternalNode viewTag(Object viewTag) {
+    getOrCreateNodeInfo().setViewTag(viewTag);
