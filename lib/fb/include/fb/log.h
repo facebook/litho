50ebc866c5c7d171e126c92960de9b7910b095de
mkonicek <mkonicek@fb.com>
2017-03-29 02:44:36 -0700

    Lines authored by mkonicek
    This commit forms part of the blame-preserving initial commit suite.

--- lib/fb/include/fb/log.h
+++ lib/fb/include/fb/log.h
@@ -330,3 +330,4 @@ FBEXPORT int fb_printLog(int prio, const char* tag, const char* fmt, ...)
 #define fb_writeLog(prio, tag, text) __android_log_write(prio, tag, text)
 
 #define fb_bWriteLog(tag, payload, len) __android_log_bwrite(tag, payload, len)
+#define fb_btWriteLog(tag, type, payload, len) \
