073c1093d38edaf74428ac72b9492470661b9ca8
lrocha <lrocha@fb.com>
2017-03-29 02:42:39 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/MountSpecHelper.java
@@ -389,3 +389,4 @@ public class MountSpecHelper extends ComponentSpecHelper {
     final ExecutableElement onCreateMountContent = Utils.getAnnotatedMethod(
         mStages.getSourceElement(),
         OnCreateMountContent.class);
+
