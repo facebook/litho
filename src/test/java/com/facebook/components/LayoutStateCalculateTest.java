9682218f32410dd4ae7010b4c9c414510c2b613e
marcocova <marcocova@fb.com>
2017-03-29 06:28:57 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -1890,3 +1890,6 @@ public class LayoutStateCalculateTest {
 
     // Check total layout outputs.
     assertEquals(2, layoutState.getMountableOutputCount());
+    Rect mountBounds = new Rect();
+    // Check host.
+    assertTrue(isHostComponent(getComponentAt(layoutState, 0)));
