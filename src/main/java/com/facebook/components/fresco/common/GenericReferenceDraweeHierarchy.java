8eedcaf2bd58e030b5b9b2176a087c7a8d08cbed
jonchiang <jonchiang@fb.com>
2017-03-29 02:42:26 -0700

    Lines authored by jonchiang
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/fresco/common/GenericReferenceDraweeHierarchy.java
+++ src/main/java/com/facebook/components/fresco/common/GenericReferenceDraweeHierarchy.java
@@ -16,3 +16,4 @@ import android.support.annotation.Nullable;
 
 import com.facebook.components.ComponentContext;
 import com.facebook.components.reference.Reference;
+import com.facebook.drawee.drawable.AutoRotateDrawable;
