649a68701aaf59de3dffb7752915e49fe934ffeb
pasqualea <pasqualea@fb.com>
2017-03-29 02:44:29 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/LayoutState.java
+++ src/main/java/com/facebook/components/LayoutState.java
@@ -1647,3 +1647,10 @@ class LayoutState {
   /**
    * Check if a cached nested tree has compatible SizeSpec to be reused as is or
    * if it needs to be recomputed.
+   *
+   * The conditions to be able to re-use previous measurements are:
+   * 1) The measureSpec is the same
+   * 2) The new measureSpec is EXACTLY and the last measured size matches the measureSpec size.
+   * 3) The old measureSpec is UNSPECIFIED, the new one is AT_MOST and the old measured size is
+   *    smaller that the maximum size the new measureSpec will allow.
+   * 4) Both measure specs are AT_MOST. The old measure spec allows a bigger size than the new and
