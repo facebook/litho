cde164a66e7aa284c8d7a30a77a8eba86ef68133
lrocha <lrocha@fb.com>
2017-03-29 06:29:45 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
+++ litho-processor/src/main/java/com/facebook/litho/processor/Stages.java
@@ -1403,3 +1403,4 @@ public class Stages {
         .eventId(getElementId(element))
         .eventName(element.getSimpleName().toString())
         .contextClass(contextClassName)
+        .eventHandlerClassName(
