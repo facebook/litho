e2df80cd0c23c603fa3c88be0ba3796f64394666
lrocha <lrocha@fb.com>
2017-03-29 02:43:44 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/ImageSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/ImageSpec.java
@@ -173,3 +173,9 @@ class ImageSpec {
   @OnBind
   static void onBind(
       ComponentContext c,
+      MatrixDrawable mountedDrawable,
+      @FromBoundsDefined Integer drawableWidth,
+      @FromBoundsDefined Integer drawableHeight) {
+    mountedDrawable.bind(drawableWidth, drawableHeight);
+  }
+
