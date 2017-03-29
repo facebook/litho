7f2e7b9886a536bf425b1679f7fd6a40f74096f2
lrocha <lrocha@fb.com>
2017-03-29 06:28:15 -0700

    Lines authored by lrocha
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentHost.java
+++ src/main/java/com/facebook/components/ComponentHost.java
@@ -488,3 +488,4 @@ public class ComponentHost extends ViewGroup {
    * Sets a touch listener on this host.
    * @param listener The listener to set on this host.
    */
+  void setComponentTouchListener(ComponentTouchListener listener) {
