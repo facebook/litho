f996770828f59b20ff8533e327490ebba371c3ab
pasqualea <pasqualea@fb.com>
2017-03-29 02:44:40 -0700

    Lines authored by pasqualea
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ComponentLifecycle.java
+++ src/main/java/com/facebook/components/ComponentLifecycle.java
@@ -434,3 +434,6 @@ public abstract class ComponentLifecycle implements EventDispatcher {
   }
 
   protected void onUnbind(ComponentContext c, Object mountedContent, Component<?> component) {
+    // Do nothing by default.
+  }
+
