967b44bfb55299f665fca2c51f3af5c046b7d0f3
rratmansky <rratmansky@fb.com>
2017-03-29 06:29:39 -0700

    Lines authored by rratmansky
    This commit forms part of the blame-preserving initial commit suite.

--- litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextDrawable.java
@@ -246,3 +246,65 @@ public class TextDrawable extends Drawable implements Touchable, TextContent {
     }
 
     if (x >= start && x <= end) {
+      final int offset = mLayout.getOffsetForHorizontal(line, x);
+
+      final ClickableSpan[] clickableSpans = ((Spanned) mText).getSpans(
+          offset,
+          offset,
+          ClickableSpan.class);
+
+      if (clickableSpans != null && clickableSpans.length > 0) {
+        return clickableSpans[0];
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Get the clickable span that's close to where the view was clicked.
+   * @param x x-position of the click
+   * @param y y-position of the click
+   * @return a clickable span that's close the click position,
+   *   or: {@code null} if no clickable span was close to the click,
+   *   or if a link was directly clicked or if more than one clickable
+   *   span was in proximity to the click.
+   */
+  @Nullable
+  private ClickableSpan getClickableSpanInProximityToClick(
+      float x,
+      float y,
+      float tapRadius) {
+    final Region touchAreaRegion= new Region();
+    final Region clipBoundsRegion = new Region();
+
+    if (mTouchAreaPath == null) {
+      mTouchAreaPath = new Path();
+    }
+
+    clipBoundsRegion.set(
+        0,
+        0,
+        LayoutMeasureUtil.getWidth(mLayout),
+        LayoutMeasureUtil.getHeight(mLayout));
+    mTouchAreaPath.reset();
+    mTouchAreaPath.addCircle(x, y, tapRadius, Path.Direction.CW);
+    touchAreaRegion.setPath(mTouchAreaPath, clipBoundsRegion);
+
+    ClickableSpan result = null;
+    for (ClickableSpan span : mClickableSpans) {
+      if (!isClickCloseToSpan(span, (Spanned) mText, mLayout, touchAreaRegion, clipBoundsRegion)) {
+        continue;
+      }
+
+      if (result != null) {
+        // This is the second span that's close to the tap, so we don't have a definitive answer
+        return null;
+      }
+
+      result = span;
+    }
+
+    return result;
+  }
+
