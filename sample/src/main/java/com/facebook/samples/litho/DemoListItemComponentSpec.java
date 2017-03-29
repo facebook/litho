b6921e173d390949b7e3d8f889d5da9363b8809e
rspencer <rspencer@fb.com>
2017-03-29 06:27:31 -0700

    Lines authored by rspencer
    This commit forms part of the blame-preserving initial commit suite.

--- sample/src/main/java/com/facebook/samples/litho/DemoListItemComponentSpec.java
+++ sample/src/main/java/com/facebook/samples/litho/DemoListItemComponentSpec.java
@@ -52,3 +52,4 @@ public class DemoListItemComponentSpec {
       @FromEvent View view,
       @Prop final String name) {
     final Intent intent = new Intent(c, DemoActivity.class);
+    intent.putExtra("demoName", name);
--- src/debug/java/com/facebook/components/testing/viewtree/ComponentQueries.java
+++ src/debug/java/com/facebook/components/testing/viewtree/ComponentQueries.java
@@ -8,3 +8,5 @@ import java.util.regex.Pattern;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
+import com.facebook.litho.ComponentHost;
+import com.facebook.litho.MatrixDrawable;
