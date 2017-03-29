01d1b418b5e6c0e80191fe5fadf8af6921298fbd
marcocova <marcocova@fb.com>
2017-03-29 02:41:57 -0700

    Lines authored by marcocova
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/LayoutStateCalculateTest.java
+++ src/test/java/com/facebook/components/LayoutStateCalculateTest.java
@@ -628,3 +628,5 @@ public class LayoutStateCalculateTest {
         RuntimeEnvironment.application,
         component2,
         -1,
+        SizeSpec.makeSizeSpec(350, SizeSpec.EXACTLY),
+        SizeSpec.makeSizeSpec(20, SizeSpec.EXACTLY));
