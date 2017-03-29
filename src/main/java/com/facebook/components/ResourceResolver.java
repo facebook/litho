639d9194f2fccc099d4720d3ba112844a331eaad
emilsj <emilsj@fb.com>
2017-03-29 02:41:32 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/main/java/com/facebook/components/ResourceResolver.java
+++ src/main/java/com/facebook/components/ResourceResolver.java
@@ -247,3 +247,24 @@ public class ResourceResolver {
   }
 
   protected final int resolveIntAttr(@AttrRes int attrResId, @IntegerRes int defResId) {
+    mAttrs[0] = attrResId;
+    TypedArray a = mTheme.obtainStyledAttributes(mAttrs);
+
+    try {
+      return a.getInt(0, resolveIntRes(defResId));
+    } finally {
+      a.recycle();
+    }
+  }
+
+  public final int[] resolveIntArrayAttr(@AttrRes int attrResId, @ArrayRes int defResId) {
+    mAttrs[0] = attrResId;
+    TypedArray a = mContext.getTheme().obtainStyledAttributes(mAttrs);
+
+    try {
+      return resolveIntArrayRes(a.getResourceId(0, defResId));
+    } finally {
+      a.recycle();
+    }
+  }
+
