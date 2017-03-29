05aad6ee98d8e45162fb7ccd94cad7e591b23c6a
emilsj <emilsj@fb.com>
2017-03-29 02:42:03 -0700

    Lines authored by emilsj
    This commit forms part of the blame-preserving initial commit suite.

--- src/test/java/com/facebook/components/TreeDiffingTest.java
+++ src/test/java/com/facebook/components/TreeDiffingTest.java
@@ -910,3 +910,4 @@ public class TreeDiffingTest {
       @Override
       protected ComponentLayout onCreateLayout(ComponentContext c) {
         return Container.create(c)
+            .paddingPx(YogaEdge.ALL, 2)
