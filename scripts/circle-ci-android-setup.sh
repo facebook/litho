f588abff6304b1afc06002dab93c345fcf3e93ee
Pascal Hartig <realpassy@fb.com>
2017-03-31 08:26:51 -0700

    Update build tool retrieval
    Summary:
    According to https://discuss.circleci.com/t/android-build-tools-23-0-2-not-available/455/3
    you need to download the version of the build tools we depend on separately now.
    
    Reviewed By: rspencer01
    
    Differential Revision: D4810277
    
    fbshipit-source-id: e9d1cae967235e80457ac67395ae7f3fbd187289

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -6,7 +6,8 @@ function getAndroidSDK {
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    echo y | android update sdk --no-ui --all --filter extra-android-m2repository &&
+    echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository,android-23 &&
+    echo y | android update sdk --no-ui --all --filter build-tools-23.0.2 &&
     touch $DEPS
   fi
 }
