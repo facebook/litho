6a4745d79c909febf882f8ded75b35364a5c6883
Pascal Hartig <realpassy@fb.com>
2017-04-04 09:04:26 -0700

    Move remaining utils tests to OSS
    Summary: per title
    
    Reviewed By: IanChilds
    
    Differential Revision: D4826531
    
    fbshipit-source-id: 08df5b5ab1b4173bfbc960b8772e20dc95ddbc63

---
+++ litho-core/src/test/java/com/facebook/litho/utils/IncrementalMountUtilsTest.java
@@ -0,0 +1,273 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.litho.utils;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.litho.ComponentView;
+import com.facebook.litho.testing.testrunner.ComponentsTestRunner;
+import com.facebook.litho.utils.IncrementalMountUtils.WrapperView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests {@link IncrementalMountUtils}
+ */
+@RunWith(ComponentsTestRunner.class)
+public class IncrementalMountUtilsTest {
+  private static final int SCROLLING_VIEW_WIDTH = 100;
+  private static final int SCROLLING_VIEW_HEIGHT = 1000;
+
+  public TestWrapperView mWrapperView = mock(TestWrapperView.class);
+  public ComponentView mComponentView = mock(ComponentView.class);
+  public ViewGroup mViewGroup = mock(ViewGroup.class);
+
+  private final Rect mMountedRect = new Rect();
+
+  @Before
+  public void setUp() {
+    when(mComponentView.isIncrementalMountEnabled()).thenReturn(true);
+    when(mWrapperView.getWrappedView()).thenReturn(mComponentView);
+
+    when(mViewGroup.getChildCount()).thenReturn(1);
+    when(mViewGroup.getChildAt(0)).thenReturn(mComponentView);
+    when(mViewGroup.getWidth()).thenReturn(SCROLLING_VIEW_WIDTH);
+    when(mViewGroup.getHeight()).thenReturn(SCROLLING_VIEW_HEIGHT);
+
+    // Can't use verify as the rect is reset when it is released back to the pool, which occurs
+    // before we can check it.
+    doAnswer(
+        new Answer() {
+          @Override
+          public Void answer(InvocationOnMock invocation) throws Throwable {
+            mMountedRect.set((Rect) invocation.getArguments()[0]);
+            return null;
+          }
+        }).when(mComponentView).performIncrementalMount(any(Rect.class));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtTop() {
+    setupViewBounds(mComponentView, 0, -10, SCROLLING_VIEW_WIDTH, 10);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 10, SCROLLING_VIEW_WIDTH, 20));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtTopWithTranslationYPartialIn() {
+    setupViewBounds(mComponentView, 0, -10, SCROLLING_VIEW_WIDTH, 10);
+    setupViewTranslations(mComponentView, 0, 5);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 5, SCROLLING_VIEW_WIDTH, 20));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtTopWithTranslationYFullyOut() {
+    setupViewBounds(mComponentView, 0, -10, SCROLLING_VIEW_WIDTH, 10);
+    setupViewTranslations(mComponentView, 0, -15);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verify(mComponentView, never()).performIncrementalMount(any(Rect.class));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtLeft() {
+    setupViewBounds(mComponentView, -10, 0, 10, SCROLLING_VIEW_HEIGHT);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(10, 0, 20, SCROLLING_VIEW_HEIGHT));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtLeftWithTranslationXFullyIn() {
+    setupViewBounds(mComponentView, -10, 0, 10, SCROLLING_VIEW_HEIGHT);
+    setupViewTranslations(mComponentView, 15, 0);
+    setupComponentViewPreviousBounds(mComponentView, 20, SCROLLING_VIEW_HEIGHT);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verify(mComponentView, never()).performIncrementalMount(any(Rect.class));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtLeftWithTranslationXPartialOut() {
+    setupViewBounds(mComponentView, -10, 0, 10, SCROLLING_VIEW_HEIGHT);
+    setupViewTranslations(mComponentView, -7, 0);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(17, 0, 20, SCROLLING_VIEW_HEIGHT));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtBottom() {
+    setupViewBounds(
+        mComponentView,
+        0,
+        SCROLLING_VIEW_HEIGHT - 5,
+        SCROLLING_VIEW_WIDTH,
+        SCROLLING_VIEW_HEIGHT + 5);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 0, SCROLLING_VIEW_WIDTH, 5));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewVisibleAtRight() {
+    setupViewBounds(
+        mComponentView,
+        SCROLLING_VIEW_WIDTH - 5,
+        0,
+        SCROLLING_VIEW_WIDTH + 5,
+        SCROLLING_VIEW_HEIGHT);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 0, 5, SCROLLING_VIEW_HEIGHT));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewNewlyFullyVisible() {
+    setupViewBounds(mComponentView, 0, 10, SCROLLING_VIEW_WIDTH, 20);
+    setupComponentViewPreviousBounds(mComponentView, SCROLLING_VIEW_WIDTH, 5);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 0, SCROLLING_VIEW_WIDTH, 10));
+  }
+
+  @Test
+  public void testIncrementalMountForComponentViewAlreadyFullyVisible() {
+    setupViewBounds(mComponentView, 0, 10, SCROLLING_VIEW_WIDTH, 20);
+    setupComponentViewPreviousBounds(mComponentView, SCROLLING_VIEW_WIDTH, 10);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verify(mComponentView, never()).performIncrementalMount(any(Rect.class));
+  }
+
+  @Test
+  public void testNoIncrementalMountWhenNotEnabled() {
+    setupViewBounds(
+        mComponentView,
+        0,
+        SCROLLING_VIEW_HEIGHT - 5,
+        SCROLLING_VIEW_WIDTH,
+        SCROLLING_VIEW_HEIGHT + 5);
+    when(mComponentView.isIncrementalMountEnabled()).thenReturn(false);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verify(mComponentView, never()).performIncrementalMount(any(Rect.class));
+  }
+
+  @Test
+  public void testIncrementalMountForWrappedViewAtTop() {
+    when(mViewGroup.getChildAt(0)).thenReturn(mWrapperView);
+    setupViewBounds(mWrapperView, 0, -10, SCROLLING_VIEW_WIDTH, 10);
+    setupViewBounds(mComponentView, 0, 0, SCROLLING_VIEW_WIDTH, 20);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 10, SCROLLING_VIEW_WIDTH, 20));
+  }
+
+  @Test
+  public void testIncrementalMountForWrappedViewAtBottom() {
+    when(mViewGroup.getChildAt(0)).thenReturn(mWrapperView);
+    setupViewBounds(
+        mWrapperView,
+        0,
+        SCROLLING_VIEW_HEIGHT - 5,
+        SCROLLING_VIEW_WIDTH,
+        SCROLLING_VIEW_HEIGHT + 5);
+    setupViewBounds(mComponentView, 0, 0, SCROLLING_VIEW_WIDTH, 10);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 0, SCROLLING_VIEW_WIDTH, 5));
+  }
+
+  @Test
+  public void testIncrementalMountForWrappedComponentViewNewlyFullyVisible() {
+    when(mViewGroup.getChildAt(0)).thenReturn(mWrapperView);
+    setupViewBounds(mWrapperView, 0, 10, SCROLLING_VIEW_WIDTH, 20);
+    setupViewBounds(mComponentView, 0, 0, SCROLLING_VIEW_WIDTH, 10);
+    setupComponentViewPreviousBounds(mComponentView, SCROLLING_VIEW_WIDTH, 5);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verifyPerformIncrementalMountCalled(new Rect(0, 0, SCROLLING_VIEW_WIDTH, 10));
+  }
+
+  @Test
+  public void testIncrementalMountForWrappedComponentViewAlreadyFullyVisible() {
+    when(mViewGroup.getChildAt(0)).thenReturn(mWrapperView);
+    setupViewBounds(mWrapperView, 0, 10, SCROLLING_VIEW_WIDTH, 20);
+    setupViewBounds(mComponentView, 0, 0, SCROLLING_VIEW_WIDTH, 10);
+    setupComponentViewPreviousBounds(mComponentView, SCROLLING_VIEW_WIDTH, 10);
+
+    IncrementalMountUtils.performIncrementalMount(mViewGroup);
+
+    verify(mComponentView, never()).performIncrementalMount(any(Rect.class));
+  }
+
+  private static void setupViewBounds(View view, int l, int t, int r, int b) {
+    when(view.getLeft()).thenReturn(l);
+    when(view.getTop()).thenReturn(t);
+    when(view.getRight()).thenReturn(r);
+    when(view.getBottom()).thenReturn(b);
+    when(view.getWidth()).thenReturn(r - l);
+    when(view.getHeight()).thenReturn(b - t);
+  }
+
+  private static void setupViewTranslations(View view, float translationX, float translationY) {
+    when(view.getTranslationX()).thenReturn(translationX);
+    when(view.getTranslationY()).thenReturn(translationY);
+  }
+
+  private static void setupComponentViewPreviousBounds(
+      ComponentView componentView,
+      int width,
+      int height) {
+    when(componentView.getPreviousMountBounds()).thenReturn(new Rect(0, 0, width, height));
+  }
+
+  private void verifyPerformIncrementalMountCalled(Rect rect) {
+    assertThat(mMountedRect).isEqualTo(rect);
+  }
+
+  public static class TestWrapperView extends View implements WrapperView {
+
+    public TestWrapperView(Context context) {
+      super(context);
+    }
+
+    @Override
+    public View getWrappedView() {
+      return null;
+    }
+  }
+}
---
+++ litho-core/src/test/java/com/facebook/litho/utils/MeasureUtilsTest.java
@@ -0,0 +1,385 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.litho.utils;
+
+import android.view.View.MeasureSpec;
+
+import com.facebook.litho.Size;
+import com.facebook.litho.SizeSpec;
+import com.facebook.litho.testing.testrunner.ComponentsTestRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.facebook.litho.SizeSpec.AT_MOST;
+import static com.facebook.litho.SizeSpec.EXACTLY;
+import static com.facebook.litho.SizeSpec.UNSPECIFIED;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(ComponentsTestRunner.class)
+public class MeasureUtilsTest {
+
+  @Test
+  public void testWidthExactlyHeightAtMost() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        0.5f,
+        size);
+
+    assertEquals(10, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthExactlyHeightUnspecified() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        0.5f,
+        size);
+
+    assertEquals(10, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightExactly() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        2f,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(10, size.height);
+  }
+
+  @Test
+  public void testWidthUnspecifiedHeightExactly() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        2f,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(10, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightAtMostWidthSmaller() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(10, AT_MOST),
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        0.5f,
+        size);
+
+    assertEquals(10, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightAtMostHeightSmaller() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(10, AT_MOST),
+        2f,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(10, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightUnspecified() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        1f,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthUnspecifiedHeightAtMost() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        1f,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWithInstrinsicSize() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        10,
+        10,
+        1f,
+        size);
+
+    assertEquals(10, size.width);
+    assertEquals(10, size.height);
+  }
+
+  @Test
+  public void testWidthExactlyHeightAtMostEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        1,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightExactlyEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        1,
+        size);
+
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthUnspecifiedHeightUnspecified() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        10,
+        size);
+
+    assertEquals(0, size.width);
+    assertEquals(0, size.height);
+  }
+
+  @Test
+  public void testWidthExactlyHeightTooSmall() {
+    final Size size = new Size();
+    MeasureUtils.measureWithAspectRatio(
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        0.1f,
+        size);
+
+    assertEquals(10, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testWidthUnspecifiedHeightUnspecifiedEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        size);
+
+    assertEquals(0, size.width);
+    assertEquals(0, size.height);
+  }
+
+  @Test
+  public void testWidthAtMostHeightAtMostEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(10, AT_MOST),
+        size);
+    assertEquals(10, size.width);
+    assertEquals(10, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void textAtMostUnspecifiedEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, AT_MOST),
+        SizeSpec.makeSizeSpec(10, UNSPECIFIED),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(10, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        size);
+    assertEquals(30, size.width);
+    assertEquals(30, size.height);
+  }
+
+  @Test
+  public void testExactlyUnspecifiedEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        SizeSpec.makeSizeSpec(10, UNSPECIFIED),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        size);
+    assertEquals(10, size.width);
+    assertEquals(10, size.height);
+  }
+
+  @Test
+  public void testExactlyAtMostSmallerEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        SizeSpec.makeSizeSpec(10, AT_MOST),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(10, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(10, AT_MOST),
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        size);
+    assertEquals(10, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testExactlyAtMostLargerEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(30, AT_MOST),
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void textExactWidthExactHeightEqual() {
+    final Size size = new Size();
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(20, EXACTLY),
+        SizeSpec.makeSizeSpec(10, EXACTLY),
+        size);
+    assertEquals(20, size.width);
+    assertEquals(10, size.height);
+
+    MeasureUtils.measureWithEqualDimens(
+        SizeSpec.makeSizeSpec(30, EXACTLY),
+        SizeSpec.makeSizeSpec(30, EXACTLY),
+        size);
+    assertEquals(30, size.width);
+    assertEquals(30, size.height);
+  }
+
+  @Test
+  public void testGetViewMeasureSpecExactly() {
+    assertEquals(
+        MeasureUtils.getViewMeasureSpec(MeasureSpec.makeMeasureSpec(10, MeasureSpec.EXACTLY)),
+        MeasureUtils.getViewMeasureSpec(SizeSpec.makeSizeSpec(10, SizeSpec.EXACTLY)));
+  }
+
+  @Test
+  public void testGetViewMeasureSpecAtMost() {
+    assertEquals(
+        MeasureUtils.getViewMeasureSpec(MeasureSpec.makeMeasureSpec(10, MeasureSpec.AT_MOST)),
+        MeasureUtils.getViewMeasureSpec(SizeSpec.makeSizeSpec(10, SizeSpec.AT_MOST)));
+  }
+
+  @Test
+  public void testGetViewMeasureSpecUnspecified() {
+    assertEquals(
+        MeasureUtils.getViewMeasureSpec(MeasureSpec.makeMeasureSpec(10, MeasureSpec.UNSPECIFIED)),
+        MeasureUtils.getViewMeasureSpec(SizeSpec.makeSizeSpec(10, SizeSpec.UNSPECIFIED)));
+  }
+
+  @Test
+  public void testMeasureWithDesiredSizeAndExactlySpec() {
+    final Size size = new Size();
+    MeasureUtils.measureWithDesiredPx(
+        SizeSpec.makeSizeSpec(50, EXACTLY),
+        SizeSpec.makeSizeSpec(30, EXACTLY),
+        80,
+        20,
+        size);
+    assertEquals(50, size.width);
+    assertEquals(30, size.height);
+  }
+
+  @Test
+  public void testMeasureWithDesiredSizeAndLargerAtMostSpec() {
+    final Size size = new Size();
+    MeasureUtils.measureWithDesiredPx(
+        SizeSpec.makeSizeSpec(81, AT_MOST),
+        SizeSpec.makeSizeSpec(21, AT_MOST),
+        80,
+        20,
+        size);
+    assertEquals(80, size.width);
+    assertEquals(20, size.height);
+  }
+
+  @Test
+  public void testMeasureWithDesiredSizeAndSmallerAtMostSpec() {
+    final Size size = new Size();
+    MeasureUtils.measureWithDesiredPx(
+        SizeSpec.makeSizeSpec(79, AT_MOST),
+        SizeSpec.makeSizeSpec(19, EXACTLY),
+        80,
+        20,
+        size);
+    assertEquals(79, size.width);
+    assertEquals(19, size.height);
+  }
+
+  @Test
+  public void testMeasureWithDesiredSizeAndUnspecifiedSpec() {
+    final Size size = new Size();
+    MeasureUtils.measureWithDesiredPx(
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+        80,
+        20,
+        size);
+    assertEquals(80, size.width);
+    assertEquals(20, size.height);
+  }
+}
