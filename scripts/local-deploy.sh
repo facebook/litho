4a8c2b3d7a84231f8fd08c436cbc1a5a41d953cd
Pascal Hartig <realpassy@fb.com>
2017-04-08 10:29:23 -0700

    Update deployment script
    Summary:
    This makes some changes to the local deployment script. Namely, it renames the
    metadata XML files to match the expected remote ones and generates the
    checksums.
    
    Side note: MD5? Seriously?
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4855387
    
    fbshipit-source-id: 3b647e1c40881200590504555b4a32dd52fa171a

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -5,8 +5,11 @@ shopt -s globstar
 
 # Deploys build artifacts to your local m2 repository.
 VERSION="0.1.0-SNAPSHOT"
+TMPDIR=/tmp/m2tmp
 
 ./gradlew assemble
+
+mkdir -p $TMPDIR
 
 for file in **/build/libs/*.jar; do
   ARTIFACT=$(basename "$file" | sed 's/\.jar$//')
@@ -16,7 +19,9 @@ for file in **/build/libs/*.jar; do
     -DgroupId=com.facebook.litho \
     -DartifactId=$ARTIFACT \
     -Dversion=$VERSION \
-    -Dpackaging=jar
+    -Dpackaging=jar \
+    -DcreateChecksum=true \
+    -DlocalRepositoryPath=$TMPDIR
 done
 
 for file in **/build/outputs/**/*-release.aar; do
@@ -27,5 +32,16 @@ for file in **/build/outputs/**/*-release.aar; do
     -DgroupId=com.facebook.litho \
     -DartifactId=$ARTIFACT \
     -Dversion=$VERSION \
-    -Dpackaging=aar
+    -Dpackaging=aar \
+    -DcreateChecksum=true \
+    -DlocalRepositoryPath=$TMPDIR
 done
+
+for file in $TMPDIR/**/maven-metadata-local.xml*; do
+  RENAMED=$(echo $file | sed 's/-local\.xml/.xml/')
+  mv $file $RENAMED
+done
+
+echo "Local artifact deployment complete."
+echo "You can now upload you stuff, e.g. with "
+echo "rsync --delete -avP /tmp/m2tmp/ $USER@remotehost:www/html/m2repo/"
