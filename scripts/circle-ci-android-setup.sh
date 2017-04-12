0d3007729a6db3fafc5667440feef0942cf102ae
Pascal Hartig <realpassy@fb.com>
2017-04-12 01:31:37 -0700

    Install build tools 25.0.2
    Summary:
    These are used by our gradle build (and it fails if we don't do that), see
    https://circleci.com/gh/facebook/litho/126
    
    Reviewed By: IanChilds
    
    Differential Revision: D4875002
    
    fbshipit-source-id: da1b9bc8c0b221c01846b1ce17c545930183b10c

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -3,13 +3,8 @@
 function getAndroidSDK {
   export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-  DEPS="$ANDROID_HOME/installed-dependencies"
-
-  if [ ! -e $DEPS ]; then
-    echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository,android-23 &&
-    echo y | android update sdk --no-ui --all --filter build-tools-23.0.2 &&
-    touch $DEPS
-  fi
+  echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository,android-23 &&
+  echo y | android update sdk --no-ui --all --filter build-tools-23.0.2 build-tools-25.0.2 &&
 }
 
 function waitForAVD {
