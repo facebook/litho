e19d4f7ba83d799a1982e0f5852cb5f54212ac70
Pascal Hartig <realpassy@fb.com>
2017-04-12 09:25:22 -0700

    Attempt to install with sdkmanager
    Summary:
    So, the Android Tools folks decided to replace `android` as SDK manager and
    replaced it with the ... LOVELY sdkmanager tool which won't even give you a full
    list of things it can install. Anyway, it appears that build tools newer than
    23.0.2 aren't available on the old one, so we have to use the new one for the
    more recent stuff.
    
    Let's see if this works for Circle.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4876010
    
    fbshipit-source-id: bd307cbedbd1ef0237661ded0d9909f0b601ecda

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -4,7 +4,7 @@ function getAndroidSDK {
   export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
   echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository,android-23 &&
-  echo y | android update sdk --no-ui --all --filter build-tools-23.0.2 build-tools-25.0.2
+  sdkmanager 'build-tools;25.0.2'
 }
 
 function waitForAVD {
