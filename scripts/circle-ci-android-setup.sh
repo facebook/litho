479155a962614ffbec8940a314666697a6d03292
Pascal Hartig <realpassy@fb.com>
2017-04-17 00:32:50 -0700

    Add Gradle support for CircleCI
    Summary:
    Ooph, this took a while, but you can see the result here:
    https://circleci.com/gh/facebook/litho/213
    
    I've ended up rebuilding the entire SDK installation and setup as the stuff we
    get from Circle is seriously outdated.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4897439
    
    fbshipit-source-id: 9981ec1841897eb6cdebb2c769f4550349625f67

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -1,41 +1,17 @@
-# inspired by https://github.com/Originate/guide/blob/master/android/guide/Continuous%20Integration.md
+function installAndroidSDK {
+  # temporarily
+  rm -rf /home/ubuntu/android-sdk
 
-function getAndroidSDK {
-  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+  TMP=/tmp/sdk$$.zip
+  curl -L -o $TMP 'https://dl.google.com/android/repository/tools_r25.2.3-linux.zip'
+  unzip -d /home/ubuntu/android-sdk $TMP
+  rm $TMP
 
-  DEPS="$ANDROID_HOME/installed-dependencies"
+  export ANDROID_HOME=/home/ubuntu/android-sdk
+  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH"
 
-  if [ ! -e $DEPS ]; then
-    echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository,android-23 &&
-    echo y | android update sdk --no-ui --all --filter build-tools-23.0.2 &&
-    touch $DEPS
-  fi
-}
+  echo > $ANDROID_HOME/licenses/android-sdk-license
+  echo -n 8933bad161af4178b1185d1a37fbf41ea5269c55 > $ANDROID_HOME/licenses/android-sdk-license
 
-function waitForAVD {
-  local bootanim=""
-  export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
-  until [[ "$bootanim" =~ "stopped" ]]; do
-    sleep 5
-    bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-    echo "emulator status=$bootanim"
-  done
-}
-
-function retry3 {
-  local n=1
-  local max=3
-  local delay=1
-  while true; do
-    "$@" && break || {
-      if [[ $n -lt $max ]]; then
-        ((n++))
-        echo "Command failed. Attempt $n/$max:"
-        sleep $delay;
-      else
-        echo "The command has failed after $n attempts." >&2
-        return 1
-      fi
-    }
-  done
+  echo y | sdkmanager 'build-tools;23.0.2' 'build-tools;25.0.2' 'build-tools;25.0.1' 'platforms;android-23' 'platforms;android-25' 'ndk-bundle' 'extras;android;m2repository'
 }
