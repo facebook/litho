56beffe12666aeade21c52f3a3151dc0044f2d5b
Andy Street <andrews@fb.com>
2017-04-27 18:39:48 -0700

    Move transitioning component state to AnimationState
    Summary: This isn't supposed to be a change in functionality, just a change to the way we track things: instead of having a bunch of maps, we can instead just a map that points to the animation state per transition key, and then recycle that object.
    
    Reviewed By: marco-cova
    
    Differential Revision: D4956834
    
    fbshipit-source-id: dededdbf364c9c602b4fd623da2efa33229bffb0

--- litho-core/src/main/java/com/facebook/litho/DataFlowTransitionManager.java
+++ litho-core/src/main/java/com/facebook/litho/DataFlowTransitionManager.java
@@ -10,13 +10,13 @@
 package com.facebook.litho;
 
 import java.util.ArrayList;
-import java.util.WeakHashMap;
 
 import android.support.v4.util.Pools;
 import android.support.v4.util.SimpleArrayMap;
 
 import com.facebook.litho.animation.AnimatedPropertyNode;
 import com.facebook.litho.animation.AnimationBinding;
+import com.facebook.litho.animation.AnimationBindingListener;
 import com.facebook.litho.animation.LazyValue;
 import com.facebook.litho.animation.AnimatedProperty;
 import com.facebook.litho.animation.Resolver;
@@ -28,51 +28,73 @@ import com.facebook.litho.internal.ArraySet;
  */
 public class DataFlowTransitionManager {
 
-  private static Pools.SimplePool<TransitionDiff> sTransitionDiffPool = new Pools.SimplePool<>(20);
+  private static final Pools.SimplePool<TransitionDiff> sTransitionDiffPool =
+      new Pools.SimplePool<>(20);
+  private static final Pools.SimplePool<AnimationState> sAnimationStatePool =
+      new Pools.SimplePool<>(20);
 
+  /**
+   * The before and after values of single component undergoing a transition.
+   */
   private static class TransitionDiff {
 
     public final SimpleArrayMap<AnimatedProperty, Float> beforeValues = new SimpleArrayMap<>();
     public final SimpleArrayMap<AnimatedProperty, Float> afterValues = new SimpleArrayMap<>();
     public int changeType = TransitionManager.KeyStatus.DISAPPEARED;
-    public Object mountItem;
 
     public void reset() {
       beforeValues.clear();
       afterValues.clear();
       changeType = TransitionManager.KeyStatus.DISAPPEARED;
+    }
+  }
+
+  /**
+   * Animation state of a MountItem.
+   */
+  private static class AnimationState {
+
+    public final ArraySet<AnimationBinding> activeAnimations = new ArraySet<>();
+    public final SimpleArrayMap<AnimatedProperty, AnimatedPropertyNode> animatedPropertyNodes =
+        new SimpleArrayMap<>();
+    public ArraySet<AnimatedProperty> animatingProperties = new ArraySet<>();
+    public Object mountItem;
+
+    public void reset() {
+      activeAnimations.clear();
+      animatedPropertyNodes.clear();
+      animatingProperties.clear();
       mountItem = null;
     }
   }
 
   private final ArrayList<AnimationBinding> mAnimationBindings = new ArrayList<>();
-  private final SimpleArrayMap<String, TransitionDiff> mKeyToTransitionDiffs =
+  private final SimpleArrayMap<AnimationBinding, ArraySet<String>> mAnimationsToKeys =
       new SimpleArrayMap<>();
-  private final SimpleArrayMap<String, ArrayList<AnimatedProperty>> mKeyToAnimatingProperties =
-      new SimpleArrayMap<>();
-  private final WeakHashMap<Object, SimpleArrayMap<String, AnimatedPropertyNode>>
-      mAnimatedPropertyNodes = new WeakHashMap<>();
+  private final SimpleArrayMap<String, TransitionDiff> mTransitionDiffs = new SimpleArrayMap<>();
+  private final SimpleArrayMap<String, AnimationState> mAnimationStates = new SimpleArrayMap<>();
+  private final TransitionsAnimationBindingListener mAnimationBindingListener =
+      new TransitionsAnimationBindingListener();
   private final TransitionsResolver mResolver = new TransitionsResolver();
 
   void onNewTransitionContext(TransitionContext transitionContext) {
     mAnimationBindings.clear();
-    mKeyToAnimatingProperties.clear();
-    for (int i = 0, size = mKeyToTransitionDiffs.size(); i < size; i++) {
-      releaseTransitionDiff(mKeyToTransitionDiffs.valueAt(i));
+    for (int i = 0, size = mTransitionDiffs.size(); i < size; i++) {
+      releaseTransitionDiff(mTransitionDiffs.valueAt(i));
     }
-    mKeyToTransitionDiffs.clear();
+    mTransitionDiffs.clear();
 
     mAnimationBindings.addAll(transitionContext.getTransitionAnimationBindings());
     recordAllTransitioningProperties();
   }
 
   void onPreMountItem(String transitionKey, Object mountItem) {
-    final ArrayList<AnimatedProperty> animatingProperties =
-        mKeyToAnimatingProperties.get(transitionKey);
-    if (animatingProperties != null) {
+
+    final AnimationState animationState = mAnimationStates.get(transitionKey);
+    if (animationState != null) {
       final TransitionDiff info = acquireTransitionDiff();
-      for (int i = 0; i < animatingProperties.size(); i++) {
-        final AnimatedProperty prop = animatingProperties.get(i);
+      for (int i = 0; i < animationState.animatingProperties.size(); i++) {
+        final AnimatedProperty prop = animationState.animatingProperties.valueAt(i);
         info.beforeValues.put(prop, prop.get(mountItem));
 
         // Unfortunately, we have no guarantee that this mountItem won't be re-used for another
@@ -80,35 +102,27 @@ public class DataFlowTransitionManager {
         // mount happens. The proper before-values will be set again before any animations start.
         prop.reset(mountItem);
       }
-      info.mountItem = mountItem;
-      mKeyToTransitionDiffs.put(transitionKey, info);
+      setMountItem(animationState, mountItem);
+      mTransitionDiffs.put(transitionKey, info);
     }
   }
 
   void onPostMountItem(String transitionKey, Object mountItem) {
-    final ArrayList<AnimatedProperty> animatingProperties =
-        mKeyToAnimatingProperties.get(transitionKey);
-    if (animatingProperties != null) {
-      TransitionDiff info = mKeyToTransitionDiffs.get(transitionKey);
+    final AnimationState animationState = mAnimationStates.get(transitionKey);
+    if (animationState != null) {
+      TransitionDiff info = mTransitionDiffs.get(transitionKey);
       if (info == null) {
         info = acquireTransitionDiff();
         info.changeType = TransitionManager.KeyStatus.APPEARED;
-        info.mountItem = mountItem;
-        mKeyToTransitionDiffs.put(transitionKey, info);
+        mTransitionDiffs.put(transitionKey, info);
       } else {
         info.changeType = TransitionManager.KeyStatus.UNCHANGED;
       }
 
-      // If these are different, this means this transition key will be rendered with a different
-      // mount item (View or Drawable) than it was during the last mount, so we need to migrate
-      // animation state from the old mount item to the new one.
-      if (info.mountItem != mountItem) {
-        migrateToNewMountItem(info.mountItem, mountItem);
-        info.mountItem = mountItem;
-      }
+      setMountItem(animationState, mountItem);
 
-      for (int i = 0; i < animatingProperties.size(); i++) {
-        final AnimatedProperty prop = animatingProperties.get(i);
+      for (int i = 0; i < animationState.animatingProperties.size(); i++) {
+        final AnimatedProperty prop = animationState.animatingProperties.valueAt(i);
         info.afterValues.put(prop, prop.get(mountItem));
       }
     }
@@ -119,17 +133,20 @@ public class DataFlowTransitionManager {
     setDisappearToValues();
     for (int i = 0, size = mAnimationBindings.size(); i < size; i++) {
       final AnimationBinding binding = mAnimationBindings.get(i);
+      binding.addListener(mAnimationBindingListener);
       binding.start(mResolver);
     }
   }
 
   private void restoreInitialStates() {
-    for (int i = 0; i < mKeyToTransitionDiffs.size(); i++) {
-      final TransitionDiff diff = mKeyToTransitionDiffs.valueAt(i);
+    for (int i = 0; i < mTransitionDiffs.size(); i++) {
+      final String transitionKey = mTransitionDiffs.keyAt(i);
+      final TransitionDiff diff = mTransitionDiffs.valueAt(i);
+      final AnimationState animationState = mAnimationStates.get(transitionKey);
       if (diff.changeType == TransitionManager.KeyStatus.UNCHANGED) {
         for (int j = 0; j < diff.beforeValues.size(); j++) {
           final AnimatedProperty property = diff.beforeValues.keyAt(j);
-          property.set(diff.mountItem, diff.beforeValues.valueAt(j));
+          property.set(animationState.mountItem, diff.beforeValues.valueAt(j));
         }
       }
     }
@@ -146,9 +163,9 @@ public class DataFlowTransitionManager {
     for (int i = 0, size = appearFromValues.size(); i < size; i++) {
       final ComponentProperty property = appearFromValues.keyAt(i);
       final LazyValue lazyValue = appearFromValues.valueAt(i);
-      final TransitionDiff diff = mKeyToTransitionDiffs.get(property.getTransitionKey());
+      final AnimationState animationState = mAnimationStates.get(property.getTransitionKey());
       final float value = lazyValue.resolve(mResolver, property);
-      property.getProperty().set(diff.mountItem, value);
+      property.getProperty().set(animationState.mountItem, value);
     }
   }
 
@@ -162,7 +179,7 @@ public class DataFlowTransitionManager {
     for (int i = 0, size = disappearToValues.size(); i < size; i++) {
       final ComponentProperty property = disappearToValues.keyAt(i);
       final LazyValue lazyValue = disappearToValues.valueAt(i);
-      final TransitionDiff diff = mKeyToTransitionDiffs.get(property.getTransitionKey());
+      final TransitionDiff diff = mTransitionDiffs.get(property.getTransitionKey());
       if (diff.changeType != TransitionManager.KeyStatus.DISAPPEARED) {
         throw new RuntimeException("Wrong transition type for disappear: " + diff.changeType);
       }
@@ -176,54 +193,65 @@ public class DataFlowTransitionManager {
    * items so that we know whether to record them in onPre/PostMountItem
    */
   private void recordAllTransitioningProperties() {
-    ArraySet<ComponentProperty> transitioningProperties = new ArraySet<>();
+    final ArraySet<ComponentProperty> transitioningProperties = ComponentsPools.acquireArraySet();
     for (int i = 0, size = mAnimationBindings.size(); i < size; i++) {
       final AnimationBinding binding = mAnimationBindings.get(i);
-      binding.collectTransitioningProperties(transitioningProperties);
-    }
+      final ArraySet<String> animatedKeys = ComponentsPools.acquireArraySet();
+      mAnimationsToKeys.put(binding, animatedKeys);
 
-    for (int i = 0, size = transitioningProperties.size(); i < size; i++) {
-      final ComponentProperty property = transitioningProperties.valueAt(i);
-      final String key = property.getTransitionKey();
-      final AnimatedProperty animatedProperty = property.getProperty();
-      ArrayList<AnimatedProperty> propertiesForKey = mKeyToAnimatingProperties.get(key);
-      if (propertiesForKey == null) {
-        propertiesForKey = new ArrayList<>();
-        mKeyToAnimatingProperties.put(key, propertiesForKey);
+      binding.collectTransitioningProperties(transitioningProperties);
+
+      for (int j = 0, propSize = transitioningProperties.size(); j < propSize; j++) {
+        final ComponentProperty property = transitioningProperties.valueAt(j);
+        final String key = property.getTransitionKey();
+        final AnimatedProperty animatedProperty = property.getProperty();
+        animatedKeys.add(key);
+
+        // This key will be animating - make sure it has an AnimationState
+        AnimationState animationState = mAnimationStates.get(key);
+        if (animationState == null) {
+          animationState = acquireAnimationState();
+          mAnimationStates.put(key, animationState);
+        }
+        animationState.animatingProperties.add(animatedProperty);
+        animationState.activeAnimations.add(binding);
       }
-      if (!propertiesForKey.contains(property)) {
-        propertiesForKey.add(animatedProperty);
-      }
+      transitioningProperties.clear();
     }
+    ComponentsPools.release(transitioningProperties);
   }
 
   private AnimatedPropertyNode getOrCreateAnimatedPropertyNode(
-      Object mountItem,
+      String key,
       AnimatedProperty animatedProperty) {
-    SimpleArrayMap<String, AnimatedPropertyNode> animatedProperties =
-        mAnimatedPropertyNodes.get(mountItem);
-    if (animatedProperties == null) {
-      animatedProperties = new SimpleArrayMap<>();
-      mAnimatedPropertyNodes.put(mountItem, animatedProperties);
-    }
-    AnimatedPropertyNode node = animatedProperties.get(animatedProperty.getName());
+    final AnimationState state = mAnimationStates.get(key);
+    AnimatedPropertyNode node = state.animatedPropertyNodes.get(animatedProperty);
     if (node == null) {
-      node = new AnimatedPropertyNode(mountItem, animatedProperty);
-      animatedProperties.put(animatedProperty.getName(), node);
+      node = new AnimatedPropertyNode(state.mountItem, animatedProperty);
+      state.animatedPropertyNodes.put(animatedProperty, node);
     }
     return node;
   }
 
-  private void migrateToNewMountItem(Object oldMountItem, Object newMountItem) {
-    SimpleArrayMap<String, AnimatedPropertyNode> oldItemNodes =
-        mAnimatedPropertyNodes.remove(oldMountItem);
-    if (oldItemNodes == null) {
+  private void setMountItem(AnimationState animationState, Object newMountItem) {
+    // If the mount item changes, this means this transition key will be rendered with a different
+    // mount item (View or Drawable) than it was during the last mount, so we need to migrate
+    // animation state from the old mount item to the new one.
+
+    if (animationState.mountItem == newMountItem) {
       return;
     }
-    for (int i = 0, size = oldItemNodes.size(); i < size; i++) {
-      oldItemNodes.valueAt(i).setMountItem(newMountItem);
+
+    if (animationState.mountItem != null) {
+      final ArraySet<AnimatedProperty> animatingProperties = animationState.animatingProperties;
+      for (int i = 0, size = animatingProperties.size(); i < size; i++) {
+        animatingProperties.valueAt(i).reset(animationState.mountItem);
+      }
     }
-    mAnimatedPropertyNodes.put(newMountItem, oldItemNodes);
+    for (int i = 0, size = animationState.animatedPropertyNodes.size(); i < size; i++) {
+      animationState.animatedPropertyNodes.valueAt(i).setMountItem(newMountItem);
+    }
+    animationState.mountItem = newMountItem;
   }
 
   private static TransitionDiff acquireTransitionDiff() {
@@ -239,24 +267,67 @@ public class DataFlowTransitionManager {
     sTransitionDiffPool.release(diff);
   }
 
+  private static AnimationState acquireAnimationState() {
+    AnimationState animationState = sAnimationStatePool.acquire();
+    if (animationState == null) {
+      animationState = new AnimationState();
+    }
+    return animationState;
+  }
+
+  private static void releaseAnimationState(AnimationState animationState) {
+    animationState.reset();
+    sAnimationStatePool.release(animationState);
+  }
+
+  private class TransitionsAnimationBindingListener implements AnimationBindingListener {
+
+    @Override
+    public void onStart(AnimationBinding binding) {
+    }
+
+    @Override
+    public void onFinish(AnimationBinding binding) {
+      final ArraySet<String> transitioningKeys = mAnimationsToKeys.remove(binding);
+
+      // When an animation finishes, we want to go through all the mount items it was animating and
+      // see if it was the last active animation. If it was, we know that item is no longer
+      // animating and we can release the animation state.
+      
+      for (int i = 0, size = transitioningKeys.size(); i < size; i++) {
+        final String key = transitioningKeys.valueAt(i);
+        final AnimationState animationState = mAnimationStates.get(key);
+        if (!animationState.activeAnimations.remove(binding)) {
+          throw new RuntimeException(
+              "Some animation bookkeeping is wrong: tried to remove an animation from the list " +
+                  "of active animations, but it wasn't there.");
+        }
+        if (animationState.activeAnimations.size() == 0) {
+          mAnimationStates.remove(key);
+          releaseAnimationState(animationState);
+        }
+      }
+      ComponentsPools.release(transitioningKeys);
+    }
+  }
+
   private class TransitionsResolver implements Resolver {
 
     @Override
     public float getCurrentState(ComponentProperty property) {
-      final TransitionDiff diff = mKeyToTransitionDiffs.get(property.getTransitionKey());
-      return property.getProperty().get(diff.mountItem);
+      final AnimationState animationState = mAnimationStates.get(property.getTransitionKey());
+      return property.getProperty().get(animationState.mountItem);
     }
 
     @Override
     public float getEndState(ComponentProperty property) {
-      final TransitionDiff diff = mKeyToTransitionDiffs.get(property.getTransitionKey());
+      final TransitionDiff diff = mTransitionDiffs.get(property.getTransitionKey());
       return diff.afterValues.get(property.getProperty());
     }
 
     @Override
     public AnimatedPropertyNode getAnimatedPropertyNode(ComponentProperty property) {
-      final TransitionDiff diff = mKeyToTransitionDiffs.get(property.getTransitionKey());
-      return getOrCreateAnimatedPropertyNode(diff.mountItem, property.getProperty());
+      return getOrCreateAnimatedPropertyNode(property.getTransitionKey(), property.getProperty());
     }
   }
 }
