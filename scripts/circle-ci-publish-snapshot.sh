cd854195a4603ca37d13cc0f6df9fe5ea70885e2
Pascal Hartig <realpassy@fb.com>
2017-05-05 11:57:20 -0700

    Set up automatic snapshot releases
    Summary:
    This will automatically publish a SNAPSHOT to Sonatype's snapshot repo if the
    following conditions are met:
    
    - It is a SNAPSHOT release.
    - It's from our facebook/litho repo.
    - It's not a pull request.
    
    Reviewed By: IanChilds
    
    Differential Revision: D5010084
    
    fbshipit-source-id: b447d8c3d80979cd9ede5321c9976de68d114cdd

---
+++ scripts/circle-ci-publish-snapshot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Deploy a SNAPSHOT JAR after every successful Circle CI To Sonatype.
+# See https://circleci.com/docs/1.0/environment-variables/
+#
+
+set -e
+
+BASEDIR="$(realpath "$(dirname "$0")/..")"
+IS_SNAPSHOT="$(grep 'VERSION_NAME=[0-9\.]\+-SNAPSHOT' "$BASEDIR/gradle.properties")"
+
+if [ "$CIRCLE_PROJECT_USERNAME" != "facebook" ]; then
+  echo "Skipping repository. Expected project username to be 'facebook', but was '$CIRCLE_PROJECT_USERNAME'."
+  exit
+elif [ "$CIRCLE_PROJECT_REPONAME" != "litho" ]; then
+  echo "Skipping repository. Expected project name to be 'litho', but was '$CIRCLE_PROJECT_REPONAME'."
+  exit
+elif [ "$CIRCLE_BRANCH" != "master" ]; then
+  echo "Skipping build. Expected branch name to be 'master', but was '$CIRCLE_BRANCH'."
+  exit
+elif [ "$CI_PULL_REQUEST" != "" ]; then
+  echo "Skipping build. Only considering non-PR builds, but URL was '$CI_PULL_REQUEST'."
+  exit
+elif [ "$IS_SNAPSHOT" == "" ]; then
+  echo "Skipping build. Given build doesn't appear to be a SNAPSHOT release."
+else
+  "$BASEDIR/gradlew" uploadArchives
+fi
