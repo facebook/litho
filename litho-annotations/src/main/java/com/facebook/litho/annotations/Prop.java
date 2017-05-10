8d97c1f2e2174990482210680c60a3bcf67192c8
Omer Strulovich <ostrulovich@fb.com>
2017-05-10 11:34:31 -0700

    Adding java-doc for Prop
    Summary: As expected. This is an important annotation. Did my best, happy to get some comments.
    
    Reviewed By: IanChilds
    
    Differential Revision: D5036429
    
    fbshipit-source-id: d5f448c19358f5294557458d624b13ebbd5b0504

--- litho-annotations/src/main/java/com/facebook/litho/annotations/Prop.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/Prop.java
@@ -12,11 +12,40 @@ package com.facebook.litho.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotates a parameter to a component's spec method indicating that it will be supplied as a prop
+ * for this component.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Prop {
 
+  /**
+   * Whether this prop can be omitted by the caller to the component, making it take its default
+   * value.
+   *
+   * If a prop is declared optional, its default value will be the standard default initialization
+   * value according to the Java standard (e.g. 0, false, null). If a constant annotated  with
+   * {@link PropDefault} and named the same as the prop is in the class it will override the
+   * default value.
+   */
   boolean optional() default false;
+
+  /**
+   * Marks this prop as one that corresponding to a specific Android resource type, and therefore
+   * generates various helper methods to initialize it.
+   *
+   * For example, a {@link CharSequence} prop named "title" may be marked as {@link ResType#STRING}.
+   * This will make the component have not only method "title(CharSequence)" but also various
+   * methods that enable initializing the prop from a resource or attribute:
+   * {@code
+   * titleRes(@StringRes int resId)
+   * titleRes(@StringRes int resId, Object... formatArgs)
+   * titleAttr(@AttrRes int attrResId, @StringRes int defResId)
+   * titleAttr(@AttrRes int attrResId)
+   * }
+   */
   ResType resType() default ResType.NONE;
+
   String docString() default "";
 
 }
