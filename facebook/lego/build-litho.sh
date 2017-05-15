e806bb913f435b57ca3152f9d3dbe291aa1ca7a3
Pascal Hartig <realpassy@fb.com>
2017-05-15 08:46:28 -0700

    Limit the cores buck uses in Sandcastle
    Summary: Per title
    
    Reviewed By: IanChilds
    
    Differential Revision: D5061182
    
    fbshipit-source-id: e1e756d3437bd28dcac90f1016b8827587fb6645

--- facebook/lego/build-litho.sh
+++ facebook/lego/build-litho.sh
@@ -7,7 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/../.."
 
-echo -e "[download]\n   proxy_host=fwdproxy.any\n   proxy_port=8080\n proxy_type=HTTP" > .buckconfig.local
+echo -e "[download]\n   proxy_host=fwdproxy.any\n   proxy_port=8080\n proxy_type=HTTP\n" > .buckconfig.local
+echo -e "[build]\n    thread_core_ratio = 1\n thread_core_ratio_reserved_cores = 0\n" >> .buckconfig.local
 
 export BUCKVERSION="$(cat "$DIR"/../../../../.buckversion)"
 export ANDROID_HOME=$HOME/android-sdk
