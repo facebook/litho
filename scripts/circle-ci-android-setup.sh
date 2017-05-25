d7a1e86ca26d8c60b05869eda67c6aab182b13d7
Pascal Hartig <realpassy@fb.com>
2017-05-25 09:33:39 -0700

    Bump build tools version
    Summary: Shouldn't affect us.
    
    Reviewed By: IanChilds
    
    Differential Revision: D5129534
    
    fbshipit-source-id: 9abdde195c5349bc31676845a0030bebe2e83d6e

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -37,5 +37,5 @@ function installAndroidSDK {
   echo > $ANDROID_HOME/licenses/android-sdk-license
   echo -n 8933bad161af4178b1185d1a37fbf41ea5269c55 > $ANDROID_HOME/licenses/android-sdk-license
 
-  installsdk 'build-tools;23.0.2' 'build-tools;25.0.2' 'build-tools;25.0.1' 'platforms;android-23' 'platforms;android-25' 'ndk-bundle' 'extras;android;m2repository'
+  installsdk 'build-tools;25.0.3' 'platforms;android-23' 'platforms;android-25' 'ndk-bundle' 'extras;android;m2repository'
 }
