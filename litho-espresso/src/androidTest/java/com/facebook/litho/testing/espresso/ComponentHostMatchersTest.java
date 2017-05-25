61fd922782d78b299856028ddad0b1d2ef00d48a
Pascal Hartig <realpassy@fb.com>
2017-05-25 08:51:00 -0700

    Move instrumentation test to OSS export
    Summary:
    It won't run as part of the export for now as we cannot get an emulator to run
    with Circle CI (or rather don't want to), but it ensures that it compiles
    together with the rest.
    
    Reviewed By: IanChilds
    
    Differential Revision: D5120283
    
    fbshipit-source-id: 201febf4f6bc5bde07064476c3a4260a2338eca3

---
+++ litho-espresso/src/androidTest/java/com/facebook/litho/testing/espresso/ComponentHostMatchersTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.testing.espresso;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.UiThreadTest;
+import android.support.test.rule.UiThreadTestRule;
+import android.widget.TextView;
+
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.ComponentTree;
+import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.Text;
+import com.facebook.testing.screenshot.ViewHelpers;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static com.facebook.litho.testing.espresso.ComponentHostMatchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests {@link ComponentHostMatchers}
+ */
+@RunWith(AndroidJUnit4.class)
+public class ComponentHostMatchersTest {
+
+  @Rule
+  public UiThreadTestRule mUiThreadRule = new UiThreadTestRule();
+
+  private ComponentContext mComponentContext;
+  private Component mTextComponent;
+  private LithoView mView;
+
+  @Before
+  public void before() throws Throwable {
+    mComponentContext = new ComponentContext(
+        InstrumentationRegistry.getTargetContext());
+    mTextComponent = MyComponent.create(mComponentContext)
+        .text("foobar")
+        .customViewTag("zoidberg")
+        .build();
+    ComponentTree tree = ComponentTree.create(
+        mComponentContext,
+        mTextComponent)
+        .build();
+    mView = new LithoView(mComponentContext);
+    mView.setComponentTree(tree);
+    ViewHelpers.setupView(mView)
+        .setExactWidthPx(200)
+        .setExactHeightPx(100)
+        .layout();
+  }
+
+  @UiThreadTest
+  @Test
+  public void testContentDescriptionMatching() throws Throwable {
+
+    assertThat(
+        mView,
+        componentHostWithText("foobar"));
+    assertThat(
+        mView,
+        not(componentHostWithText("bar")));
+    assertThat(
+        mView,
+        componentHostWithText(containsString("oob")));
+  }
+
+  @UiThreadTest
+  @Test
+  public void testIsComponentHost() throws Throwable {
+    assertThat(
+        new TextView(InstrumentationRegistry.getTargetContext()),
+        is(not(componentHost())));
+    assertThat(
+        mView,
+        is(componentHost()));
+  }
+
+  @UiThreadTest
+  @Test
+  public void testIsComponentHostWithMatcher() throws Throwable {
+    assertThat(
+        mView,
+        is(componentHost(withText("foobar"))));
+    assertThat(
+        mView,
+        is(not(componentHost(withText("blah")))));
+  }
+
+  @UiThreadTest
+  @Test
+  public void testContentDescription() throws Throwable {
+    assertThat(
+        mView,
+        is(componentHost(withContentDescription("foobar2"))));
+  }
+
+  @UiThreadTest
+  @Test
+  public void testMountedComponent() throws Throwable {
+    assertThat(
+        mView,
+        is(componentHost(withLifecycle(isA(Text.class)))));
+  }
+}
---
+++ litho-espresso/src/androidTest/java/com/facebook/litho/testing/espresso/MyComponentSpec.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.testing.espresso;
+
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.ComponentLayout;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.Prop;
+import com.facebook.litho.widget.Text;
+
+@LayoutSpec
+public class MyComponentSpec {
+  @OnCreateLayout
+  public static ComponentLayout onCreateLayout(
+      ComponentContext c,
+      @Prop String text,
+      @Prop(optional=true) Object customViewTag) {
+    return Text.create(c)
+          .text(text)
+          .withLayout()
+          .contentDescription("foobar2")
+          .viewTag(customViewTag)
+          .build();
+  }
+}
