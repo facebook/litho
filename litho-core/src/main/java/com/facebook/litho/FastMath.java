50b2da5a6044889868b12bc3e00d6471f672d6bd
Uts Sikder <uts@fb.com>
2017-06-05 10:13:56 -0700

    use Litho's FastMath.round instead of Java's Math.round
    Summary: We use the round function a lot to calculate pixel values. Litho has a faster implementation of it that's incorrect in cases that don't matter. It should be good for perf if we use FastMath as well.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D5182535
    
    fbshipit-source-id: 58734737b928bb726e1ab6e0f6fe9163970fe1cc

--- litho-core/src/main/java/com/facebook/litho/FastMath.java
+++ litho-core/src/main/java/com/facebook/litho/FastMath.java
@@ -14,7 +14,7 @@ package com.facebook.litho;
  * This will always have the downside of not supporting all the edge cases which the java Math
  * package does support so please read up on those edge cases before using these methods.
  */
-class FastMath {
+public class FastMath {
 
   /**
    * This stack overflow post has more context around what cases this implementation won't handle.
@@ -23,7 +23,7 @@ class FastMath {
    * @param val The value to round
    * @return The rounded value
    */
-  static int round(float val) {
+  public static int round(float val) {
     if (val > 0) {
       return (int) (val + 0.5);
     } else {
