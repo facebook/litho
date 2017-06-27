2de554c28d2760845cf875d513c7d5eb72d9b09a
Pasquale Anatriello <pasqualea@fb.com>
2017-06-27 07:58:35 -0700

    Disable View measurement caches in LinearLayoutManager for RecyclerSpec
    Summary: We rely on measure being called to make sure that we have a computed layout available before mounting. The way linearLayoutManager decides to cache measurements seems to be incompatible with that in some cases.
    
    Reviewed By: astreet
    
    Differential Revision: D5328947
    
    fbshipit-source-id: 8a775b9b90fd88262b369d8e1ad60cf04703aaca

--- litho-widget/src/main/java/com/facebook/litho/widget/LinearLayoutInfo.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/LinearLayoutInfo.java
@@ -43,6 +43,8 @@ public class LinearLayoutInfo implements LayoutInfo {
     } else {
       mLinearLayoutManager = new LinearLayoutManager(context, orientation, reverseLayout);
     }
+
+    mLinearLayoutManager.setMeasurementCacheEnabled(false);
   }
 
   @Override
