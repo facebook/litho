9e69371133b81c7e2abe9d34365f27c9b54b9e97
Andy Street <andrews@fb.com>
2017-07-06 11:20:46 -0700

    Resubmit: [litho] Track last mounted LayoutState in MountState
    Summary:
    Resubmit of D5346859
    
    For animations, we need access to the last mounted layoutstate: currently we only have access to it for the duration of the `MountState#mount` call. We need access to it to:
     - clean up after animations (e.g. figure out if the animated key is still mounted)
     - to run a disappearing animation for content that wasn't mounted yet via incremental mount
     - to lazily generate the transition key -> LayoutOutput mapping only at the time it's needed
    
    Reviewed By: marco-cova
    
    Differential Revision: D5364470
    
    fbshipit-source-id: 8626ffe9eb7a7e28714a6dea90734de41ca98ed2

--- litho-core/src/main/java/com/facebook/litho/MountState.java
+++ litho-core/src/main/java/com/facebook/litho/MountState.java
@@ -106,6 +106,7 @@ class MountState {
   private int mPreviousBottomsIndex;
   private int mLastMountedComponentTreeId;
   private final HashMap<String, Integer> mMountedTransitionKeys = new HashMap<>();
+  private LayoutState mLastMountedLayoutState;
 
   private final MountItem mRootHostMountItem;
 
@@ -166,6 +167,11 @@ class MountState {
     final ComponentTree componentTree = mLithoView.getComponentTree();
     final ComponentsLogger logger = componentTree.getContext().getLogger();
     final int componentTreeId = layoutState.getComponentTreeId();
+    if (componentTreeId != mLastMountedComponentTreeId) {
+      // If we're mounting a new ComponentTree, don't keep around and use the previous LayoutState
+      // since things like transition animations aren't relevant.
+      releaseLastMountedLayoutState();
+    }
 
     LogEvent mountEvent = null;
     if (logger != null) {
@@ -262,8 +268,6 @@ class MountState {
       mPreviousLocalVisibleRect.set(localVisibleRect);
     }
 
-    processVisibilityOutputs(layoutState, localVisibleRect);
-
     if (shouldAnimateTransitions) {
       recordMountedItemsWithTransitionKeys(
           mTransitionManager,
@@ -272,11 +276,15 @@ class MountState {
       mTransitionManager.runTransitions();
     }
 
+    releaseLastMountedLayoutState();
+    mLastMountedComponentTreeId = componentTreeId;
+    mLastMountedLayoutState = layoutState.acquireRef();
+
+    processVisibilityOutputs(layoutState, localVisibleRect);
+
     processTestOutputs(layoutState);
 
     suppressInvalidationsOnHosts(false);
-
-    mLastMountedComponentTreeId = componentTreeId;
 
     if (logger != null) {
       mountEvent.addParam(PARAM_LOG_TAG, componentTree.getContext().getLogTag());
@@ -2266,6 +2274,13 @@ class MountState {
   private ComponentContext getContextForComponent(Component component) {
     final ComponentContext c = component.getScopedContext();
     return c == null ? mContext : c;
+  }
+
+  private void releaseLastMountedLayoutState() {
+    if (mLastMountedLayoutState != null) {
+      mLastMountedLayoutState.releaseRef();
+      mLastMountedLayoutState = null;
+    }
   }
 
   private static class LayoutOutputLog {
