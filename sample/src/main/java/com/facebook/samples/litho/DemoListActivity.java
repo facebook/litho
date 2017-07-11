541e2d97caf7ec56737e45b4622b34c8ff836ca4
Andy Street <andrews@fb.com>
2017-07-11 13:11:23 -0700

    Refactor sample app to use Activities
    Summary: This should hopefully make things easier to understand and makes it easier for me to add more demos :)
    
    Reviewed By: muraziz
    
    Differential Revision: D5382588
    
    fbshipit-source-id: fc9cffb483adf8456d7c61ad09073f4c169ab0d5

--- sample/src/main/java/com/facebook/samples/litho/DemoListActivity.java
+++ sample/src/main/java/com/facebook/samples/litho/DemoListActivity.java
@@ -1,18 +1,29 @@
 /*
- * Copyright 2014-present, Facebook, Inc.
- * All rights reserved.
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
  *
- * This source code is licensed under the license found in the
- * LICENSE-examples file in the root directory of this source tree.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 
 package com.facebook.samples.litho;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.OrientationHelper;
 
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.LinearLayoutInfo;
+import com.facebook.litho.widget.RecyclerBinder;
+import com.facebook.samples.litho.lithography.LithographyActivity;
+import com.facebook.samples.litho.playground.PlaygroundActivity;
 
 public class DemoListActivity extends AppCompatActivity {
 
@@ -20,12 +31,40 @@ public class DemoListActivity extends AppCompatActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    Demos.initialize(this);
+    final ComponentContext componentContext = new ComponentContext(this);
+    final RecyclerBinder recyclerBinder = new RecyclerBinder.Builder()
+        .layoutInfo(new LinearLayoutInfo(componentContext, OrientationHelper.VERTICAL, false))
+        .build(componentContext);
+
+    addDemo(
+        componentContext,
+        recyclerBinder,
+        "Lithography",
+        new Intent(this, LithographyActivity.class));
+    addDemo(
+        componentContext,
+        recyclerBinder,
+        "Playground",
+        new Intent(this, PlaygroundActivity.class));
 
     setContentView(
         LithoView.create(
             this,
-            DemoListComponent.create(new ComponentContext(this))
+            DemoListComponent.create(componentContext)
+                .recyclerBinder(recyclerBinder)
                 .build()));
+  }
+
+  private void addDemo(
+      ComponentContext c,
+      RecyclerBinder recyclerBinder,
+      String name,
+      Intent intentToLaunch) {
+    recyclerBinder.insertItemAt(
+        recyclerBinder.getItemCount(),
+        DemoListItemComponent.create(c)
+            .name(name)
+            .intent(intentToLaunch)
+            .build());
   }
 }
--- sample/src/main/java/com/facebook/samples/litho/DemoListComponentSpec.java
+++ sample/src/main/java/com/facebook/samples/litho/DemoListComponentSpec.java
@@ -1,20 +1,23 @@
 /*
- * Copyright 2014-present, Facebook, Inc.
- * All rights reserved.
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
  *
- * This source code is licensed under the license found in the
- * LICENSE-examples file in the root directory of this source tree.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.facebook.samples.litho;
 
-import android.support.v7.widget.OrientationHelper;
+package com.facebook.samples.litho;
 
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
-import com.facebook.litho.widget.LinearLayoutInfo;
+import com.facebook.litho.annotations.Prop;
 import com.facebook.litho.widget.Recycler;
 import com.facebook.litho.widget.RecyclerBinder;
 
@@ -24,13 +27,9 @@ public class DemoListComponentSpec {
   private static final String MAIN_SCREEN = "main_screen";
 
   @OnCreateLayout
-  static ComponentLayout onCreateLayout(ComponentContext c) {
-    final RecyclerBinder recyclerBinder = new RecyclerBinder.Builder()
-        .layoutInfo(new LinearLayoutInfo(c, OrientationHelper.VERTICAL, false))
-        .build(c);
-
-    Demos.addAllToBinder(recyclerBinder, c);
-
+  static ComponentLayout onCreateLayout(
+      ComponentContext c,
+      @Prop RecyclerBinder recyclerBinder) {
     return Recycler.create(c)
         .binder(recyclerBinder)
         .withLayout()
