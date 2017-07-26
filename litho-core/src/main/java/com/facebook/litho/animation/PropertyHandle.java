9e6246e322ec620419c437d525c75fd08acf8a1a
Andy Street <andrews@fb.com>
2017-07-26 10:55:37 -0700

    Add PropertyHandle for animations
    Summary:
    Adds PropertyHandle with proper equals and hashCode functions. This is going to be part of a larger cleanup refactor where I try to get rid of a lot of the old animation API code (e.g. ChangingComponent etc).
    
    This pair of data is important and tracked in a bunch of different ways right now, I want to consolidate to use just this class for it.
    
    Reviewed By: muraziz
    
    Differential Revision: D5482151
    
    fbshipit-source-id: 32e0589fd1bd1f9341bc8507bf19c64f5cd452ea

---
+++ litho-core/src/main/java/com/facebook/litho/animation/PropertyHandle.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.litho.animation;
+
+/**
+ * A pair of transition key and {@link AnimatedProperty} which can be used to identify a single
+ * animating component property at runtime.
+ */
+final public class PropertyHandle {
+
+  private final String mTransitionKey;
+  private final AnimatedProperty mProperty;
+
+  public PropertyHandle(String transitionKey, AnimatedProperty property) {
+    mTransitionKey = transitionKey;
+    mProperty = property;
+  }
+
+  public String getTransitionKey() {
+    return mTransitionKey;
+  }
+
+  public AnimatedProperty getProperty() {
+    return mProperty;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    final PropertyHandle other = (PropertyHandle) o;
+    return mTransitionKey.equals(other.mTransitionKey) && mProperty.equals(other.mProperty);
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * mTransitionKey.hashCode() + mProperty.hashCode();
+  }
+}
