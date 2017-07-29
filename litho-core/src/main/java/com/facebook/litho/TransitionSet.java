56612332b89bbaf90e871a6df2b1c7243d12e58d
Andy Street <andrews@fb.com>
2017-07-28 19:41:21 -0700

    Easy: Remove tracking disappearing keys from TransitionSet
    Summary: No longer used.
    
    Reviewed By: marco-cova
    
    Differential Revision: D5522058
    
    fbshipit-source-id: 853a692a594eaf8ae95bb7efd88d8240badae1bf

--- litho-core/src/main/java/com/facebook/litho/TransitionSet.java
+++ litho-core/src/main/java/com/facebook/litho/TransitionSet.java
@@ -10,24 +10,17 @@ package com.facebook.litho;
 
 import java.util.ArrayList;
 
-import com.facebook.litho.internal.ArraySet;
-
 /**
  * A set of {@link Transition}s.
  */
 public class TransitionSet {
 
   private final ArrayList<Transition> mTransitions = new ArrayList<>();
-  private final ArraySet<String> mKeysWithDisappearAnimations = new ArraySet<>();
 
   TransitionSet(Transition.Builder... transitionBuilders) {
     for (int i = 0; i < transitionBuilders.length; i++) {
       mTransitions.addAll(transitionBuilders[i].getTransitions());
     }
-  }
-
-  boolean hasDisappearAnimation(String key) {
-    return mKeysWithDisappearAnimations.contains(key);
   }
 
   ArrayList<Transition> getTransitions() {
@@ -36,11 +29,9 @@ public class TransitionSet {
 
   void mergeIn(TransitionSet set) {
     mTransitions.addAll(set.mTransitions);
-    mKeysWithDisappearAnimations.addAll(set.mKeysWithDisappearAnimations);
   }
 
   void clear() {
     mTransitions.clear();
-    mKeysWithDisappearAnimations.clear();
   }
 }
