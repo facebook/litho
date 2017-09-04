798e7bb372f60a64836319728239154a76d9b15d
Pascal Hartig <realpassy@fb.com>
2017-09-04 07:56:04 -0700

    Move sections logger to OSS
    Summary: Sets up a fallback to allow existing deps to continue working.
    
    Reviewed By: muraziz
    
    Differential Revision: D5755158
    
    fbshipit-source-id: 80f48da2abb13f2113fcfac09f44ad8c29b8be8c

---
+++ litho-sections-core/src/main/java/com/facebook/litho/sections/logger/SectionComponentLogger.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.sections.logger;
+
+import com.facebook.litho.widget.RenderInfo;
+
+/**
+ * Section component logger interface
+ */
+public interface SectionComponentLogger {
+
+  /**
+   * fired when a component is being inserted
+   *
+   * @param tag tag defining component's section tree
+   * @param index position in the recyclerview
+   * @param renderInfo component type that is being operated on
+   * @param thread name of thread that wanted to make this change
+   */
+  void logInsert(String tag, int index, RenderInfo renderInfo, String thread);
+
+  /**
+   * fired when a component is being updated
+   *
+   * @param tag tag defining component's section tree
+   * @param index position in the reyclerview
+   * @param renderInfo component type that is being operated on
+   * @param thread name of thread that wanted to make this change
+   */
+  void logUpdate(String tag, int index, RenderInfo renderInfo, String thread);
+
+  /**
+   * fired when a component is being deleted
+   *
+   * @param tag tag defining component's section tree
+   * @param index position in the reyclerview
+   * @param thread name of thread that wanted to make this change
+   */
+  void logDelete(String tag, int index, String thread);
+
+  /**
+   * fired when a component is being focused
+   *
+   * @param tag tag defining component's section tree
+   * @param index position in the reyclerview
+   * @param renderInfo component type that is being operated on
+   * @param thread name of thread that wanted to make this change
+   */
+  void logRequestFocus(String tag, int index, RenderInfo renderInfo, String thread);
+
+  /**
+   * fired when a component is being focused
+   *
+   * @param tag tag defining component's section tree
+   * @param index position in the reyclerview
+   * @param offset offset from index to scroll to
+   * @param renderInfo component type that is being operated on
+   * @param thread name of thread that wanted to make this change
+   */
+  void logRequestFocusWithOffset(
+      String tag, int index, int offset, RenderInfo renderInfo, String thread);
+
+  /**
+   * fired when a component is being moved
+   *
+   * @param tag tag defining component's section tree
+   * @param fromPosition source index
+   * @param toPosition destination index
+   * @param thread name of thread that wanted to make this change
+   */
+  void logMove(String tag, int fromPosition, int toPosition, String thread);
+
+  /**
+   * fired when determining if a section should re-compute changesets
+   *
+   * @param tag tag defining component's section tree
+   * @param previous previous section that's already made
+   * @param next next section being built
+   * @param previousPrefix string of component's prefix
+   * @param nextPrefix string of component's prefix
+   * @param shouldUpdate boolean value describing if previous & next are the same
+   * @param thread name of thread that wanted to make this change
+   */
+  void logShouldUpdate(
+      String tag,
+      Object previous,
+      Object next,
+      String previousPrefix,
+      String nextPrefix,
+      Boolean shouldUpdate,
+      String thread);
+}
