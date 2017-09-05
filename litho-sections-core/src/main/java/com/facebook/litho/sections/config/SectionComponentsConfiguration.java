7d340ce41bbc84f694f21f679d326bffb03c555b
Pascal Hartig <realpassy@fb.com>
2017-09-05 00:14:38 -0700

    Move config to OSS
    Summary:
    The `ConfigModule` seem to be unused, but I may just misunderstand how DI works,
    so I have to rely on sandcastle to prove this.
    
    Reviewed By: muraziz
    
    Differential Revision: D5755280
    
    fbshipit-source-id: b2e6c898f1febd326612cd2a3ef2c7c57b43bffc

---
+++ litho-sections-core/src/main/java/com/facebook/litho/sections/config/SectionComponentsConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.sections.config;
+
+import com.facebook.litho.sections.logger.SectionComponentLogger;
+import java.util.List;
+
+/** Configuration for the SectionComponents library */
+public class SectionComponentsConfiguration {
+
+  /**
+   * Loggers for the core framework
+   */
+  public static List<SectionComponentLogger> LOGGERS;
+
+  /** Force all section component prop updates to be async */
+  public static boolean sectionComponentsAsyncPropUpdates = false;
+
+  /** Force all section component state updates to be async */
+  public static boolean sectionComponentsAsyncStateUpdates = false;
+
+  /**
+   * If this is False, then a Service created from SectionLifecycle#createService(SectionContext,
+   * Section) is registered and kept in ServiceRegistry.
+   *
+   * <p>SectionLifecycle#destroyService(SectionContext, Object) will not be called if this is True
+   */
+  public static boolean noServiceRegistration = false;
+}
