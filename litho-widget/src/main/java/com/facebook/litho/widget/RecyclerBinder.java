e3bdf75dbadeabf5f9c55e8838db4d392462e099
leochuan <ruochuan.leo@gmail.com>
2017-09-05 13:03:17 +0800

    Add appendItem for RecyclerBinder

--- litho-widget/src/main/java/com/facebook/litho/widget/RecyclerBinder.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/RecyclerBinder.java
@@ -322,6 +322,26 @@ public class RecyclerBinder
   }
 
   /**
+   * See {@link RecyclerBinder#appendItem(RenderInfo)}.
+   */
+  @UiThread
+  public final void appendItem(Component component) {
+    insertItemAt(getItemCount(), component);
+  }
+
+  /**
+   * Inserts a new item at tail. The {@link RecyclerView} gets notified immediately about the
+   * new item being inserted. If the item's position falls within the currently visible range, the
+   * layout is immediately computed on the] UiThread.
+   * The RenderInfo contains the component that will be inserted in the Binder and extra info
+   * like isSticky or spanCount.
+   */
+  @UiThread
+  public final void appendItem(RenderInfo renderInfo) {
+    insertItemAt(getItemCount(), renderInfo);
+  }
+
+  /**
    * See {@link RecyclerBinder#insertItemAt(int, RenderInfo)}.
    */
   @UiThread
