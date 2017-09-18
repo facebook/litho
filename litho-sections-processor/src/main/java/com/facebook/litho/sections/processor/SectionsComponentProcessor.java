13783c62bc96554b2eadf7e7b7002d717be5e7fb
Dan Yang <dsyang@fb.com>
2017-09-18 12:52:04 -0700

    delete old processor and replace with new processor
    Summary: This diff replaces the old SectionsComponentProcessor with SpecModelSectionsComponentProcessor
    
    Reviewed By: IanChilds
    
    Differential Revision: D5824442
    
    fbshipit-source-id: f436b1d39c6bad76c5f9ef64184cd3ada86f6dd6

--- litho-sections-processor/src/main/java/com/facebook/litho/sections/processor/SectionsComponentProcessor.java
+++ litho-sections-processor/src/main/java/com/facebook/litho/sections/processor/SectionsComponentProcessor.java
@@ -7,82 +7,32 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+
 package com.facebook.litho.sections.processor;
 
-import com.facebook.litho.sections.annotations.DiffSectionSpec;
-import com.facebook.litho.sections.annotations.GroupSectionSpec;
-import com.facebook.litho.sections.processor.specmodels.model.DelegateMethodDescriptions;
-import com.facebook.litho.sections.processor.specmodels.model.DiffSectionSpecModel;
-import com.facebook.litho.sections.processor.specmodels.model.GroupSectionSpecModel;
-import com.facebook.litho.sections.processor.specmodels.model.HasService;
-import com.facebook.litho.specmodels.generator.BuilderGenerator;
-import com.facebook.litho.specmodels.generator.ClassAnnotationsGenerator;
-import com.facebook.litho.specmodels.generator.ComponentImplGenerator;
-import com.facebook.litho.specmodels.generator.DelegateMethodGenerator;
-import com.facebook.litho.specmodels.generator.EventGenerator;
-import com.facebook.litho.specmodels.generator.PreambleGenerator;
-import com.facebook.litho.specmodels.generator.StateGenerator;
-import com.facebook.litho.specmodels.generator.TreePropGenerator;
-import com.facebook.litho.specmodels.model.DependencyInjectionHelper;
-import com.facebook.litho.specmodels.model.SpecModel;
-import com.squareup.javapoet.TypeSpec;
-import javax.annotation.Nullable;
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
+import com.facebook.litho.sections.processor.specmodels.processor.DiffSectionSpecModelFactory;
+import com.facebook.litho.sections.processor.specmodels.processor.GroupSectionSpecModelFactory;
+import com.facebook.litho.specmodels.internal.ImmutableList;
+import com.facebook.litho.specmodels.processor.AbstractComponentsProcessor;
+import com.facebook.litho.specmodels.processor.SpecModelFactory;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-/**
- * Processor used to generate <code>com.facebook.litho.sections.SectionLifecycle</code> and
- * <code>com.facebook.litho.sections.Section</code> classes for a {@link GroupSectionSpec}
- * or a {@link DiffSectionSpec}.
- */
-@SupportedSourceVersion(SourceVersion.RELEASE_7)
-public class SectionsComponentProcessor extends AbstractSectionsComponentProcessor {
+public class SectionsComponentProcessor extends AbstractComponentsProcessor {
 
-  @Override
-  protected void generate(GroupSectionSpecHelper groupSectionSpecHelper) {
-    final GroupSectionSpecModel specModel = groupSectionSpecHelper.getSpecModel();
-    final TypeSpec.Builder typeSpec = groupSectionSpecHelper.getTypeSpec();
-
-    generateCommonListComponent(specModel, typeSpec);
-    DelegateMethodGenerator.generateDelegates(
-            specModel, DelegateMethodDescriptions.getGroupSectionSpecDelegatesMap(specModel))
-        .addToTypeSpec(typeSpec);
-    TreePropGenerator.generate(specModel).addToTypeSpec(typeSpec);
+  public SectionsComponentProcessor() {
+    super(
+        ImmutableList.<SpecModelFactory>of(
+            new GroupSectionSpecModelFactory(), new DiffSectionSpecModelFactory()),
+        null);
   }
 
   @Override
-  protected void generate(DiffSectionSpecHelper diffSectionSpecHelper) {
-    final DiffSectionSpecModel specModel = diffSectionSpecHelper.getSpecModel();
-    final TypeSpec.Builder typeSpec = diffSectionSpecHelper.getTypeSpec();
-
-    generateCommonListComponent(specModel, typeSpec);
-    DelegateMethodGenerator.generateDelegates(
-            specModel, DelegateMethodDescriptions.getDiffSectionSpecDelegatesMap(specModel))
-        .addToTypeSpec(diffSectionSpecHelper.getTypeSpec());
-  }
-
-  private <S extends SpecModel & HasService> void generateCommonListComponent(
-      S specModel, TypeSpec.Builder typeSpec) {
-
-    if (specModel.hasInjectedDependencies()) {
-      specModel.getDependencyInjectionHelper().generate(specModel).addToTypeSpec(typeSpec);
-    } else {
-      typeSpec.addModifiers(Modifier.FINAL);
-    }
-
-    PreambleGenerator.generate(specModel).addToTypeSpec(typeSpec);
-    ClassAnnotationsGenerator.generate(specModel).addToTypeSpec(typeSpec);
-    ComponentImplGenerator.generate(specModel, specModel.getServiceParam()).addToTypeSpec(typeSpec);
-    BuilderGenerator.generate(specModel).addToTypeSpec(typeSpec);
-    StateGenerator.generate(specModel).addToTypeSpec(typeSpec);
-    EventGenerator.generate(specModel).addToTypeSpec(typeSpec);
-  }
-
-  @Override
-  @Nullable
-  protected DependencyInjectionHelper getDependencyInjectionGenerator(TypeElement typeElement) {
-    return null;
+  public Set<String> getSupportedAnnotationTypes() {
+    return new LinkedHashSet<>(
+        Arrays.asList(
+            SectionClassNames.GROUP_SECTION_SPEC.toString(),
+            SectionClassNames.DIFF_SECTION_SPEC.toString()));
   }
 }
