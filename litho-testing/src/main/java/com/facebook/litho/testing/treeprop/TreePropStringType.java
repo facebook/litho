3b6e4b9ddb904609de4232c61b6a499a064656de
Pascal Hartig <realpassy@fb.com>
2017-09-27 10:29:53 -0700

    Open-source treeprop testing helpers
    Summary: Just plain java models. Not much to see here, really.
    
    Reviewed By: dsyang
    
    Differential Revision: D5921396
    
    fbshipit-source-id: 6d5660bebdfcda8eb11239819e170fffca91c8ed

---
+++ litho-testing/src/main/java/com/facebook/litho/testing/treeprop/TreePropStringType.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.testing.litho.treeprop;
+
+public class TreePropStringType {
+
+  final private String mValue;
+
+  public TreePropStringType(String value) {
+    mValue = value;
+  }
+
+  public String getValue() {
+    return mValue;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    TreePropStringType other = (TreePropStringType) obj;
+    return other.getValue().equals(mValue);
+  }
+
+  @Override
+  public int hashCode() {
+    return mValue.hashCode();
+  }
+}
