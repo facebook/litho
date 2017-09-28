e5639cbbb1a4051b56a79c0874295ccff2840977
Pascal Hartig <realpassy@fb.com>
2017-09-28 08:49:18 -0700

    Style fixes for SectionComponentTestHelper
    Summary: Address some nits.
    
    Reviewed By: IanChilds
    
    Differential Revision: D5911671
    
    fbshipit-source-id: 1413745f428de94c8b2cd66adbd1bb82799ac704

--- litho-testing/src/main/java/com/facebook/litho/testing/sections/SectionComponentTestHelper.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/sections/SectionComponentTestHelper.java
@@ -15,8 +15,8 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
 import com.facebook.litho.EventHandler;
+import com.facebook.litho.HasEventDispatcher;
 import com.facebook.litho.sections.Children;
 import com.facebook.litho.sections.Section;
 import com.facebook.litho.sections.SectionContext;
@@ -26,6 +26,8 @@ import com.facebook.litho.sections.SectionTree;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -34,16 +36,15 @@ import org.mockito.stubbing.Answer;
  */
 public class SectionComponentTestHelper extends SectionLifecycle {
 
-  private final SectionContext sc;
-  private final SectionTree st;
+  private final SectionContext mSectionContext;
 
   // A set of sections that are ready to be tested (createInitialState called).
-  private final HashMap<Section, SectionContext> preparedSections;
+  private final Map<Section, SectionContext> preparedSections;
 
   public SectionComponentTestHelper(Context c) {
-    SectionContext base = new SectionContext(c);
-    st = SectionTree.create(base, new TestTarget()).build();
-    sc = SectionContext.withSectionTree(base, st);
+    final SectionContext base = new SectionContext(c);
+    final SectionTree st = SectionTree.create(base, new TestTarget()).build();
+    mSectionContext = SectionContext.withSectionTree(base, st);
     preparedSections = new HashMap<>();
   }
 
@@ -51,12 +52,11 @@ public class SectionComponentTestHelper extends SectionLifecycle {
    * Return a generic {@link SectionContext} you can use to create sections/components.
    */
   public SectionContext getContext() {
-    return sc;
+    return mSectionContext;
   }
 
-  /**
-   * Return the specific {@link SectionContext} that's been scoped to the given section.
-   */
+  /** Return the specific {@link SectionContext} that's been scoped to the given section. */
+  @Nullable
   public SectionContext getScopedContext(Section s) {
     ensurePrepared(s);
     return preparedSections.get(s);
@@ -67,15 +67,16 @@ public class SectionComponentTestHelper extends SectionLifecycle {
    * it's initial state.
    */
   public Section prepare(Section s) {
-    SectionContext context = SectionContext.withScope(sc, s);
-    SectionContext spyContext = spy(context);
+    final SectionContext context = SectionContext.withScope(mSectionContext, s);
+    final SectionContext spyContext = spy(context);
 
     doAnswer(
             new Answer() {
               @Override
+              @Nullable
               public Object answer(InvocationOnMock invocation) throws Throwable {
-                Section scope = ((SectionContext) invocation.getMock()).getSectionScope();
-                StateUpdate stateUpdate = (StateUpdate) invocation.getArguments()[0];
+                final Section scope = ((SectionContext) invocation.getMock()).getSectionScope();
+                final StateUpdate stateUpdate = (StateUpdate) invocation.getArguments()[0];
                 stateUpdate.updateState(SectionLifecycleTestUtil.getStateContainer(scope), scope);
                 return null;
               }
@@ -83,7 +84,7 @@ public class SectionComponentTestHelper extends SectionLifecycle {
         .when(spyContext)
         .updateState(any(StateUpdate.class));
 
-    doReturn(sc.getResourceCache()).when(spyContext).getResourceCache();
+    doReturn(mSectionContext.getResourceCache()).when(spyContext).getResourceCache();
 
     SectionLifecycleTestUtil.setScopedContext(s, spyContext);
     SectionLifecycleTestUtil.createInitialState(s.getLifecycle(), spyContext, s);
@@ -92,9 +93,8 @@ public class SectionComponentTestHelper extends SectionLifecycle {
     return s;
   }
 
-  /**
-   * Get child sections for the given section.
-   */
+  /** Get child sections for the given section. */
+  @Nullable
   public List<SubSection> getChildren(Section section) {
     ensurePrepared(section);
     final Children children =
@@ -106,31 +106,30 @@ public class SectionComponentTestHelper extends SectionLifecycle {
   }
 
   /** Get subSections for the given Children. */
-  public @Nullable List<SubSection> getSubSections(Children children) {
+  @Nullable
+  public static List<SubSection> getSubSections(Children children) {
     if (children == null || children.getChildren() == null) {
       return null;
     }
 
-    ArrayList<SubSection> subsection = new ArrayList<>(children.getChildren().size());
+    final List<SubSection> subsection = new ArrayList<>(children.getChildren().size());
 
-    for (Section s: children.getChildren()) {
+    for (final Section s : children.getChildren()) {
       subsection.add(SubSection.of(s));
     }
     return subsection;
   }
 
-  /**
-   * Return the state container of the given section.
-   */
+  /** Return the state container of the given section. */
+  @SuppressWarnings("unchecked")
   public <T extends StateContainer> T getStateContainer(Section<?> section) {
     return (T)
         SectionLifecycleTestUtil.getStateContainer(getScopedContext(section).getSectionScope());
   }
 
-  /**
-   * Dispatch the given event to a event handler.
-   */
-  public Object dispatchEvent(Section section, EventHandler eventHandler, Object event) {
+  /** Dispatch the given event to a event handler. */
+  public static Object dispatchEvent(
+      HasEventDispatcher section, EventHandler eventHandler, Object event) {
     return section.getEventDispatcher().dispatchOnEvent(eventHandler, event);
   }
 
