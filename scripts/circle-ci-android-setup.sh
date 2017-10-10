209eaca84783767d5bf060c16ee0d8a6ded934bd
Pascal Hartig <realpassy@fb.com>
2017-10-10 10:16:24 -0700

    Bump NDK to 15c
    Summary:
    Third attempt! This time I actually tested that it worked.
    The result can be seen here: https://circleci.com/gh/facebook/litho/938
    
    Reviewed By: IanChilds
    
    Differential Revision: D6020290
    
    fbshipit-source-id: 1032ba07972cb7dd7c39915d010473a4646e3f4d

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -31,10 +31,10 @@ function installAndroidSDK {
   fi
 
   export ANDROID_NDK_REPOSITORY=$HOME/android-ndk
-  if [[ ! -d "$ANDROID_NDK_REPOSITORY/android-ndk-r13b" ]]; then
+  if [[ ! -d "$ANDROID_NDK_REPOSITORY/android-ndk-r15c" ]]; then
     TMP=/tmp/ndk$$.zip
     mkdir -p "$ANDROID_NDK_REPOSITORY"
-    download 'https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip' $TMP
+    download 'https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip' $TMP
     unzip -qod "$ANDROID_NDK_REPOSITORY" "$TMP"
     rm $TMP
   fi
