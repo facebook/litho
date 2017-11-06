5f13e5be8ccbe8965d3824a58fdc760046870c5d
Jingbo Yang <jingbo@fb.com>
2017-11-06 15:09:03 -0800

    Mark ComponentLayout as ThreadConfined
    Summary: ComponentLayout, as the interface of Internal Node , is also ThreadConfined
    
    Reviewed By: sblackshear
    
    Differential Revision: D6221329
    
    fbshipit-source-id: fcb2e169aec65e05c82f1f9c40a4b1b9506fc6c7

--- litho-core/src/main/java/com/facebook/litho/ComponentLayout.java
+++ litho-core/src/main/java/com/facebook/litho/ComponentLayout.java
@@ -22,6 +22,7 @@ import android.support.annotation.StringRes;
 import android.util.SparseArray;
 import android.view.ViewOutlineProvider;
 import com.facebook.infer.annotation.ReturnsOwnership;
+import com.facebook.infer.annotation.ThreadConfined;
 import com.facebook.litho.annotations.ImportantForAccessibility;
 import com.facebook.litho.reference.Reference;
 import com.facebook.yoga.YogaAlign;
@@ -32,14 +33,15 @@ import com.facebook.yoga.YogaPositionType;
 import com.facebook.yoga.YogaWrap;
 
 /**
- * Represents a {@link Component}'s computed layout state. The computed bounds will be
- * used by the framework to define the size and position of the component's mounted
- * {@link android.view.View}s and {@link android.graphics.drawable.Drawable}s returned.
- * by {@link ComponentLifecycle#mount(ComponentContext, Object, Component)}.
+ * Represents a {@link Component}'s computed layout state. The computed bounds will be used by the
+ * framework to define the size and position of the component's mounted {@link android.view.View}s
+ * and {@link android.graphics.drawable.Drawable}s returned. by {@link
+ * ComponentLifecycle#mount(ComponentContext, Object, Component)}.
  *
  * @see ComponentLifecycle#createLayout(ComponentContext, Component, boolean)
  * @see ComponentLifecycle#mount(ComponentContext, Object, Component)
  */
+@ThreadConfined(ThreadConfined.ANY)
 public interface ComponentLayout {
 
   @Px
