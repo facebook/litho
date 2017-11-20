17573bf242cf14a82265fb9c4c615b3262456d8d
Pascal Hartig <realpassy@fb.com>
2017-11-20 02:25:23 -0800

    Add TestTextSpec
    Summary:
    I plan on adding test specs for all built-in widgets, but just use this one for
    prototyping purposes for now. The specs won't actually compile for all of them
    yet, anyway.
    
    I'm not quite sure if this is the right call or not, but I'm putting them into
    `c.f.l.widget`, instead of `c.f.l.testing.widget` in order to avoid having to
    make the specs themselves public.
    
    I wish I wouldn't have to do that, but the namespace pollution is actually
    minimal as there's an inherent namespace through the `Test` prefix.
    
    Reviewed By: IanChilds
    
    Differential Revision: D6336685
    
    fbshipit-source-id: 037e800cc5904d2c4e1da55d7c1554c1d4d1f442

---
+++ sample/src/test/java/com/facebook/samples/litho/lithography/FooterComponentSpecTest.java
@@ -0,0 +1,51 @@
+/*
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.samples.litho.lithography;
+
+import static com.facebook.litho.testing.assertj.LithoAssertions.assertThat;
+import static com.facebook.litho.testing.assertj.SubComponentExtractor.subComponentWith;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assume.assumeThat;
+
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.config.ComponentsConfiguration;
+import com.facebook.litho.testing.ComponentsRule;
+import com.facebook.litho.testing.testrunner.ComponentsTestRunner;
+import com.facebook.litho.widget.TestText;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ComponentsTestRunner.class)
+public class FooterComponentSpecTest {
+  @Rule public ComponentsRule mComponentsRule = new ComponentsRule();
+
+  @Before
+  public void setUp() {
+    assumeThat(
+        "These tests can only be run in debug mode.",
+        ComponentsConfiguration.IS_INTERNAL_BUILD,
+        is(true));
+  }
+
+  @Test
+  public void testSubComponentWithMatcher() {
+    final ComponentContext c = mComponentsRule.getContext();
+    final Component<FooterComponent> component =
+        FooterComponent.create(c).text("I hate Mumunmununsdays").build();
+
+    assertThat(c, component)
+        .has(subComponentWith(c, TestText.matcher(c).text("I hate Mumunmununsdays").build()));
+  }
+}
