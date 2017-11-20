951ceed2459353ae250da54352b20d31f7640202
Pascal Hartig <realpassy@fb.com>
2017-11-20 03:48:58 -0800

    Re-enable setting style values for Text
    Summary:
    Fixes https://github.com/facebook/litho/issues/276.
    
    This was disabled because `Typeface.create()` were not thread-safe
    (https://github.com/facebook/litho/issues/204), but IanChilds has sinced fixed this
    with
    https://github.com/facebook/litho/commit/ca96aba0fd81b4ca24203243bcc2894713307079.
    
    Reviewed By: IanChilds
    
    Differential Revision: D6358856
    
    fbshipit-source-id: 5ee9b2f4ef0677171897f6ed7317d2cfda66b588

--- litho-widget/src/main/java/com/facebook/litho/widget/TextSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextSpec.java
@@ -357,10 +357,7 @@ class TextSpec {
 
     if (fontFamily != null) {
       final Integer styleValue = textStyle.get();
-      // https://github.com/facebook/litho/issues/204
-      // Typeface.create() reads from a static cache and is not thread-safe.
-      // Disable support until we can use our own cache or come up with another solution.
-      // typeface.set(Typeface.create(fontFamily, styleValue == null ? -1 : styleValue));
+      typeface.set(Typeface.create(fontFamily, styleValue == null ? -1 : styleValue));
     }
   }
 
