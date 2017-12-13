02f1ea0d22ba44bca1a3b89cfb77e48a315a41f0
Pascal Hartig <realpassy@fb.com>
2017-12-13 03:54:26 -0800

    Breaking: Move state testing helper to own pacakge
    Summary:
    Only breaking because it requires a codemod. This is necessary to untangle an
    otherwise cyclic dependency.
    
    Updated via
    
    ```
    ag -F 'com.facebook.litho.testing.StateUpdatesTestHelper' -l | xargs sed -i 's/com.facebook.litho.testing.StateUpdatesTestHelper/com.facebook.litho.testing.state.StateUpdatesTestHelper/'
    ```
    
    Reviewed By: IanChilds
    
    Differential Revision: D6545231
    
    fbshipit-source-id: e6138816b2d18a695d7af102c27d14904fba9a9e

--- litho-testing/src/main/java/com/facebook/litho/testing/state/StateUpdatesTestHelper.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/state/StateUpdatesTestHelper.java
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-package com.facebook.litho.testing;
+package com.facebook.litho.testing.state;
 
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
