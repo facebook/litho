94dd3fa2d2c1a46f1101cf2be0e9f46cef4b94c9
Pasquale Anatriello <pasqualea@fb.com>
2017-12-22 18:53:19 +0000

    style

--- sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
+++ sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
@@ -54,26 +54,26 @@ class ListSectionSpec {
     @OnCreateChildren
     static Children onCreateChildren(final SectionContext c) {
         return Children.create()
-                .child(
-                        SingleComponentSection.create(c)
-                                .component(
-                                        RecyclerCollectionComponent.create(c)
-                                                .disablePTR(true)
-                                                .recyclerConfiguration(
-                                                        new ListRecyclerConfiguration(
-                                                                LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
-                                                .section(
-                                                        DataDiffSection.<Integer>create(c)
-                                                                .data(generateData(32))
-                                                                .renderEventHandler(ListSection.onRender(c))
-                                                                .build())
-                                                .canMeasureRecycler(true))
-                                .build())
-                .child(
-                        DataDiffSection.<Integer>create(c)
-                                .data(generateData(32))
-                                .renderEventHandler(ListSection.onRender(c)))
-                .build();
+            .child(
+                SingleComponentSection.create(c)
+                    .component(
+                        RecyclerCollectionComponent.create(c)
+                            .disablePTR(true)
+                            .recyclerConfiguration(
+                                new ListRecyclerConfiguration(
+                                    LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
+                                        .section(
+                                            DataDiffSection.<Integer>create(c)
+                                                .data(generateData(32))
+                                                    .renderEventHandler(ListSection.onRender(c))
+                                                    .build())
+                            .canMeasureRecycler(true))
+                    .build())
+            .child(
+                DataDiffSection.<Integer>create(c)
+                    .data(generateData(32))
+                    .renderEventHandler(ListSection.onRender(c)))
+            .build();
     }
 
     @OnEvent(RenderEvent.class)
