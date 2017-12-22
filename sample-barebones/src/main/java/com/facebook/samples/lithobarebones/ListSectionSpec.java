575ac58fac291b148c85aa2ceba88eb9dac76067
Pasquale Anatriello <pasqualea@fb.com>
2017-12-22 18:38:44 +0000

    View demo

--- sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
+++ sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
@@ -11,10 +11,12 @@
  */
 package com.facebook.samples.lithobarebones;
 
-import static com.facebook.litho.sections.widget.ListRecyclerConfiguration.SNAP_TO_CENTER;
-
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.sections.Children;
@@ -26,56 +28,83 @@ import com.facebook.litho.sections.common.RenderEvent;
 import com.facebook.litho.sections.common.SingleComponentSection;
 import com.facebook.litho.sections.widget.ListRecyclerConfiguration;
 import com.facebook.litho.sections.widget.RecyclerCollectionComponent;
+import com.facebook.litho.viewcompat.SimpleViewBinder;
+import com.facebook.litho.viewcompat.ViewCreator;
 import com.facebook.litho.widget.ComponentRenderInfo;
 import com.facebook.litho.widget.RenderInfo;
+import com.facebook.litho.widget.ViewRenderInfo;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.facebook.litho.sections.widget.ListRecyclerConfiguration.SNAP_TO_CENTER;
 
 @GroupSectionSpec
 class ListSectionSpec {
 
-  @OnCreateChildren
-  static Children onCreateChildren(final SectionContext c) {
-    return Children.create()
-        .child(
-            SingleComponentSection.create(c)
-                .component(
-                    RecyclerCollectionComponent.create(c)
-                        .disablePTR(true)
-                        .recyclerConfiguration(
-                            new ListRecyclerConfiguration(
-                                LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
-                        .section(
-                            DataDiffSection.<Integer>create(c)
-                                .data(generateData(32))
-                                .renderEventHandler(ListSection.onRender(c))
+    // It's important for the view creator to be defined only once as the recycling for views is driven by the instance
+    // of the view creator that was used to create them.
+    private final static ViewCreator VIEW_CREATOR = new ViewCreator() {
+        @Override
+        public View createView(Context c) {
+            return LayoutInflater.from(c).inflate(R.layout.simple_view, null);
+        }
+    };
+
+    @OnCreateChildren
+    static Children onCreateChildren(final SectionContext c) {
+        return Children.create()
+                .child(
+                        SingleComponentSection.create(c)
+                                .component(
+                                        RecyclerCollectionComponent.create(c)
+                                                .disablePTR(true)
+                                                .recyclerConfiguration(
+                                                        new ListRecyclerConfiguration(
+                                                                LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
+                                                .section(
+                                                        DataDiffSection.<Integer>create(c)
+                                                                .data(generateData(32))
+                                                                .renderEventHandler(ListSection.onRender(c))
+                                                                .build())
+                                                .canMeasureRecycler(true))
                                 .build())
-                        .canMeasureRecycler(true))
-                .build())
-        .child(
-            DataDiffSection.<Integer>create(c)
-                .data(generateData(32))
-                .renderEventHandler(ListSection.onRender(c)))
-        .build();
-  }
-
-  @OnEvent(RenderEvent.class)
-  static RenderInfo onRender(final SectionContext c, @FromEvent Integer model) {
-    return ComponentRenderInfo.create()
-        .component(
-            ListItem.create(c)
-                .color(model % 2 == 0 ? Color.WHITE : Color.LTGRAY)
-                .title(model + ". Hello, world!")
-                .subtitle("Litho tutorial")
-                .build())
-        .build();
-  }
-
-  private static List<Integer> generateData(int count) {
-    final List<Integer> data = new ArrayList<>(count);
-    for (int i = 0; i < count; i++) {
-      data.add(i);
+                .child(
+                        DataDiffSection.<Integer>create(c)
+                                .data(generateData(32))
+                                .renderEventHandler(ListSection.onRender(c)))
+                .build();
     }
-    return data;
-  }
+
+    @OnEvent(RenderEvent.class)
+    static RenderInfo onRender(final SectionContext c, @FromEvent Integer model) {
+        if (model.intValue() == 1) {
+            return ViewRenderInfo.create().
+                    viewBinder(new SimpleViewBinder<TextView>() {
+                        @Override
+                        public void bind(TextView textView) {
+                            textView.setText("I'm a view in a Litho world");
+                        }
+                    })
+                    .viewCreator(VIEW_CREATOR)
+                    .build();
+        }
+
+        return ComponentRenderInfo.create()
+                .component(
+                        ListItem.create(c)
+                                .color(model % 2 == 0 ? Color.WHITE : Color.LTGRAY)
+                                .title(model + ". Hello, world!")
+                                .subtitle("Litho tutorial")
+                                .build())
+                .build();
+    }
+
+    private static List<Integer> generateData(int count) {
+        final List<Integer> data = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            data.add(i);
+        }
+        return data;
+    }
 }
