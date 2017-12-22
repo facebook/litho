eac358c743b4dfce26467a39b36b61d324eb382a
Pasquale Anatriello <pasqualea@fb.com>
2017-12-22 18:56:06 +0000

    style

--- sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
+++ sample-barebones/src/main/java/com/facebook/samples/lithobarebones/ListSectionSpec.java
@@ -17,6 +17,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+
 import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.sections.Children;
@@ -42,69 +43,69 @@ import static com.facebook.litho.sections.widget.ListRecyclerConfiguration.SNAP_
 @GroupSectionSpec
 class ListSectionSpec {
 
-    // It's important for the view creator to be defined only once as the recycling for views is driven by the instance
-    // of the view creator that was used to create them.
-    private final static ViewCreator VIEW_CREATOR = new ViewCreator() {
-        @Override
-        public View createView(Context c) {
-            return LayoutInflater.from(c).inflate(R.layout.simple_view, null);
-        }
-    };
-
-    @OnCreateChildren
-    static Children onCreateChildren(final SectionContext c) {
-        return Children.create()
-            .child(
-                SingleComponentSection.create(c)
-                    .component(
-                        RecyclerCollectionComponent.create(c)
-                            .disablePTR(true)
-                            .recyclerConfiguration(
-                                new ListRecyclerConfiguration(
-                                    LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
-                                        .section(
-                                            DataDiffSection.<Integer>create(c)
-                                                .data(generateData(32))
-                                                    .renderEventHandler(ListSection.onRender(c))
-                                                    .build())
-                            .canMeasureRecycler(true))
-                    .build())
-            .child(
-                DataDiffSection.<Integer>create(c)
-                    .data(generateData(32))
-                    .renderEventHandler(ListSection.onRender(c)))
-            .build();
+  // It's important for the view creator to be defined only once as the recycling for views is driven by the instance
+  // of the view creator that was used to create them.
+  private final static ViewCreator VIEW_CREATOR = new ViewCreator() {
+    @Override
+    public View createView(Context c) {
+      return LayoutInflater.from(c).inflate(R.layout.simple_view, null);
     }
+  };
 
-    @OnEvent(RenderEvent.class)
-    static RenderInfo onRender(final SectionContext c, @FromEvent Integer model) {
-        if (model.intValue() == 1) {
-            return ViewRenderInfo.create().
-                    viewBinder(new SimpleViewBinder<TextView>() {
-                        @Override
-                        public void bind(TextView textView) {
-                            textView.setText("I'm a view in a Litho world");
-                        }
-                    })
-                    .viewCreator(VIEW_CREATOR)
-                    .build();
-        }
-
-        return ComponentRenderInfo.create()
+  @OnCreateChildren
+  static Children onCreateChildren(final SectionContext c) {
+    return Children.create()
+        .child(
+            SingleComponentSection.create(c)
                 .component(
-                        ListItem.create(c)
-                                .color(model % 2 == 0 ? Color.WHITE : Color.LTGRAY)
-                                .title(model + ". Hello, world!")
-                                .subtitle("Litho tutorial")
+                    RecyclerCollectionComponent.create(c)
+                        .disablePTR(true)
+                        .recyclerConfiguration(
+                            new ListRecyclerConfiguration(
+                                LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
+                        .section(
+                            DataDiffSection.<Integer>create(c)
+                                .data(generateData(32))
+                                .renderEventHandler(ListSection.onRender(c))
                                 .build())
-                .build();
+                        .canMeasureRecycler(true))
+                .build())
+        .child(
+            DataDiffSection.<Integer>create(c)
+                .data(generateData(32))
+                .renderEventHandler(ListSection.onRender(c)))
+        .build();
+  }
+
+  @OnEvent(RenderEvent.class)
+  static RenderInfo onRender(final SectionContext c, @FromEvent Integer model) {
+    if (model.intValue() == 1) {
+      return ViewRenderInfo.create().
+          viewBinder(new SimpleViewBinder<TextView>() {
+            @Override
+            public void bind(TextView textView) {
+              textView.setText("I'm a view in a Litho world");
+            }
+          })
+          .viewCreator(VIEW_CREATOR)
+          .build();
     }
 
-    private static List<Integer> generateData(int count) {
-        final List<Integer> data = new ArrayList<>(count);
-        for (int i = 0; i < count; i++) {
-            data.add(i);
-        }
-        return data;
+    return ComponentRenderInfo.create()
+        .component(
+            ListItem.create(c)
+                .color(model % 2 == 0 ? Color.WHITE : Color.LTGRAY)
+                .title(model + ". Hello, world!")
+                .subtitle("Litho tutorial")
+                .build())
+        .build();
+  }
+
+  private static List<Integer> generateData(int count) {
+    final List<Integer> data = new ArrayList<>(count);
+    for (int i = 0; i < count; i++) {
+      data.add(i);
     }
+    return data;
+  }
 }
