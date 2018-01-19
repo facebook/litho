909e241c22ec9ec7ea7e186e953eeac8c920ff33
Waseem Ahmad <waseemahmad@fb.com>
2018-01-19 14:04:52 -0800

    Add LazyComponentSelector
    Summary:
    This diff adds a utility class to reduce boilerplate code around making lots of Component.willRender calls wrapped in if else statements.
    
    This Component is equivalent to ComponentSelector but takes ComponentCreators as arguments instead of Components to avoid the unnecessary instantiation of Components that to do not get selected.
    
    The test code shows sample usage.
    
    Differential Revision: D6760356
    
    fbshipit-source-id: b81ec57546f84ec38021cc70e0e427d39fa02732

---
+++ litho-widget/src/main/java/com/facebook/litho/widget/LazySelectorComponentSpec.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.widget;
+
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.Prop;
+import java.util.List;
+
+/**
+ * Selects the first Component that will render by calling {@link
+ * Component#willRender(ComponentContext, Component)} on the Components provided.
+ *
+ * <p>The Component creation is made lazy by taking a {@link ComponentCreator} instead of a {@link
+ * Component}.
+ *
+ * <p>This is useful when a single Component is to be rendered amongst a large number of candidate
+ * Components or when multiple Components can potentially render some content using the same props
+ * and the first one capable of rendering the content needs to be used.
+ */
+@LayoutSpec
+public class LazySelectorComponentSpec {
+
+  @OnCreateLayout
+  static Component onCreateLayout(
+      final ComponentContext c,
+      final @Prop(varArg = "component", optional = true) List<ComponentCreator> components) {
+    if (components == null) {
+      return null;
+    }
+
+    for (int i = 0; i < components.size(); i++) {
+      final Component component = components.get(i).create();
+      if (Component.willRender(c, component)) {
+        return component;
+      }
+    }
+
+    return null;
+  }
+}
