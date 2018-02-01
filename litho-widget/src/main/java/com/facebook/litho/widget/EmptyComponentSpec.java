a248db3f81effc49cb39b63cba1ef00513196076
Pasquale Anatriello <pasqualea@fb.com>
2018-02-01 09:12:51 -0800

    Introduce EmptyComponent
    Summary: Introduce Empty component as part of the widget library
    
    Reviewed By: IanChilds, mihaelao
    
    Differential Revision: D6871856
    
    fbshipit-source-id: eeab2c0f229c7b8315957829faf823bb73b6313b

---
+++ litho-widget/src/main/java/com/facebook/litho/widget/EmptyComponentSpec.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.litho.widget;
+
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+
+/** A component that doesn't render anything. */
+@LayoutSpec
+class EmptyComponentSpec {
+
+  @OnCreateLayout
+  static Component onCreateLayout(ComponentContext c) {
+    return null;
+  }
+}
