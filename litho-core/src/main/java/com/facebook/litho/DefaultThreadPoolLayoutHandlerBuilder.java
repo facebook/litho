06c6a7932475ffacbba04da57a393c3eb2085546
Mihaela Ogrezeanu <mihaelao@fb.com>
2018-02-13 02:45:49 -0800

    Add ThreadPoolExecutor to calculate different layouts on multiple threads
    Summary: as title. This posts a background layout runnable on a thread from a pool of threads instead of calculating async layout on a single background thread.
    
    Reviewed By: astreet
    
    Differential Revision: D6949287
    
    fbshipit-source-id: b3b54be931515552475519e9c24121f0a7fe9479

---
+++ litho-core/src/main/java/com/facebook/litho/DefaultThreadPoolLayoutHandlerBuilder.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.litho;
+
+/**
+ * Default provider for a ThreadPoolLayoutHandler. If no values are configured for the pool sizes,
+ * it will create a pool with a size of 1.
+ *
+ * <p>If a fixed size pool is enabled, a ThreadPoolLayoutHandler with a fixed core and max pool size
+ * will be provided. Otherwise, it uses the multiplier and increment values passed to the builder to
+ * define the core pool size and max pool size as (numProcessors * multiplier + increment).
+ */
+public class DefaultThreadPoolLayoutHandlerBuilder {
+
+  private boolean mHasFixedSizePool;
+  private int mCorePoolSize = 1;
+  private int mMaxPoolSize = 1;
+  private int mCorePoolSizeMultiplier = 1;
+  private int mCorePoolSizeIncrement = 0;
+  private int mMaxPoolSizeMultiplier = 1;
+  private int mMaxPoolSizeIncrement = 0;
+
+  public DefaultThreadPoolLayoutHandlerBuilder() {}
+
+  public DefaultThreadPoolLayoutHandlerBuilder hasFixedSizePool(boolean hasFixedSizePool) {
+    mHasFixedSizePool = hasFixedSizePool;
+    return this;
+  }
+
+  public DefaultThreadPoolLayoutHandlerBuilder fixedSizePoolConfiguration(
+      int corePoolSize, int maxPoolSize) {
+    mCorePoolSize = corePoolSize;
+    mMaxPoolSize = maxPoolSize;
+    return this;
+  }
+
+  public DefaultThreadPoolLayoutHandlerBuilder coreDependentPoolConfiguration(
+      int corePoolSizeMultiplier,
+      int corePoolSizeIncrement,
+      int maxPoolSizeMultiplier,
+      int maxPoolSizeIncrement) {
+    mCorePoolSizeMultiplier = corePoolSizeMultiplier;
+    mCorePoolSizeIncrement = corePoolSizeIncrement;
+    mMaxPoolSizeMultiplier = maxPoolSizeMultiplier;
+    mMaxPoolSizeIncrement = maxPoolSizeIncrement;
+    return this;
+  }
+
+  /**
+   * Creates a ThreadPoolLayoutHandler based on the Builder configuration. If no
+   *
+   * @return
+   */
+  public ThreadPoolLayoutHandler build() {
+    if (mHasFixedSizePool) {
+      return new ThreadPoolLayoutHandler(mCorePoolSize, mMaxPoolSize);
+    }
+
+    return new ThreadPoolLayoutHandler(
+        new ThreadPoolLayoutHandler.PoolSizeCalculator() {
+          @Override
+          public int getCorePoolSize(int numProcessors) {
+            return numProcessors * mCorePoolSizeMultiplier + mCorePoolSizeIncrement;
+          }
+
+          @Override
+          public int getMaxPoolSize(int numProcessors) {
+            return numProcessors * mMaxPoolSizeMultiplier + mMaxPoolSizeIncrement;
+          }
+        });
+  }
+}
---
+++ litho-core/src/main/java/com/facebook/litho/ThreadPoolLayoutHandler.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.litho;
+
+import android.os.Process;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/** LayoutHandler implementation that uses a thread pool to calculate the layout. */
+public class ThreadPoolLayoutHandler implements LayoutHandler {
+
+  /**
+   * A PoolSizeCalculator instance can be passed to construct the ThreadPoolLayoutHandler for a
+   * custom way of calculating the number of threads based on the number of cores available on the
+   * device.
+   */
+  public interface PoolSizeCalculator {
+    int getCorePoolSize(int numProcessors);
+
+    int getMaxPoolSize(int numProcessors);
+  }
+
+  private final ThreadPoolExecutor mLayoutThreadPoolExecutor;
+
+  public ThreadPoolLayoutHandler(int corePoolSize, int maxPoolSize) {
+    mLayoutThreadPoolExecutor = new LayoutThreadPoolExecutor(corePoolSize, maxPoolSize);
+  }
+
+  public ThreadPoolLayoutHandler(PoolSizeCalculator poolSizeCalculator) {
+    final int numProcessors = DeviceInfoUtils.getNumberOfCPUCores();
+    mLayoutThreadPoolExecutor =
+        new LayoutThreadPoolExecutor(
+            poolSizeCalculator.getCorePoolSize(numProcessors),
+            poolSizeCalculator.getMaxPoolSize(numProcessors));
+  }
+
+  @Override
+  public boolean post(Runnable runnable) {
+    try {
+      mLayoutThreadPoolExecutor.execute(runnable);
+      return true;
+    } catch (RejectedExecutionException e) {
+      throw new RuntimeException("Cannot execute layout calculation task; " + e);
+    }
+  }
+
+  @Override
+  public void removeCallbacks(Runnable runnable) {
+    mLayoutThreadPoolExecutor.remove(runnable);
+  }
+
+  @Override
+  public void removeCallbacksAndMessages(Object token) {
+    throw new RuntimeException("Operation not supported");
+  }
+
+  private static class LayoutThreadPoolExecutor extends ThreadPoolExecutor {
+
+    private static final ThreadFactory sThreadFactory =
+        new ThreadFactory() {
+
+          private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+          @Override
+          public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r, "ComponentLayoutThread" + threadNumber.getAndIncrement());
+            thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            return thread;
+          }
+        };
+
+    public LayoutThreadPoolExecutor(int corePoolSize, int maxPoolSize) {
+      super(
+          corePoolSize,
+          maxPoolSize,
+          1,
+          TimeUnit.SECONDS,
+          new LinkedBlockingQueue<Runnable>(),
+          sThreadFactory);
+    }
+  }
+}
