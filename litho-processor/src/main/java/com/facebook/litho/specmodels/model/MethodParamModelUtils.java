62b153662eea31d63725f283e6a58681a50107a4
Pascal Hartig <realpassy@fb.com>
2018-02-13 12:25:18 -0800

    Fix validation for differently typed, same-named props
    Summary:
    Two props with the same name but different props would wrongly get deduplicated,
    causing only the first (by internal order of insertion) to be manifested in the
    spec model.
    
    See the test case for a specific example.
    
    Reviewed By: marco-cova
    
    Differential Revision: D6975624
    
    fbshipit-source-id: 401dcd7766e09fa7a91c17e7d085c8e6c64752c1

--- litho-processor/src/main/java/com/facebook/litho/specmodels/model/MethodParamModelUtils.java
+++ litho-processor/src/main/java/com/facebook/litho/specmodels/model/MethodParamModelUtils.java
@@ -80,6 +80,12 @@ public class MethodParamModelUtils {
   public static Comparator<MethodParamModel> shallowParamComparator() {
     return Comparator.nullsLast(
         Comparator.comparing(MethodParamModel::getName)
+            .thenComparing(
+                (a, b) ->
+                    (a.getTypeName() == null && a.getTypeName() == b.getTypeName())
+                            || a.getTypeName().equals(b.getTypeName())
+                        ? 0
+                        : -1)
             .thenComparing((a, b) -> a.getAnnotations().equals(b.getAnnotations()) ? 0 : -1));
   }
 }
