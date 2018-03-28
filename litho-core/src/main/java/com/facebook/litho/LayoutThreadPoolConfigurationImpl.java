66838c6d361fdee8b32c2e32a6d83b2fd6bbb792
Mihaela Ogrezeanu <mihaelao@fb.com>
2018-03-28 16:05:05 -0700

    Refactor layout config so we can use logging
    Summary: To use ComponentsLogger in the config module, we'd either need to put all the logging classes in a logging module or have this workaround. The first one is a bit more instrusive and it ends up in circular dependency that's not really worth getting around for now. This makes LayoutThreadPoolConfiguration an interface in the config module and moves the default implementation to the litho module.
    
    Reviewed By: astreet
    
    Differential Revision: D7427284
    
    fbshipit-source-id: fb165360097a151d54e3c91b0792b57f6e2964c4

---
+++ litho-core/src/main/java/com/facebook/litho/LayoutThreadPoolConfigurationImpl.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.litho;
+
+import android.os.Process;
+import com.facebook.litho.config.DeviceInfoUtils;
+import com.facebook.litho.config.LayoutThreadPoolConfiguration;
+
+/** Configures a thread pool used for layout calculations. */
+public class LayoutThreadPoolConfigurationImpl implements LayoutThreadPoolConfiguration {
+
+  private int mCorePoolSize;
+  private int mMaxPoolSize;
+  private int mThreadPriority;
+
+  public LayoutThreadPoolConfigurationImpl(int corePoolSize, int maxPoolSize, int threadPriority) {
+    mCorePoolSize = corePoolSize;
+    mMaxPoolSize = maxPoolSize;
+    mThreadPriority = threadPriority;
+  }
+
+  @Override
+  public int getCorePoolSize() {
+    return mCorePoolSize;
+  }
+
+  @Override
+  public int getMaxPoolSize() {
+    return mMaxPoolSize;
+  }
+
+  @Override
+  public int getThreadPriority() {
+    return mThreadPriority;
+  }
+
+  public static class Builder {
+    private boolean hasFixedSizePool;
+    private int corePoolSize = 1;
+    private int maxPoolSize = 1;
+    private double corePoolSizeMultiplier = 1;
+    private int corePoolSizeIncrement = 0;
+    private double maxPoolSizeMultiplier = 1;
+    private int maxPoolSizeIncrement = 0;
+    private int threadPriority = Process.THREAD_PRIORITY_BACKGROUND;
+
+    public Builder hasFixedSizePool(boolean hasFixedSizePool) {
+      this.hasFixedSizePool = hasFixedSizePool;
+      return this;
+    }
+
+    public Builder fixedSizePoolConfiguration(int corePoolSize, int maxPoolSize) {
+      this.corePoolSize = corePoolSize;
+      this.maxPoolSize = maxPoolSize;
+      return this;
+    }
+
+    public Builder coreDependentPoolConfiguration(
+        double corePoolSizeMultiplier,
+        int corePoolSizeIncrement,
+        double maxPoolSizeMultiplier,
+        int maxPoolSizeIncrement) {
+      this.corePoolSizeMultiplier = corePoolSizeMultiplier;
+      this.corePoolSizeIncrement = corePoolSizeIncrement;
+      this.maxPoolSizeMultiplier = maxPoolSizeMultiplier;
+      this.maxPoolSizeIncrement = maxPoolSizeIncrement;
+      return this;
+    }
+
+    public Builder threadPriority(int threadPriority) {
+      this.threadPriority = threadPriority;
+      return this;
+    }
+
+    public LayoutThreadPoolConfigurationImpl build() {
+      if (hasFixedSizePool) {
+        return new LayoutThreadPoolConfigurationImpl(corePoolSize, maxPoolSize, threadPriority);
+      }
+
+      int numProcessors = DeviceInfoUtils.getNumberOfCPUCores();
+
+      int procDepCorePoolSize =
+          (int) (numProcessors * corePoolSizeMultiplier + corePoolSizeIncrement);
+      int procDepMaxPoolSize = (int) (numProcessors * maxPoolSizeMultiplier + maxPoolSizeIncrement);
+
+      return new LayoutThreadPoolConfigurationImpl(
+          procDepCorePoolSize, procDepMaxPoolSize, threadPriority);
+    }
+  }
+}
