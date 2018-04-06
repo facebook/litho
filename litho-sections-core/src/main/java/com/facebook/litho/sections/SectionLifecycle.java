975f2501b0d5028e603fdd8a1648f3c4ac9585d7
Kaiang Jia <kjia@fb.com>
2018-04-06 15:46:32 -0700

    Clean up API signature of requestFocusWithOffset()
    Summary: This diff re-organizes the API calls in a cleaner manner. puts all requestFocus() call and requestFocusWithOffset() together. Add better comment for each of the methods.
    
    Differential Revision: D7491526
    
    fbshipit-source-id: 71f750a625fa0829c5ff76edf1f696de57ef1ac5

--- litho-sections-core/src/main/java/com/facebook/litho/sections/SectionLifecycle.java
+++ litho-sections-core/src/main/java/com/facebook/litho/sections/SectionLifecycle.java
@@ -270,6 +270,7 @@ public abstract class SectionLifecycle implements EventDispatcher, EventTriggerT
     }
   }
 
+  /** Scroll to the index of the section. */
   public static void requestFocus(SectionContext c, int index) {
     final Section scopedSection = c.getSectionScope();
     final SectionTree sectionTree = c.getSectionTree();
@@ -281,65 +282,12 @@ public abstract class SectionLifecycle implements EventDispatcher, EventTriggerT
     sectionTree.requestFocus(scopedSection, index);
   }
 
-  /**
-   * Scroll to the index in the section with an additional offset. One notable difference between
-   * this and the {@link #requestFocus(SectionContext, int)} API is that this will *always* take
-   * an action, while {@link #requestFocus(SectionContext, int)} will ignore the command if the
-   * item is visible on the screen.
-   * @param offset distance, in pixels, from the top of the screen to scroll the requested item
-   */
-  public static void requestFocusWithOffset(SectionContext c, int index, int offset) {
-    final Section scopedSection = c.getSectionScope();
-    final SectionTree sectionTree = c.getSectionTree();
-
-    if (scopedSection == null || sectionTree == null) {
-      return;
-    }
-
-    sectionTree.requestFocusWithOffset(scopedSection, index, offset);
-  }
-
-  /**
-   * Scroll to the beginning of the section with the given key.
-   */
+  /** Scroll to the beginning of the section with the given key. */
   public static void requestFocus(SectionContext c, String sectionKey) {
     requestFocus(c, sectionKey, FocusType.START);
   }
 
-  /**
-   * Scroll to the index in the section with an additional offset. One notable difference between
-   * this and the {@link #requestFocus(SectionContext, String)} API is that this will *always* take
-   * an action, while {@link #requestFocus(SectionContext, String)} will ignore the command if the
-   * item is visible on the screen.
-   * @param offset distance, in pixels, from the top of the screen to scroll the requested item
-   */
-  public static void requestFocusWithOffset(SectionContext c, String sectionKey, int offset) {
-    final Section scopedSection = c.getSectionScope();
-    final SectionTree sectionTree = c.getSectionTree();
-
-    if (scopedSection == null || sectionTree == null) {
-      return;
-    }
-
-    final String globalKey = scopedSection.getGlobalKey() + sectionKey;
-
-    sectionTree.requestFocusWithOffset(globalKey, offset);
-  }
-
-  public static void requestFocusWithOffset(
-      SectionContext c, String sectionKey, int index, int offset) {
-    final Section scopedSection = c.getSectionScope();
-    final SectionTree sectionTree = c.getSectionTree();
-
-    if (scopedSection == null || sectionTree == null) {
-      return;
-    }
-
-    final String globalKey = scopedSection.getGlobalKey() + sectionKey;
-
-    sectionTree.requestFocusWithOffset(globalKey, index, offset);
-  }
-
+  /** Scroll to the index in the section with the given key. */
   public static void requestFocus(SectionContext c, String sectionKey, FocusType focusType) {
     final Section scopedSection = c.getSectionScope();
     final SectionTree sectionTree = c.getSectionTree();
@@ -358,5 +306,61 @@ public abstract class SectionLifecycle implements EventDispatcher, EventTriggerT
         sectionTree.requestFocusEnd(globalKey);
         break;
     }
+  }
+
+  /**
+   * Scroll to the index in the section with an additional offset. One notable difference between
+   * this and the {@link #requestFocus(SectionContext, int)} API is that this will *always* take an
+   * action, while {@link #requestFocus(SectionContext, int)} will ignore the command if the item is
+   * visible on the screen.
+   *
+   * @param offset distance, in pixels, from the top of the screen to scroll the requested item
+   */
+  public static void requestFocusWithOffset(SectionContext c, int index, int offset) {
+    final Section scopedSection = c.getSectionScope();
+    final SectionTree sectionTree = c.getSectionTree();
+
+    if (scopedSection == null || sectionTree == null) {
+      return;
+    }
+
+    sectionTree.requestFocusWithOffset(scopedSection, index, offset);
+  }
+
+  /**
+   * Scroll to the beginning of the section with the given key and an additional offset. One notable
+   * difference between this and the {@link #requestFocus(SectionContext, String)} API is that this
+   * will *always* take an action, while {@link #requestFocus(SectionContext, String)} will ignore
+   * the command if the item is visible on the screen.
+   *
+   * @param sectionKey the key of the section component.
+   * @param offset distance, in pixels, from the top of the screen to scroll the requested item.
+   */
+  public static void requestFocusWithOffset(SectionContext c, String sectionKey, int offset) {
+    requestFocusWithOffset(c, sectionKey, 0, offset);
+  }
+
+  /**
+   * Scroll to the index in the section with the given key and an additional offset. One notable
+   * difference between this and the {@link #requestFocus(SectionContext, String)} API is that this
+   * will *always* take an action, while {@link #requestFocus(SectionContext, String)} will ignore
+   * the command if the item is visible on the screen.
+   *
+   * @param sectionKey the key of the section component.
+   * @param index the index the section component
+   * @param offset distance, in pixels, from the top of the screen to scroll the requested item.
+   */
+  public static void requestFocusWithOffset(
+      SectionContext c, String sectionKey, int index, int offset) {
+    final Section scopedSection = c.getSectionScope();
+    final SectionTree sectionTree = c.getSectionTree();
+
+    if (scopedSection == null || sectionTree == null) {
+      return;
+    }
+
+    final String globalKey = scopedSection.getGlobalKey() + sectionKey;
+
+    sectionTree.requestFocusWithOffset(globalKey, index, offset);
   }
 }
