3e7d0d3bb2690cde08420a0c4a31a3a0b7ec549f
Pascal Hartig <realpassy@fb.com>
2018-04-16 08:28:54 -0700

    Resync fbjni
    Summary:
    The sync is manual due to the differences in the build system. This was
    relicensed as part of Yoga, but our version was still under BSD+Patents.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D7638353
    
    fbshipit-source-id: 56fccb90edf92343d7210274b2c3dc577c398518

--- lib/fbjni/src/main/cpp/jni/fbjni.cpp
+++ lib/fbjni/src/main/cpp/jni/fbjni.cpp
@@ -1,10 +1,8 @@
 /*
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #include <fb/fbjni.h>
@@ -18,24 +16,25 @@ namespace facebook {
 namespace jni {
 
 jint initialize(JavaVM* vm, std::function<void()>&& init_fn) noexcept {
+  static std::once_flag flag{};
   // TODO (t7832883): DTRT when we have exception pointers
   static auto error_msg = std::string{"Failed to initialize fbjni"};
-  static bool error_occured = [vm] {
-      bool retVal = false;
+  static auto error_occured = false;
+
+  std::call_once(flag, [vm] {
+    try {
+      Environment::initialize(vm);
+    } catch (std::exception& ex) {
+      error_occured = true;
       try {
-        Environment::initialize(vm);
-      } catch (std::exception& ex) {
-        retVal = true;
-        try {
-          error_msg = std::string{"Failed to initialize fbjni: "} + ex.what();
-        } catch (...) {
-          // Ignore, we already have a fall back message
-        }
+        error_msg = std::string{"Failed to initialize fbjni: "} + ex.what();
       } catch (...) {
-        retVal = true;
+        // Ignore, we already have a fall back message
       }
-      return retVal;
-    }();
+    } catch (...) {
+      error_occured = true;
+    }
+  });
 
   try {
     if (error_occured) {
