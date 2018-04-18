fbc8f77d75c276a4eedf82c4a91f28e55c9645d8
Sergey Nikolaienkov <sergeyn@fb.com>
2018-04-18 01:42:25 -0700

    take into account special case of ROOT_HOST_ID, when extracting type from layout output id
    Summary: Fix LayoutStateOutputIdCalculator#getTypeFromId(). Now returns correct OutputUnitType.HOST for ROOT_HOST_ID
    
    Reviewed By: muraziz
    
    Differential Revision: D7653839
    
    fbshipit-source-id: 17d6dfe2a63aba7724a3df1d33acff5d5f179f49

--- litho-core/src/main/java/com/facebook/litho/LayoutStateOutputIdCalculator.java
+++ litho-core/src/main/java/com/facebook/litho/LayoutStateOutputIdCalculator.java
@@ -199,6 +199,10 @@ class LayoutStateOutputIdCalculator {
 
   /** @return the type part of an id. */
   static @OutputUnitType int getTypeFromId(long id) {
+    if (id == MountState.ROOT_HOST_ID) {
+      // special case
+      return OutputUnitType.HOST;
+    }
     return (int) ((id >> TYPE_SHIFT) & 0x3);
   }
 
