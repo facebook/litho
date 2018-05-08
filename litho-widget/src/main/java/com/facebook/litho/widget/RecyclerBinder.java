9bb60cc5aaeea6129112d1fe19b82117f79f82f5
Mihaela Ogrezeanu <mihaelao@fb.com>
2018-05-08 07:09:47 -0700

    Fix components logging in RecyclerBinder
    Summary: D7789327 broke logging for ComponentTrees created by RecyclerBinder.
    
    Reviewed By: IanChilds
    
    Differential Revision: D7908790
    
    fbshipit-source-id: 810f85fa16103cf7f58b83bc15948682eaff5f58

--- litho-widget/src/main/java/com/facebook/litho/widget/RecyclerBinder.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/RecyclerBinder.java
@@ -462,7 +462,7 @@ public class RecyclerBinder
 
     /** @param c The {@link ComponentContext} the RecyclerBinder will use. */
     public RecyclerBinder build(ComponentContext c) {
-      componentContext = new ComponentContext(c.getBaseContext());
+      componentContext = new ComponentContext(c.getBaseContext(), c.getLogTag(), c.getLogger());
 
       if (layoutInfo == null) {
         layoutInfo = new LinearLayoutInfo(c, VERTICAL, false);
