e1cb4b14e0ba01894d21f81294fa1f617fb9b844
Dan Yang <dsyang@fb.com>
2018-05-21 17:55:16 -0700

    Extract TextSpec#onLoadStyle into a helper utility
    Summary: This diff extracts the OnLoadStyle method inside TextSpec into a helper utility.  This is useful when writing thin wrappers over TextSpec to ensure that the thin wrapper can adhere to the same functionality TextSpec has when using styles to define certain props.
    
    Reviewed By: IanChilds, boxme
    
    Differential Revision: D8063085
    
    fbshipit-source-id: 9cc6656a29b59468384a85a738b4283e8f7c7d96

--- litho-widget/src/main/java/com/facebook/litho/widget/TextSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextSpec.java
@@ -17,9 +17,6 @@
 package com.facebook.litho.widget;
 
 import static android.support.v4.widget.ExploreByTouchHelper.INVALID_ID;
-import static android.text.Layout.Alignment.ALIGN_CENTER;
-import static android.text.Layout.Alignment.ALIGN_NORMAL;
-import static android.text.Layout.Alignment.ALIGN_OPPOSITE;
 import static com.facebook.litho.FrameworkLogEvents.EVENT_ERROR;
 import static com.facebook.litho.FrameworkLogEvents.PARAM_MESSAGE;
 import static com.facebook.litho.SizeSpec.AT_MOST;
@@ -27,12 +24,14 @@ import static com.facebook.litho.SizeSpec.EXACTLY;
 import static com.facebook.litho.SizeSpec.UNSPECIFIED;
 import static com.facebook.litho.annotations.ResType.BOOL;
 import static com.facebook.litho.annotations.ResType.STRING;
-import static com.facebook.litho.widget.VerticalGravity.BOTTOM;
-import static com.facebook.litho.widget.VerticalGravity.CENTER;
-import static com.facebook.litho.widget.VerticalGravity.TOP;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_BREAK_STRATEGY;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_EMS;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_HYPHENATION_FREQUENCY;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_JUSTIFICATION_MODE;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_MAX_WIDTH;
+import static com.facebook.litho.widget.TextStylesHelper.DEFAULT_MIN_WIDTH;
 
 import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Path;
 import android.graphics.Rect;
@@ -53,8 +52,6 @@ import android.text.TextUtils.TruncateAt;
 import android.text.style.ClickableSpan;
 import android.text.style.ImageSpan;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
 import com.facebook.fbui.textlayoutbuilder.TextLayoutBuilder;
 import com.facebook.fbui.textlayoutbuilder.util.LayoutMeasureUtil;
 import com.facebook.litho.ComponentContext;
@@ -63,7 +60,6 @@ import com.facebook.litho.ComponentsLogger;
 import com.facebook.litho.EventHandler;
 import com.facebook.litho.LogEvent;
 import com.facebook.litho.Output;
-import com.facebook.litho.R;
 import com.facebook.litho.Size;
 import com.facebook.litho.SizeSpec;
 import com.facebook.litho.annotations.FromBoundsDefined;
@@ -135,26 +131,13 @@ import com.facebook.yoga.YogaDirection;
   events = {TextOffsetOnTouchEvent.class}
 )
 class TextSpec {
-  static {
-    SynchronizedTypefaceHelper.setupSynchronizedTypeface();
-  }
-
-  private static final TruncateAt[] TRUNCATE_AT = TruncateAt.values();
 
   private static final Typeface DEFAULT_TYPEFACE = Typeface.DEFAULT;
   private static final int DEFAULT_COLOR = 0;
-  private static final int DEFAULT_EMS = -1;
-  private static final int DEFAULT_MIN_WIDTH = 0;
-  private static final int DEFAULT_MAX_WIDTH = Integer.MAX_VALUE;
-  private static final int DEFAULT_BREAK_STRATEGY = 0; // BREAK_STRATEGY_SIMPLE (AOSP Default)
-  private static final int DEFAULT_HYPHENATION_FREQUENCY =
-      0; // HYPHENATION_FREQUENCY_NONE (AOSP Default)
-  private static final int DEFAULT_JUSTIFICATION_MODE = 0; // JUSTIFICATION_MODE_NONE (AOSP Default)
+  private static final String TAG = "TextSpec";
 
   private static final int[][] DEFAULT_TEXT_COLOR_STATE_LIST_STATES = {{0}};
   private static final int[] DEFAULT_TEXT_COLOR_STATE_LIST_COLORS = {Color.BLACK};
-
-  private static final String TAG = "TextSpec";
 
   @PropDefault protected static final int minLines = Integer.MIN_VALUE;
   @PropDefault protected static final int maxLines = Integer.MAX_VALUE;
@@ -175,7 +158,10 @@ class TextSpec {
   @PropDefault protected static final VerticalGravity verticalGravity = VerticalGravity.TOP;
   @PropDefault protected static final boolean glyphWarming = false;
   @PropDefault protected static final boolean shouldIncludeFontPadding = true;
-  @PropDefault protected static final Alignment textAlignment = ALIGN_NORMAL;
+
+  @PropDefault
+  protected static final Alignment textAlignment = TextStylesHelper.textAlignmentDefault;
+
   @PropDefault protected static final int breakStrategy = DEFAULT_BREAK_STRATEGY;
   @PropDefault protected static final int hyphenationFrequency = DEFAULT_HYPHENATION_FREQUENCY;
   @PropDefault protected static final int justificationMode = DEFAULT_JUSTIFICATION_MODE;
@@ -220,57 +206,8 @@ class TextSpec {
       Output<VerticalGravity> verticalGravity,
       Output<Typeface> typeface) {
 
-    //check first if provided attributes contain textAppearance. As an analogy to TextView behavior,
-    //we will parse textAppearance attributes first and then will override leftovers from main style
-    TypedArray a = c.obtainStyledAttributes(
-        R.styleable.Text_TextAppearanceAttr,
-        0);
-
-    int textAppearanceResId = a.getResourceId(
-        R.styleable.Text_TextAppearanceAttr_android_textAppearance,
-        -1);
-    a.recycle();
-    if (textAppearanceResId != -1) {
-      a = c.getTheme().obtainStyledAttributes(
-          textAppearanceResId,
-          R.styleable.Text);
-      resolveStyleAttrsForTypedArray(
-          a,
-          ellipsize,
-          shouldIncludeFontPadding,
-          spacingMultiplier,
-          minLines,
-          maxLines,
-          minEms,
-          maxEms,
-          minTextWidth,
-          maxTextWidth,
-          isSingleLine,
-          text,
-          textColorStateList,
-          linkColor,
-          highlightColor,
-          textSize,
-          textAlignment,
-          breakStrategy,
-          hyphenationFrequency,
-          justificationMode,
-          textStyle,
-          shadowRadius,
-          shadowDx,
-          shadowDy,
-          shadowColor,
-          verticalGravity,
-          typeface);
-      a.recycle();
-    }
-
-    //now (after we parsed textAppearance) we can move on to main style attributes
-    a = c.obtainStyledAttributes(
-        R.styleable.Text,
-        0);
-    resolveStyleAttrsForTypedArray(
-        a,
+    TextStylesHelper.onLoadStyle(
+        c,
         ellipsize,
         shouldIncludeFontPadding,
         spacingMultiplier,
@@ -297,114 +234,6 @@ class TextSpec {
         shadowColor,
         verticalGravity,
         typeface);
-
-    a.recycle();
-  }
-
-  private static void resolveStyleAttrsForTypedArray(
-      TypedArray a,
-      Output<TruncateAt> ellipsize,
-      Output<Boolean> shouldIncludeFontPadding,
-      Output<Float> spacingMultiplier,
-      Output<Integer> minLines,
-      Output<Integer> maxLines,
-      Output<Integer> minEms,
-      Output<Integer> maxEms,
-      Output<Integer> minTextWidth,
-      Output<Integer> maxTextWidth,
-      Output<Boolean> isSingleLine,
-      Output<CharSequence> text,
-      Output<ColorStateList> textColorStateList,
-      Output<Integer> linkColor,
-      Output<Integer> highlightColor,
-      Output<Integer> textSize,
-      Output<Alignment> textAlignment,
-      Output<Integer> breakStrategy,
-      Output<Integer> hyphenationFrequency,
-      Output<Integer> justificationMode,
-      Output<Integer> textStyle,
-      Output<Float> shadowRadius,
-      Output<Float> shadowDx,
-      Output<Float> shadowDy,
-      Output<Integer> shadowColor,
-      Output<VerticalGravity> verticalGravity,
-      Output<Typeface> typeface) {
-    int viewTextAlignment = View.TEXT_ALIGNMENT_GRAVITY;
-    int gravity = Gravity.NO_GRAVITY;
-    String fontFamily = null;
-
-    for (int i = 0, size = a.getIndexCount(); i < size; i++) {
-      final int attr = a.getIndex(i);
-
-      if (attr == R.styleable.Text_android_text) {
-        text.set(a.getString(attr));
-      } else if (attr == R.styleable.Text_android_textColor) {
-        textColorStateList.set(a.getColorStateList(attr));
-      } else if (attr == R.styleable.Text_android_textSize) {
-        textSize.set(a.getDimensionPixelSize(attr, 0));
-      } else if (attr == R.styleable.Text_android_ellipsize) {
-        final int index = a.getInteger(attr, 0);
-        if (index > 0) {
-          ellipsize.set(TRUNCATE_AT[index - 1]);
-        }
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
-          attr == R.styleable.Text_android_textAlignment) {
-        viewTextAlignment = a.getInt(attr, -1);
-        textAlignment.set(getAlignment(viewTextAlignment, gravity));
-      } else if (attr == R.styleable.Text_android_gravity) {
-        gravity = a.getInt(attr, -1);
-        textAlignment.set(getAlignment(viewTextAlignment, gravity));
-        verticalGravity.set(getVerticalGravity(gravity));
-      } else if (attr == R.styleable.Text_android_includeFontPadding) {
-        shouldIncludeFontPadding.set(a.getBoolean(attr, false));
-      } else if (attr == R.styleable.Text_android_minLines) {
-        minLines.set(a.getInteger(attr, -1));
-      } else if (attr == R.styleable.Text_android_maxLines) {
-        maxLines.set(a.getInteger(attr, -1));
-      } else if (attr == R.styleable.Text_android_singleLine) {
-        isSingleLine.set(a.getBoolean(attr, false));
-      } else if (attr == R.styleable.Text_android_textColorLink) {
-        linkColor.set(a.getColor(attr, 0));
-      } else if (attr == R.styleable.Text_android_textColorHighlight) {
-        highlightColor.set(a.getColor(attr, 0));
-      } else if (attr == R.styleable.Text_android_textStyle) {
-        textStyle.set(a.getInteger(attr, 0));
-      } else if (attr == R.styleable.Text_android_lineSpacingMultiplier) {
-        spacingMultiplier.set(a.getFloat(attr, 0));
-      } else if (attr == R.styleable.Text_android_shadowDx) {
-        shadowDx.set(a.getFloat(attr, 0));
-      } else if (attr == R.styleable.Text_android_shadowDy) {
-        shadowDy.set(a.getFloat(attr, 0));
-      } else if (attr == R.styleable.Text_android_shadowRadius) {
-        shadowRadius.set(a.getFloat(attr, 0));
-      } else if (attr == R.styleable.Text_android_shadowColor) {
-        shadowColor.set(a.getColor(attr, 0));
-      } else if (attr == R.styleable.Text_android_minEms) {
-        minEms.set(a.getInteger(attr, DEFAULT_EMS));
-      } else if (attr == R.styleable.Text_android_maxEms) {
-        maxEms.set(a.getInteger(attr, DEFAULT_EMS));
-      } else if (attr == R.styleable.Text_android_minWidth) {
-        minTextWidth.set(a.getDimensionPixelSize(attr, DEFAULT_MIN_WIDTH));
-      } else if (attr == R.styleable.Text_android_maxWidth) {
-        maxTextWidth.set(a.getDimensionPixelSize(attr, DEFAULT_MAX_WIDTH));
-      } else if (attr == R.styleable.Text_android_fontFamily) {
-        fontFamily = a.getString(attr);
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-          && attr == R.styleable.Text_android_breakStrategy) {
-        breakStrategy.set(a.getInt(attr, DEFAULT_BREAK_STRATEGY));
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-          && attr == R.styleable.Text_android_hyphenationFrequency) {
-        hyphenationFrequency.set(a.getInt(attr, DEFAULT_HYPHENATION_FREQUENCY));
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-          && attr == R.styleable.Text_android_justificationMode) {
-        justificationMode.set(a.getInt(attr, DEFAULT_JUSTIFICATION_MODE));
-      }
-    }
-
-    if (fontFamily != null) {
-      final Integer styleValue = textStyle.get();
-      typeface.set(Typeface.create(fontFamily, styleValue == null ? -1 : styleValue));
-    }
   }
 
   @OnMeasure
@@ -641,81 +470,6 @@ class TextSpec {
     }
 
     return newLayout;
-  }
-
-  private static Alignment getAlignment(int viewTextAlignment, int gravity) {
-    final Alignment alignment;
-    switch (viewTextAlignment) {
-      case View.TEXT_ALIGNMENT_GRAVITY:
-        alignment = getAlignment(gravity);
-        break;
-      case View.TEXT_ALIGNMENT_TEXT_START:
-        alignment = ALIGN_NORMAL;
-        break;
-      case View.TEXT_ALIGNMENT_TEXT_END:
-        alignment = ALIGN_OPPOSITE;
-        break;
-      case View.TEXT_ALIGNMENT_CENTER:
-        alignment = ALIGN_CENTER;
-        break;
-      case View.TEXT_ALIGNMENT_VIEW_START: // unsupported, default to normal
-        alignment = ALIGN_NORMAL;
-        break;
-      case View.TEXT_ALIGNMENT_VIEW_END: // unsupported, default to opposite
-        alignment = ALIGN_OPPOSITE;
-        break;
-      case View.TEXT_ALIGNMENT_INHERIT: // unsupported, default to gravity
-        alignment = getAlignment(gravity);
-        break;
-      default:
-        alignment = textAlignment;
-        break;
-    }
-    return alignment;
-  }
-
-  private static Alignment getAlignment(int gravity) {
-    final Alignment alignment;
-    switch (gravity & Gravity.RELATIVE_HORIZONTAL_GRAVITY_MASK) {
-      case Gravity.START:
-        alignment = ALIGN_NORMAL;
-        break;
-      case Gravity.END:
-        alignment = ALIGN_OPPOSITE;
-        break;
-      case Gravity.LEFT: // unsupported, default to normal
-        alignment = ALIGN_NORMAL;
-        break;
-      case Gravity.RIGHT: // unsupported, default to opposite
-        alignment = ALIGN_OPPOSITE;
-        break;
-      case Gravity.CENTER_HORIZONTAL:
-        alignment = ALIGN_CENTER;
-        break;
-      default:
-        alignment = textAlignment;
-        break;
-    }
-    return alignment;
-  }
-
-  private static VerticalGravity getVerticalGravity(int gravity) {
-    final VerticalGravity verticalGravity;
-    switch (gravity & Gravity.VERTICAL_GRAVITY_MASK) {
-      case Gravity.TOP:
-        verticalGravity = TOP;
-        break;
-      case Gravity.CENTER_VERTICAL:
-        verticalGravity = CENTER;
-        break;
-      case Gravity.BOTTOM:
-        verticalGravity = BOTTOM;
-        break;
-      default:
-        verticalGravity = TextSpec.verticalGravity;
-        break;
-    }
-    return verticalGravity;
   }
 
   @OnBoundsDefined
