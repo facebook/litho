2f869c051483d985bd77f279dc1cf8198b0bf2c0
Pascal Hartig <realpassy@fb.com>
2018-05-22 02:12:37 -0700

    Add integration test for Section tree props
    Summary:
    Testing TreeProps in Sections with just unit tests is not possible without
    stubbing out massive parts of the runtime which ultimately influences its
    behavior. In order to get more confidence for upcoming changes, I want to have
    an end-to-end test which involves a RecyclerView and a GroupSectionSpec.
    
    Right now, it documents the old behavior, which makes it easier to observe the
    effect of the upcoming change. That change will enable Sections to propagate
    TreeProps through Sections and ultimately allow Sections to manipulate treeprops
    themselves.
    
    This sets up a new high-level project as we need a full activity to run this and
    want to have very fine-grained dependencies for this. It also surfaced some
    setup issues with `:litho-espresso` that had gone unnoticed before.
    
    Reviewed By: IanChilds
    
    Differential Revision: D8075296
    
    fbshipit-source-id: c01748274b792b14d2c89be3a8b0c825b6f3284c

---
+++ litho-instrumentation-tests/src/main/java/com/facebook/litho/sections/treeprops/TestGroupSectionSpec.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.sections.treeprops;
+
+import com.facebook.litho.annotations.OnCreateTreeProp;
+import com.facebook.litho.annotations.TreeProp;
+import com.facebook.litho.sections.Children;
+import com.facebook.litho.sections.SectionContext;
+import com.facebook.litho.sections.annotations.GroupSectionSpec;
+import com.facebook.litho.sections.annotations.OnCreateChildren;
+import com.facebook.litho.sections.common.SingleComponentSection;
+
+@GroupSectionSpec
+public class TestGroupSectionSpec {
+
+  @OnCreateChildren
+  protected static Children onCreateChildren(SectionContext c) {
+    return Children.create()
+        .child(SingleComponentSection.create(c).component(LeafComponent.create(c).build()))
+        .build();
+  }
+
+  @OnCreateTreeProp
+  static TestTreeProp onCreateTestTreeProp(SectionContext c, @TreeProp TestTreeProp t) {
+    return t == null ? new TestTreeProp("section") : TestTreeProp.append(t, "section");
+  }
+}
