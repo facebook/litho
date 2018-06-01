4b11c1d9321d9165c41e51710ebea7e02679ba26
Andy Street <andrews@fb.com>
2018-06-01 04:36:25 -0700

    Add systrace markers for onMount and onBind
    Summary: See title. Using to figure out breakdown of mount vs bind for fresco.
    
    Reviewed By: muraziz
    
    Differential Revision: D8226344
    
    fbshipit-source-id: 946a4cf27b093e5e1ee3b3476a4251099b34dc29

--- litho-core/src/main/java/com/facebook/litho/ComponentLifecycle.java
+++ litho-core/src/main/java/com/facebook/litho/ComponentLifecycle.java
@@ -238,18 +238,40 @@ public abstract class ComponentLifecycle implements EventDispatcher, EventTrigge
 
   void mount(ComponentContext c, Object convertContent) {
     c.enterNoStateUpdatesMethod("mount");
+
+    final boolean isTracing = ComponentsSystrace.isTracing();
+    if (isTracing) {
+      ComponentsSystrace.beginSection("onMount:" + ((Component) this).getSimpleName());
+    }
     try {
       onMount(c, convertContent);
     } catch (Exception e) {
       c.exitNoStateUpdatesMethod();
       dispatchErrorEvent(c, e);
+    } finally {
+      if (isTracing) {
+        ComponentsSystrace.endSection();
+      }
     }
+
     c.exitNoStateUpdatesMethod();
   }
 
   void bind(ComponentContext c, Object mountedContent) {
     c.enterNoStateUpdatesMethod("bind");
-    onBind(c, mountedContent);
+
+    final boolean isTracing = ComponentsSystrace.isTracing();
+    if (isTracing) {
+      ComponentsSystrace.beginSection("onBind:" + ((Component) this).getSimpleName());
+    }
+    try {
+      onBind(c, mountedContent);
+    } finally {
+      if (isTracing) {
+        ComponentsSystrace.endSection();
+      }
+    }
+
     c.exitNoStateUpdatesMethod();
   }
 
