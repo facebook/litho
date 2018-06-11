ac7e0568f9472aa33f177bdb6ade5ed33b96790d
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2018-06-11 22:11:21 +0300

    Fix Border opposite orientation edges
    * In Border class setting a path effect on edges, was using the opposite
    edges of those requested in order to apply the effect.
    
    * Addresses #381

--- litho-core/src/main/java/com/facebook/litho/Border.java
+++ litho-core/src/main/java/com/facebook/litho/Border.java
@@ -133,12 +133,12 @@ public class Border {
         }
         break;
       case VERTICAL:
-        edges[EDGE_TOP] = value;
-        edges[EDGE_BOTTOM] = value;
-        break;
-      case HORIZONTAL:
         edges[EDGE_LEFT] = value;
         edges[EDGE_RIGHT] = value;
+        break;
+      case HORIZONTAL:
+        edges[EDGE_TOP] = value;
+        edges[EDGE_BOTTOM] = value;
         break;
       case LEFT:
       case TOP:
