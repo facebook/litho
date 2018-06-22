4c197ff644216d39c0cf90d6e0641e100bc38d69
Mihaela Ogrezeanu <mihaelao@fb.com>
2018-06-22 08:26:42 -0700

    Log changeset count getting to < 0
    Summary: trying to understand more when this can happen
    
    Reviewed By: topwu
    
    Differential Revision: D8562070
    
    fbshipit-source-id: 0260cdf07d23df842fa0d211b814ca106622065e

--- litho-sections-core/src/main/java/com/facebook/litho/sections/ChangeSetState.java
+++ litho-sections-core/src/main/java/com/facebook/litho/sections/ChangeSetState.java
@@ -86,6 +86,8 @@ public class ChangeSetState {
       logger.logPerfEvent(logEvent);
     }
 
+    checkCount(currentRoot, newRoot, changeSetState);
+
     return changeSetState;
   }
 
@@ -411,5 +413,62 @@ public class ChangeSetState {
       return prefix + "->" + root.getClass().getSimpleName();
     }
     return "";
+  }
+
+  private static void checkCount(
+      Section currentRoot, Section newRoot, ChangeSetState changeSetState) {
+    final boolean hasNegativeCount =
+        (currentRoot != null && currentRoot.getCount() < 0)
+            || (newRoot != null && newRoot.getCount() < 0);
+
+    if (!hasNegativeCount) {
+      return;
+    }
+
+    final StringBuilder message = new StringBuilder();
+    message.append("Changet count is below 0! ");
+
+    message.append("Current section: ");
+    if (currentRoot == null) {
+      message.append("null; ");
+    } else {
+      message.append(
+          currentRoot.getSimpleName()
+              + " , key="
+              + currentRoot.getGlobalKey()
+              + ", count="
+              + currentRoot.getCount()
+              + ", childrenSize="
+              + currentRoot.getChildren().size()
+              + "; ");
+    }
+
+    message.append("Next section: ");
+    if (newRoot == null) {
+      message.append("null; ");
+    } else {
+      message.append(
+          newRoot.getSimpleName()
+              + " , key="
+              + newRoot.getGlobalKey()
+              + ", count="
+              + newRoot.getCount()
+              + ", childrenSize="
+              + newRoot.getChildren().size()
+              + "; ");
+    }
+
+    message.append("Changes: [");
+
+    final ChangeSet changeSet = changeSetState.mChangeSet;
+    for (int i = 0; i < changeSet.getCount(); i++) {
+      final Change change = changeSet.getChangeAt(i);
+      message.append(change.getType() + " " + change.getIndex() + " " + change.getToIndex());
+      message.append(", ");
+    }
+
+    message.append("]");
+
+    throw new IllegalStateException(message.toString());
   }
 }
