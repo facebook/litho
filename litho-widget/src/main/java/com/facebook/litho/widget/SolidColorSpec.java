b52eaccd2e4bcaa8f7eab87d3f4b83711283b8c6
Hilal Alsibai <halsibai@fb.com>
2018-08-20 11:41:47 -0700

    Optimize setting an alpha on SolidColor
    Summary: Setting alpha previously would do it at the layer level. We can just modify the color's alpha value instead.
    
    Reviewed By: IanChilds
    
    Differential Revision: D9388796
    
    fbshipit-source-id: 22f1a11ea4bbaf1110e0694a6a717126e66204a0

--- litho-widget/src/main/java/com/facebook/litho/widget/SolidColorSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/SolidColorSpec.java
@@ -20,23 +20,34 @@ import static android.widget.ImageView.ScaleType.FIT_XY;
 import static com.facebook.litho.annotations.ResType.COLOR;
 
 import android.graphics.drawable.ColorDrawable;
+import android.support.v4.graphics.ColorUtils;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
 import com.facebook.litho.annotations.Prop;
+import com.facebook.litho.annotations.PropDefault;
 
 /**
  * A component that renders a solid color.
  *
  * @uidocs https://fburl.com/SolidColor:b0df
  * @prop color Color to be shown.
+ * @prop alpha The alpha of the color, in the range [0.0, 1.0]
  */
 @LayoutSpec
 class SolidColorSpec {
+  @PropDefault static final float alpha = -1.0f;
 
   @OnCreateLayout
-  static Component onCreateLayout(ComponentContext c, @Prop(resType = COLOR) int color) {
+  static Component onCreateLayout(
+      ComponentContext c,
+      @Prop(resType = COLOR) int color,
+      @Prop(optional = true, isCommonProp = true, overrideCommonPropBehavior = true) float alpha) {
+    if (alpha >= 0f) {
+      alpha = Math.min(1f, alpha);
+      color = ColorUtils.setAlphaComponent(color, (int) (alpha * 255f));
+    }
     return Image.create(c)
         .scaleType(FIT_XY)
         .drawable(new ColorDrawable(color))
