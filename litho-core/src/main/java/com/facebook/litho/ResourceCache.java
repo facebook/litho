b075fccaf2a8bb07a4d18f5c2b6fbf84f60f23b5
Andy Street <andrews@fb.com>
2018-08-21 04:33:45 -0700

    Fix bug where ResourceCache was not properly invalidated
    Summary:
    We were using equals on the new Configuration object, but it turns out that that often is the same Configuration object we had before, just mutated.
    
    Creating a new Configuration object maintains the equals check without our Configuration object getting mutated.
    
    Reviewed By: IanChilds, muraziz
    
    Differential Revision: D9403632
    
    fbshipit-source-id: fef2f38811e6a45760f7db8e0ab7873c3022a9f5

--- litho-core/src/main/java/com/facebook/litho/ResourceCache.java
+++ litho-core/src/main/java/com/facebook/litho/ResourceCache.java
@@ -24,7 +24,7 @@ public abstract class ResourceCache {
 
   static synchronized ResourceCache getLatest(Configuration configuration) {
     if (latest == null || !latest.mConfiguration.equals(configuration)) {
-      latest = new LruResourceCache(configuration);
+      latest = new LruResourceCache(new Configuration(configuration));
     }
     return latest;
   }
