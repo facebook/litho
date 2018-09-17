3601c4ac0bf578dadcea4246e9ee5f13d284d505
Andy Street <andrews@fb.com>
2018-09-17 06:25:06 -0700

    Add current root section name to debug info in SectionTree
    Summary: The tag is set from the builder as the first section that was set, but there are places where we change the root section.
    
    Reviewed By: mihaelao
    
    Differential Revision: D9850588
    
    fbshipit-source-id: 1c61523cb5c34eca963a71a0f3239052dfd7ccbc

--- litho-sections-core/src/main/java/com/facebook/litho/sections/SectionTree.java
+++ litho-sections-core/src/main/java/com/facebook/litho/sections/SectionTree.java
@@ -1111,7 +1111,11 @@ public class SectionTree {
 
   private void postNewChangeSets(Throwable tracedThrowable) {
     if (isMainThread()) {
-      postChangesetsToHandler();
+      try {
+        postChangesetsToHandler();
+      } catch (IndexOutOfBoundsException e) {
+        throw new RuntimeException(getDebugInfo(this) + e.getMessage(), e);
+      }
     } else {
       sMainThreadHandler
           .obtainMessage(
@@ -1444,28 +1448,39 @@ public class SectionTree {
     }
   }
 
-  @GuardedBy("tree")
   private static String getDebugInfo(SectionTree tree) {
-    final StringBuilder sb = new StringBuilder();
-    sb.append("tag: ");
-    sb.append(tree.mTag);
+    synchronized (tree) {
+      if (tree.isReleased()) {
+        return "[Released Tree]";
+      }
 
-    sb.append(", currentSection.size: ");
-    sb.append(tree.mCurrentSection != null ? tree.mCurrentSection.getCount() : null);
+      final StringBuilder sb = new StringBuilder();
+      sb.append("tag: ");
+      sb.append(tree.mTag);
 
-    sb.append(", nextSection.size: ");
-    sb.append(tree.mNextSection != null ? tree.mNextSection.getCount() : null);
+      sb.append(", currentSection.size: ");
+      sb.append(tree.mCurrentSection != null ? tree.mCurrentSection.getCount() : null);
 
-    sb.append(", pendingChangeSets.size: ");
-    sb.append(tree.mPendingChangeSets.size());
+      sb.append(", currentSection.name: ");
+      sb.append(tree.mCurrentSection != null ? tree.mCurrentSection.getSimpleName() : null);
 
-    sb.append(", pendingStateUpdates.size: ");
-    sb.append(tree.mPendingStateUpdates.mAllStateUpdates.size());
+      sb.append(", nextSection.size: ");
+      sb.append(tree.mNextSection != null ? tree.mNextSection.getCount() : null);
 
-    sb.append(", pendingNonLazyStateUpdates.size: ");
-    sb.append(tree.mPendingStateUpdates.mNonLazyStateUpdates.size());
-    sb.append("\n");
-    return sb.toString();
+      sb.append(", nextSection.name: ");
+      sb.append(tree.mNextSection != null ? tree.mNextSection.getSimpleName() : null);
+
+      sb.append(", pendingChangeSets.size: ");
+      sb.append(tree.mPendingChangeSets.size());
+
+      sb.append(", pendingStateUpdates.size: ");
+      sb.append(tree.mPendingStateUpdates.mAllStateUpdates.size());
+
+      sb.append(", pendingNonLazyStateUpdates.size: ");
+      sb.append(tree.mPendingStateUpdates.mNonLazyStateUpdates.size());
+      sb.append("\n");
+      return sb.toString();
+    }
   }
 
   /**
