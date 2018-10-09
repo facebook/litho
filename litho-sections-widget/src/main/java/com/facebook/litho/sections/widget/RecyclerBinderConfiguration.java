3bb89f4a4d1dc3a428cf2d57a326b7080aa411c2
Kata Zavorotchenko <kata@fb.com>
2018-10-09 07:36:03 -0700

    Added Builder to the RecyclerBinderConfiguration
    Summary: Added bulder to the RecyclerBinderConfiguration to pass in params
    
    Reviewed By: astreet
    
    Differential Revision: D10244329
    
    fbshipit-source-id: b06eadf81f51e4e1d674e5dcffc07d1334f5fe92

--- litho-sections-widget/src/main/java/com/facebook/litho/sections/widget/RecyclerBinderConfiguration.java
+++ litho-sections-widget/src/main/java/com/facebook/litho/sections/widget/RecyclerBinderConfiguration.java
@@ -26,36 +26,44 @@ import com.facebook.litho.widget.LayoutHandlerFactory;
 import com.facebook.litho.widget.RecyclerBinder;
 import java.util.List;
 
-/**
- * Configuration setting for {@link RecyclerBinder}.
- */
+/** Configuration setting for {@link RecyclerBinder}. */
 public class RecyclerBinderConfiguration {
-  private static final double DEFAULT_RANGE = 5.0;
   private final double mRangeRatio;
   @Nullable private final LayoutHandlerFactory mLayoutHandlerFactory;
   private final boolean mCanPrefetchDisplayLists;
   private final boolean mIsCircular;
+  private final boolean mIsWrapContent;
+  // TODO T34627443 make all fields final after removing setters
   private boolean mHasDynamicItemHeight;
-  private boolean mIsWrapContent;
-  @Nullable private String mSplitLayoutTag;
   private boolean mUseBackgroundChangeSets = SectionsConfiguration.useBackgroundChangeSets;
-  private boolean mHScrollAsyncMode = false;
+  private boolean mHScrollAsyncMode;
   private boolean mEnableStableIds;
-  @Nullable private List<ComponentLogParams> mInvalidStateLogParamsList;
   private boolean mUseSharedLayoutStateFuture = ComponentsConfiguration.useSharedLayoutStateFuture;
   private LayoutThreadPoolConfiguration mThreadPoolForSharedLayoutStateFutureConfig =
       ComponentsConfiguration.sharedLayoutStateFutureThreadPoolConfig;
   private boolean mAsyncInitRange = ComponentsConfiguration.asyncInitRange;
+  @Nullable private String mSplitLayoutTag;
+  @Nullable private List<ComponentLogParams> mInvalidStateLogParamsList;
 
+  public static Builder create() {
+    return new Builder();
+  }
+
+  /** Use {@link #create()} instead. */
+  @Deprecated
   public RecyclerBinderConfiguration(double rangeRatio) {
     this(rangeRatio, null, false);
   }
 
+  /** Use {@link #create()} instead. */
+  @Deprecated
   public RecyclerBinderConfiguration(
       double rangeRatio, @Nullable LayoutHandlerFactory idleExecutor) {
     this(rangeRatio, idleExecutor, false);
   }
 
+  /** Use {@link #create()} instead. */
+  @Deprecated
   public RecyclerBinderConfiguration(
       double rangeRatio,
       @Nullable LayoutHandlerFactory idleExecutor,
@@ -63,6 +71,8 @@ public class RecyclerBinderConfiguration {
     this(rangeRatio, idleExecutor, canPrefetchDisplayLists, false);
   }
 
+  /** Use {@link #create()} instead. */
+  @Deprecated
   public RecyclerBinderConfiguration(
       double rangeRatio,
       @Nullable LayoutHandlerFactory idleExecutor,
@@ -71,87 +81,103 @@ public class RecyclerBinderConfiguration {
     this(rangeRatio, idleExecutor, canPrefetchDisplayLists, isCircular, false);
   }
 
-  /**
-   * @param rangeRatio Ratio to determine the number of components before and after the {@link
-   *     android.support.v7.widget.RecyclerView}'s total number of currently visible items to have
-   *     their Component layout computed ahead of time.
-   *     <p>e.g total number of visible items = 5 rangeRatio = 10 total number of items before the
-   *     1st visible item to be computed = 5 * 10 = 50 total number of items after the last visible
-   *     item to be computed = 5 * 10 = 50
-   * @param idleExecutor This determines the thread on which the Component layout calculation will
-   *     be processed in. Null means that the computation will be done in the background thread.
-   * @param canPrefetchDisplayLists If this is true, displaylists for the Android Views and
-   *     Drawables that are not collected during the ComponentTree layout computation will be
-   *     collected whenever the UI thread is free.
-   * @param isCircular If true, the underlying RecyclerBinder will have a circular behaviour. Note:
-   *     circular lists DO NOT support any operation that changes the size of items like insert,
-   *     remove, insert range, remove range
-   * @param isWrapContent If true, the underlying RecyclerBinder will measure the parent height by
-   *     the height of children if the orientation is vertical, or measure the parent width by the
-   *     width of children if the orientation is horizontal.
-   */
+  /** Use {@link #create()} instead. */
+  @Deprecated
   public RecyclerBinderConfiguration(
       double rangeRatio,
       @Nullable LayoutHandlerFactory idleExecutor,
       boolean canPrefetchDisplayLists,
       boolean isCircular,
       boolean isWrapContent) {
-    mRangeRatio = rangeRatio > 0 ? rangeRatio : DEFAULT_RANGE;
+    mRangeRatio = rangeRatio > 0 ? rangeRatio : Builder.DEFAULT_RANGE;
     mLayoutHandlerFactory = idleExecutor;
     mCanPrefetchDisplayLists = canPrefetchDisplayLists;
     mIsCircular = isCircular;
     mIsWrapContent = isWrapContent;
   }
 
-  /**
-   * TODO T23919104 mihaelao Do not enable this. This is an experimental feature and your Section
-   * surface will take a perf hit if you use it. Talk to the Litho team if you think you need this.
-   */
+  RecyclerBinderConfiguration(
+      double rangeRatio,
+      @Nullable LayoutHandlerFactory layoutHandlerFactory,
+      boolean canPrefetchDisplayLists,
+      boolean circular,
+      boolean wrapContent,
+      @Nullable List<ComponentLogParams> invalidStateLogParamsList,
+      @Nullable String splitLayoutTag,
+      LayoutThreadPoolConfiguration threadPoolForSharedLayoutStateFutureConfig,
+      boolean dynamicItemHeight,
+      boolean useBackgroundChangeSets,
+      boolean hScrollAsyncMode,
+      boolean enableStableIds,
+      boolean useSharedLayoutStateFuture,
+      boolean asyncInitRange) {
+    mRangeRatio = rangeRatio;
+    mLayoutHandlerFactory = layoutHandlerFactory;
+    mCanPrefetchDisplayLists = canPrefetchDisplayLists;
+    mIsCircular = circular;
+    mIsWrapContent = wrapContent;
+    mInvalidStateLogParamsList = invalidStateLogParamsList;
+    mSplitLayoutTag = splitLayoutTag;
+    mThreadPoolForSharedLayoutStateFutureConfig = threadPoolForSharedLayoutStateFutureConfig;
+    mHasDynamicItemHeight = dynamicItemHeight;
+    mUseBackgroundChangeSets = useBackgroundChangeSets;
+    mHScrollAsyncMode = hScrollAsyncMode;
+    mEnableStableIds = enableStableIds;
+    mUseSharedLayoutStateFuture = useSharedLayoutStateFuture;
+    mAsyncInitRange = asyncInitRange;
+  }
+
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setHasDynamicItemHeight(boolean hasDynamicItemHeight) {
     mHasDynamicItemHeight = hasDynamicItemHeight;
     return this;
   }
 
-  /** Experimental feature, do not enable! */
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setSplitLayoutTag(String splitLayoutTag) {
     mSplitLayoutTag = splitLayoutTag;
     return this;
   }
 
-  /** Experimental. See {@link SectionTree.Target#supportsBackgroundChangeSets()} for more info. */
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setUseBackgroundChangeSets(boolean useBackgroundChangeSets) {
     mUseBackgroundChangeSets = useBackgroundChangeSets;
     return this;
   }
 
-  /** Experimental. See {@link RecyclerBinder.Builder#hscrollAsyncMode(boolean)} for more info. */
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setHScrollAsyncMode(boolean hscrollAsyncMode) {
     mHScrollAsyncMode = hscrollAsyncMode;
     return this;
   }
 
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setUseSharedLayoutStateFuture(
       boolean useSharedLayoutStateFuture) {
     mUseSharedLayoutStateFuture = useSharedLayoutStateFuture;
     return this;
   }
 
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setThreadPoolForSharedLayoutStateFutureConfig(
       LayoutThreadPoolConfiguration config) {
     mThreadPoolForSharedLayoutStateFutureConfig = config;
     return this;
   }
 
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setAsyncInitRange(boolean asyncInitRange) {
     mAsyncInitRange = asyncInitRange;
     return this;
   }
 
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setEnableStableIds(boolean enableStableIds) {
     mEnableStableIds = enableStableIds;
     return this;
   }
 
+  /** TODO T34627443 replace usage with builder */
   public RecyclerBinderConfiguration setInvalidStateLogParamsList(
       List<ComponentLogParams> invalidStateLogParamsList) {
     mInvalidStateLogParamsList = invalidStateLogParamsList;
@@ -212,5 +238,154 @@ public class RecyclerBinderConfiguration {
 
   public @Nullable List<ComponentLogParams> getInvalidStateLogParamsList() {
     return mInvalidStateLogParamsList;
+  }
+
+  public static class Builder {
+    static final double DEFAULT_RANGE = 5.0;
+    @Nullable private LayoutHandlerFactory mLayoutHandlerFactory;
+    @Nullable private List<ComponentLogParams> mInvalidStateLogParamsList;
+    @Nullable private String mSplitLayoutTag;
+    private LayoutThreadPoolConfiguration mThreadPoolForSharedLayoutStateFutureConfig =
+        ComponentsConfiguration.sharedLayoutStateFutureThreadPoolConfig;
+    private double mRangeRatio = DEFAULT_RANGE;
+    private boolean mCanPrefetchDisplayLists = false;
+    private boolean mCircular = false;
+    private boolean mWrapContent = false;
+    private boolean mDynamicItemHeight = false;
+    private boolean mUseBackgroundChangeSets = SectionsConfiguration.useBackgroundChangeSets;
+    private boolean mHScrollAsyncMode = false;
+    private boolean mEnableStableIds = false;
+    private boolean mUseSharedLayoutStateFuture =
+        ComponentsConfiguration.useSharedLayoutStateFuture;
+    private boolean mAsyncInitRange = ComponentsConfiguration.asyncInitRange;
+
+    Builder() {}
+
+    /**
+     * @param idleExecutor This determines the thread on which the Component layout calculation will
+     *     be processed in. Null means that the computation will be done in the background thread.
+     */
+    public Builder idleExecutor(@Nullable LayoutHandlerFactory idleExecutor) {
+      mLayoutHandlerFactory = idleExecutor;
+      return this;
+    }
+
+    public Builder invalidStateLogParamsList(
+        @Nullable List<ComponentLogParams> invalidStateLogParamsList) {
+      mInvalidStateLogParamsList = invalidStateLogParamsList;
+      return this;
+    }
+
+    /** Experimental feature, do not enable! */
+    public Builder splitLayoutTag(@Nullable String splitLayoutTag) {
+      mSplitLayoutTag = splitLayoutTag;
+      return this;
+    }
+
+    public Builder threadPoolForSharedLayoutStateFutureConfig(
+        LayoutThreadPoolConfiguration threadPoolForSharedLayoutStateFutureConfig) {
+      mThreadPoolForSharedLayoutStateFutureConfig = threadPoolForSharedLayoutStateFutureConfig;
+      return this;
+    }
+
+    /**
+     * @param rangeRatio Ratio to determine the number of components before and after the {@link
+     *     android.support.v7.widget.RecyclerView}'s total number of currently visible items to have
+     *     their Component layout computed ahead of time.
+     *     <p>e.g total number of visible items = 5 rangeRatio = 10 total number of items before the
+     *     1st visible item to be computed = 5 * 10 = 50 total number of items after the last
+     *     visible item to be computed = 5 * 10 = 50
+     */
+    public Builder rangeRatio(double rangeRatio) {
+      mRangeRatio = rangeRatio > 0 ? rangeRatio : DEFAULT_RANGE;
+      return this;
+    }
+
+    /**
+     * @param canPrefetchDisplayLists If this is true, displaylists for the Android Views and
+     *     Drawables that are not collected during the ComponentTree layout computation will be
+     *     collected whenever the UI thread is free.
+     */
+    public Builder canPrefetchDisplayLists(boolean canPrefetchDisplayLists) {
+      mCanPrefetchDisplayLists = canPrefetchDisplayLists;
+      return this;
+    }
+
+    /**
+     * @param isCircular If true, the underlying RecyclerBinder will have a circular behaviour.
+     *     Note: circular lists DO NOT support any operation that changes the size of items like
+     *     insert, remove, insert range, remove range
+     */
+    public Builder isCircular(boolean isCircular) {
+      mCircular = isCircular;
+      return this;
+    }
+
+    /**
+     * @param isWrapContent If true, the underlying RecyclerBinder will measure the parent height by
+     *     the height of children if the orientation is vertical, or measure the parent width by the
+     *     width of children if the orientation is horizontal.
+     */
+    public Builder wrapContent(boolean isWrapContent) {
+      mWrapContent = isWrapContent;
+      return this;
+    }
+
+    /**
+     * TODO T23919104 mihaelao Do not enable this. This is an experimental feature and your Section
+     * surface will take a perf hit if you use it. Talk to the Litho team if you think you need
+     * this.
+     */
+    public Builder hasDynamicItemHeight(boolean hasDynamicItemHeight) {
+      mDynamicItemHeight = hasDynamicItemHeight;
+      return this;
+    }
+
+    /**
+     * Experimental. See {@link SectionTree.Target#supportsBackgroundChangeSets()} for more info.
+     */
+    public Builder useBackgroundChangeSets(boolean useBackgroundChangeSets) {
+      mUseBackgroundChangeSets = useBackgroundChangeSets;
+      return this;
+    }
+
+    /** Experimental. See {@link RecyclerBinder.Builder#hscrollAsyncMode(boolean)} for more info. */
+    public Builder hScrollAsyncMode(boolean hScrollAsyncMode) {
+      mHScrollAsyncMode = hScrollAsyncMode;
+      return this;
+    }
+
+    public Builder enableStableIds(boolean enableStableIds) {
+      mEnableStableIds = enableStableIds;
+      return this;
+    }
+
+    public Builder useSharedLayoutStateFuture(boolean useSharedLayoutStateFuture) {
+      mUseSharedLayoutStateFuture = useSharedLayoutStateFuture;
+      return this;
+    }
+
+    public Builder asyncInitRange(boolean asyncInitRange) {
+      mAsyncInitRange = asyncInitRange;
+      return this;
+    }
+
+    public RecyclerBinderConfiguration build() {
+      return new RecyclerBinderConfiguration(
+          mRangeRatio,
+          mLayoutHandlerFactory,
+          mCanPrefetchDisplayLists,
+          mCircular,
+          mWrapContent,
+          mInvalidStateLogParamsList,
+          mSplitLayoutTag,
+          mThreadPoolForSharedLayoutStateFutureConfig,
+          mDynamicItemHeight,
+          mUseBackgroundChangeSets,
+          mHScrollAsyncMode,
+          mEnableStableIds,
+          mUseSharedLayoutStateFuture,
+          mAsyncInitRange);
+    }
   }
 }
