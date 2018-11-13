c9404b9bab3b8b357fcea77d06cd198c82951ba7
Eric Desfosses <edesfosses@fb.com>
2018-11-13 15:22:54 -0800

    Create SetSelectionEvent, add setSelection trigger to TextInputSpec
    Summary: Add suport for a SetSelection trigger and its corresponding event in TextInputSpec, giving users the ability to set and mutate the selection when, for example, adding content to the TextInput's char sequence, or when using ranges over its content.
    
    Reviewed By: pasqualeanatriello
    
    Differential Revision: D13037785
    
    fbshipit-source-id: cc33b48720c7c2844577b6ebe2dfeb22fe7bb34e

--- litho-widget/src/main/java/com/facebook/litho/widget/TextInputSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextInputSpec.java
@@ -602,6 +602,18 @@ class TextInputSpec {
     }
   }
 
+  @OnTrigger(SetSelectionEvent.class)
+  static void setSelection(
+      ComponentContext c,
+      @State AtomicReference<EditTextWithEventHandlers> mountedView,
+      @FromTrigger int start,
+      @FromTrigger int end) {
+    EditTextWithEventHandlers view = mountedView.get();
+    if (view != null) {
+      view.setSelection(start, end < start ? start : end);
+    }
+  }
+
   @OnUpdateState
   static void remeasureForUpdatedText(StateValue<Integer> measureSeqNumber) {
     measureSeqNumber.set(measureSeqNumber.get() + 1);
