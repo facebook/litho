8064108f99ec26c8e2909c48d3266715b090914a
Aditya Sharat <adityasharat@fb.com>
2018-11-20 03:00:25 -0800

    fixes broken javadoc build
    Summary: javadoc of `OnShouldCreateLayoutWithNewSizeSpec` was invalid this diff will fix it.
    
    Reviewed By: IanChilds
    
    Differential Revision: D13136457
    
    fbshipit-source-id: c7d114bff4ea05afee8a8d60a66d985140ec10f8

--- litho-annotations/src/main/java/com/facebook/litho/annotations/OnShouldCreateLayoutWithNewSizeSpec.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/OnShouldCreateLayoutWithNewSizeSpec.java
@@ -23,12 +23,14 @@ import java.lang.annotation.RetentionPolicy;
  * This indicates that the annotated method will be called when the component's checks if it can use
  * the last cached layout returned from the Layout Spec with a new Size Spec. This is used in
  * conjunction with {@link OnCreateLayoutWithSizeSpec}. The annotated method must have the following
- * signature: <code><pre>
+ * signature:
+ *
+ * <pre><code>
  *  static boolean onShouldCreateLayoutWithNewSizeSpec(
  *    ComponentContext context,
  *    int newWidthSpec,
  *    int newHeightSpec, ...)
- * </pre></code>
+ * </code></pre>
  *
  * <p>The annotated method should return {@code true} iff the Layout Spec should create a new layout
  * for this new size spec. If the method returns {@code false} then the Component will use the last
