7cd287f5c2608ced51f80194cb392b3384d5d323
Kata Zavorotchenko <kata@fb.com>
2018-12-04 04:17:02 -0800

    Fixed DebugOverlayDrawable drawing bounds.
    Summary: Made drawing bounds of the DebugOverlayDrawable equals to the bounds of the drawable itself instead of occupying all provided canvas.
    
    Reviewed By: muraziz
    
    Differential Revision: D13234744
    
    fbshipit-source-id: c031636bf22014ddc2306e5f6e69289dfb6c4b81

--- litho-core/src/main/java/com/facebook/litho/DebugOverlayDrawable.java
+++ litho-core/src/main/java/com/facebook/litho/DebugOverlayDrawable.java
@@ -20,45 +20,50 @@ import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.support.annotation.VisibleForTesting;
 import com.facebook.litho.drawable.ComparableDrawable;
 import java.util.List;
 
 public class DebugOverlayDrawable extends ComparableDrawable {
 
-  private static final int COLOR_RED_TRANSPARENT = Color.parseColor("#22FF0000");
-  private static final int COLOR_GREEN_TRANSPARENT = Color.parseColor("#2200FF00");
+  @VisibleForTesting static final int COLOR_RED_SEMITRANSPARENT = Color.parseColor("#22FF0000");
+  @VisibleForTesting static final int BOX_WIDTH_PX = 16;
+  private static final int COLOR_GREEN_SEMITRANSPARENT = Color.parseColor("#2200FF00");
   private static final int COLOR_RED_OPAQUE = Color.parseColor("#CCFF0000");
   private static final int COLOR_GREEN_OPAQUE = Color.parseColor("#CC00FF00");
-  private static final float TEXT_SIZE = 80f;
-  private static final int BOX_HEIGHT = 100;
-  private static final int TEXT_THRESHOLD = 3;
+  private static final float TEXT_SIZE_PX = 80f;
+  private static final int BOX_HEIGHT_PX = 100;
+  private static final int SHOW_TEXT_THRESHOLD_CNT = 3;
 
   private final Paint textPaint = new Paint();
   private final Paint colorPaint = new Paint();
 
-  private final List<Boolean> mainThreadCalculations;
+  private final List<Boolean> isLayoutCalculatedOnMainThread;
 
-  // Drawing params. Package-private for testing purpose only.
-  final String text;
-  final int overlayColor;
+  // Drawing params.
+  @VisibleForTesting final String text;
+  @VisibleForTesting final int overlayColor;
 
   /**
-   * @param mainThreadCalculations will be used to draw on the Canvas. It should be guaranteed by
-   *     the caller to not modify this list.
+   * @param isLayoutCalculatedOnMainThread will be used to draw on the Canvas. It should be
+   *     guaranteed by the caller to not modify this list.
    */
-  public DebugOverlayDrawable(List<Boolean> mainThreadCalculations) {
+  public DebugOverlayDrawable(List<Boolean> isLayoutCalculatedOnMainThread) {
     textPaint.setColor(Color.BLACK);
     textPaint.setAntiAlias(true);
     textPaint.setStyle(Paint.Style.FILL);
-    textPaint.setTextSize(TEXT_SIZE);
+    textPaint.setTextSize(TEXT_SIZE_PX);
     textPaint.setTextAlign(Paint.Align.LEFT);
 
-    this.mainThreadCalculations = mainThreadCalculations;
-    int size = mainThreadCalculations.size();
+    this.isLayoutCalculatedOnMainThread = isLayoutCalculatedOnMainThread;
+    int size = isLayoutCalculatedOnMainThread.size();
     if (size > 0) {
       text = size + "x";
       overlayColor =
-          mainThreadCalculations.get(size - 1) ? COLOR_RED_TRANSPARENT : COLOR_GREEN_TRANSPARENT;
+          isLayoutCalculatedOnMainThread.get(size - 1)
+              ? COLOR_RED_SEMITRANSPARENT
+              : COLOR_GREEN_SEMITRANSPARENT;
     } else {
       text = "";
       overlayColor = Color.TRANSPARENT;
@@ -68,26 +73,29 @@ public class DebugOverlayDrawable extends ComparableDrawable {
   @Override
   public void draw(Canvas canvas) {
     colorPaint.setColor(overlayColor);
-    canvas.drawRect(getBounds(), colorPaint);
+    final Rect bounds = getBounds();
+    canvas.drawRect(bounds, colorPaint);
 
-    int count = mainThreadCalculations.size();
-    for (int i = 0, width = canvas.getWidth(), l, r; i < count; i++) {
-      l = i * 20;
-      r = l + 16;
-      if (r < width) {
-        if (mainThreadCalculations.get(i)) {
+    final int count = isLayoutCalculatedOnMainThread.size();
+    final int leftEnd = bounds.left, rightEnd = bounds.right;
+    final int top = bounds.top, bottom = Math.min(top + BOX_HEIGHT_PX, bounds.bottom);
+    for (int i = 0, left, right; i < count; i++) {
+      left = leftEnd + i * 20;
+      right = left + BOX_WIDTH_PX;
+      if (right < rightEnd) {
+        if (isLayoutCalculatedOnMainThread.get(i)) {
           colorPaint.setColor(COLOR_RED_OPAQUE);
         } else {
           colorPaint.setColor(COLOR_GREEN_OPAQUE);
         }
-        canvas.drawRect(l, 0, r, BOX_HEIGHT, colorPaint);
+        canvas.drawRect(left, top, right, bottom, colorPaint);
       } else {
         break;
       }
     }
 
-    if (count > TEXT_THRESHOLD) {
-      canvas.drawText(text, 0, TEXT_SIZE, textPaint);
+    if (count > SHOW_TEXT_THRESHOLD_CNT) {
+      canvas.drawText(text, leftEnd, top + TEXT_SIZE_PX, textPaint);
     }
   }
 
@@ -104,7 +112,7 @@ public class DebugOverlayDrawable extends ComparableDrawable {
 
   @Override
   public int hashCode() {
-    return mainThreadCalculations.hashCode();
+    return isLayoutCalculatedOnMainThread.hashCode();
   }
 
   @Override
@@ -118,10 +126,10 @@ public class DebugOverlayDrawable extends ComparableDrawable {
     }
 
     DebugOverlayDrawable other = (DebugOverlayDrawable) obj;
-    // mainThreadCalculations is the only important field here but we want to stop earlier
+    // isLayoutCalculatedOnMainThread is the only important field here but we want to stop earlier
     return overlayColor == other.overlayColor
         && text.equals(other.text)
-        && mainThreadCalculations.equals(other.mainThreadCalculations);
+        && isLayoutCalculatedOnMainThread.equals(other.isLayoutCalculatedOnMainThread);
   }
 
   @Override
--- litho-it/src/test/java/com/facebook/litho/DebugOverlayDrawableTest.java
+++ litho-it/src/test/java/com/facebook/litho/DebugOverlayDrawableTest.java
@@ -20,6 +20,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import com.facebook.litho.testing.testrunner.ComponentsTestRunner;
 import java.util.ArrayList;
 import java.util.List;
@@ -103,5 +107,70 @@ public class DebugOverlayDrawableTest {
 
     assertEquals(testD.text, sameD.text);
     assertEquals(testD.overlayColor, sameD.overlayColor);
+  }
+
+  @Test
+  public void testDrawingBounds() {
+    List<Boolean> single = new ArrayList<>();
+    single.add(true);
+    DebugOverlayDrawable debugOverlayDrawable = new DebugOverlayDrawable(single);
+    int left = 10;
+    int top = 15;
+    int right = 30;
+    int bottom = 35;
+    debugOverlayDrawable.setBounds(left, top, right, bottom);
+    int size = 40;
+    TestCanvas c = new TestCanvas(size, size);
+    debugOverlayDrawable.draw(c);
+
+    for (int i = 0; i < size; i++) {
+      assertEquals(0, c.getColor(i, top - 2));
+      assertEquals(0, c.getColor(i, bottom + 2));
+      assertEquals(0, c.getColor(left - 2, i));
+      assertEquals(0, c.getColor(right + 2, i));
+    }
+
+    for (int i = left + DebugOverlayDrawable.BOX_WIDTH_PX; i < right; i++) {
+      assertEquals(
+          "Wrong color for " + i + "-" + top + " pixel",
+          DebugOverlayDrawable.COLOR_RED_SEMITRANSPARENT,
+          c.getColor(i, top));
+    }
+  }
+
+  static class TestCanvas extends Canvas {
+    private final int canvasColor[][];
+
+    TestCanvas(int width, int height) {
+      canvasColor = new int[width][height];
+    }
+
+    @Override
+    public void drawRect(RectF rect, Paint paint) {
+      drawRect(rect.left, rect.top, rect.right, rect.bottom, paint);
+    }
+
+    @Override
+    public void drawRect(Rect rect, Paint paint) {
+      drawRect(rect.left, rect.top, rect.right, rect.bottom, paint);
+    }
+
+    @Override
+    public void drawRect(float left, float top, float right, float bottom, Paint paint) {
+      int color = paint.getColor();
+      for (int x = (int) left; x < right; x++) {
+        for (int y = (int) top; y < bottom; y++) {
+          setColor(color, x, y);
+        }
+      }
+    }
+
+    private void setColor(int color, int x, int y) {
+      canvasColor[x][y] = color;
+    }
+
+    int getColor(int x, int y) {
+      return canvasColor[x][y];
+    }
   }
 }
