07366e3f2809808e29bc0d64f80a30baebf5f28f
Sergey Nikolaienkov <sergeyn@fb.com>
2018-12-07 13:09:40 -0800

    only process autogenerated TransitionIDs in TransitionManager when necessary
    Summary:
    Introducing optimized approach to processing autogenerated transition ids in `TransitionManager.setupTransitions(), based on 2 facts:
    1) there is no need to process autogenerated ids when there is no transition that targets all layout defined
    2) appear and disappear animations cannot be defined for autogenerated ids, thus there is no need to process such ids, if they are not present in current/next layout state
    
    Reviewed By: astreet
    
    Differential Revision: D13357820
    
    fbshipit-source-id: a8723aafcf4e3ab832134bd58f2caa747e5a078a

--- litho-core/src/main/java/com/facebook/litho/TransitionUtils.java
+++ litho-core/src/main/java/com/facebook/litho/TransitionUtils.java
@@ -114,4 +114,38 @@ class TransitionUtils {
       throw new RuntimeException("Unhandled transition type: " + transition);
     }
   }
+
+  static boolean targetsAllLayout(@Nullable Transition transition) {
+    if (transition == null) {
+      return false;
+    } else if (transition instanceof TransitionSet) {
+      ArrayList<Transition> children = ((TransitionSet) transition).getChildren();
+      for (int i = 0, size = children.size(); i < size; i++) {
+        if (targetsAllLayout(children.get(i))) {
+          return true;
+        }
+      }
+    } else if (transition instanceof Transition.TransitionUnit) {
+      final Transition.TransitionUnit unit = (Transition.TransitionUnit) transition;
+      final Transition.ComponentTargetType targetType =
+          unit.getAnimationTarget().componentTarget.componentTargetType;
+      if (targetType == Transition.ComponentTargetType.ALL
+          || targetType == Transition.ComponentTargetType.AUTO_LAYOUT) {
+        return true;
+      }
+    } else if (transition instanceof Transition.BaseTransitionUnitsBuilder) {
+      final Transition.BaseTransitionUnitsBuilder builder =
+          (Transition.BaseTransitionUnitsBuilder) transition;
+      final List<Transition.TransitionUnit> units = builder.getTransitionUnits();
+      for (int i = 0, size = units.size(); i < size; i++) {
+        if (targetsAllLayout(units.get(i))) {
+          return true;
+        }
+      }
+    } else {
+      throw new RuntimeException("Unhandled transition type: " + transition);
+    }
+
+    return false;
+  }
 }
