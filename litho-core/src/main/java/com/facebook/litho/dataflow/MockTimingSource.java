ac49f0b151ed1f0190fce206e7ec10f269709655
Andrew Chen (Eng) <ayc@fb.com>
2019-01-28 09:37:25 -0800

    Fix infinite loop in MockTimingSource
    Summary:
    There cases where FrameCallbacks will post another FrameCallback within its doFrame() method in order to do some processing on every frame, such as
    
    ```
    Choreographer.getInstance().postFrameCallback(
        new FrameCallback() {
          Override
          public void doFrame(int frameTimeNanos) {
            if (!terminalCondition) {
              Choreographer.getInstance().postFrameCallback(this)
            }
          }
        });
    ```
    
    The real Choreographer delays the nested FrameCallback until the next frame. MockTimingSource however will end up processing both the original FrameCallback and the nested FrameCallback within the same step because it processes every callback in the queue including the newly added one (which can potentially infinitely loop).
    
    This diff updates MockTimingSource to only process the FrameCallbacks that were present at the start of the frame on each frame.
    
    Reviewed By: astreet
    
    Differential Revision: D13828849
    
    fbshipit-source-id: e7fd9fd3b222d7749491422210b0f2a476bdc191

--- litho-core/src/main/java/com/facebook/litho/dataflow/MockTimingSource.java
+++ litho-core/src/main/java/com/facebook/litho/dataflow/MockTimingSource.java
@@ -59,12 +59,14 @@ public class MockTimingSource implements TimingSource, ChoreographerCompat {
   }
 
   private void fireChoreographerCallbacks() {
-    for (int i = 0; i < mChoreographerCallbacksToStartTimes.size(); i++) {
+    int size = mChoreographerCallbacksToStartTimes.size();
+    for (int i = 0; i < size; i++) {
       final Pair<FrameCallback, Long> entry = mChoreographerCallbacksToStartTimes.get(i);
       if (entry.second <= mCurrentTimeNanos) {
         entry.first.doFrame(mCurrentTimeNanos);
         mChoreographerCallbacksToStartTimes.remove(i);
         i--;
+        size--;
       }
     }
   }
