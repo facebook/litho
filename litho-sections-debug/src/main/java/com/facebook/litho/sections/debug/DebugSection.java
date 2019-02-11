04cf917973d2291f93c57b90662e6533b9f83420
Mihaela Ogrezeanu <mihaelao@fb.com>
2019-02-11 04:10:08 -0800

    Provide section info from DebugSection
    Summary: need to land this to fix flipper oss build; split litho part from D13900789 stack
    
    Reviewed By: pasqualeanatriello
    
    Differential Revision: D14022562
    
    fbshipit-source-id: 355ebd90220d13badc6d329ebdd3596fb10d1034

--- litho-sections-debug/src/main/java/com/facebook/litho/sections/debug/DebugSection.java
+++ litho-sections-debug/src/main/java/com/facebook/litho/sections/debug/DebugSection.java
@@ -16,6 +16,7 @@
 
 package com.facebook.litho.sections.debug;
 
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.view.View;
 import com.facebook.litho.sections.Section;
@@ -90,11 +91,49 @@ public final class DebugSection {
     }
   }
 
+  public boolean isDiffSectionSpec() {
+    return mSectionDebugNode.isDiffSectionSpec();
+  }
+
   public String getGlobalKey() {
     return mSectionDebugNode.getGlobalKey();
   }
 
   public String getName() {
     return mSectionDebugNode.getSimpleName();
+  }
+
+  /**
+   * @return a Rect containing the bounds of the views inside theSection represented by this node.
+   */
+  public Rect getBounds() {
+    List<?> children = getSectionChildren();
+    int count = children.size();
+
+    if (count == 0) {
+      return null;
+    }
+
+    Rect rect = new Rect();
+
+    if (isDiffSectionSpec()) {
+      View firstView = (View) children.get(0);
+      View lastView = (View) children.get(count - 1);
+      rect.left = firstView.getLeft();
+      rect.top = firstView.getTop();
+      rect.right = lastView.getRight();
+      rect.bottom = lastView.getBottom();
+    } else {
+      DebugSection firstSection = (DebugSection) children.get(0);
+      DebugSection lastSection = (DebugSection) children.get(count - 1);
+      Rect first = firstSection.getBounds();
+      Rect last = lastSection.getBounds();
+      rect.left = first.left;
+      rect.top = first.top;
+      rect.right = last.right;
+      rect.bottom = last.bottom;
+    }
+
+    return rect;
   }
 }
