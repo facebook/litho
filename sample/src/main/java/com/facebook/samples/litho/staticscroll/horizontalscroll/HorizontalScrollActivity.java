ca98b780bae1ba1156327b0778a893c76e47fb7e
Aziz Murtazaev <aziz@fb.com>
2019-02-26 12:02:04 -0800

    Add sample usage of HorizontalScrollSpec
    Summary: This will be good place to test any changes to HorizontalScrollSpec. Eventually we need to add VerticalScrollSpec counterpart.
    
    Reviewed By: marco-cova
    
    Differential Revision: D14225051
    
    fbshipit-source-id: be483c29ac097a1267fcd8dfcf42230a7ab4842d

---
+++ sample/src/main/java/com/facebook/samples/litho/staticscroll/horizontalscroll/HorizontalScrollActivity.java
@@ -0,0 +1,29 @@
+/*
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.samples.litho.staticscroll.horizontalscroll;
+
+import android.os.Bundle;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.LithoView;
+import com.facebook.samples.litho.NavigatableDemoActivity;
+
+public class HorizontalScrollActivity extends NavigatableDemoActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(
+        LithoView.create(
+            this, HorizontalScrollRootComponent.create(new ComponentContext(this)).build()));
+  }
+}
