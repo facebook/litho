0c139da1f6436cbc2369405ff19a258ee45dc252
Anushree Ganjam <ganjam@google.com>
2019-03-29 17:56:24 -0700

    Migrating changes to androidx since Litho has migrated to androidx

--- litho-it/src/test/java/com/facebook/litho/widget/TestLithoRecyclerView.java
+++ litho-it/src/test/java/com/facebook/litho/widget/TestLithoRecyclerView.java
@@ -1,9 +1,9 @@
 package com.facebook.litho.widget;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
