3ff33e8cf2610f86337d340df924fe37193862d4
Aditya Sharat <adityasharat@fb.com>
2019-05-09 09:23:49 -0700

    Add JavaDoc for State
    Summary: Add JavaDoc for State
    
    Reviewed By: mihaelao
    
    Differential Revision: D15276772
    
    fbshipit-source-id: 8e8f5e1a316affdd363caabf2f36a154198767e4

--- litho-annotations/src/main/java/com/facebook/litho/annotations/State.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/State.java
@@ -19,7 +19,75 @@ package com.facebook.litho.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * States for a given Component are the union of all arguments annotated with {@code State} in the
+ * spec. While both {@link Prop} and {@link State} hold information that influences the output of
+ * the component, they are different in one important way: props get passed to the component from
+ * it's parent whereas states are managed within the component.
+ *
+ * <p>The initial values of states can be set using the {@link OnCreateInitialState} method and
+ * states can be updated in {@link OnUpdateState} methods. Updating states in the {@link
+ * OnUpdateState} methods will cause the component to invoke its {@link OnCreateLayout} method.
+ * States <em>should</em> be immutable since the layout can be calculated on multiple threads.
+ * Immutability of the states ensures that no thread safety issues can occur in the component
+ * hierarchy.
+ *
+ * <p><b>Using State:</b> <br>
+ *
+ * <pre><code>{@literal @LayoutSpec}
+ * public class CounterSpec {
+ *
+ *  {@literal @OnCreateLayout}
+ *   static Component onCreateLayout(
+ *     ComponentContext c,
+ *    {@literal @State} int count) {
+ *
+ *     return Row.create(c)
+ *       .backgroundColor(Color.WHITE)
+ *       .heightDip(64)
+ *       .paddingDip(YogaEdge.ALL, 8)
+ *       .child(
+ *         Text.create(c)
+ *           .text(" + ")
+ *           .clickHandler(Counter.onClick(c))
+ *       )
+ *       .child(
+ *         Text.create(c)
+ *           .text(String.valueOf(count))
+ *       )
+ *       .build();
+ *   }
+ *
+ *  {@literal @OnCreateInitialState}
+ *   static void onCreateInitialState(
+ *       ComponentContext c,
+ *       StateValue&lt;Integer&gt; count) {
+ *     count.set(0);
+ *   }
+ *
+ *  {@literal @OnEvent(ClickEvent.class)}
+ *   static void onClick(ComponentContext c) {
+ *     Counter.increment(c);
+ *   }
+ *
+ *  {@literal @OnUpdateState}
+ *   static void increment(StateValue&lt;Integer&gt; count) {
+ *     count.set(count.get() + 1);
+ *   }
+ * }</code></pre>
+ *
+ * @see Param
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface State {
+
+  /**
+   * Declares that this state will not trigger a new layout calculations on update. After a lazy
+   * state update the component will continue to host the older value until the next layout
+   * calculation is triggered. This is useful for updating internal Component information and
+   * persisting it between re-layouts when an immediate layout calculation is not needed.
+   *
+   * @return {@code true} iff this state should update lazily.
+   */
   boolean canUpdateLazily() default false;
 }
