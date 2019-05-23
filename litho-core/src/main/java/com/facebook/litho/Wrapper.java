1ec605e26f7a959161a7ebeceafc3fc1324b8727
Aditya Sharat <adityasharat@fb.com>
2019-05-23 10:40:54 -0700

    Append Wrapper to the InternalNode of the delegate.
    Summary: `Wrapper` are used when global keys are calculated but are not added to the components list in `InternalNode`. This is inconsistent and furthermore causes unstable global key regeneration during reconciliation. This diff add the `Wrapper` component to the components list of the `InternalNode` resolved by the delegate.
    
    Reviewed By: astreet
    
    Differential Revision: D15448922
    
    fbshipit-source-id: ae048b2a25455c26fee6394ce47155b0106b43b0

--- litho-core/src/main/java/com/facebook/litho/Wrapper.java
+++ litho-core/src/main/java/com/facebook/litho/Wrapper.java
@@ -58,7 +58,12 @@ public final class Wrapper extends Component {
       return ComponentContext.NULL_LAYOUT;
     }
 
-    return c.newLayoutBuilder(delegate, 0, 0);
+    InternalNode node = c.newLayoutBuilder(delegate, 0, 0);
+
+    // Add this to the components list for stable global key regeneration.
+    node.appendComponent(this);
+
+    return node;
   }
 
   @Override
