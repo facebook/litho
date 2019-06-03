ed074f89643a9f175711c6a47f559ff9e39a136b
Udi Cohen <udinic@fb.com>
2019-06-03 01:02:18 -0700

    Adding API to dispatch Visibility events for components with disabled incremental mount
    Summary:
    Adding a few methods to allow dispatching visibility events from outside fo Litho, in case incremental mount is disabled. This is a step forward towards better supporting disabled incremental mount.
    
    We can't dispatch these events from within Litho because Litho doesn't keep track of views as they move the screen. Even if we decide to process visibility outputs when incremental mount is disabled, there's still a problem of inconsistency with the screen positioning data. I was able to reproduce cases where a visible component bounces between invisible and visible states while scrolling a view pager. That's why it's best, for now at least, to let the surface using the component to makes the decision for us.
    
    Reviewed By: astreet
    
    Differential Revision: D15552441
    
    fbshipit-source-id: dffd18d4323f3260424bdc02d2582a2061ee1922

--- litho-core/src/main/java/com/facebook/litho/LithoView.java
+++ litho-core/src/main/java/com/facebook/litho/LithoView.java
@@ -815,6 +815,68 @@ public class LithoView extends ComponentHost {
     mMountState.mount(layoutState, currentVisibleArea, processVisibilityOutputs);
   }
 
+  /**
+   * Dispatch a visibility events to all the components hosted in this LithoView.
+   *
+   * <p>Marked as @Deprecated to indicate this method is experimental and should not be widely used.
+   *
+   * <p>NOTE: Can only be used when Incremental Mount is disabled! Call this method when the
+   * LithoView is considered eligible for the visibility event (i.e. only dispatch VisibleEvent when
+   * the LithoView is visible in its container).
+   *
+   * @param visibilityEventType The class type of the visibility event to dispatch. Supported:
+   *     VisibleEvent.class, InvisibleEvent.class, FocusedVisibleEvent.class,
+   *     UnfocusedVisibleEvent.class, FullImpressionVisibleEvent.class.
+   */
+  @Deprecated
+  public void dispatchVisibilityEvent(Class<?> visibilityEventType) {
+    if (isIncrementalMountEnabled()) {
+      throw new IllegalStateException(
+          "dispatchVisibilityEvent - "
+              + "Can't manually trigger visibility events when incremental mount is enabled");
+    }
+
+    LayoutState layoutState =
+        mComponentTree == null ? null : mComponentTree.getMainThreadLayoutState();
+
+    if (layoutState != null && visibilityEventType != null) {
+      for (int i = 0; i < layoutState.getVisibilityOutputCount(); i++) {
+        dispatchVisibilityEvent(layoutState.getVisibilityOutputAt(i), visibilityEventType);
+      }
+
+      List<LithoView> childViews = mMountState.getChildLithoViewsFromCurrentlyMountedItems();
+      for (LithoView lithoView : childViews) {
+        lithoView.dispatchVisibilityEvent(visibilityEventType);
+      }
+    }
+  }
+
+  private void dispatchVisibilityEvent(
+      VisibilityOutput visibilityOutput, Class<?> visibilityEventType) {
+    if (visibilityEventType == VisibleEvent.class) {
+      if (visibilityOutput.getVisibleEventHandler() != null) {
+        EventDispatcherUtils.dispatchOnVisible(visibilityOutput.getVisibleEventHandler());
+      }
+    } else if (visibilityEventType == InvisibleEvent.class) {
+      if (visibilityOutput.getInvisibleEventHandler() != null) {
+        EventDispatcherUtils.dispatchOnInvisible(visibilityOutput.getInvisibleEventHandler());
+      }
+    } else if (visibilityEventType == FocusedVisibleEvent.class) {
+      if (visibilityOutput.getFocusedEventHandler() != null) {
+        EventDispatcherUtils.dispatchOnFocused(visibilityOutput.getFocusedEventHandler());
+      }
+    } else if (visibilityEventType == UnfocusedVisibleEvent.class) {
+      if (visibilityOutput.getUnfocusedEventHandler() != null) {
+        EventDispatcherUtils.dispatchOnUnfocused(visibilityOutput.getUnfocusedEventHandler());
+      }
+    } else if (visibilityEventType == FullImpressionVisibleEvent.class) {
+      if (visibilityOutput.getFullImpressionEventHandler() != null) {
+        EventDispatcherUtils.dispatchOnFullImpression(
+            visibilityOutput.getFullImpressionEventHandler());
+      }
+    }
+  }
+
   void processVisibilityOutputs(LayoutState layoutState, Rect currentVisibleArea) {
     mMountState.processVisibilityOutputs(layoutState, currentVisibleArea, null);
   }
