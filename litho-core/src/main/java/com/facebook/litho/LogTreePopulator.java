aec19615350e46dbafa6fdb2acb9ad0b38185519
Bo Duan <boduan@boduan-macbookpro.roam.corp.google.com>
2019-06-06 16:02:50 -0700

    Use the injected treeProps for Layout PerfEvent

--- litho-core/src/main/java/com/facebook/litho/LogTreePopulator.java
+++ litho-core/src/main/java/com/facebook/litho/LogTreePopulator.java
@@ -38,10 +38,24 @@ public final class LogTreePopulator {
   @CheckReturnValue
   public static PerfEvent populatePerfEventFromLogger(
       ComponentContext c, ComponentsLogger logger, @Nullable PerfEvent perfEvent) {
+    return populatePerfEventFromLogger(c, c.getTreeProps(), logger, perfEvent);
+  }
+
+  /**
+   * Annotate a log event with the log tag set in the context, and extract the treeprops from a
+   * given {@link ComponentContext} and convert them into perf event annotations using a {@link
+   * ComponentsLogger} implementation.
+   *
+   * @return Annotated perf event, or <code>null</code> if the resulting event isn't deemed worthy
+   *     of reporting.
+   */
+  @Nullable
+  @CheckReturnValue
+  public static PerfEvent populatePerfEventFromLogger(
+          ComponentContext c, @Nullable TreeProps treeProps, ComponentsLogger logger, @Nullable PerfEvent perfEvent) {
     if (perfEvent == null) {
       return null;
     }
-
     final String logTag = c.getLogTag();
     if (logTag == null) {
       logger.cancelPerfEvent(perfEvent);
@@ -50,7 +64,6 @@ public final class LogTreePopulator {
 
     perfEvent.markerAnnotate(FrameworkLogEvents.PARAM_LOG_TAG, logTag);
 
-    @Nullable final TreeProps treeProps = c.getTreeProps();
     if (treeProps == null) {
       return perfEvent;
     }
