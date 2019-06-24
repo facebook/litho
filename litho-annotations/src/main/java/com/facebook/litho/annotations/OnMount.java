65d9e4601a4a74ce39388c5f82209f8cc8efe1c6
Mihaela Ogrezeanu <mihaelao@fb.com>
2019-06-24 04:57:27 -0700

    OnMount
    Summary: javadocs for OnMount
    
    Reviewed By: adityasharat
    
    Differential Revision: D15940459
    
    fbshipit-source-id: 8a4f7fbe0ac9a78e440d4fd62eaed6fe92b3d8db

--- litho-annotations/src/main/java/com/facebook/litho/annotations/OnMount.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/OnMount.java
@@ -19,7 +19,63 @@ package com.facebook.litho.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * A method annotated with {@code OnMount} is called on a {@code MountSpec} component before the
+ * component is attached to a hosting view. This will happen when the component is about to become
+ * visible when incremental mount is enabled (it is enabled by default). The framework will always
+ * invoke this method on the UI thread. This method is critical for performance as it is UI-bound;
+ * expensive operations should not be performed here.
+ *
+ * <p>You can think of methods annotated with {@code OnMount} as the equivalent of the
+ * RecyclerView.Adapter's bindViewHolder method. It will be called to bind the recycler Views or
+ * Drawables to the correct data when they are about to be mounted on screen.
+ *
+ * <p>The annotated method has a void return type and will be passed the following arguments when
+ * the framework invokes it:
+ *
+ * <p><em>Required:</em><br>
+ *
+ * <ol>
+ *   <li>ComponentContext
+ *   <li>{MountContent} - must be the same type as the return type of the method annotated with
+ *       {@literal OnCreateMountContent}, which is either a View or a Drawable
+ * </ol>
+ *
+ * <p><em>Optional annotated arguments:</em><br>
+ *
+ * <ul>
+ *   <li>{@link Prop}
+ *   <li>{@link TreeProp}
+ *   <li>{@link InjectProp}
+ *   <li>{@link State}
+ *   <li>{@link FromPrepare}
+ *   <li>{@link FromMeasure}
+ * </ul>
+ *
+ * <p>The annotation processor will validate this and other invariants in the API at build time.
+ *
+ * <p>For example: <br>
+ *
+ * <pre><code>{@literal @MountSpec}
+ * class ExampleMountSpec {
+ *
+ *  {@literal @OnCreateMountContent}
+ *   static ColorDrawable onCreateMountContent(
+ *       ComponentContext c) {
+ *     return new ColorDrawable();
+ *   }
+ *
+ *  {@literal @OnMount}
+ *   static void onMount(
+ *       ComponentContext c,
+ *       ColorDrawable colorDrawable,
+ *      {@literal @FromPrepare}int color){
+ *     colorDrawable.setColor(color);
+ *   }
+ * }</code></pre>
+ *
+ * @see OnCreateMountContent
+ * @see OnUnmount
+ */
 @Retention(RetentionPolicy.CLASS)
-public @interface OnMount {
-
-}
+public @interface OnMount {}
