59456db02665b924b056dd0ad78f0478f10874b4
Aditya Sharat <adityasharat@fb.com>
2019-07-24 16:02:53 -0700

    Add JavaDoc for FromCreateLayout
    Summary: Add JavaDoc for FromCreateLayout
    
    Reviewed By: muraziz
    
    Differential Revision: D16440235
    
    fbshipit-source-id: 1c8a1aa4c67298afd6d5ecc37f4eef5c3d39ed92

--- litho-annotations/src/main/java/com/facebook/litho/annotations/FromPreviousCreateLayout.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/FromPreviousCreateLayout.java
@@ -20,8 +20,68 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Indicates that the annotated parameter's value should be equivalent to the named output set in
- * {@link OnCreateLayoutWithSizeSpec}.
+ * Annotates that the argument's value should be set to the named inter-stage output from the {@link
+ * OnCreateLayoutWithSizeSpec} lifecycle method. The name and type of the argument must match with
+ * the inter-stage output requested. This inter-stage output is different from the other because
+ * this {@link OnShouldCreateLayoutWithNewSizeSpec} is called in subsequent layout calculations when
+ * the framework has a cached layout to reuse.
+ *
+ * <p><em><b>This can be used in:</b></em>
+ *
+ * <ul>
+ *   <li>{@link OnShouldCreateLayoutWithNewSizeSpec}
+ * </ul>
+ *
+ * <p>The annotation processor will validate this and other invariants in the API at build time.
+ *
+ * <p>For example: <br>
+ *
+ * <pre><code>{@literal @LayoutSpec}
+ * class MyComponentSpec {
+ *
+ *  {@literal @OnCreateLayoutWithSizeSpec}
+ *   static Component onCreateLayoutWithSizeSpec(
+ *       ComponentContext c,
+ *       int widthSpec,
+ *       int heightSpec) {
+ *
+ *     final Component textComponent =
+ *         Text.create(c).textSizeSp(16).text("Some text to measure.").build();
+ *
+ *     // UNSPECIFIED sizeSpecs will measure the text as being one line only,
+ *     // having unlimited width.
+ *     final Size textOutputSize = new Size();
+ *     textComponent.measure(
+ *         c,
+ *         SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+ *         SizeSpec.makeSizeSpec(0, UNSPECIFIED),
+ *         textOutputSize);
+ *
+ *     // Small component to use in case textComponent does not fit within the current layout.
+ *     final Component imageComponent = Image.create(c).drawableRes(R.drawable.ic_launcher).build();
+ *
+ *     // Assuming SizeSpec.getMode(widthSpec) == EXACTLY or AT_MOST.
+ *     final int layoutWidth = SizeSpec.getSize(widthSpec);
+ *     final boolean textFits = (textOutputSize.width &lt;= layoutWidth);
+ *
+ *     return textFits ? textComponent : imageComponent;
+ *   }
+ *
+ *  {@literal @OnShouldCreateLayoutWithNewSizeSpec}
+ *   static boolean onShouldCreateLayoutWithNewSizeSpec(
+ *       ComponentContext context,
+ *       int newWidthSpec,
+ *       int newHeightSpec,
+ *      {@literal @FromPreviousCreateLayout} int textWidth,  // Get the output value
+ *      {@literal @FromPreviousCreateLayout} boolean didItFit) {
+ *
+ *     final int newLayoutWidth = SizeSpec.getSize(newWidthSpec);
+ *     final boolean doesItStillFit = (textWidth &lt;= newLayoutWidth);
+ *
+ *     // false if it still fits or if still doesn't fit
+ *     return doesItStillFit ^ didItFit;
+ *   }
+ * }</code></pre>
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FromPreviousCreateLayout {}
