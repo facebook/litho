fa87c022fdb3c94db62f5605e4d64832302dee35
Kata Zavorotchenko <kata@fb.com>
2019-08-01 17:18:09 -0700

    Reuse FieldsExtractorTest for processor and intellij modules (#570)
    Summary:
    Pull Request resolved: https://github.com/facebook/litho/pull/570
    
    Extracted FieldsExtractorTestHelper to reuse in FieldsExtractorTest and PsiFieldsExtractorTest
    
    Reviewed By: muraziz
    
    Differential Revision: D15512731
    
    fbshipit-source-id: bf98aa9a4bececd5e7d0277f5145453da484f4e8

--- litho-it/src/test/java/com/facebook/litho/specmodels/processor/FieldsExtractorTest.java
+++ litho-it/src/test/java/com/facebook/litho/specmodels/processor/FieldsExtractorTest.java
@@ -15,13 +15,9 @@
  */
 package com.facebook.litho.specmodels.processor;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
-
 import com.facebook.litho.specmodels.internal.ImmutableList;
 import com.facebook.litho.specmodels.model.FieldModel;
 import com.google.testing.compile.CompilationRule;
-import com.squareup.javapoet.FieldSpec;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import org.junit.Rule;
 import org.junit.Test;
@@ -50,30 +46,18 @@ public class FieldsExtractorTest {
   }
 
   @Test
-  public void testFieldExtraction() {
+  public void fieldExtraction() {
     final TypeElement element =
         compilationRule.getElements().getTypeElement(TwoFieldsClass.class.getCanonicalName());
     ImmutableList<FieldModel> fieldModels = FieldsExtractor.extractFields(element);
-    assertThat(fieldModels).hasSize(2);
-
-    FieldSpec extractedIntField = fieldModels.get(0).field;
-    assertThat(extractedIntField.name).isEqualTo("intField");
-    assertThat(extractedIntField.modifiers).hasSize(3);
-    assertThat(extractedIntField.hasModifier(Modifier.PRIVATE));
-    assertThat(extractedIntField.hasModifier(Modifier.STATIC));
-    assertThat(extractedIntField.hasModifier(Modifier.FINAL));
-
-    FieldSpec extractedFloatField = fieldModels.get(1).field;
-    assertThat(extractedFloatField.name).isEqualTo("floatField");
-    assertThat(extractedFloatField.modifiers).hasSize(1);
-    assertThat(extractedFloatField.hasModifier(Modifier.STATIC));
+    FieldsExtractorTestHelper.fieldExtraction(fieldModels);
   }
 
   @Test
-  public void testNoFieldExtraction() {
+  public void noFieldExtraction() {
     final TypeElement element =
         compilationRule.getElements().getTypeElement(NoFieldsClass.class.getCanonicalName());
     ImmutableList<FieldModel> fieldModels = FieldsExtractor.extractFields(element);
-    assertThat(fieldModels).hasSize(0);
+    FieldsExtractorTestHelper.noFieldExtraction(fieldModels);
   }
 }
