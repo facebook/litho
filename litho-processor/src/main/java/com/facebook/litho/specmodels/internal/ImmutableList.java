60fb463a056cf29e8bb08b6d8f762a7f342653c4
Andy Street <andrews@fb.com>
2019-08-09 09:47:23 -0700

    Make ImmutableList actually immutable
    Summary: This is only used in our AP anyway, but we should enforce it's actually immutable, even if only at runtime.
    
    Reviewed By: colriot
    
    Differential Revision: D16710414
    
    fbshipit-source-id: 225c9a323cbeb058a031891aeb03e4449bca82fe

--- litho-processor/src/main/java/com/facebook/litho/specmodels/internal/ImmutableList.java
+++ litho-processor/src/main/java/com/facebook/litho/specmodels/internal/ImmutableList.java
@@ -17,30 +17,171 @@
 package com.facebook.litho.specmodels.internal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-/**
- * A dummy representation of an immutable set. This can be used temporarily as a type until we have
- * an actual non-guava implementation.
- */
-public class ImmutableList<E> extends ArrayList<E> {
+/** A sad, standard Java implementation of an immutable List. */
+public final class ImmutableList<E> implements List<E> {
 
-  private ImmutableList(final int capacity) {
-    super(capacity);
+  private final List<E> mBackingList;
+
+  private ImmutableList(List<E> list) {
+    mBackingList = Collections.unmodifiableList(list);
   }
 
-  private ImmutableList(Collection<E> list) {
-    super(list);
-  }
-
-  public static <E> ImmutableList<E> copyOf(Collection<E> list) {
+  public static <E> ImmutableList<E> copyOf(List<E> list) {
     return new ImmutableList<>(list);
   }
 
+  public static <E> ImmutableList<E> copyOf(Collection<E> collection) {
+    return new ImmutableList<>(new ArrayList<>(collection));
+  }
+
   public static <E> ImmutableList<E> of(E... elements) {
-    final ImmutableList<E> list = new ImmutableList<>(elements.length);
-    Collections.addAll(list, elements);
-    return list;
+    return new ImmutableList<>(Arrays.asList(elements));
+  }
+
+  @Override
+  public int size() {
+    return mBackingList.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return mBackingList.isEmpty();
+  }
+
+  @Override
+  public boolean contains(Object o) {
+    return mBackingList.contains(o);
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+    return mBackingList.iterator();
+  }
+
+  @Override
+  public Object[] toArray() {
+    return mBackingList.toArray();
+  }
+
+  @Override
+  public <T> T[] toArray(T[] a) {
+    return mBackingList.toArray(a);
+  }
+
+  @Deprecated
+  @Override
+  public boolean add(E e) {
+    return mBackingList.add(e);
+  }
+
+  @Deprecated
+  @Override
+  public boolean remove(Object o) {
+    return mBackingList.remove(o);
+  }
+
+  @Override
+  public boolean containsAll(Collection<?> c) {
+    return mBackingList.containsAll(c);
+  }
+
+  @Deprecated
+  @Override
+  public boolean addAll(Collection<? extends E> c) {
+    return mBackingList.addAll(c);
+  }
+
+  @Deprecated
+  @Override
+  public boolean addAll(int index, Collection<? extends E> c) {
+    return mBackingList.addAll(index, c);
+  }
+
+  @Deprecated
+  @Override
+  public boolean removeAll(Collection<?> c) {
+    return mBackingList.removeAll(c);
+  }
+
+  @Deprecated
+  @Override
+  public boolean retainAll(Collection<?> c) {
+    return mBackingList.retainAll(c);
+  }
+
+  @Deprecated
+  @Override
+  public void clear() {
+    mBackingList.clear();
+  }
+
+  @Override
+  public E get(int index) {
+    return mBackingList.get(index);
+  }
+
+  @Deprecated
+  @Override
+  public E set(int index, E element) {
+    return mBackingList.set(index, element);
+  }
+
+  @Deprecated
+  @Override
+  public void add(int index, E element) {
+    mBackingList.add(index, element);
+  }
+
+  @Deprecated
+  @Override
+  public E remove(int index) {
+    return mBackingList.remove(index);
+  }
+
+  @Override
+  public int indexOf(Object o) {
+    return mBackingList.indexOf(o);
+  }
+
+  @Override
+  public int lastIndexOf(Object o) {
+    return mBackingList.lastIndexOf(o);
+  }
+
+  @Override
+  public ListIterator<E> listIterator() {
+    return mBackingList.listIterator();
+  }
+
+  @Override
+  public ListIterator<E> listIterator(int index) {
+    return mBackingList.listIterator(index);
+  }
+
+  @Override
+  public List<E> subList(int fromIndex, int toIndex) {
+    return mBackingList.subList(fromIndex, toIndex);
+  }
+
+  @Override
+  public int hashCode() {
+    return mBackingList.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return mBackingList.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return mBackingList.toString();
   }
 }
