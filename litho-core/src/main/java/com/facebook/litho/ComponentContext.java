63bf7e0368819210c332bf7f9d56114acc01c44f
Aditya Sharat <adityasharat@fb.com>
2019-08-15 06:13:03 -0700

    Remove the non static implementation of ComponentContext#resolveLayout
    Summary: Remove the non static implementation of `ComponentContext#resolveLayout`. Also refactor the implementation of resolve to be exactly same as the previous implementation to fix failing tests.
    
    Reviewed By: astreet
    
    Differential Revision: D16826644
    
    fbshipit-source-id: df19cb2953bc5813bc29041064502a1272a88dad

--- litho-core/src/main/java/com/facebook/litho/ComponentContext.java
+++ litho-core/src/main/java/com/facebook/litho/ComponentContext.java
@@ -401,28 +401,7 @@ public class ComponentContext {
   }
 
   InternalNode resolveLayout(Component component) {
-    final InternalNode layoutCreatedInWillRender = component.consumeLayoutCreatedInWillRender();
-    if (layoutCreatedInWillRender != null) {
-      return layoutCreatedInWillRender;
-    }
-
-    component = component.getThreadSafeInstance();
-
-    component.updateInternalChildState(this);
-
-    if (ComponentsConfiguration.isDebugModeEnabled) {
-      DebugComponent.applyOverrides(this, component);
-    }
-
-    final InternalNode node = (InternalNode) component.resolve(component.getScopedContext());
-    if (component.canResolve()) {
-      final CommonPropsCopyable props = component.getCommonPropsCopyable();
-      if (props != null) {
-        props.copyInto(component.getScopedContext(), node);
-      }
-    }
-
-    return node;
+    return LayoutState.resolve(this, component);
   }
 
   int getWidthSpec() {
--- litho-core/src/main/java/com/facebook/litho/LayoutState.java
+++ litho-core/src/main/java/com/facebook/litho/LayoutState.java
@@ -2544,21 +2544,17 @@ class LayoutState {
       DebugComponent.applyOverrides(parentContext, component);
     }
 
-    // 5. Resolve the Component into an InternalNode.
-    final InternalNode node;
     final ComponentContext c = component.getScopedContext();
-    if (component.canResolve()) { // 5.1 If the Component can resolve its own InternalNode.
 
-      // Resolve the component into an InternalNode.
-      node = (InternalNode) component.resolve(c);
+    // 5. Resolve the Component into an InternalNode.
+    final InternalNode node = (InternalNode) component.resolve(c);
 
-      // Explicitly copy the common props of the component into its InternalNode.
+    // Explicitly copy the common props into the InternalNode if it did resolve itself.
+    if (component.canResolve()) {
       final CommonPropsCopyable props = component.getCommonPropsCopyable();
       if (props != null) {
         props.copyInto(c, node);
       }
-    } else { // 5.2 Create this component's layout.
-      node = createLayout(c, component, false);
     }
 
     return node;
