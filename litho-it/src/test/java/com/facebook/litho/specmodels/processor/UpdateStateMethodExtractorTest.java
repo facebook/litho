8faa31f4a32c226aa22e11826a5f0d1aa224e50c
Andres Sanchis <asanchis@whatsapp.com>
2019-08-23 11:52:45 -0700

    Share PsiUpdateStateMethodExtractor testing for Litho IntelliJ Plugin
    Summary:
    Refactor to re-use the test logic from the litho-it UpdateStateMethodExtractor for litho-intellij-plugin similar PsiUpdateStateMethodExtractor.
    
    The asserts have been moved to a common UpdateStateMethodExtratorTest class, where all assertions are performed, leaving only the two unit tests to do the test data set-up, and calling the corresponding extractor method.
    
    Reviewed By: Katalune
    
    Differential Revision: D16969106
    
    fbshipit-source-id: 8aef81f47d870a36bdc631841d8b0adf348e701d

--- litho-it/src/test/java/com/facebook/litho/specmodels/processor/UpdateStateMethodExtractorTest.java
+++ litho-it/src/test/java/com/facebook/litho/specmodels/processor/UpdateStateMethodExtractorTest.java
@@ -16,7 +16,6 @@
 
 package com.facebook.litho.specmodels.processor;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import com.facebook.litho.annotations.OnCreateLayout;
@@ -29,13 +28,10 @@ import com.facebook.litho.specmodels.internal.ImmutableList;
 import com.facebook.litho.specmodels.model.SpecMethodModel;
 import com.facebook.litho.specmodels.model.UpdateStateMethod;
 import com.google.testing.compile.CompilationRule;
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.TypeName;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.processing.Messager;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import org.junit.Rule;
@@ -58,9 +54,7 @@ public class UpdateStateMethodExtractorTest {
 
     @OnUpdateState
     public void testMethod(
-        @Prop boolean testProp,
-        @State int testState,
-        @Param Object testPermittedAnnotation) {
+        @Prop boolean testProp, @State int testState, @Param Object testPermittedAnnotation) {
       // Don't do anything.
     }
   }
@@ -76,32 +70,6 @@ public class UpdateStateMethodExtractorTest {
         UpdateStateMethodExtractor.getOnUpdateStateMethods(
             typeElement, permittedParamAnnotations, mock(Messager.class));
 
-    assertThat(methods).hasSize(1);
-
-    SpecMethodModel<UpdateStateMethod, Void> updateStateMethod = methods.iterator().next();
-    assertThat(updateStateMethod.annotations).hasAtLeastOneElementOfType(OnUpdateState.class);
-
-    assertThat(updateStateMethod.modifiers).hasSize(1);
-    assertThat(updateStateMethod.modifiers).contains(Modifier.PUBLIC);
-
-    assertThat(updateStateMethod.name.toString()).isEqualTo("testMethod");
-
-    assertThat(updateStateMethod.returnType).isEqualTo(TypeName.VOID);
-
-    assertThat(updateStateMethod.methodParams).hasSize(3);
-
-    assertThat(updateStateMethod.methodParams.get(0).getName()).isEqualTo("testProp");
-    assertThat(updateStateMethod.methodParams.get(0).getTypeName()).isEqualTo(TypeName.BOOLEAN);
-    assertThat(updateStateMethod.methodParams.get(0).getAnnotations()).hasSize(1);
-
-    assertThat(updateStateMethod.methodParams.get(1).getName()).isEqualTo("testState");
-    assertThat(updateStateMethod.methodParams.get(1).getTypeName()).isEqualTo(TypeName.INT);
-    assertThat(updateStateMethod.methodParams.get(1).getAnnotations()).hasSize(1);
-
-    assertThat(updateStateMethod.methodParams.get(2).getName())
-        .isEqualTo("testPermittedAnnotation");
-    assertThat(updateStateMethod.methodParams.get(2).getTypeName())
-        .isEqualTo(ClassName.bestGuess("java.lang.Object"));
-    assertThat(updateStateMethod.methodParams.get(2).getAnnotations()).hasSize(1);
+    UpdateStateMethodExtractorTestHelper.assertMethodExtraction(methods);
   }
 }
