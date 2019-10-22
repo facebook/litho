bdfa33f01ded034f797855f59f6e5fae1503c8d0
Sergey Ryabov <colriot@fb.com>
2019-10-22 12:50:27 -0700

    Add suppport for Image and FrescoImage specs
    Reviewed By: astreet
    
    Differential Revision: D17980063
    
    fbshipit-source-id: b91e6ac4481360009d975e65e64badd23b3e2cf5

---
+++ litho-kotlin/src/main/kotlin/com/facebook/litho/widget/Image.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.facebook.litho.widget
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView.ScaleType
+import androidx.annotation.DrawableRes
+import com.facebook.litho.ComponentContext
+
+/**
+ * Builder function for creating [ImageSpec] components.
+ */
+inline fun ComponentContext.Image(
+    drawable: Drawable,
+    scaleType: ScaleType = ScaleType.FIT_CENTER
+): Image.Builder =
+    Image.create(this)
+        .drawable(drawable)
+        .scaleType(scaleType)
+
+/**
+ * Builder function for creating [ImageSpec] components.
+ */
+inline fun ComponentContext.Image(
+    @DrawableRes drawableRes: Int,
+    scaleType: ScaleType = ScaleType.FIT_CENTER
+): Image.Builder =
+    Image.create(this)
+        .drawableRes(drawableRes)
+        .scaleType(scaleType)
