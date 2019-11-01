0f91d2e6225a0d6ec8ecc125c70cb54fed18c46a
Kata Zavorotchenko <kata@fb.com>
2019-11-01 07:44:44 -0700

    Moved lazy init to inner class in LithoLoggerProvider
    Summary: Moved lazy initialisatin to the inner class, to invoke it when it's called, instead of the time when outer class is loaded.
    
    Reviewed By: colriot
    
    Differential Revision: D18269037
    
    fbshipit-source-id: 9950b3137670526fff025c8bb89ca6ae3ca53b52

--- litho-intellij-plugin/src/main/java/com/facebook/litho/intellij/logging/LithoLoggerProvider.java
+++ litho-intellij-plugin/src/main/java/com/facebook/litho/intellij/logging/LithoLoggerProvider.java
@@ -29,15 +29,15 @@ import java.util.concurrent.Executor;
  * Provides logger to track common user flow events: completion action usage, dialog opening, etc.
  */
 public final class LithoLoggerProvider {
-  private static final EventLogger EVENT_LOGGER = new LithoEventLogger();
 
   private LithoLoggerProvider() {}
 
   public static EventLogger getEventLogger() {
-    return EVENT_LOGGER;
+    return LithoEventLogger.INSTANCE;
   }
 
   static class LithoEventLogger implements EventLogger {
+    static final EventLogger INSTANCE = new LithoEventLogger();
     static final ExtensionPointName<EventLogger> EP_NAME =
         ExtensionPointName.create("com.facebook.litho.intellij.eventLogger");
     private final EventLogger[] loggers;
