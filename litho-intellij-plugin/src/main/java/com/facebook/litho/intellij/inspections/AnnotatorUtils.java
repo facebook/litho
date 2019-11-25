aa0f45fd88d13adaf0bd33a5cbbcecb6267096f5
Kata Zavorotchenko <kata@fb.com>
2019-11-25 03:47:48 -0800

    Added quick fix parameter to AnnotatorUtils
    Summary: Annotator Utils could create Annotation and register a fix on it
    
    Reviewed By: muraziz
    
    Differential Revision: D18672836
    
    fbshipit-source-id: a8851f40af45c43f9f135b53baf09d93611f49ae

--- litho-intellij-plugin/src/main/java/com/facebook/litho/intellij/inspections/AnnotatorUtils.java
+++ litho-intellij-plugin/src/main/java/com/facebook/litho/intellij/inspections/AnnotatorUtils.java
@@ -17,11 +17,15 @@
 package com.facebook.litho.intellij.inspections;
 
 import com.facebook.litho.specmodels.model.SpecModelValidationError;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiNameIdentifierOwner;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 class AnnotatorUtils {
@@ -29,13 +33,24 @@ class AnnotatorUtils {
 
   /** Adds error annotation to the provided holder. */
   static void addError(AnnotationHolder holder, SpecModelValidationError error) {
+    addError(holder, error, Collections.emptyList());
+  }
+
+  static void addError(
+      AnnotationHolder holder, SpecModelValidationError error, List<IntentionAction> fixes) {
     PsiElement errorElement = (PsiElement) error.element;
-    holder.createErrorAnnotation(
-        Optional.of(errorElement)
-            .filter(element -> element instanceof PsiClass || element instanceof PsiMethod)
-            .map(PsiNameIdentifierOwner.class::cast)
-            .map(PsiNameIdentifierOwner::getNameIdentifier)
-            .orElse(errorElement),
-        error.message);
+    Annotation errorAnnotation =
+        holder.createErrorAnnotation(
+            Optional.of(errorElement)
+                .filter(element -> element instanceof PsiClass || element instanceof PsiMethod)
+                .map(PsiNameIdentifierOwner.class::cast)
+                .map(PsiNameIdentifierOwner::getNameIdentifier)
+                .orElse(errorElement),
+            error.message);
+    if (!fixes.isEmpty()) {
+      for (IntentionAction fix : fixes) {
+        errorAnnotation.registerFix(fix);
+      }
+    }
   }
 }
