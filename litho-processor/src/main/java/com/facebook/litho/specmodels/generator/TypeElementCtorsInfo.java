a8bed81685ac90c269106081348c04583f09d36c
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2019-12-17 01:28:31 +0200

    Add comments in TypeElementCtorsInfo enum class

--- litho-processor/src/main/java/com/facebook/litho/specmodels/generator/TypeElementCtorsInfo.java
+++ litho-processor/src/main/java/com/facebook/litho/specmodels/generator/TypeElementCtorsInfo.java
@@ -1,7 +1,10 @@
 package com.facebook.litho.specmodels.generator;
 
 public enum TypeElementCtorsInfo {
+  /* Single empty POJO constructor */
   SINGLE_EMPTY,
+  /* Single non empty POJO constructor */
   SINGLE_NON_EMPTY,
+  /* Multiple constructors available (possibly default values) */
   MULTIPLE
 }
