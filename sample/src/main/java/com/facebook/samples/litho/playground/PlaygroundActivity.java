057362382de6ff4f3c976e585cdffeb432fafbd3
Bo Duan <boduan402@gmail.com>
2020-03-19 13:53:02 -0700

    Reproduce the a11y issue during state update.

--- sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundActivity.java
+++ sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundActivity.java
@@ -16,18 +16,62 @@
 
 package com.facebook.samples.litho.playground;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.litho.Column;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.Text;
 import com.facebook.samples.litho.NavigatableDemoActivity;
 
-public class PlaygroundActivity extends NavigatableDemoActivity {
+import static com.facebook.yoga.YogaEdge.ALL;
+
+public class PlaygroundActivity extends NavigatableDemoActivity implements View.OnClickListener {
+
+  LithoView lithoView;
+  Text helloWorld;
+  Text lithoTutorial;
+  PlaygroundComponent playgroundComponent;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    final ComponentContext componentContext = new ComponentContext(this);
-    setContentView(LithoView.create(this, PlaygroundComponent.create(componentContext).build()));
+    final ComponentContext c = new ComponentContext(this);
+    lithoView = new LithoView(c);
+
+    playgroundComponent = PlaygroundComponent.create(c).clickListener(this).build();
+    helloWorld = Text.create(c)
+        .text("Hello world")
+        .textSizeSp(40).build();
+    lithoTutorial = Text.create(c)
+        .text("Litho tutorial")
+        .textSizeSp(20).build();
+
+    Column column = Column.create(c)
+        .paddingDip(ALL, 16)
+        .backgroundColor(Color.WHITE)
+        .child(helloWorld)
+        .child(playgroundComponent)
+        .build();
+
+    lithoView.setComponent(column);
+    setContentView(lithoView);
+  }
+
+  @Override
+  public void onClick(View v) {
+    Column column = Column.create(lithoView.getComponentContext())
+        .paddingDip(ALL, 16)
+        .backgroundColor(Color.WHITE)
+        .child(helloWorld)
+        .child(lithoTutorial)
+        .child(playgroundComponent)
+        .build();
+
+    lithoView.setComponent(column);
   }
 }
--- sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundComponentSpec.java
+++ sample/src/main/java/com/facebook/samples/litho/playground/PlaygroundComponentSpec.java
@@ -17,21 +17,63 @@
 package com.facebook.samples.litho.playground;
 
 import android.graphics.Color;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.facebook.litho.ClickEvent;
 import com.facebook.litho.Column;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
+import com.facebook.litho.OnPopulateAccessibilityEventEvent;
+import com.facebook.litho.StateValue;
+import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.OnEvent;
+import com.facebook.litho.annotations.OnUpdateState;
+import com.facebook.litho.annotations.Prop;
+import com.facebook.litho.annotations.State;
 import com.facebook.litho.widget.Text;
+
+import androidx.core.view.AccessibilityDelegateCompat;
 
 @LayoutSpec
 public class PlaygroundComponentSpec {
 
   @OnCreateLayout
-  static Component onCreateLayout(ComponentContext c) {
+  static Component onCreateLayout(ComponentContext c, @State boolean isChecked) {
     return Column.create(c)
         .backgroundColor(Color.WHITE)
-        .child(Text.create(c).textSizeSp(20).text("Playground sample"))
+        .clickHandler(PlaygroundComponent.onClick(c))
+        .onPopulateAccessibilityEventHandler(PlaygroundComponent.onPopulateAccessibilityEvent(c))
+        .child(Text.create(c).textSizeSp(20).text(isChecked ? "I am clicked" : "Playground sample"))
         .build();
   }
+
+  @OnUpdateState
+  static void updateCheckbox(StateValue<Boolean> isChecked) {
+    isChecked.set(!isChecked.get());
+  }
+
+  @OnEvent(ClickEvent.class)
+  static void onClick(
+      ComponentContext c,
+      @FromEvent View view,
+      @Prop View.OnClickListener clickListener) {
+    // clickListener.onClick(view);
+    PlaygroundComponent.updateCheckboxSync(c);
+  }
+
+  @OnEvent(OnPopulateAccessibilityEventEvent.class)
+  static void onPopulateAccessibilityEvent(
+      ComponentContext c,
+      @FromEvent AccessibilityDelegateCompat superDelegate,
+      @FromEvent View host,
+      @FromEvent AccessibilityEvent event) {
+    superDelegate.onPopulateAccessibilityEvent(host, event);
+//    host.performAccessibilityAction(
+//        AccessibilityNodeInfo.ACTION_CLEAR_ACCESSIBILITY_FOCUS, null);
+  }
+
 }
