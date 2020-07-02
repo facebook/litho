bb850914c04593ecbf19dc3acb6a29532dcaf6f9
teikjun <teikjunhci@gmail.com>
2020-07-02 15:49:35 +0800

    use wrapper component for versioned codeblocks

--- website/docs/versionedCodeBlock.js
+++ website/docs/versionedCodeBlock.js
@@ -1,33 +1,26 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import github from 'prism-react-renderer/themes/github';
 import {site} from '../lithoVersions.js';
 
-const exampleCode = `dependencies {
-  // ...
-  // Litho
-  implementation 'com.facebook.litho:litho-core:{{site.lithoVersion}}'
-  implementation 'com.facebook.litho:litho-widget:{{site.lithoVersion}}'
-
-  annotationProcessor 'com.facebook.litho:litho-processor:{{site.lithoVersion}}'
-
-  // SoLoader
-  implementation 'com.facebook.soloader:soloader:{{site.soloaderVersion}}'
-
-  // For integration with Fresco
-  implementation 'com.facebook.litho:litho-fresco:{{site.lithoVersion}}'
-
-  // For testing
-  testImplementation 'com.facebook.litho:litho-testing:{{site.lithoVersion}}'
-}
-`;
-
+// VersionedCodeBlock is a wrapper component for the normal codeblock. It replaces
+// placeholders e.g. {{site.lithoVersion}} with current versions stated in ../lithoVersion.js
+// When updating versions in documentation, it suffices to update the versions in ../lithoVersion.js 
 export const VersionedCodeBlock = ({language, code}) => {
+  // replace versions
   const modifiedCode = code.replaceAll('{{site.lithoVersion}}', site.lithoVersion)
     .replaceAll('{{site.soloaderVersion}}', site.soloaderVersion)
     .replaceAll('{{site.lithoSnapshotVersion}}', site.lithoSnapshotVersion)
     .replaceAll('{{site.flipperVersion}}', site.flipperVersion);
-
+  // render as a codeblock
   return(
   <Highlight {...defaultProps} code={modifiedCode} language={language} theme={github}>
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
