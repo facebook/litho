f7a07bc1b78da3ce6e89ec0aea489d4d1421f16b
Lara Lu <laralu@fb.com>
2020-07-24 12:22:40 -0700

    Add hook for focus changed on TextInput
    Summary: Add FocusChangedEvent to TextInputSpec
    
    Reviewed By: Katalune
    
    Differential Revision: D22538518
    
    fbshipit-source-id: 05a9f0407ea38c96ed9c1eef0c187829745117d7

--- litho-widget/src/main/java/com/facebook/litho/widget/TextInputSpec.java
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextInputSpec.java
@@ -51,6 +51,7 @@ import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.Diff;
 import com.facebook.litho.EventHandler;
+import com.facebook.litho.FocusChangedEvent;
 import com.facebook.litho.Output;
 import com.facebook.litho.Size;
 import com.facebook.litho.SizeSpec;
@@ -175,6 +176,7 @@ import javax.annotation.Nullable;
     isPureRender = true,
     events = {
       TextChangedEvent.class,
+      FocusChangedEvent.class,
       SelectionChangedEvent.class,
       KeyUpEvent.class,
       KeyPreImeEvent.class,
@@ -721,6 +723,7 @@ class TextInputSpec {
     editText.setKeyPreImeEventEventHandler(TextInput.getKeyPreImeEventHandler(c));
     editText.setEditorActionEventHandler(TextInput.getEditorActionEventHandler(c));
     editText.setInputConnectionEventHandler(TextInput.getInputConnectionEventHandler(c));
+    editText.setFocusChangedEventHandler(TextInput.getFocusChangedEventHandler(c));
   }
 
   @OnUnmount
@@ -743,6 +746,7 @@ class TextInputSpec {
     editText.setKeyPreImeEventEventHandler(null);
     editText.setEditorActionEventHandler(null);
     editText.setInputConnectionEventHandler(null);
+    editText.setFocusChangedEventHandler(null);
   }
 
   @Nullable
@@ -847,6 +851,7 @@ class TextInputSpec {
 
     private static final int UNMEASURED_LINE_COUNT = -1;
     @Nullable private EventHandler<TextChangedEvent> mTextChangedEventHandler;
+    @Nullable private EventHandler<FocusChangedEvent> mFocusChangedEventHandler;
     @Nullable private EventHandler<SelectionChangedEvent> mSelectionChangedEventHandler;
     @Nullable private EventHandler<KeyUpEvent> mKeyUpEventHandler;
     @Nullable private EventHandler<KeyPreImeEvent> mKeyPreImeEventEventHandler;
@@ -880,6 +885,16 @@ class TextInputSpec {
           && mLineCount != lineCount
           && mComponentContext != null) {
         com.facebook.litho.widget.TextInput.remeasureForUpdatedTextSync(mComponentContext);
+      }
+    }
+
+    @Override
+    protected void onFocusChanged(
+        boolean focused, int direction, @Nullable Rect previouslyFocusedRect) {
+      super.onFocusChanged(focused, direction, previouslyFocusedRect);
+      if (mFocusChangedEventHandler != null) {
+        TextInput.dispatchFocusChangedEvent(
+            mFocusChangedEventHandler, EditTextWithEventHandlers.this, focused);
       }
     }
 
@@ -935,6 +950,11 @@ class TextInputSpec {
     void setTextChangedEventHandler(
         @Nullable EventHandler<TextChangedEvent> textChangedEventHandler) {
       mTextChangedEventHandler = textChangedEventHandler;
+    }
+
+    void setFocusChangedEventHandler(
+        @Nullable EventHandler<FocusChangedEvent> focusChangedEventHandler) {
+      mFocusChangedEventHandler = focusChangedEventHandler;
     }
 
     void setSelectionChangedEventHandler(
