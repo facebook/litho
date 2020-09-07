593990a78f2a77cb0ab98fa4f7eb9f561511b875
Paco Estevez Garcia <festevezga@fb.com>
2020-09-07 10:58:07 -0700

    Add simple editor for Mutable classes
    Summary:
    This diff creates a new `SimpleEditor` that greatly reduces the API surface of Editor classes for simple use cases.
    
    This diff has the "mutable" flavor. An example of the usage will help understand the code below.
    
    ```
    EditorRegistry.registerEditor(
        Patata.class,
        SimpleEditor.makeMutable(
            new SimpleEditor.MutablePropertyEditor<Patata>() {
    
                Override
                public void writeStringProperty(Patata value, String property, String newValue) {
                  if ("name".equals(property)) {
                    value.name = newValue;
                  }
                }
    
                Override
                public void writeNumberProperty(Patata value, String property, Number newValue) {
                  if ("date".equals(property)) {
                    value.date = newValue.intValue();
                  }
                }
    
                Override
                public void writeBoolProperty(Patata value, String property, boolean newValue) {
                  if ("isUser".equals(property)) {
                    value.isUser = newValue;
                  }
                }
    
                Override
                public Map<String, SimpleEditor.SimpleEditorValue> readProperties(
                    final Patata value) {
                  return new HashMap<String, SimpleEditor.SimpleEditorValue>() {
                    {
                      put("name", SimpleEditor.SimpleEditorValue.string(value.name));
                      put("date", SimpleEditor.SimpleEditorValue.number(value.date));
                      put("isUser", SimpleEditor.SimpleEditorValue.bool(value.isUser));
                    }
                  };
                }
            }));
    ```
    
    Reviewed By: Katalune
    
    Differential Revision: D23565738
    
    fbshipit-source-id: ff04af8b15c2bad0cfa0bb49b7182d3b026bb993

---
+++ litho-editor-core/src/main/java/com/facebook/litho/editor/SimpleEditor.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.editor;
+
+import com.facebook.litho.editor.instances.EditorUtils;
+import com.facebook.litho.editor.model.EditorArray;
+import com.facebook.litho.editor.model.EditorBool;
+import com.facebook.litho.editor.model.EditorNumber;
+import com.facebook.litho.editor.model.EditorShape;
+import com.facebook.litho.editor.model.EditorString;
+import com.facebook.litho.editor.model.EditorValue;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+public final class SimpleEditor {
+
+  private SimpleEditor() {}
+
+  // Constructors
+
+  public static <T> Editor makeMutable(final MutablePropertyEditor<T> propertyEditor) {
+    return new Editor() {
+      @Override
+      public EditorValue read(Field f, Object node) {
+        return makeValue(f, node, propertyEditor);
+      }
+
+      @Override
+      public boolean write(final Field f, final Object node, final EditorValue values) {
+        final T value = EditorUtils.getNodeUNSAFE(f, node);
+        values.when(
+            new EditorValue.DefaultEditorVisitor() {
+              @Override
+              public Void isShape(EditorShape object) {
+                final EditorValue editorValue = object.value.get(f.getName());
+                return editorValue != null
+                    ? editorValue.when(writeMutable(propertyEditor, value))
+                    : null;
+              }
+            });
+        return true;
+      }
+    };
+  }
+
+  // Read data
+
+  private static <T> EditorValue makeValue(Field f, Object node, PropertyReader<T> propertyReader) {
+    T value = EditorUtils.getNodeUNSAFE(f, node);
+    final Map<String, SimpleEditorValue> properties = propertyReader.readProperties(value);
+    Map<String, EditorValue> shape = new HashMap<>();
+    for (Map.Entry<String, SimpleEditorValue> property : properties.entrySet()) {
+      shape.put(property.getKey(), property.getValue().value);
+    }
+    return EditorValue.shape(shape);
+  }
+
+  // Write data
+
+  private static <T> EditorValue.DefaultEditorVisitor writeMutable(
+      final MutablePropertyEditor<T> propertyEditor, final T value) {
+    return new EditorValue.DefaultEditorVisitor() {
+      @Override
+      public Void isShape(EditorShape shape) {
+        final Map<String, SimpleEditorValue> properties = propertyEditor.readProperties(value);
+        final Map<String, EditorValue> updates = shape.value;
+        for (final Map.Entry<String, EditorValue> updatedProperty : updates.entrySet()) {
+          final SimpleEditorValue newValue =
+              SimpleEditorValue.fromEditorValueOrNull(updatedProperty.getValue());
+          final SimpleEditorValue oldValue = properties.get(updatedProperty.getKey());
+          if (newValue != null && oldValue != null && newValue.type == oldValue.type) {
+            newValue.value.whenPrimitive(
+                new EditorValue.EditorPrimitiveVisitor() {
+                  @Override
+                  public boolean isNumber(String[] path, EditorNumber number) {
+                    propertyEditor.writeNumberProperty(
+                        value, updatedProperty.getKey(), number.value);
+                    return true;
+                  }
+
+                  @Override
+                  public boolean isString(String[] path, EditorString string) {
+                    propertyEditor.writeStringProperty(
+                        value, updatedProperty.getKey(), string.value);
+                    return false;
+                  }
+
+                  @Override
+                  public boolean isBool(String[] path, EditorBool bool) {
+                    propertyEditor.writeBoolProperty(value, updatedProperty.getKey(), bool.value);
+                    return false;
+                  }
+                });
+          }
+        }
+        return null;
+      }
+    };
+  }
+
+  // Types
+
+  public interface PropertyReader<T> {
+    Map<String, SimpleEditorValue> readProperties(T value);
+  }
+
+  public interface MutablePropertyEditor<T> extends PropertyReader<T> {
+    void writeStringProperty(T value, String property, String newValue);
+
+    void writeNumberProperty(T value, String property, Number newValue);
+
+    void writeBoolProperty(T value, String property, boolean newValue);
+  }
+
+  private static final int PRIMITIVE_TYPE_NUMBER = 0;
+  private static final int PRIMITIVE_TYPE_STRING = 1;
+  private static final int PRIMITIVE_TYPE_BOOL = 2;
+
+  /** A class that wraps either a number, string or bool */
+  public static final class SimpleEditorValue {
+
+    public final EditorValue value;
+
+    public final int type;
+
+    private SimpleEditorValue(EditorValue value, int type) {
+      this.value = value;
+      this.type = type;
+    }
+
+    // Constructors
+
+    public static SimpleEditorValue number(Number n) {
+      return new SimpleEditorValue(new EditorNumber(n), PRIMITIVE_TYPE_NUMBER);
+    }
+
+    public static SimpleEditorValue string(String s) {
+      return new SimpleEditorValue(new EditorString(s), PRIMITIVE_TYPE_STRING);
+    }
+
+    public static SimpleEditorValue bool(boolean b) {
+      return new SimpleEditorValue(new EditorBool(b), PRIMITIVE_TYPE_BOOL);
+    }
+
+    public static @Nullable SimpleEditorValue fromEditorValueOrNull(EditorValue v) {
+      return v.when(isPrimitive);
+    }
+
+    private static final EditorValue.EditorVisitor</*@Nullable*/ SimpleEditorValue> isPrimitive =
+        new EditorValue.EditorVisitor</*@Nullable*/ SimpleEditorValue>() {
+          @Override
+          public @Nullable SimpleEditorValue isShape(EditorShape object) {
+            return null;
+          }
+
+          @Override
+          public @Nullable SimpleEditorValue isArray(EditorArray array) {
+            return null;
+          }
+
+          @Override
+          public @Nullable SimpleEditorValue isNumber(EditorNumber number) {
+            return SimpleEditorValue.number(number.value);
+          }
+
+          @Override
+          public @Nullable SimpleEditorValue isString(EditorString string) {
+            return SimpleEditorValue.string(string.value);
+          }
+
+          @Override
+          public @Nullable SimpleEditorValue isBool(EditorBool bool) {
+            return SimpleEditorValue.bool(bool.value);
+          }
+        };
+  }
+}
