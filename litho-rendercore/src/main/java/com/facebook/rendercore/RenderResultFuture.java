864ccfbf349d74f8682dba42e5da3950337079e9
Pasquale Anatriello <pasqualea@fb.com>
2020-10-07 14:14:55 -0700

    Make RenderResultFuture public
    Summary: As per title. I want to be able to re-use the same future in collections. It doesn't make sense for me to write a new one as this also already does priority inheritance
    
    Reviewed By: yousuf-haque
    
    Differential Revision: D24017053
    
    fbshipit-source-id: a8ff3ada4dbc2b1fdf207d112beb51a2653b2582

---
+++ litho-rendercore/src/main/java/com/facebook/rendercore/RenderResultFuture.java
@@ -0,0 +1,101 @@
+// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+package com.facebook.rendercore;
+
+import android.content.Context;
+import android.os.Process;
+import androidx.annotation.Nullable;
+import com.facebook.rendercore.extensions.RenderCoreExtension;
+import com.facebook.rendercore.utils.ThreadUtils;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RenderResultFuture<State, RenderContext> {
+
+  private final RenderState.LazyTree<State> mLazyTree;
+  private final RenderResult<State> mPreviousResult;
+  private final int mSetRootId;
+  private final int mWidthSpec;
+  private final int mHeightSpec;
+  private final FutureTask<RenderResult<State>> mFutureTask;
+  private final AtomicInteger mRunningThreadId = new AtomicInteger(-1);
+
+  public RenderResultFuture(
+      final Context context,
+      RenderState.LazyTree<State> lazyTree,
+      final @Nullable RenderContext renderContext,
+      final @Nullable RenderCoreExtension<?>[] extensions,
+      final @Nullable RenderResult<State> previousResult,
+      final int setRootId,
+      final int widthSpec,
+      final int heightSpec) {
+    mLazyTree = lazyTree;
+    mPreviousResult = previousResult;
+    mSetRootId = setRootId;
+    mWidthSpec = widthSpec;
+    mHeightSpec = heightSpec;
+    mFutureTask =
+        new FutureTask<>(
+            new Callable<RenderResult<State>>() {
+              @Override
+              public RenderResult<State> call() {
+                return RenderResult.resolve(
+                    context,
+                    mLazyTree,
+                    renderContext,
+                    extensions,
+                    mPreviousResult,
+                    mSetRootId,
+                    mWidthSpec,
+                    mHeightSpec);
+              }
+            });
+  }
+
+  @Nullable
+  public RenderResult<State> runAndGet() {
+    if (mRunningThreadId.compareAndSet(-1, Process.myTid())) {
+      mFutureTask.run();
+      try {
+        return mFutureTask.get();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      } catch (ExecutionException e) {
+        // Unwrap so that stacktraces are more clear in logs
+        if (e.getCause() instanceof RuntimeException) {
+          throw (RuntimeException) e.getCause();
+        } else {
+          throw new RuntimeException(e.getCause());
+        }
+      }
+    }
+
+    return ThreadUtils.getResultInheritingPriority(mFutureTask, mRunningThreadId.get());
+  }
+
+  public boolean isDone() {
+    return mFutureTask.isDone();
+  }
+
+  public void cancel() {
+    mFutureTask.cancel(false);
+  }
+
+  public boolean isCanceled() {
+    return mFutureTask.isCancelled();
+  }
+
+  public int getSetRootId() {
+    return mSetRootId;
+  }
+
+  public int getWidthSpec() {
+    return mWidthSpec;
+  }
+
+  public int getHeightSpec() {
+    return mHeightSpec;
+  }
+}
--- litho-rendercore/src/main/java/com/facebook/rendercore/RenderState.java
+++ litho-rendercore/src/main/java/com/facebook/rendercore/RenderState.java
@@ -20,7 +20,6 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.os.Process;
 import android.util.Pair;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
@@ -28,9 +27,6 @@ import com.facebook.infer.annotation.ThreadConfined;
 import com.facebook.rendercore.extensions.RenderCoreExtension;
 import com.facebook.rendercore.utils.MeasureSpecUtils;
 import com.facebook.rendercore.utils.ThreadUtils;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /** todo: javadocs * */
@@ -362,82 +358,6 @@ public class RenderState<State, RenderContext> {
   @Nullable
   RenderCoreExtension<?>[] getExtensions() {
     return extensions;
-  }
-
-  private static class RenderResultFuture<State, RenderContext> {
-
-    private final LazyTree<State> mLazyTree;
-    private final RenderResult<State> mPreviousResult;
-    private final int mSetRootId;
-    private final int mWidthSpec;
-    private final int mHeightSpec;
-    private final FutureTask<RenderResult<State>> mFutureTask;
-    private final AtomicInteger mRunningThreadId = new AtomicInteger(-1);
-
-    private RenderResultFuture(
-        final Context context,
-        LazyTree<State> lazyTree,
-        final @Nullable RenderContext renderContext,
-        final @Nullable RenderCoreExtension<?>[] extensions,
-        final @Nullable RenderResult<State> previousResult,
-        final int setRootId,
-        final int widthSpec,
-        final int heightSpec) {
-      mLazyTree = lazyTree;
-      mPreviousResult = previousResult;
-      mSetRootId = setRootId;
-      mWidthSpec = widthSpec;
-      mHeightSpec = heightSpec;
-      mFutureTask =
-          new FutureTask<>(
-              new Callable<RenderResult<State>>() {
-                @Override
-                public RenderResult<State> call() {
-                  return RenderResult.resolve(
-                      context,
-                      mLazyTree,
-                      renderContext,
-                      extensions,
-                      mPreviousResult,
-                      mSetRootId,
-                      mWidthSpec,
-                      mHeightSpec);
-                }
-              });
-    }
-
-    @Nullable
-    RenderResult<State> runAndGet() {
-      if (mRunningThreadId.compareAndSet(-1, Process.myTid())) {
-        mFutureTask.run();
-        try {
-          return mFutureTask.get();
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-          // Unwrap so that stacktraces are more clear in logs
-          if (e.getCause() instanceof RuntimeException) {
-            throw (RuntimeException) e.getCause();
-          } else {
-            throw new RuntimeException(e.getCause());
-          }
-        }
-      }
-
-      return ThreadUtils.getResultInheritingPriority(mFutureTask, mRunningThreadId.get());
-    }
-
-    public int getSetRootId() {
-      return mSetRootId;
-    }
-
-    public int getWidthSpec() {
-      return mWidthSpec;
-    }
-
-    public int getHeightSpec() {
-      return mHeightSpec;
-    }
   }
 
   private class RenderStateHandler extends Handler {
