7719a10046aceb7a7703ba1626f55f4de28cc124
Paco Estevez Garcia <festevezga@fb.com>
2020-11-06 08:25:04 -0800

    Add @format directive to js files in repo
    Summary: Added by FB's Prettier formatter
    
    Reviewed By: colriot
    
    Differential Revision: D24783264
    
    fbshipit-source-id: 444de8bedfa0a8f47b8f794754746ff97b749602

--- website/versionConfig.js
+++ website/versionConfig.js
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @format
  */
 
 // The versions here are used across the documentation
