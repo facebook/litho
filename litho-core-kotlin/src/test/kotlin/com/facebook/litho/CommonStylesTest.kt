3b7085d32933b2029bd136ca9d3435133f161822
Andy Street <andrews@fb.com>
2021-01-14 04:31:32 -0800

    Add test for common styles
    Summary: Adds tests for all currently defined styles. This puts us in a place where when we add new styles, we can expect to add a test.
    
    Reviewed By: strulovich
    
    Differential Revision: D25850448
    
    fbshipit-source-id: c78c424a813cd57a38de5247e7cb865fd5820ce5

---
+++ litho-core-kotlin/src/test/kotlin/com/facebook/litho/CommonStylesTest.kt
@@ -0,0 +1,401 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.facebook.litho.testing.LithoViewRule
+import com.facebook.litho.testing.assertMatches
+import com.facebook.litho.testing.child
+import com.facebook.litho.testing.match
+import com.facebook.litho.testing.setRoot
+import com.facebook.litho.testing.testrunner.LithoTestRunner
+import com.facebook.litho.testing.unspecified
+import com.facebook.yoga.YogaAlign
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/** Unit tests for common styles defined in [Style]. */
+@RunWith(LithoTestRunner::class)
+class CommonStylesTest {
+
+  @Rule @JvmField val lithoViewRule = LithoViewRule()
+
+  @Test
+  fun widthAndHeight_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot { Row(style = Style.size(width = 100.px, height = 100.px)) }
+        .assertMatches(match<LithoView> { bounds(0, 0, 100, 100) })
+  }
+
+  @Test
+  fun maxWidth_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(height = 100.px).width(maxWidth = 200.px),
+              children = listOf(Row(style = Style.size(width = 500.px))))
+        }
+        .assertMatches(match<LithoView> { bounds(0, 0, 200, 100) })
+  }
+
+  @Test
+  fun minWidth_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot { Row(style = Style.size(height = 100.px).width(minWidth = 200.px)) }
+        .assertMatches(match<LithoView> { bounds(0, 0, 200, 100) })
+  }
+
+  @Test
+  fun maxHeight_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px).height(maxHeight = 200.px),
+              children = listOf(Row(style = Style.size(height = 500.px))))
+        }
+        .assertMatches(match<LithoView> { bounds(0, 0, 100, 200) })
+  }
+
+  @Test
+  fun minHeight_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot { Row(style = Style.size(width = 100.px).height(minHeight = 200.px)) }
+        .assertMatches(match<LithoView> { bounds(0, 0, 100, 200) })
+  }
+
+  @Test
+  fun flexBasis_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px, height = 100.px),
+              children = listOf(Row(style = Style.flex(basis = 50.px).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 50, 100) }
+            })
+  }
+
+  @Test
+  fun flexGrow_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px, height = 100.px),
+              children = listOf(Row(style = Style.flex(grow = 1f).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 100, 100) }
+            })
+  }
+
+  @Test
+  fun flexShrink_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = Style.size(width = 100.px, height = 100.px),
+              children =
+                  listOf(
+                      Row(
+                          style =
+                              Style.size(height = 100.px)
+                                  .width(minWidth = 50.px)
+                                  .flex(shrink = 1f)
+                                  .wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 50, 100) }
+            })
+  }
+
+  @Test
+  fun alignSelf_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px, height = 100.px),
+              children =
+                  listOf(
+                      Row(
+                          style =
+                              Style.size(width = 100.px)
+                                  .alignSelf(YogaAlign.STRETCH)
+                                  .wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 100, 100) }
+            })
+  }
+
+  @Test
+  fun padding_whenGranularPaddingSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style =
+                  Style.size(width = 100.px, height = 100.px)
+                      .padding(start = left.dp, top = top.dp, end = right.dp, bottom = bottom.dp),
+              children = listOf(Row(style = Style.flex(grow = 1f).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  @Test
+  fun padding_whenHorizontalVerticalPaddingSet_isRespected() {
+    val horizontal = 10
+    val vertical = 20
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style =
+                  Style.size(width = 100.px, height = 100.px)
+                      .padding(horizontal = horizontal.dp, vertical = vertical.dp),
+              children = listOf(Row(style = Style.flex(grow = 1f).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(horizontal, vertical, 100 - 2 * horizontal, 100 - 2 * vertical)
+              }
+            })
+  }
+
+  @Test
+  fun padding_whenAllPaddingSet_isRespected() {
+    val padding = 32
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = Style.size(width = 100.px, height = 100.px).padding(padding.dp),
+              children = listOf(Row(style = Style.flex(grow = 1f).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(padding, padding, 100 - 2 * padding, 100 - 2 * padding)
+              }
+            })
+  }
+
+  @Test
+  fun margin_whenGranularMarginSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = Style.size(width = 100.px, height = 100.px),
+              children =
+                  listOf(
+                      Row(
+                          style =
+                              Style.margin(
+                                      start = left.dp,
+                                      top = top.dp,
+                                      end = right.dp,
+                                      bottom = bottom.dp)
+                                  .flex(grow = 1f)
+                                  .wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  @Test
+  fun margin_whenHorizontalVerticalMarginSet_isRespected() {
+    val horizontal = 10
+    val vertical = 20
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = Style.size(width = 100.px, height = 100.px),
+              children =
+                  listOf(
+                      Row(
+                          style =
+                              Style.margin(horizontal = horizontal.dp, vertical = vertical.dp)
+                                  .flex(grow = 1f)
+                                  .wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(horizontal, vertical, 100 - 2 * horizontal, 100 - 2 * vertical)
+              }
+            })
+  }
+
+  @Test
+  fun margin_whenAllMarginSet_isRespected() {
+    val margin = 32
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = Style.size(width = 100.px, height = 100.px),
+              children = listOf(Row(style = Style.margin(margin.dp).flex(grow = 1f).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(margin, margin, 100 - 2 * margin, 100 - 2 * margin) }
+            })
+  }
+
+  @Test
+  fun position_whenSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px, height = 100.px),
+              children =
+                  listOf(
+                      Row(
+                          style =
+                              Style.position(
+                                      start = left.px,
+                                      top = top.px,
+                                      end = right.px,
+                                      bottom = bottom.px)
+                                  .wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  @Test
+  fun background_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style =
+                  Style.size(width = 100.px, height = 100.px)
+                      .background(ColorDrawable(Color.WHITE)))
+        }
+        .measure()
+        .layout()
+        .attachToWindow()
+
+    assertHasColorDrawableOfColor(lithoViewRule.lithoView, Color.WHITE)
+  }
+
+  @Test
+  fun foreground_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style =
+                  Style.size(width = 100.px, height = 100.px)
+                      .foreground(ColorDrawable(Color.WHITE)))
+        }
+        .measure()
+        .layout()
+        .attachToWindow()
+
+    assertHasColorDrawableOfColor(lithoViewRule.lithoView, Color.WHITE)
+  }
+
+  @Test
+  fun wrapInView_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              style = Style.size(width = 100.px, height = 100.px),
+              children = listOf(Row(style = Style.size(width = 1.px, height = 1.px).wrapInView())))
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost>()
+            })
+  }
+
+  private fun assertHasColorDrawableOfColor(componentHost: ComponentHost, color: Int) {
+    assertThat(componentHost.drawables).hasSize(1).first().isInstanceOf(MatrixDrawable::class.java)
+    assertThat((componentHost.drawables[0] as MatrixDrawable<ColorDrawable>).mountedDrawable)
+        .isInstanceOf(ColorDrawable::class.java)
+        .extracting("color")
+        .containsExactly(color)
+  }
+}
