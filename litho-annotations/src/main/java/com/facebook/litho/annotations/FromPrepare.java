d8b9e17fb8f3d8d882f1d059fe24bf630d115928
Sidharth Guglani <sidharthguglani@fb.com>
2021-03-29 04:26:20 -0700

    Add docs for FromPrepare and FromBoundsDefined
    Summary: Add docs for FromPrepare and FromBoundsDefined
    
    Reviewed By: adityasharat
    
    Differential Revision: D27360646
    
    fbshipit-source-id: 7b10a4095ded35d3436a8ba186a8fec32df61009

--- litho-annotations/src/main/java/com/facebook/litho/annotations/FromPrepare.java
+++ litho-annotations/src/main/java/com/facebook/litho/annotations/FromPrepare.java
@@ -19,5 +19,63 @@ package com.facebook.litho.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * {@code FromPrepare} is used to pass objects from the {@link OnPrepare} lifecycle methods of
+ * {@link MountSpec} to lifecycle methods called successively such as {@link OnMount} or {@link
+ * OnBind} method, use {@code FromPrepare} with the same type and name to retrieve your previously
+ * set
+ *
+ * <p>To use it, simply declare a parameter of type {@code com.facebook.litho.Output<>} within the
+ * method annotated with {@link OnPrepare}. {@code com.facebook.litho.Output<>} accepts a generic
+ * type which should be the type of the object you want to pass around. Then, in a successive
+ * lifecycle method, use {@code FromPrepare} with the same type and name to retrieve your previously
+ * set object.
+ *
+ * <p><em><b>This can be used in:</b></em>
+ *
+ * <ul>
+ *   <li>{@link OnMeasure}
+ *   <li>{@link OnMeasureBaseline}
+ *   <li>{@link OnBoundsDefined}
+ *   <li>{@link OnMount}
+ *   <li>{@link OnBind}
+ *   <li>{@link OnUnbind}
+ *   <li>{@link OnUnmount}
+ *   <li>{@link OnPopulateAccessibilityNode}
+ *   <li>{@link OnPopulateExtraAccessibilityNode}
+ *   <li>{@link GetExtraAccessibilityNodeAt}
+ *   <li>{@link GetExtraAccessibilityNodesCount}
+ * </ul>
+ *
+ * <p>Example:<br>
+ *
+ * <pre><code>{@literal @MountSpec}
+ * public class MyComponentSpec {
+ *
+ *  {@literal @}OnCreateMountContent
+ *   MyDrawable onCreateMountContent(Context context) {
+ *     return new MyDrawable(c);
+ *   }
+ *
+ *
+ *  {@literal @}OnPrepare
+ *   void onPrepare(
+ *       ComponentContext c,
+ *       Output&lt;MyFromPrepareObject&gt; fromPrepareObject) {
+ *     MyFromPrepareObject myFromPrepareObject = new MyFromPrepareObject();
+ *     fromPrepareObject.set(myFromPrepareObject);
+ *   }
+ *
+ *  {@literal @}OnMount
+ *   void onMount(
+ *       ComponentContext c,
+ *       MyDrawable myDrawable,
+ *       {@literal @}FromPrepare MyFromPrepareObject fromPrepareObject) {
+ *     fromPrepareObject.doSomething();
+ *   }
+ *
+ * }
+ * </code></pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FromPrepare {}
