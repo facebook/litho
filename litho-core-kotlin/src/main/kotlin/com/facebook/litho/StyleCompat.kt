09e2748a56fcfe950ba7730daaa4d651dcb04dd4
Omer Strulovich <ostrulovich@fb.com>
2021-06-23 08:31:20 -0700

    Add many more methods to StyleCompat
    Summary: Adding more to StyleCompat and copying the tests from Style. This should make things much better for usage now.
    
    Differential Revision: D29275910
    
    fbshipit-source-id: d5571a5b538e1a007f28e2e0860d801e1ada9a94

--- litho-core-kotlin/src/main/kotlin/com/facebook/litho/StyleCompat.kt
+++ litho-core-kotlin/src/main/kotlin/com/facebook/litho/StyleCompat.kt
@@ -21,11 +21,22 @@ import com.facebook.litho.accessibility.accessibilityRole
 import com.facebook.litho.core.height
 import com.facebook.litho.core.heightPercent
 import com.facebook.litho.core.margin
+import com.facebook.litho.core.maxHeight
+import com.facebook.litho.core.maxWidth
+import com.facebook.litho.core.minHeight
+import com.facebook.litho.core.minWidth
+import com.facebook.litho.core.padding
 import com.facebook.litho.core.width
 import com.facebook.litho.core.widthPercent
+import com.facebook.litho.flexbox.alignSelf
+import com.facebook.litho.flexbox.border
 import com.facebook.litho.flexbox.flex
+import com.facebook.litho.flexbox.position
+import com.facebook.litho.flexbox.positionType
 import com.facebook.litho.view.wrapInView
+import com.facebook.yoga.YogaAlign
 import com.facebook.yoga.YogaEdge
+import com.facebook.yoga.YogaPositionType
 import com.facebook.yoga.YogaWrap
 
 /**
@@ -56,6 +67,15 @@ object StyleCompat {
   /** @see [JavaStyle.flexShrink] */
   @JvmStatic fun flexShrink(value: Float): JavaStyle = JavaStyle().flexShrink(value)
 
+  /** @see [JavaStyle.flexGrow] */
+  @JvmStatic fun flexGrow(value: Float): JavaStyle = JavaStyle().flexGrow(value)
+
+  /** @see [JavaStyle.flexBasisDip]] */
+  @JvmStatic fun flexBasisPx(value: Float): JavaStyle = JavaStyle().flexBasisDip(value)
+
+  /** @see [JavaStyle.flexBasisPx] */
+  @JvmStatic fun flexBasisPx(value: Int): JavaStyle = JavaStyle().flexBasisPx(value)
+
   /** @see [JavaStyle.widthDip] */
   @JvmStatic fun widthDip(value: Float): JavaStyle = JavaStyle().widthDip(value)
 
@@ -74,10 +94,57 @@ object StyleCompat {
   /** @see [JavaStyle.heightPercent] */
   @JvmStatic fun heightPercent(value: Float): JavaStyle = JavaStyle().heightPercent(value)
 
+  /** @see [JavaStyle.maxWidthDip] */
+  @JvmStatic fun maxWidthDip(value: Float): JavaStyle = JavaStyle().maxWidthDip(value)
+
+  /** @see [JavaStyle.maxHeightDip] */
+  @JvmStatic fun maxHeightDip(value: Float): JavaStyle = JavaStyle().maxHeightDip(value)
+
+  /** @see [JavaStyle.maxWidthPx] */
+  @JvmStatic fun maxWidthPx(value: Int): JavaStyle = JavaStyle().maxWidthPx(value)
+
+  /** @see [JavaStyle.maxHeightPx] */
+  @JvmStatic fun maxHeightPx(value: Int): JavaStyle = JavaStyle().maxHeightPx(value)
+
+  /** @see [JavaStyle.minWidthDip] */
+  @JvmStatic fun minWidthDip(value: Float): JavaStyle = JavaStyle().minWidthDip(value)
+
+  /** @see [JavaStyle.minHeightDip] */
+  @JvmStatic fun minHeightDip(value: Float): JavaStyle = JavaStyle().minHeightDip(value)
+
+  /** @see [JavaStyle.minWidthPx] */
+  @JvmStatic fun minWidthPx(value: Int): JavaStyle = JavaStyle().minWidthPx(value)
+
+  /** @see [JavaStyle.minHeightPx] */
+  @JvmStatic fun minHeightPx(value: Int): JavaStyle = JavaStyle().minHeightPx(value)
+
   /** @see [JavaStyle.marginDip] */
   @JvmStatic
   fun marginDip(yogaEdge: YogaEdge, value: Float): JavaStyle =
       JavaStyle().marginDip(yogaEdge, value)
+
+  /** @see [JavaStyle.marginPx] */
+  @JvmStatic
+  fun marginPx(yogaEdge: YogaEdge, value: Int): JavaStyle = JavaStyle().marginPx(yogaEdge, value)
+
+  /** @see [JavaStyle.paddingDip] */
+  @JvmStatic
+  fun paddingDip(yogaEdge: YogaEdge, value: Float): JavaStyle =
+      JavaStyle().paddingDip(yogaEdge, value)
+
+  /** @see [JavaStyle.paddingPx] */
+  @JvmStatic
+  fun paddingPx(yogaEdge: YogaEdge, value: Int): JavaStyle = JavaStyle().paddingPx(yogaEdge, value)
+
+  /** @see [JavaStyle.positionDip] */
+  @JvmStatic
+  fun positionDip(yogaEdge: YogaEdge, value: Float): JavaStyle =
+      JavaStyle().positionDip(yogaEdge, value)
+
+  /** @see [JavaStyle.positionPx] */
+  @JvmStatic
+  fun positionPx(yogaEdge: YogaEdge, value: Int): JavaStyle =
+      JavaStyle().positionPx(yogaEdge, value)
 
   /** @see [JavaStyle.wrapInView] */
   @JvmStatic fun wrapInView(): JavaStyle = JavaStyle().wrapInView()
@@ -86,6 +153,15 @@ object StyleCompat {
   @JvmStatic
   fun accessibilityRole(@AccessibilityRoleType role: String): JavaStyle =
       JavaStyle().accessibilityRole(role)
+
+  /** @see [JavaStyle.alignSelf] */
+  @JvmStatic fun alignSelf(value: YogaAlign): JavaStyle = JavaStyle().alignSelf(value)
+
+  /** @see [JavaStyle.positionType] */
+  @JvmStatic fun positionType(value: YogaPositionType): JavaStyle = JavaStyle().positionType(value)
+
+  /** @see [JavaStyle.border] */
+  @JvmStatic fun border(border: Border): JavaStyle = JavaStyle().border(border)
 }
 
 class JavaStyle {
@@ -94,6 +170,21 @@ class JavaStyle {
 
   fun flexShrink(value: Float): JavaStyle {
     style = style.flex(shrink = value)
+    return this
+  }
+
+  fun flexGrow(value: Float): JavaStyle {
+    style = style.flex(grow = value)
+    return this
+  }
+
+  fun flexBasisDip(value: Float): JavaStyle {
+    style = style.flex(basis = value.dp)
+    return this
+  }
+
+  fun flexBasisPx(value: Int): JavaStyle {
+    style = style.flex(basis = value.px)
     return this
   }
 
@@ -127,22 +218,158 @@ class JavaStyle {
     return this
   }
 
+  fun maxWidthDip(value: Float): JavaStyle {
+    style = style.maxWidth(value.dp)
+    return this
+  }
+
+  fun maxHeightDip(value: Float): JavaStyle {
+    style = style.maxHeight(value.dp)
+    return this
+  }
+
+  fun maxWidthPx(value: Int): JavaStyle {
+    style = style.maxWidth(value.px)
+    return this
+  }
+
+  fun maxHeightPx(value: Int): JavaStyle {
+    style = style.maxHeight(value.px)
+    return this
+  }
+
+  fun minWidthDip(value: Float): JavaStyle {
+    style = style.minWidth(value.dp)
+    return this
+  }
+
+  fun minHeightDip(value: Float): JavaStyle {
+    style = style.minHeight(value.dp)
+    return this
+  }
+
+  fun minWidthPx(value: Int): JavaStyle {
+    style = style.minWidth(value.px)
+    return this
+  }
+
+  fun minHeightPx(value: Int): JavaStyle {
+    style = style.minHeight(value.px)
+    return this
+  }
+
   fun wrapInView(): JavaStyle {
     style = style.wrapInView()
     return this
   }
 
+  fun alignSelf(value: YogaAlign): JavaStyle {
+    style = style.alignSelf(value)
+    return this
+  }
+
+  fun positionType(value: YogaPositionType): JavaStyle {
+    style = style.positionType(value)
+    return this
+  }
+
+  fun border(border: Border): JavaStyle {
+    style = style.border(border)
+    return this
+  }
+
   fun marginDip(yogaEdge: YogaEdge, value: Float): JavaStyle {
+    val valueDip = value.dp
     when (yogaEdge) {
-      YogaEdge.LEFT -> style = style.margin(left = value.dp)
-      YogaEdge.VERTICAL -> style = style.margin(vertical = value.dp)
-      YogaEdge.TOP -> style = style.margin(top = value.dp)
-      YogaEdge.RIGHT -> style = style.margin(right = value.dp)
-      YogaEdge.BOTTOM -> style = style.margin(bottom = value.dp)
-      YogaEdge.START -> style = style.margin(start = value.dp)
-      YogaEdge.END -> style = style.margin(end = value.dp)
-      YogaEdge.HORIZONTAL -> style = style.margin(horizontal = value.dp)
-      YogaEdge.ALL -> style = style.margin(all = value.dp)
+      YogaEdge.LEFT -> style = style.margin(left = valueDip)
+      YogaEdge.VERTICAL -> style = style.margin(vertical = valueDip)
+      YogaEdge.TOP -> style = style.margin(top = valueDip)
+      YogaEdge.RIGHT -> style = style.margin(right = valueDip)
+      YogaEdge.BOTTOM -> style = style.margin(bottom = valueDip)
+      YogaEdge.START -> style = style.margin(start = valueDip)
+      YogaEdge.END -> style = style.margin(end = valueDip)
+      YogaEdge.HORIZONTAL -> style = style.margin(horizontal = valueDip)
+      YogaEdge.ALL -> style = style.margin(all = valueDip)
+    }.exhaustive
+    return this
+  }
+
+  fun marginPx(yogaEdge: YogaEdge, value: Int): JavaStyle {
+    val valuePx = value.px
+    when (yogaEdge) {
+      YogaEdge.LEFT -> style = style.margin(left = valuePx)
+      YogaEdge.VERTICAL -> style = style.margin(vertical = valuePx)
+      YogaEdge.TOP -> style = style.margin(top = valuePx)
+      YogaEdge.RIGHT -> style = style.margin(right = valuePx)
+      YogaEdge.BOTTOM -> style = style.margin(bottom = valuePx)
+      YogaEdge.START -> style = style.margin(start = valuePx)
+      YogaEdge.END -> style = style.margin(end = valuePx)
+      YogaEdge.HORIZONTAL -> style = style.margin(horizontal = valuePx)
+      YogaEdge.ALL -> style = style.margin(all = valuePx)
+    }.exhaustive
+    return this
+  }
+
+  fun paddingDip(yogaEdge: YogaEdge, value: Float): JavaStyle {
+    val valueDip = value.dp
+    when (yogaEdge) {
+      YogaEdge.LEFT -> style = style.padding(left = valueDip)
+      YogaEdge.VERTICAL -> style = style.padding(vertical = valueDip)
+      YogaEdge.TOP -> style = style.padding(top = valueDip)
+      YogaEdge.RIGHT -> style = style.padding(right = valueDip)
+      YogaEdge.BOTTOM -> style = style.padding(bottom = valueDip)
+      YogaEdge.START -> style = style.padding(start = valueDip)
+      YogaEdge.END -> style = style.padding(end = valueDip)
+      YogaEdge.HORIZONTAL -> style = style.padding(horizontal = valueDip)
+      YogaEdge.ALL -> style = style.padding(all = valueDip)
+    }.exhaustive
+    return this
+  }
+
+  fun paddingPx(yogaEdge: YogaEdge, value: Int): JavaStyle {
+    val valuePx = value.px
+    when (yogaEdge) {
+      YogaEdge.LEFT -> style = style.padding(left = valuePx)
+      YogaEdge.VERTICAL -> style = style.padding(vertical = valuePx)
+      YogaEdge.TOP -> style = style.padding(top = valuePx)
+      YogaEdge.RIGHT -> style = style.padding(right = valuePx)
+      YogaEdge.BOTTOM -> style = style.padding(bottom = valuePx)
+      YogaEdge.START -> style = style.padding(start = valuePx)
+      YogaEdge.END -> style = style.padding(end = valuePx)
+      YogaEdge.HORIZONTAL -> style = style.padding(horizontal = valuePx)
+      YogaEdge.ALL -> style = style.padding(all = valuePx)
+    }.exhaustive
+    return this
+  }
+
+  fun positionDip(yogaEdge: YogaEdge, value: Float): JavaStyle {
+    val valueDip = value.dp
+    when (yogaEdge) {
+      YogaEdge.LEFT -> style = style.position(left = valueDip)
+      YogaEdge.VERTICAL -> TODO("No matching functionality on Style yet")
+      YogaEdge.TOP -> style = style.position(top = valueDip)
+      YogaEdge.RIGHT -> style = style.position(right = valueDip)
+      YogaEdge.BOTTOM -> style = style.position(bottom = valueDip)
+      YogaEdge.START -> style = style.position(start = valueDip)
+      YogaEdge.END -> style = style.position(end = valueDip)
+      YogaEdge.HORIZONTAL -> TODO("No matching functionality on Style yet")
+      YogaEdge.ALL -> TODO("No matching functionality on Style yet")
+    }.exhaustive
+    return this
+  }
+
+  fun positionPx(yogaEdge: YogaEdge, value: Int): JavaStyle {
+    val valuePx = value.px
+    when (yogaEdge) {
+      YogaEdge.LEFT -> style = style.position(left = valuePx)
+      YogaEdge.VERTICAL -> style = TODO("No matching functionality on Style yet")
+      YogaEdge.TOP -> style = style.position(top = valuePx)
+      YogaEdge.RIGHT -> style = style.position(right = valuePx)
+      YogaEdge.BOTTOM -> style = style.position(bottom = valuePx)
+      YogaEdge.START -> style = style.position(start = valuePx)
+      YogaEdge.END -> style = style.position(end = valuePx)
+      YogaEdge.HORIZONTAL -> style = TODO("No matching functionality on Style yet")
+      YogaEdge.ALL -> style = TODO("No matching functionality on Style yet")
     }.exhaustive
     return this
   }
--- litho-core-kotlin/src/test/kotlin/com/facebook/litho/StyleCompatTest.kt
+++ litho-core-kotlin/src/test/kotlin/com/facebook/litho/StyleCompatTest.kt
@@ -16,13 +16,20 @@
 
 package com.facebook.litho
 
+import android.graphics.Color
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.facebook.litho.flexbox.border
 import com.facebook.litho.testing.LithoViewRule
 import com.facebook.litho.testing.assertMatches
 import com.facebook.litho.testing.child
 import com.facebook.litho.testing.match
 import com.facebook.litho.testing.setRoot
 import com.facebook.litho.testing.unspecified
+import com.facebook.yoga.YogaAlign
+import com.facebook.yoga.YogaEdge
+import com.facebook.yoga.YogaPositionType
+import com.facebook.yoga.YogaWrap
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -56,5 +63,353 @@ class StyleCompatTest {
               bounds(0, 0, 100, 100)
               child<ComponentHost> { bounds(0, 0, 50, 50) }
             })
+  }
+
+  @Test
+  fun maxWidth_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.heightPx(100).maxWidthPx(200).build()) {
+            child(Row(style = StyleCompat.widthPx(500).build()))
+          }
+        }
+        .assertMatches(match<LithoView> { bounds(0, 0, 200, 100) })
+  }
+
+  @Test
+  fun minWidth_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot { Row(style = StyleCompat.heightPx(100).minWidthPx(200).build()) }
+        .assertMatches(match<LithoView> { bounds(0, 0, 200, 100) })
+  }
+
+  @Test
+  fun maxHeight_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.widthPx(100).maxHeightPx(200).build()) {
+            child(Row(style = StyleCompat.heightPx(500).build()))
+          }
+        }
+        .assertMatches(match<LithoView> { bounds(0, 0, 100, 200) })
+  }
+
+  @Test
+  fun minHeight_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot { Row(style = StyleCompat.widthPx(100).minHeightPx(200).build()) }
+        .assertMatches(match<LithoView> { bounds(0, 0, 100, 200) })
+  }
+
+  @Test
+  fun flexBasis_whenSet_becomesChildWidth() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(Row(style = StyleCompat.flexBasisPx(50).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 50, 100) }
+            })
+  }
+
+  @Test
+  fun flexGrow_whenSet_childTakesWholeSpace() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(Row(style = StyleCompat.flexGrow(1f).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 100, 100) }
+            })
+  }
+
+  @Test
+  fun flexShrink_whenSet_makesChildAsSmallAsPossible() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.heightPx(100)
+                            .minWidthPx(50)
+                            .flexShrink(1f)
+                            .wrapInView()
+                            .build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 50, 100) }
+            })
+  }
+
+  @Test
+  fun alignSelf_whenSet_isRespected() {
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.widthPx(100).alignSelf(YogaAlign.STRETCH).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(0, 0, 100, 100) }
+            })
+  }
+
+  @Test
+  fun padding_whenGranularPaddingSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style =
+                  StyleCompat.widthPx(100)
+                      .heightPx(100)
+                      .paddingPx(YogaEdge.START, left)
+                      .paddingPx(YogaEdge.TOP, top)
+                      .paddingPx(YogaEdge.END, right)
+                      .paddingPx(YogaEdge.BOTTOM, bottom)
+                      .build()) {
+            child(Row(style = StyleCompat.flexGrow(1f).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  @Test
+  fun padding_whenHorizontalVerticalPaddingSet_isRespected() {
+    val horizontal = 10
+    val vertical = 20
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style =
+                  StyleCompat.widthPx(100)
+                      .heightPx(100)
+                      .paddingPx(YogaEdge.HORIZONTAL, horizontal)
+                      .paddingPx(YogaEdge.VERTICAL, vertical)
+                      .build()) {
+            child(Row(style = StyleCompat.flexGrow(1f).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(horizontal, vertical, 100 - 2 * horizontal, 100 - 2 * vertical)
+              }
+            })
+  }
+
+  @Test
+  fun padding_whenAllPaddingSet_isRespected() {
+    val padding = 32
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style =
+                  StyleCompat.widthPx(100).heightPx(100).paddingPx(YogaEdge.ALL, padding).build()) {
+            child(Row(style = StyleCompat.flexGrow(1f).wrapInView().build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(padding, padding, 100 - 2 * padding, 100 - 2 * padding)
+              }
+            })
+  }
+
+  @Test
+  fun margin_whenGranularMarginSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.marginPx(YogaEdge.START, left)
+                            .marginPx(YogaEdge.TOP, top)
+                            .marginPx(YogaEdge.END, right)
+                            .marginPx(YogaEdge.BOTTOM, bottom)
+                            .flexGrow(1f)
+                            .wrapInView()
+                            .build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  @Test
+  fun margin_whenHorizontalVerticalMarginSet_isRespected() {
+    val horizontal = 10
+    val vertical = 20
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.marginPx(YogaEdge.HORIZONTAL, horizontal)
+                            .marginPx(YogaEdge.VERTICAL, vertical)
+                            .flexGrow(1f)
+                            .wrapInView()
+                            .build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> {
+                bounds(horizontal, vertical, 100 - 2 * horizontal, 100 - 2 * vertical)
+              }
+            })
+  }
+
+  @Test
+  fun margin_whenAllMarginSet_isRespected() {
+    val margin = 32
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(
+              alignItems = YogaAlign.STRETCH,
+              style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.marginPx(YogaEdge.ALL, margin)
+                            .flexGrow(1f)
+                            .wrapInView()
+                            .build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(margin, margin, 100 - 2 * margin, 100 - 2 * margin) }
+            })
+  }
+
+  @Test
+  fun position_whenSet_isRespected() {
+    val left = 10
+    val top = 20
+    val right = 30
+    val bottom = 40
+
+    lithoViewRule
+        .setSizeSpecs(unspecified(), unspecified())
+        .setRoot {
+          Row(style = StyleCompat.widthPx(100).heightPx(100).build()) {
+            child(
+                Row(
+                    style =
+                        StyleCompat.positionType(YogaPositionType.ABSOLUTE)
+                            .positionPx(YogaEdge.START, left)
+                            .positionPx(YogaEdge.TOP, top)
+                            .positionPx(YogaEdge.END, right)
+                            .positionPx(YogaEdge.BOTTOM, bottom)
+                            .wrapInView()
+                            .build()))
+          }
+        }
+        .assertMatches(
+            match<LithoView> {
+              bounds(0, 0, 100, 100)
+              child<ComponentHost> { bounds(left, top, 100 - left - right, 100 - top - bottom) }
+            })
+  }
+
+  /**
+   * Test is using [Layout] and [NodeInfo] classes as a workaround for the issue with 'libyoga.so
+   * already loaded in another classloader exception' caused by multiple ClassLoaders trying to load
+   * Yoga when using @Config to specify a different target sdk. See:
+   * https://www.internalfb.com/intern/staticdocs/litho/docs/testing/unit-testing/
+   */
+  @Test
+  fun border_whenSet_isRespected() {
+    class ComponentWithBorder : KComponent() {
+      override fun ComponentScope.render(): Component {
+        return Row(
+            style =
+                StyleCompat.border(
+                        Border.create(context)
+                            .color(YogaEdge.LEFT, Color.BLUE)
+                            .color(YogaEdge.TOP, Color.RED)
+                            .color(YogaEdge.RIGHT, Color.BLACK)
+                            .color(YogaEdge.BOTTOM, Color.WHITE)
+                            .radiusDip(Border.Corner.TOP_LEFT, 5f)
+                            .radiusDip(Border.Corner.TOP_RIGHT, 6f)
+                            .radiusDip(Border.Corner.BOTTOM_RIGHT, 7f)
+                            .radiusDip(Border.Corner.BOTTOM_LEFT, 8f)
+                            .build())
+                    .build())
+      }
+    }
+
+    val node = resolveComponentToNodeForTest(lithoViewRule.context, ComponentWithBorder())
+    assertThat(node.borderColors)
+        .isEqualTo(intArrayOf(Color.BLUE, Color.RED, Color.BLACK, Color.WHITE))
+    assertThat(node.borderRadius).isEqualTo(floatArrayOf(5f, 6f, 7f, 8f))
   }
 }
