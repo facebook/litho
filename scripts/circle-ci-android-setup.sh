fa56fa9a60305b64a14a6c0c545789b6675bfb31
Aditya Sharat <adityasharat@fb.com>
2021-06-29 05:22:21 -0700

    Bump compile SDK version to 30
    Summary: Bump platform version to 30
    
    Reviewed By: colriot
    
    Differential Revision: D29392771
    
    fbshipit-source-id: 0eaf70b4696f3558adcd4e60f9b6e3e4b39554fe

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -78,5 +78,5 @@ function installAndroidSDK {
   echo > "$ANDROID_HOME/licenses/android-sdk-license"
   echo -n 24333f8a63b6825ea9c5514f83c2829b004d1fee > "$ANDROID_HOME/licenses/android-sdk-license"
 
-  installsdk 'platforms;android-29' 'cmake;3.6.4111459' 'build-tools;30.0.2'
+  installsdk 'platforms;android-30' 'cmake;3.6.4111459' 'build-tools;30.0.2'
 }
