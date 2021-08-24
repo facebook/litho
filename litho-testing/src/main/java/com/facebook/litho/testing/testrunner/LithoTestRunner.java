7d0f3612bf9359c1f28c73d802eec6646a9964fb
Andy Street <andrews@fb.com>
2021-08-24 11:21:00 -0700

    Add ability for all Litho unit tests to run under different Litho configurations
    Summary:
    This change makes it so all tests using LithoTestRunner run under default configuration as well as any other configurations specified in getExtraRunConfigurations. For instance, in this diff we add a StatelessComponents run configuration.
    
    The purpose is so that as we iterate on framework changes, we make sure our unit test suite is passing for configs we plan to test in production automatically.
    
    Reviewed By: adityasharat
    
    Differential Revision: D30511504
    
    fbshipit-source-id: c65a044a1e5100df095922a1b578d87986f5999e

--- litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
@@ -16,18 +16,25 @@
 
 package com.facebook.litho.testing.testrunner;
 
+import androidx.annotation.Nullable;
 import com.facebook.litho.ComponentsSystrace;
 import com.facebook.litho.config.ComponentsConfiguration;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.DefaultTestLifecycle;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.TestLifecycle;
 import org.robolectric.annotation.Config;
+import org.robolectric.internal.bytecode.Sandbox;
+import org.robolectric.util.ReflectionHelpers;
 
 public class LithoTestRunner extends RobolectricTestRunner {
   /**
@@ -54,9 +61,121 @@ public class LithoTestRunner extends RobolectricTestRunner {
     return super.buildGlobalConfig();
   }
 
+  /**
+   * @return a list of LithoTestRunConfiguration that the test suite should also be run with. The
+   *     test suite will always be run without any extra configurations in addition to these run
+   *     configurations.
+   */
+  private List<? extends Class<? extends LithoTestRunConfiguration>> getExtraRunConfigurations() {
+    return Arrays.asList(StatelessTestRunConfiguration.class);
+  }
+
+  @Override
+  protected List<FrameworkMethod> getChildren() {
+    final List<FrameworkMethod> children = super.getChildren();
+    final List<? extends Class<? extends LithoTestRunConfiguration>> extraRunConfigurations =
+        getExtraRunConfigurations();
+    if (extraRunConfigurations.isEmpty()) {
+      return children;
+    }
+
+    // Add instances of the method to run for each extra run configuration we have
+    final ArrayList<FrameworkMethod> res = new ArrayList<>();
+    for (FrameworkMethod method : children) {
+      res.add(method);
+      for (Class<? extends LithoTestRunConfiguration> configuration : extraRunConfigurations) {
+        res.add(
+            new LithoRobolectricFrameworkMethod(
+                (RobolectricFrameworkMethod) method, configuration));
+      }
+    }
+
+    return res;
+  }
+
+  @SuppressWarnings("CatchGeneralException")
+  @Override
+  protected void beforeTest(Sandbox sandbox, FrameworkMethod method, Method bootstrappedMethod)
+      throws Throwable {
+    super.beforeTest(sandbox, method, bootstrappedMethod);
+
+    // The reason we accept LithoTestRunConfigurations as classes is because we need to load them in
+    // the Sandbox class loader - otherwise they will only be able to modify the
+    // ComponentsConfiguration loaded in the 'app' classloader, meaning changes to it won't be seen
+    // within tests.
+    if (method instanceof LithoRobolectricFrameworkMethod) {
+      LithoRobolectricFrameworkMethod lithoMethod = (LithoRobolectricFrameworkMethod) method;
+      lithoMethod.sandbox = sandbox;
+      lithoMethod.configurationInstance =
+          ReflectionHelpers.newInstance(
+              sandbox.<LithoTestRunConfiguration>bootstrappedClass(lithoMethod.configurationClass));
+
+      // This hack is because LithoTestRunConfiguration is loaded in both the main 'app' classloader
+      // and in the sandbox classloader the test runs with. Normally, we would exclude
+      // LithoTestRunConfiguration by overriding createClassLoaderConfig and in this class using
+      // doNotAcquireClass, however doing leads to the dreaded "Cannot load NodeConfig" error due
+      // to the change in classloader config, thus we hack around it with reflection instead.
+      Class<LithoTestRunner> testRunnerClass =
+          lithoMethod.sandbox.bootstrappedClass(LithoTestRunConfiguration.class);
+      try {
+        testRunnerClass
+            .getMethod("beforeTest", FrameworkMethod.class)
+            .invoke(lithoMethod.configurationInstance, method);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  @SuppressWarnings("CatchGeneralException")
+  @Override
+  protected void afterTest(FrameworkMethod method, Method bootstrappedMethod) {
+    if (method instanceof LithoRobolectricFrameworkMethod) {
+      LithoRobolectricFrameworkMethod lithoMethod = (LithoRobolectricFrameworkMethod) method;
+      Class<LithoTestRunner> testRunnerClass =
+          lithoMethod.sandbox.bootstrappedClass(LithoTestRunConfiguration.class);
+
+      // See comment in beforeTest above
+      try {
+        testRunnerClass
+            .getMethod("afterTest", FrameworkMethod.class)
+            .invoke(lithoMethod.configurationInstance, method);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+      lithoMethod.sandbox = null;
+      lithoMethod.configurationInstance = null;
+    }
+    super.afterTest(method, bootstrappedMethod);
+  }
+
   @Override
   protected Class<? extends TestLifecycle> getTestLifecycleClass() {
     return LithoTestLifecycle.class;
+  }
+
+  private static class LithoRobolectricFrameworkMethod extends RobolectricFrameworkMethod {
+
+    public final Class<? extends LithoTestRunConfiguration> configurationClass;
+
+    // This is meant to be a LithoTestRunConfiguration instance - it isn't because the
+    // LithoTestRunConfiguration that will be the parent class of this instance is from a different
+    // class loader than LithoRobolectricFrameworkMethod will be loaded in.
+    @Nullable Object configurationInstance;
+    @Nullable Sandbox sandbox;
+
+    protected LithoRobolectricFrameworkMethod(
+        RobolectricFrameworkMethod other,
+        Class<? extends LithoTestRunConfiguration> configurationClass) {
+      super(other);
+      this.configurationClass = configurationClass;
+    }
+
+    @SuppressWarnings("ReflectionMethodUse")
+    @Override
+    public String getName() {
+      return super.getName() + "[" + configurationClass.getSimpleName() + "]";
+    }
   }
 
   public static class LithoTestLifecycle extends DefaultTestLifecycle {
