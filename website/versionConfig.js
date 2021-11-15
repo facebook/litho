ffde3d6fb13a64e8905df7d973fe76e7237f97a7
Nicolas Palermo <nipalermo@fb.com>
2021-11-15 10:03:57 -0800

    Release version 0.41.0
    Summary: Release
    
    Reviewed By: colriot
    
    Differential Revision: D32432088
    
    fbshipit-source-id: 3cc8835f9c12ced353e5cd454d3f1763d9c0f898

--- website/versionConfig.js
+++ website/versionConfig.js
@@ -23,7 +23,7 @@
 
 export const site = {
   lithoVersion: '0.41.0',
-  lithoSnapshotVersion: '0.40.1-SNAPSHOT',
+  lithoSnapshotVersion: '0.41.1-SNAPSHOT',
   soloaderVersion: '0.9.0',
   flipperVersion: '0.46.0',
 };
