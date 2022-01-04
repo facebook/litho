2679fb5263ef44eeee0c9f557b464fd2fe5d70fb
Andy Smith <andysmith@fb.com>
2022-01-04 03:52:51 -0800

    Hoisting State
    Summary: New Hoisting State article
    
    Differential Revision: D33161587
    
    fbshipit-source-id: d2dfa9c5b92d3c4d012152b2fa2b045bff811fe3

--- docs/mainconcepts/coordinate-state-actions/hoisting-state.md
+++ docs/mainconcepts/coordinate-state-actions/hoisting-state.md
@@ -2,6 +2,16 @@
 id: hoisting-state
 title: Hoisting State
 ---
-:::note
-🚧   THIS PAGE IS UNDER CONSTRUCTION
-:::
+
+Often, several components need to reflect the same changing value. Rather than a [state](docs/mainconcepts/coordinate-state-actions/state-overview) for each component, it is better to host a single state in their closest common ancestor.
+
+Consider an interface that converts temperatures between Celsius and Fahrenheit, where modifying one value will cause the other to be updated.
+
+Using separate Celsius and Fahrenheit states would be difficult and error prone. It would require complicated book-keeping to keep them syncronised. Furhtermore, these values would represent the same temperature, so there is repetition of data and no single source of truth.
+
+It would be better to make each temperature input component stateless and to use a single state in the parent. This is called "Hoisting State", as state is lifted up from the children into the parent.
+
+A single state can be introduced in the parent to represent the temperature in Celsius. The latest value and an update callback can be passed down as a prop to the inputs. For the Fahrenheit component the temperature conversion formula is applied to the prop and in the update callback. Now all the child components can use and modify the temperature value.
+
+```kotlin file=sample/src/main/java/com/facebook/samples/litho/kotlin/documentation/HoistState.kt  start=start_example end=end_example
+```
---
+++ sample/src/main/java/com/facebook/samples/litho/kotlin/documentation/HoistState.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.samples.litho.kotlin.documentation
+
+import android.view.inputmethod.EditorInfo
+import com.facebook.litho.Column
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentScope
+import com.facebook.litho.Handle
+import com.facebook.litho.KComponent
+import com.facebook.litho.Row
+import com.facebook.litho.eventHandlerWithReturn
+import com.facebook.litho.useEffect
+import com.facebook.litho.useRef
+import com.facebook.litho.useState
+import com.facebook.litho.widget.Text
+import com.facebook.litho.widget.TextInput
+
+// start_example
+class TemperatureConvertor : KComponent() {
+  override fun ComponentScope.render(): Component {
+    // Singe hoisted state
+    val temperatureCelsius = useState { 0.0 }
+
+    return Column {
+      child(
+          TemperatureInput(
+              value = temperatureCelsius.value,
+              scale = TemperatureScale.Celsius,
+              onReturn = { newTemp -> temperatureCelsius.update(newTemp) }))
+      child(
+          TemperatureInput(
+              value = toFahrenheit(temperatureCelsius.value),
+              scale = TemperatureScale.Fahrenheit,
+              onReturn = { newTemp -> temperatureCelsius.update(toCelsius(newTemp)) }))
+    }
+  }
+}
+// end_example
+
+fun toFahrenheit(degreesCelsius: Double): Double = degreesCelsius * 9 / 5 + 32
+
+fun toCelsius(degreesFahrenheit: Double): Double = (degreesFahrenheit - 32) * 5 / 9
+
+enum class TemperatureScale(val unit: Char) {
+  Celsius('C'),
+  Fahrenheit('F');
+
+  val degreeUnit: String = "°$unit"
+}
+
+/** User input for a temperature, including the unit */
+class TemperatureInput(
+    val value: Double,
+    val scale: TemperatureScale,
+    private val onReturn: ((Double) -> Unit)? = null
+) : KComponent() {
+  override fun ComponentScope.render(): Component = Row {
+    child(DoubleInput(value, onReturn))
+    child(Text("${scale.degreeUnit}"))
+  }
+}
+
+/** User input for a Double */
+class DoubleInput(val double: Double, private val onReturn: ((Double) -> Unit)? = null) :
+    KComponent() {
+  override fun ComponentScope.render(): Component? {
+    val handle = useRef { Handle() }.value
+
+    useEffect(double) {
+      TextInput.setText(context, handle, "$double")
+      null
+    }
+
+    return TextInput.create(context)
+        .handle(handle)
+        .inputType(
+            EditorInfo.TYPE_CLASS_NUMBER or
+                EditorInfo.TYPE_NUMBER_FLAG_SIGNED or
+                EditorInfo.TYPE_NUMBER_FLAG_DECIMAL)
+        .editorActionEventHandler(
+            eventHandlerWithReturn {
+              it.view.text.toString().toDoubleOrNull()?.takeIf { it != double }?.let {
+                onReturn?.invoke(it)
+              }
+              true
+            })
+        .flexGrow(1f)
+        .build()
+  }
+}
