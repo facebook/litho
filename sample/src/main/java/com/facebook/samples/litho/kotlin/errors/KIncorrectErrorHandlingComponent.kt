156a26306eaebebcc84e40271e4f8a8598fe3ab5
Michal Karpinski <mkarpinski@fb.com>
2022-01-19 14:23:47 -0800

    Add examples of incorrect error handling with try-catch clauses
    Summary: A few people asked about this, so there's value in having it in documentation - this code will be used to link from docs with additional prose.
    
    Reviewed By: adityasharat
    
    Differential Revision: D33636244
    
    fbshipit-source-id: 435371571f91d5b6f22ee9abb273547a60c1fefc

---
+++ sample/src/main/java/com/facebook/samples/litho/kotlin/errors/KIncorrectErrorHandlingComponent.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.samples.litho.kotlin.errors
+
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentScope
+import com.facebook.litho.KComponent
+import java.lang.Exception
+
+class KIncorrectErrorHandlingComponent : KComponent() {
+
+  override fun ComponentScope.render(): Component {
+    // This implementation won't work - you need to implement error boundary with useErrorBoundary
+    // hook instead.
+    return try {
+      // PossiblyCrashingSubTitleComponent can crash
+      PossiblyCrashingSubTitleComponent.create(context).subtitle("example").build()
+    } catch (e: Exception) {
+      // error handling code
+      KDebugComponent(message = "Error Component", throwable = e)
+    }
+  }
+}
