d835f145e2b4036006e9320ccc7ee45933644879
Michael Bailey <michaelbailey1@fb.com>
2022-02-11 03:35:55 -0800

    Created litho test for collections testing API
    Summary:
    Created a new skeleton test suite for the collections testing API.
    
    currently includes Three tests:
    - Two testing RecyclerView and sections.
    - One testing collections (LazyList)
    
    Reviewed By: mihaelao
    
    Differential Revision: D34042537
    
    fbshipit-source-id: 3510c7f44231a38f85f02821b8a374914d52256a

---
+++ litho-core-kotlin/src/test/kotlin/com/facebook/litho/examples/LithoViewRuleListExampleTest.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.examples
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentScope
+import com.facebook.litho.KComponent
+import com.facebook.litho.testing.LithoViewRule
+import com.facebook.litho.testing.exactly
+import com.facebook.litho.widget.Text
+import com.facebook.litho.widget.collection.LazyList
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.LooperMode
+
+/** Examples of LithoViewRuleList usage */
+@LooperMode(LooperMode.Mode.LEGACY)
+@RunWith(AndroidJUnit4::class)
+class LithoViewRuleListExampleTest {
+
+  @Rule @JvmField val lithoViewRule = LithoViewRule()
+
+  @Test
+  fun `test child is present in LazyList`() {
+    class TestComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        return LazyList() {
+          for (i in 1..10) {
+            child(id = i, component = Text("child$i"))
+          }
+        }
+      }
+    }
+
+    val testView =
+        lithoViewRule.render(widthPx = exactly(100), heightPx = exactly(100)) { TestComponent() }
+
+    assertThat(testView).isNotNull()
+    assertThat(testView.findAllComponents(Text::class)).isEmpty()
+  }
+
+  @Test
+  fun `test child is present in LazyList with multiple component types`() {
+
+    data class Item(val id: Int, val name: String, val quantity: Int)
+
+    val items =
+        listOf(
+            Item(1, "Flour", 2),
+            Item(2, "Cinnamon", 3),
+            Item(3, "Salt", 100),
+            Item(4, "Butter", 1),
+        )
+
+    class ItemComponent(val model: Item) : KComponent() {
+      override fun ComponentScope.render(): Component? {
+        return Text("${model.name}: ${model.quantity}")
+      }
+    }
+
+    class TestComponent : KComponent() {
+      override fun ComponentScope.render(): Component? {
+        return LazyList() {
+          child(Text("Item List"))
+          for (item in items) {
+            child(id = item.id, component = ItemComponent(item))
+          }
+        }
+      }
+    }
+
+    val testView =
+        lithoViewRule.render(widthPx = exactly(100), heightPx = exactly(100)) { TestComponent() }
+
+    assertThat(testView.findViewWithTextOrNull("Flour: 2")).isNotNull()
+    assertThat(testView.findAllComponents(Text::class))
+        // this should return empty for now.
+        .isEmpty()
+  }
+}
