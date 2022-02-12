c592b63bdaa28dc13b6a77d07b181051e6a1720b
Richard Zadorozny <rz@fb.com>
2022-02-11 16:31:42 -0800

    Litho Kotlin Coroutines 3: useProducer
    Summary:
    `useProducer` launches a coroutine whose purpose is to update a state value. It combines `useState` with something like `produce` in Kotlin except instead of sending values to a Channel, it updates a State.
    
    This allows any coroutine to run and update some Litho `State` value, causing a component to re-render. Only the resulting value is available to the component. The update methods are only available to the coroutine.
    
    This will be also be used for upcoming `useFlow`, where the producer coroutine collects a flow to produce state.
    
    Reviewed By: astreet
    
    Differential Revision: D34055918
    
    fbshipit-source-id: b3f8a641509031e353158e034c3ecf799690ff3d

---
+++ litho-coroutines-kotlin/src/main/kotlin/com/facebook/litho/UseProducer.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import com.facebook.litho.annotations.Hook
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.CoroutineScope
+
+interface StateProducerScope<T> : CoroutineScope {
+  fun update(newValue: T)
+  fun update(newValueFunction: (T) -> T)
+  fun updateSync(newValue: T)
+  fun updateSync(newValueFunction: (T) -> T)
+}
+
+private class StateProducerScopeImpl<T>(
+    private val state: State<T>,
+    override var coroutineContext: CoroutineContext = EmptyCoroutineContext,
+) : StateProducerScope<T> {
+
+  override fun update(newValue: T) {
+    state.update(newValue)
+  }
+
+  override fun update(newValueFunction: (T) -> T) {
+    state.update(newValueFunction)
+  }
+
+  override fun updateSync(newValue: T) {
+    state.updateSync(newValue)
+  }
+
+  override fun updateSync(newValueFunction: (T) -> T) {
+    state.updateSync(newValueFunction)
+  }
+}
+
+/**
+ * Creates a value that is updatable by a given [producer] coroutine. Whenever producer updates the
+ * value, the component will be re-rendered.
+ *
+ * The [producer] will be re-run whenever the given keys change.
+ *
+ * The [producer] is run in the CoroutineScope specified by [getTreeProp].
+ *
+ * @return the latest value given by the [producer], or by [initialValue].
+ */
+@Hook
+fun <T> ComponentScope.useProducer(
+    initialValue: () -> T,
+    vararg keys: Any?,
+    producer: suspend StateProducerScope<T>.() -> Unit,
+): T {
+  val state = useState { initialValue() }
+  val producerScope = useCached { StateProducerScopeImpl(state) }
+  useCoroutine(*keys) {
+    producerScope.coroutineContext = coroutineContext
+    producerScope.producer()
+  }
+  return state.value
+}
---
+++ litho-coroutines-kotlin/src/test/kotlin/com/facebook/litho/UseProducerTest.kt
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.facebook.litho.testing.LithoViewRule
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.setMain
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/** Unit tests for [useProducer]. */
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(AndroidJUnit4::class)
+class UseProducerTest {
+
+  @Rule @JvmField val lithoViewRule = LithoViewRule()
+
+  private val testDispatcher = TestCoroutineDispatcher()
+
+  @Before
+  fun setUp() {
+    testDispatcher.pauseDispatcher()
+    Dispatchers.setMain(testDispatcher)
+  }
+
+  @Test
+  fun `produces state`() {
+    val stateRef = AtomicReference<Boolean>()
+    val stateFlow = MutableStateFlow(false)
+
+    class UseStateProducerComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        val value = useProducer(initialValue = { false }, Unit) { stateFlow.collect { update(it) } }
+        stateRef.set(value)
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateProducerComponent() }
+    lithoViewRule.act(lithoView) {
+      // Make sure flow starts collecting, so that emissions aren't dropped.
+      testDispatcher.runCurrent()
+      stateFlow.value = true
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isTrue
+  }
+
+  @Test
+  fun `produces state synchronously`() {
+    val stateRef = AtomicReference<Boolean>()
+    val stateFlow = MutableStateFlow(false)
+
+    class UseStateProducerComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        val value =
+            useProducer(initialValue = { false }, Unit) { stateFlow.collect { updateSync(it) } }
+        stateRef.set(value)
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateProducerComponent() }
+    lithoViewRule.act(lithoView) {
+      testDispatcher.runCurrent()
+      stateFlow.value = true
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isTrue
+  }
+
+  @Test
+  fun `produces state updates based on current value`() {
+    val stateRef = AtomicInteger()
+    val sharedFlow = MutableSharedFlow<Unit>(extraBufferCapacity = 1)
+
+    class UseStateProducerComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        val value =
+            useProducer(initialValue = { 0 }, Unit) { sharedFlow.collect { update { it + 1 } } }
+        stateRef.set(value)
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateProducerComponent() }
+    lithoViewRule.act(lithoView) {
+      testDispatcher.runCurrent()
+      sharedFlow.tryEmit(Unit)
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isEqualTo(1)
+
+    lithoViewRule.act(lithoView) {
+      sharedFlow.tryEmit(Unit)
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isEqualTo(2)
+  }
+
+  @Test
+  fun `produces state updates synchronously based on current value`() {
+    val stateRef = AtomicInteger()
+    val sharedFlow = MutableSharedFlow<Unit>(extraBufferCapacity = 1)
+
+    class UseStateProducerComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        val value =
+            useProducer(initialValue = { 0 }, Unit) { sharedFlow.collect { updateSync { it + 1 } } }
+        stateRef.set(value)
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateProducerComponent() }
+    lithoViewRule.act(lithoView) {
+      testDispatcher.runCurrent()
+      sharedFlow.tryEmit(Unit)
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isEqualTo(1)
+
+    lithoView.setRoot(UseStateProducerComponent())
+    lithoViewRule.act(lithoView) {
+      sharedFlow.tryEmit(Unit)
+      testDispatcher.runCurrent()
+    }
+
+    assertThat(stateRef.get()).isEqualTo(2)
+  }
+
+  @Test
+  fun `producer restarts on key change`() {
+    val events = mutableListOf<String>()
+
+    class UseStateCounterComponent(private val dep: Int) : KComponent() {
+      override fun ComponentScope.render(): Component {
+        useProducer(initialValue = { 0 }, dep) {
+          events.add("produce $dep")
+          try {
+            awaitCancellation()
+          } finally {
+            events.add("cancel $dep")
+          }
+        }
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateCounterComponent(dep = 0) }
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).containsExactly("produce 0")
+    events.clear()
+
+    lithoView.setRoot(UseStateCounterComponent(dep = 0))
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).isEmpty()
+
+    lithoView.setRoot(UseStateCounterComponent(dep = 1))
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).containsExactly("cancel 0", "produce 1")
+  }
+
+  @Test
+  fun `restart producer when one of many keys change`() {
+    val events = mutableListOf<String>()
+
+    class UseStateCounterComponent(private val dep1: Int, private val dep2: Int) : KComponent() {
+      override fun ComponentScope.render(): Component {
+        useProducer(initialValue = { 0 }, dep1, dep2) {
+          events.add("produce $dep1:$dep2")
+          try {
+            awaitCancellation()
+          } finally {
+            events.add("cancel $dep1:$dep2")
+          }
+        }
+        return Row()
+      }
+    }
+
+    val lithoView = lithoViewRule.render { UseStateCounterComponent(dep1 = 1, dep2 = 1) }
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).containsExactly("produce 1:1")
+    events.clear()
+
+    lithoView.setRoot(UseStateCounterComponent(dep1 = 1, dep2 = 1))
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).isEmpty()
+    events.clear()
+
+    lithoView.setRoot(UseStateCounterComponent(dep1 = 100, dep2 = 1))
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).containsExactly("cancel 1:1", "produce 100:1")
+    events.clear()
+
+    lithoView.setRoot(UseStateCounterComponent(dep1 = 100, dep2 = 100))
+    lithoViewRule.act(lithoView) { testDispatcher.runCurrent() }
+
+    assertThat(events).containsExactly("cancel 100:1", "produce 100:100")
+    events.clear()
+  }
+}
