f0eb7aa4caa29fea5a60e9d2006d3b525aad9d1a
Michal Karpinski <mkarpinski@fb.com>
2022-02-17 13:33:11 -0800

    Add SimpleMountable
    Summary: TSIA
    
    Reviewed By: astreet
    
    Differential Revision: D34307637
    
    fbshipit-source-id: 97eeca8743b81eeedf3e4f3bd5bbbe4a1bf176db

---
+++ litho-core-kotlin/src/main/kotlin/com/facebook/litho/SimpleMountable.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.experimental
+
+import android.content.Context
+import com.facebook.litho.Mountable
+import com.facebook.rendercore.RenderUnit
+
+/** Experimental. Currently for Litho team internal use only. */
+abstract class SimpleMountable<ContentT> : Mountable<ContentT> {
+
+  abstract fun mount(c: Context?, content: ContentT, layoutData: Any?)
+
+  abstract fun unmount(c: Context?, content: ContentT, layoutData: Any?)
+
+  abstract fun shouldUpdate(
+      currentMountable: SimpleMountable<ContentT>,
+      newMountable: SimpleMountable<ContentT>,
+      currentLayoutData: Any?,
+      nextLayoutData: Any?
+  ): Boolean
+
+  override fun getBinders(): List<RenderUnit.Binder<*, ContentT>> =
+      BINDER_LIST as List<RenderUnit.Binder<*, ContentT>>
+}
+
+private val BINDER_LIST: List<RenderUnit.Binder<*, *>> =
+    listOf(
+        object : RenderUnit.Binder<SimpleMountable<Any>, Any> {
+          override fun shouldUpdate(
+              currentMountable: SimpleMountable<Any>,
+              newMountable: SimpleMountable<Any>,
+              currentLayoutData: Any?,
+              nextLayoutData: Any?
+          ): Boolean {
+            return newMountable.shouldUpdate(
+                currentMountable, newMountable, currentLayoutData, nextLayoutData)
+          }
+
+          override fun bind(
+              context: Context?,
+              content: Any,
+              mountable: SimpleMountable<Any>,
+              layoutData: Any?
+          ) {
+            mountable.mount(context, content, layoutData)
+          }
+
+          override fun unbind(
+              context: Context,
+              content: Any,
+              mountable: SimpleMountable<Any>,
+              layoutData: Any?
+          ) {
+            mountable.unmount(context, content, layoutData)
+          }
+        })
