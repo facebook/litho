624579eb7e799d35284d5509d339ac823c7744c1
Andy Street <andrews@fb.com>
2022-02-21 10:44:54 -0800

    Make useCallback behave correctly
    Summary:
    The previous implementation of useCallback was quite broken: this diff fixes it as well as updates the tests to ensure that we are re-rendering only the expected number of times.
    
    The approach is for us to have useCallback return a function of the same signature passed into useCallback, but which is a cached object that will maintain referential equality across render passes.
    
    As Kotlin doesn't support decorators, I've had to do this by providing overloads for different function signatures, up to 7 params - from what I can tell, this is a pretty standard solution to this problem in Kotlin.
    
    Reviewed By: andrewpmsmith
    
    Differential Revision: D34178656
    
    fbshipit-source-id: c6088d0302b9fb0da221a5385a76a48b99ad1432

--- litho-core-kotlin/src/test/kotlin/com/facebook/litho/UseCallbackTest.kt
+++ litho-core-kotlin/src/test/kotlin/com/facebook/litho/UseCallbackTest.kt
@@ -20,10 +20,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.facebook.litho.core.height
 import com.facebook.litho.core.width
 import com.facebook.litho.testing.LithoViewRule
-import com.facebook.litho.testing.setRoot
 import com.facebook.litho.view.onClick
 import com.facebook.litho.view.viewTag
 import com.facebook.litho.widget.collection.LazyList
+import java.util.concurrent.atomic.AtomicInteger
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
@@ -37,9 +37,11 @@ class UseCallbackTest {
 
   @Test
   fun useCallbackWithCollection_whenUseCallbackCapturesState_stateInCallbackIsUpToDate() {
+    val renderCount = AtomicInteger(0)
     class CollectionWithSelectedRows(
         private val data: List<Int>,
-        private val onSelectedChanged: (List<Int>) -> Unit
+        private val onSelectedChanged: (List<Int>) -> Unit,
+        private val renderCount: AtomicInteger,
     ) : KComponent() {
       override fun ComponentScope.render(): Component? {
         val selected = useState { listOf<Int>() }
@@ -59,8 +61,10 @@ class UseCallbackTest {
                 id = item,
                 component =
                     CollectionRow(
-                        style =
-                            Style.viewTag("item_$item").onClick { clickCallback.current(item) }))
+                        style = Style.viewTag("item_$item"),
+                        item = item,
+                        onRowClick = clickCallback,
+                        renderCount = renderCount))
           }
         }
       }
@@ -71,31 +75,43 @@ class UseCallbackTest {
         lithoViewRule.render(widthPx = 1000, heightPx = 1000) {
           CollectionWithSelectedRows(
               data = listOf(1, 2, 3, 4, 5),
-              onSelectedChanged = { list -> selectionEvents.add(list) })
+              onSelectedChanged = { list -> selectionEvents.add(list) },
+              renderCount = renderCount)
         }
 
+    assertThat(renderCount.get()).isEqualTo(5)
+
     lithoViewRule.act(lithoView) { clickOnTag("item_1") }
+
+    assertThat(renderCount.get()).isEqualTo(5)
+
     lithoViewRule.act(lithoView) { clickOnTag("item_2") }
 
     assertThat(selectionEvents).containsExactly(listOf(), listOf(1), listOf(1, 2))
+    assertThat(renderCount.get()).isEqualTo(5)
   }
 
   @Test
   fun useCallbackWithCollection_whenUseCallbackCapturesProps_propsAreUpToDate() {
+    val renderCount = AtomicInteger(0)
     class CollectionWithUseCallback(
         private val data: List<Int>,
         private val rowClickedTag: String,
-        private val onRowClick: (String) -> Unit
+        private val onRowClick: (String) -> Unit,
+        private val renderCount: AtomicInteger,
     ) : KComponent() {
       override fun ComponentScope.render(): Component? {
-        val clickCallback = useCallback { clickEvent: ClickEvent -> onRowClick(rowClickedTag) }
+        val clickCallback = useCallback { _: Int -> onRowClick(rowClickedTag) }
         return LazyList {
           data.forEach { item ->
             child(
                 id = item,
                 component =
                     CollectionRow(
-                        style = Style.viewTag("item_$item").onClick { clickCallback.current(it) }))
+                        style = Style.viewTag("item_$item"),
+                        item = item,
+                        onRowClick = clickCallback,
+                        renderCount = renderCount))
           }
         }
       }
@@ -107,77 +123,32 @@ class UseCallbackTest {
           CollectionWithUseCallback(
               data = listOf(1, 2, 3, 4, 5),
               rowClickedTag = "setRoot1",
-              onRowClick = { data -> rowClickedEvents.add(data) })
+              onRowClick = { data -> rowClickedEvents.add(data) },
+              renderCount = renderCount)
         }
+
+    assertThat(renderCount.get()).isEqualTo(5)
 
     lithoViewRule.act(lithoView) { clickOnTag("item_1") }
     lithoViewRule.act(lithoView) { clickOnTag("item_2") }
+
+    assertThat(renderCount.get()).isEqualTo(5)
 
     lithoView.setRoot(
         CollectionWithUseCallback(
             data = listOf(1, 2, 3, 4, 5),
             rowClickedTag = "setRoot2",
-            onRowClick = { data -> rowClickedEvents.add(data) }))
+            onRowClick = { data -> rowClickedEvents.add(data) },
+            renderCount = renderCount))
     lithoViewRule.idle()
 
+    assertThat(renderCount.get()).isEqualTo(5)
+
     lithoViewRule.act(lithoView) { clickOnTag("item_1") }
     lithoViewRule.act(lithoView) { clickOnTag("item_2") }
 
+    assertThat(renderCount.get()).isEqualTo(5)
     assertThat(rowClickedEvents).containsExactly("setRoot1", "setRoot1", "setRoot2", "setRoot2")
-  }
-
-  /**
-   * This isn't exactly desired behavior, but it's behavior we don't want to accidentally change.
-   * Because onIsSameItem/onIsSameContent return true, the second click on item_2 invokes a callback
-   * which has captured the initial state and not the updated state.
-   *
-   * See [useCallbackWithCollection_whenUseCallbackCapturesState_stateInCallbackIsUpToDate] for how
-   * to achieve the desired behavior correctly via [useCallback].
-   */
-  @Test
-  fun collection_whenCallbackCapturesState_stateInCallbackIsStale() {
-    class CollectionWithSelectedRows(
-        private val data: List<Int>,
-        private val onSelectedChanged: (List<Int>) -> Unit
-    ) : KComponent() {
-      override fun ComponentScope.render(): Component? {
-        val selected = useState { listOf<Int>() }
-
-        useEffect(selected) {
-          onSelectedChanged(selected.value)
-          null
-        }
-
-        return LazyList {
-          data.forEach { item ->
-            child(
-                id = item,
-                component =
-                    CollectionRow(
-                        style =
-                            Style.viewTag("item_$item").onClick {
-                              selected.updateSync(selected.value + listOf(item))
-                            }))
-          }
-        }
-      }
-    }
-
-    val selectionEvents = mutableListOf<List<Int>>()
-    val lithoView =
-        lithoViewRule.render(widthPx = 1000, heightPx = 1000) {
-          CollectionWithSelectedRows(
-              data = listOf(1, 2, 3, 4, 5),
-              onSelectedChanged = { list -> selectionEvents.add(list) })
-        }
-
-    lithoViewRule.act(lithoView) { clickOnTag("item_1") }
-    lithoViewRule.act(lithoView) { clickOnTag("item_2") }
-
-    // If we had our desired behavior (see docs on this test case), the assertion would instead be:
-    //   assertThat(selectionEvents).containsExactly(listOf(), listOf(1), listOf(1, 2))
-    // TODO: Fix up this test in next diff
-    assertThat(selectionEvents).containsExactly(listOf(), listOf(1), listOf(1, 2))
   }
 
   /**
@@ -186,9 +157,11 @@ class UseCallbackTest {
    */
   @Test
   fun collection_whenCallbackUsesLambdaStateUpdate_stateInCallbackIsUpToDate() {
+    val renderCount = AtomicInteger(0)
     class CollectionWithSelectedRows(
         private val data: List<Int>,
-        private val onSelectedChanged: (List<Int>) -> Unit
+        private val onSelectedChanged: (List<Int>) -> Unit,
+        private val renderCount: AtomicInteger,
     ) : KComponent() {
       override fun ComponentScope.render(): Component? {
         val selected = useState { listOf<Int>() }
@@ -198,16 +171,20 @@ class UseCallbackTest {
           null
         }
 
+        val onClick = useCallback { item: Int ->
+          selected.updateSync { oldList -> oldList + listOf(item) }
+        }
+
         return LazyList {
           data.forEach { item ->
             child(
                 id = item,
                 component =
                     CollectionRow(
-                        style =
-                            Style.viewTag("item_$item").onClick {
-                              selected.updateSync { oldList -> oldList + listOf(item) }
-                            }))
+                        style = Style.viewTag("item_$item"),
+                        item = item,
+                        onRowClick = onClick,
+                        renderCount = renderCount))
           }
         }
       }
@@ -218,75 +195,29 @@ class UseCallbackTest {
         lithoViewRule.render(widthPx = 1000, heightPx = 1000) {
           CollectionWithSelectedRows(
               data = listOf(1, 2, 3, 4, 5),
-              onSelectedChanged = { list -> selectionEvents.add(list) })
+              onSelectedChanged = { list -> selectionEvents.add(list) },
+              renderCount = renderCount)
         }
+
+    assertThat(renderCount.get()).isEqualTo(5)
 
     lithoViewRule.act(lithoView) { clickOnTag("item_1") }
     lithoViewRule.act(lithoView) { clickOnTag("item_2") }
 
+    assertThat(renderCount.get()).isEqualTo(5)
     assertThat(selectionEvents).containsExactly(listOf(), listOf(1), listOf(1, 2))
-  }
-
-  /**
-   * This isn't exactly desired behavior, but it's behavior we don't want to accidentally change.
-   * Because onIsSameItem/onIsSameContent return true, the second pair of clicks both use the stale
-   * value of `clickEventData` even after the root has been updated.
-   *
-   * See [useCallbackWithCollection_whenUseCallbackCapturesProps_propsAreUpToDate] for how to
-   * achieve the desired behavior correctly via [useCallback].
-   */
-  @Test
-  fun collection_whenCallbackCapturesProps_propInCallbackIsStale() {
-    class CollectionWithSelectedRows(
-        private val data: List<Int>,
-        private val rowClickedTag: String,
-        private val onRowClick: (String) -> Unit
-    ) : KComponent() {
-      override fun ComponentScope.render(): Component? {
-        return LazyList {
-          data.forEach { item ->
-            child(
-                id = item,
-                component =
-                    CollectionRow(
-                        style = Style.viewTag("item_$item").onClick { onRowClick(rowClickedTag) }))
-          }
-        }
-      }
-    }
-
-    val onRowClickEvents = mutableListOf<String>()
-    val lithoView =
-        lithoViewRule.render(widthPx = 1000, heightPx = 1000) {
-          CollectionWithSelectedRows(
-              data = listOf(1, 2, 3, 4, 5),
-              rowClickedTag = "setRoot1",
-              onRowClick = { data -> onRowClickEvents.add(data) })
-        }
-
-    lithoViewRule.act(lithoView) { clickOnTag("item_1") }
-    lithoViewRule.act(lithoView) { clickOnTag("item_2") }
-
-    lithoView.setRoot(
-        CollectionWithSelectedRows(
-            data = listOf(1, 2, 3, 4, 5),
-            rowClickedTag = "setRoot2",
-            onRowClick = { data -> onRowClickEvents.add(data) }))
-    lithoViewRule.idle()
-
-    lithoViewRule.act(lithoView) { clickOnTag("item_1") }
-    lithoViewRule.act(lithoView) { clickOnTag("item_2") }
-
-    // If we had our desired behavior (see docs on this test case), the assertion would instead be:
-    //   assertThat(onRowClickEvents)
-    //       .containsExactly("setRoot1", "setRoot1", "setRoot2", "setRoot2")
-    // TODO: Fix up this test in next diff
-    assertThat(onRowClickEvents).containsExactly("setRoot1", "setRoot1", "setRoot2", "setRoot2")
   }
 }
 
-private class CollectionRow(val style: Style) : KComponent() {
+private class CollectionRow(
+    val style: Style,
+    val item: Int,
+    val onRowClick: ((Int) -> Unit)? = null,
+    val renderCount: AtomicInteger
+) : KComponent() {
   override fun ComponentScope.render(): Component? {
-    return Row(style = Style.width(100.px).height(100.px) + style)
+    renderCount.incrementAndGet()
+    return Row(
+        style = Style.onClick { onRowClick?.invoke(item) }.width(100.px).height(100.px) + style)
   }
 }
