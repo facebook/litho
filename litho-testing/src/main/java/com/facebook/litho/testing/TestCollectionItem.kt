c694e676089f07b085dd5b1935d46fe1da553d27
Michael Bailey <michaelbailey1@fb.com>
2022-03-21 05:34:47 -0700

    Created TestListItemComponent for LazyCollection item testing
    Summary:
    Created a wraper class that holds information about a component managed by the recycler binder.
    
    Currently exposes:
    - its current visibility.
    - if the component will stick to the top of the collection
    - The id of the child in the collection.
    - The component use in the child
    - span information for grids
    
    Reviewed By: andrewpmsmith
    
    Differential Revision: D34820943
    
    fbshipit-source-id: 651754a852754e8abf6e92bc2af7b09da05a392a

---
+++ litho-testing/src/main/java/com/facebook/litho/testing/TestCollectionItem.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.testing
+
+import com.facebook.litho.Component
+import com.facebook.litho.widget.ComponentTreeHolder
+import com.facebook.litho.widget.RecyclerBinder
+
+/**
+ * A wrapper class that exposes methods specific to testing collection children.
+ *
+ * @property componentTreeHolder The [ComponentTreeHolder] that holds information about the child.
+ *
+ * @property index The item's index in the collection.
+ * @property component The [Component] contained in this child.
+ *
+ * @property id The id of the child in the collection.
+ * @property isSticky True if the component can stick to the top of the collection.
+ *
+ * @property isFullSpan True if the component spans the whole grid
+ * @property spanSize Number of columns the component spans in a grid
+ */
+class TestCollectionItem(
+    private val componentTreeHolder: ComponentTreeHolder,
+    val index: Int,
+) {
+
+  private val renderInfo
+    get() = componentTreeHolder.renderInfo
+  private val componentTree
+    get() = componentTreeHolder.componentTree
+
+  val id
+    get() = renderInfo.getCustomAttribute(RecyclerBinder.ID_CUSTOM_ATTR_KEY)
+
+  val component
+    get() = renderInfo.component
+
+  val isSticky
+    get() = renderInfo.isSticky
+
+  val isFullSpan: Boolean
+    get() = renderInfo.isFullSpan
+
+  val spanSize: Int
+    get() = renderInfo.spanSize
+}
