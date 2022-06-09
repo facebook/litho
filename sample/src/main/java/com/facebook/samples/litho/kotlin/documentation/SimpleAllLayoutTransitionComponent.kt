aceee82bf9e7d1e19a984cb7d20875cc7b51a354
Andrew Wang <wys@fb.com>
2022-06-09 04:41:10 -0700

    Animations Kotlin focussed docs - Animation Basics
    Summary: In order to make it Kotlin focused, replace Java code with Kotlin.
    
    Reviewed By: adityasharat
    
    Differential Revision: D36551286
    
    fbshipit-source-id: 196cba3dd52b5093ead9df1856c9cc20f5687c07

---
+++ sample/src/main/java/com/facebook/samples/litho/kotlin/documentation/SimpleAllLayoutTransitionComponent.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.samples.litho.kotlin.documentation
+
+import android.graphics.Color
+import com.facebook.litho.Column
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentScope
+import com.facebook.litho.KComponent
+import com.facebook.litho.Style
+import com.facebook.litho.Transition
+import com.facebook.litho.core.margin
+import com.facebook.litho.dp
+import com.facebook.litho.transition.useTransition
+import com.facebook.litho.useState
+import com.facebook.litho.view.onClick
+import com.facebook.litho.widget.SolidColor
+import com.facebook.yoga.YogaAlign
+
+class SimpleAllLayoutTransitionKComponent : KComponent() {
+
+  // layout_start
+  override fun ComponentScope.render(): Component {
+    val toRight = useState { false }
+    return Column(
+        style = Style.margin(all = 10.dp).onClick { toRight.update { !it } },
+        alignItems = if (toRight.value) YogaAlign.FLEX_END else YogaAlign.FLEX_START) {
+      child(SolidColor.create(context).color(Color.YELLOW).widthDip(80f).heightDip(80f).build())
+    }
+  }
+  // layout_end
+}
+
+class SimpleAllLayoutTransitionKComponentV2 : KComponent() {
+
+  override fun ComponentScope.render(): Component {
+    val toRight = useState { false }
+    // transition_start
+    useTransition(Transition.allLayout())
+    // transition_end
+    return Column(
+        style = Style.margin(all = 10.dp).onClick { toRight.update { !it } },
+        alignItems = if (toRight.value) YogaAlign.FLEX_END else YogaAlign.FLEX_START) {
+      child(SolidColor.create(context).color(Color.YELLOW).widthDip(80f).heightDip(80f).build())
+    }
+  }
+}
