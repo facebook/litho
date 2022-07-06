ebb7542c406b8bb8e34a800b7c8e3709d07b8e29
Fabio Carballo <fabiocarballo@fb.com>
2022-07-06 01:39:00 -0700

    Add more tests to batching scenarios
    Summary: Adds two more tests to verify the batching behavior when a sync update happens in the middle of the batching. I explain the scope of the tests in some inline comments attached to each test.
    
    Reviewed By: astreet
    
    Differential Revision: D37613059
    
    fbshipit-source-id: 972054dcb9c9a5411c08f213d1b6c535cc7eccca

--- litho-core-kotlin/src/test/kotlin/com/facebook/litho/KBatchedStateUpdatesTest.kt
+++ litho-core-kotlin/src/test/kotlin/com/facebook/litho/KBatchedStateUpdatesTest.kt
@@ -16,74 +16,39 @@
 
 package com.facebook.litho
 
-import com.facebook.litho.config.BatchedUpdatesConfiguration
 import com.facebook.litho.config.ComponentsConfiguration
 import com.facebook.litho.core.height
 import com.facebook.litho.core.width
 import com.facebook.litho.kotlin.widget.Text
 import com.facebook.litho.testing.BackgroundLayoutLooperRule
 import com.facebook.litho.testing.LithoViewRule
+import com.facebook.litho.testing.testrunner.LithoTestRunner
 import com.facebook.litho.testing.viewtree.ViewPredicates.hasVisibleText
 import com.facebook.litho.view.onClick
 import com.facebook.litho.view.viewTag
 import java.util.concurrent.atomic.AtomicInteger
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
-import org.robolectric.ParameterizedRobolectricTestRunner
 import org.robolectric.annotation.LooperMode
 import org.robolectric.shadows.ShadowLooper
 
 /** Unit tests for [useState]. */
 @LooperMode(LooperMode.Mode.LEGACY)
-@RunWith(ParameterizedRobolectricTestRunner::class)
+@RunWith(LithoTestRunner::class)
 class KBatchedStateUpdatesTest {
 
-  @Rule @JvmField val lithoViewRule = LithoViewRule()
-  @Rule @JvmField val expectedException = ExpectedException.none()
+  @get:Rule val lithoViewRule = LithoViewRule()
 
-  @Rule
-  @JvmField
+  @get:Rule val expectedException = ExpectedException.none()
+
+  @get:Rule
   val backgroundLayoutLooperRule: BackgroundLayoutLooperRule = BackgroundLayoutLooperRule()
-
-  companion object {
-
-    @JvmStatic
-    @ParameterizedRobolectricTestRunner.Parameters
-    fun configurations(): Collection<Array<Any?>> =
-        listOf(
-            /*
-            This is the case of when we have no batched update strategies. The test is setup so that
-            it will calculate layout once per state update. The expected render count is 3, because
-            there is 1 initial render + 2 update-triggered renders
-             */
-            arrayOf(TestConfig(null, 3)),
-
-            /*
-            From this section below, is the case when we have batched update strategies. In this case it will
-            batch the updates and do only one extra render (besides the initial one) - therefore the
-            expected render count is 2.
-            */
-            arrayOf(TestConfig(BatchedUpdatesConfiguration.POST_TO_FRONT_OF_MAIN_THREAD, 2)),
-            arrayOf(TestConfig(BatchedUpdatesConfiguration.POST_TO_CHOREOGRAPHER_CALLBACK, 2)),
-            arrayOf(TestConfig(BatchedUpdatesConfiguration.BEST_EFFORT_FRONT_AND_CHOREOGRAPHER, 2)),
-        )
-  }
-
-  @ParameterizedRobolectricTestRunner.Parameter lateinit var config: TestConfig
-
-  @After
-  fun tearDown() {
-    ComponentsConfiguration.sBatchedUpdatesConfiguration = null
-  }
 
   @Test
   fun stateUpdates_numberOfRendersTriggeredCorrectly() {
-    ComponentsConfiguration.sBatchedUpdatesConfiguration = config.batchedUpdatesConfiguration
-
     val renderCount = AtomicInteger(0)
 
     class TestComponent : KComponent() {
@@ -98,13 +63,10 @@ class KBatchedStateUpdatesTest {
                   // have to block the main looper while we are queueing updates to make
                   // sure that if any scheduling is posted to the main thread, it only
                   // happens after the work inside the onClick finishes.
-                  ShadowLooper.pauseMainLooper()
-                  counter.update { it + 1 }
-                  backgroundLayoutLooperRule.runToEndOfTasksSync()
-
-                  counter.update { it + 1 }
-                  backgroundLayoutLooperRule.runToEndOfTasksSync()
-                  ShadowLooper.unPauseMainLooper()
+                  withMainLooperPaused {
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                  }
                 }) { child(Text(text = "Counter: ${counter.value}")) }
       }
     }
@@ -117,11 +79,132 @@ class KBatchedStateUpdatesTest {
     backgroundLayoutLooperRule.runToEndOfTasksSync()
 
     lithoViewRule.act(testLithoView) { hasVisibleText("Counter: 2") }
-    assertThat(renderCount.get()).isEqualTo(config.expectedRenderCount)
+    val expectedRenderCount =
+        if (ComponentsConfiguration.sBatchedUpdatesConfiguration != null) {
+          2 /* initial render + render triggered by batched updates */
+        } else {
+          3 /* initial render + 1 render per update */
+        }
+
+    assertThat(renderCount.get()).isEqualTo(expectedRenderCount)
   }
 
-  data class TestConfig(
-      val batchedUpdatesConfiguration: BatchedUpdatesConfiguration?,
-      val expectedRenderCount: Int,
-  )
+  /**
+   * In this test we:
+   * 1. Run two async updates (which are being batched when batching is enabled).
+   * 2. Run on sync update. This sync update will end up consuming the currently batched updates and
+   * trigger one layout calculation.
+   * 3. Then we start another batching with two async updates, which will trigger another layout
+   * calculation.
+   *
+   * The goal of this test is that the batching mechanism still works correctly, even when
+   * interrupted by another sync update.
+   */
+  @Test
+  fun stateUpdates_concurrent_sync_update_consumes_pending_state_updates_and_batching_can_be_resumed() {
+    val renderCount = AtomicInteger(0)
+
+    class TestComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        renderCount.incrementAndGet()
+
+        val counter = useState { 0 }
+
+        return Row(
+            style =
+                Style.height(100.dp).width(100.dp).viewTag("test_view").onClick {
+                  withMainLooperPaused {
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+
+                    /* this sync should consume all pending batched updates when batching is enabled */
+                    executeAndRunPendingBgTasks { counter.updateSync { it + 1 } }
+                  }
+
+                  withMainLooperPaused {
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                  }
+                }) { child(Text(text = "Counter: ${counter.value}")) }
+      }
+    }
+
+    val testLithoView = lithoViewRule.render { TestComponent() }
+    lithoViewRule.act(testLithoView) { hasVisibleText("Counter: 0") }
+    assertThat(renderCount.get()).isEqualTo(1)
+
+    lithoViewRule.act(testLithoView) { clickOnTag("test_view") }
+    backgroundLayoutLooperRule.runToEndOfTasksSync()
+
+    lithoViewRule.act(testLithoView) { hasVisibleText("Counter: 4") }
+    val expectedCount =
+        if (ComponentsConfiguration.sBatchedUpdatesConfiguration != null) {
+          3 /* initial render + render triggered by sync update (which interrupts batching) + render triggered by async update */
+        } else {
+          6 /* initial render + 1 render per update */
+        }
+
+    assertThat(renderCount.get()).isEqualTo(expectedCount)
+  }
+
+  /**
+   * In this test we:
+   *
+   * 1. Execute two async updates (which are being batched when batching is enabled)
+   * 2. Execute one sync update. This sync update actually ends up on consuming the already enqueued
+   * updates.
+   * 3. In this scenario we only have one layout calculation (besides the initial one).
+   *
+   * This test guarantees that we don't do any extra layout calculation due to the batching.
+   */
+  @Test
+  fun stateUpdates_concurrent_sync_update_consumes_all_updates_and_consumes_all_batched_updates() {
+    val renderCount = AtomicInteger(0)
+
+    class TestComponent : KComponent() {
+      override fun ComponentScope.render(): Component {
+        renderCount.incrementAndGet()
+
+        val counter = useState { 0 }
+
+        return Row(
+            style =
+                Style.height(100.dp).width(100.dp).viewTag("test_view").onClick {
+                  withMainLooperPaused {
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+                    executeAndRunPendingBgTasks { counter.update { it + 1 } }
+
+                    /* this sync should consume all pending batched updates when batching is enabled */
+                    executeAndRunPendingBgTasks { counter.updateSync { it + 1 } }
+                  }
+                }) { child(Text(text = "Counter: ${counter.value}")) }
+      }
+    }
+
+    val testLithoView = lithoViewRule.render { TestComponent() }
+    lithoViewRule.act(testLithoView) { hasVisibleText("Counter: 0") }
+    assertThat(renderCount.get()).isEqualTo(1)
+
+    executeAndRunPendingBgTasks { lithoViewRule.act(testLithoView) { clickOnTag("test_view") } }
+
+    lithoViewRule.act(testLithoView) { hasVisibleText("Counter: 3") }
+    val expectedCount =
+        if (ComponentsConfiguration.sBatchedUpdatesConfiguration != null) {
+          2 /* initial render + render triggered by sync update */
+        } else {
+          4 /* initial render + 1 render per update */
+        }
+    assertThat(renderCount.get()).isEqualTo(expectedCount)
+  }
+
+  private fun withMainLooperPaused(body: () -> Unit) {
+    ShadowLooper.pauseMainLooper()
+    body()
+    ShadowLooper.unPauseMainLooper()
+  }
+
+  private fun executeAndRunPendingBgTasks(body: () -> Unit) {
+    body()
+    backgroundLayoutLooperRule.runToEndOfTasksSync()
+  }
 }
