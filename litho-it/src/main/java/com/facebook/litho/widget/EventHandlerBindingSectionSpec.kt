a8235bc51d23b339160a6e6bace424cfd26622a8
Andy Street <andrews@fb.com>
2022-08-02 09:20:07 -0700

    Add unit test to verify EventHandler rebinding logic
    Summary: We were missing a unit test for the end-to-end behavior here. Notice the behavior currently has a bug (reflected in a comment in the test)
    
    Reviewed By: adityasharat
    
    Differential Revision: D38353258
    
    fbshipit-source-id: aac89852a79f170c0dd0525617104afa87f235df

--- litho-it/src/main/java/com/facebook/litho/widget/EventHandlerBindingSectionSpec.kt
+++ litho-it/src/main/java/com/facebook/litho/widget/EventHandlerBindingSectionSpec.kt
@@ -16,6 +16,18 @@
 
 package com.facebook.litho.widget
 
+import androidx.annotation.ColorInt
+import com.facebook.litho.ClickEvent
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.StateValue
+import com.facebook.litho.Wrapper
+import com.facebook.litho.annotations.OnCreateInitialState
+import com.facebook.litho.annotations.OnEvent
+import com.facebook.litho.annotations.OnUpdateState
+import com.facebook.litho.annotations.Param
+import com.facebook.litho.annotations.Prop
+import com.facebook.litho.annotations.State
 import com.facebook.litho.sections.Children
 import com.facebook.litho.sections.SectionContext
 import com.facebook.litho.sections.annotations.GroupSectionSpec
@@ -25,11 +37,65 @@ import com.facebook.litho.sections.common.SingleComponentSection
 @GroupSectionSpec
 object EventHandlerBindingSectionSpec {
 
+  class StateUpdater {
+    internal var sectionContext: SectionContext? = null
+
+    fun updateCounterSync(counter: Int) {
+      sectionContext?.let { EventHandlerBindingSection.updateCounterSync(it, counter) }
+    }
+  }
+
+  fun interface OnButtonClickListener {
+    fun onClick(counter: Int)
+  }
+
+  fun interface ButtonCreator {
+    fun createButton(c: ComponentContext): Component
+  }
+
+  @OnCreateInitialState
+  fun onCreateInitialState(
+      c: SectionContext,
+      counter: StateValue<Int>,
+  ) {
+    counter.set(0)
+  }
+
+  @OnUpdateState
+  fun updateCounter(counter: StateValue<Int>, @Param newCounter: Int) {
+    counter.set(newCounter)
+  }
+
   @OnCreateChildren
-  fun onCreateChildren(c: SectionContext): Children {
+  fun onCreateChildren(
+      c: SectionContext,
+      @Prop stateUpdater: StateUpdater,
+      @Prop buttonCreator: ButtonCreator,
+      @State counter: Int,
+      @State @ColorInt buttonBgColor: Int,
+  ): Children {
+    stateUpdater.sectionContext = c
+
     return Children.create()
         .child(
-            SingleComponentSection.create(c).component(Text.create(c).text("Hello World!").build()))
+            SingleComponentSection.create(c)
+                .component(Text.create(c).text("Counter: $counter").build()))
+        .child(
+            SingleComponentSection.create(c)
+                .component(
+                    Wrapper.create(c)
+                        .delegate(buttonCreator.createButton(c))
+                        .clickHandler(EventHandlerBindingSection.onRowClick(c))
+                        .build()))
         .build()
+  }
+
+  @OnEvent(ClickEvent::class)
+  fun onRowClick(
+      c: SectionContext,
+      @Prop onButtonClickListener: OnButtonClickListener,
+      @State counter: Int
+  ) {
+    onButtonClickListener.onClick(counter)
   }
 }
--- litho-it/src/test/java/com/facebook/litho/EventHandlerRebindTest.kt
+++ litho-it/src/test/java/com/facebook/litho/EventHandlerRebindTest.kt
@@ -16,30 +16,197 @@
 
 package com.facebook.litho
 
+import com.facebook.litho.kotlin.widget.Text
 import com.facebook.litho.sections.SectionContext
 import com.facebook.litho.sections.widget.RecyclerCollectionComponent
 import com.facebook.litho.testing.LithoViewRule
+import com.facebook.litho.testing.assertj.LithoViewAssert
 import com.facebook.litho.testing.testrunner.LithoTestRunner
 import com.facebook.litho.widget.EventHandlerBindingSection
+import com.facebook.litho.widget.EventHandlerBindingSectionSpec
+import java.util.concurrent.atomic.AtomicInteger
+import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.LooperMode
 
 /**
  * Tests to make sure we properly dispatch events to the latest versions of event handlers when
  * eventhandlers are used across trees.
  */
+@LooperMode(LooperMode.Mode.LEGACY)
 @RunWith(LithoTestRunner::class)
 class EventHandlerRebindTest {
 
   @Rule @JvmField val lithoViewRule: LithoViewRule = LithoViewRule()
 
+  /**
+   * This test makes sure that we re-bind EventHandlers even if the particular row the EventHandler
+   * is used in doesn't get re-rendered.
+   *
+   * BACKGROUND
+   *
+   * Say we have a Section which has a button as one of its rows. Say that button invokes a
+   * clickHandler which is defined on the section, and that click handler references section @State
+   * (or @Props). Then, if the section updates state and the only thing that has changed on the
+   * button is the click handler, we don't want to re-render that button. But we do want to make
+   * sure that when it's clicked, we see the latest state. To do this, we have mechanisms in Litho (
+   * [EventHandlersController]) that make sure EventHandlers point to their latest versions.
+   *
+   * THE TESTS
+   *
+   * There are 4 groups of assertions:
+   *
+   * 1. Ensure things are set up properly
+   * 2. Update the counter (@State on the Section): ensure the button doesn't re-render but the
+   * event handler does reflect the latest state.
+   * 3. Cause the button to re-render (by changing the textSize prop) and update the counter: ensure
+   * the button re-renders as expected and that the event handler reflects the latest state again
+   * 4. Update the counter again: ensure the button doesn't re-render but the event handler reflects
+   * the latest state. This is the same as (2) but makes sure things still work after the button
+   * re-renders.
+   */
   @Test
-  fun `test section renders`() {
-    lithoViewRule.render {
-      RecyclerCollectionComponent.create(context)
-          .section(EventHandlerBindingSection.create(SectionContext(context)))
-          .build()
+  fun `test section event handlers update even if row doesn't re-render`() {
+    class RecordingButton(private val numRenders: AtomicInteger, private val textSize: Dimen) :
+        KComponent() {
+      override fun ComponentScope.render(): Component? {
+        numRenders.incrementAndGet()
+        return Text(text = "Click Me", textSize = textSize)
+      }
+    }
+
+    val updater = EventHandlerBindingSectionSpec.StateUpdater()
+    val clickListener = RecordingClickListener()
+    val numButtonRenders = AtomicInteger()
+    val buttonCreator =
+        EventHandlerBindingSectionSpec.ButtonCreator {
+          RecordingButton(numRenders = numButtonRenders, textSize = 10.sp)
+        }
+
+    val testLithoView =
+        lithoViewRule.render(widthPx = 1000, heightPx = 1000) {
+          RecyclerCollectionComponent.create(context)
+              .section(
+                  EventHandlerBindingSection.create(SectionContext(context))
+                      .stateUpdater(updater)
+                      .onButtonClickListener(clickListener)
+                      .buttonCreator(buttonCreator))
+              .build()
+        }
+
+    lithoViewRule.act(testLithoView) { clickOnText("Click Me") }
+
+    // 1. First assertions: just make sure everything is set up properly.
+
+    LithoViewAssert.assertThat(testLithoView.lithoView).hasVisibleText("Counter: 0")
+    assertThat(numButtonRenders.get())
+        .describedAs("There should be one initial render of the button")
+        .isEqualTo(1)
+    assertThat(clickListener.events)
+        .describedAs("There should be one click of the button")
+        .hasSize(1)
+    assertThat(clickListener.events[0])
+        .describedAs("The latest counter is 0, so the event should have value 0")
+        .isEqualTo(0)
+    clickListener.clear()
+
+    // 2. Update the counter (@State on the Section): ensure the button doesn't re-render but the
+    // event handler does reflect the latest state.
+
+    updater.updateCounterSync(1)
+    lithoViewRule.idle()
+    lithoViewRule.act(testLithoView) { clickOnText("Click Me") }
+
+    LithoViewAssert.assertThat(testLithoView.lithoView).hasVisibleText("Counter: 1")
+    assertThat(numButtonRenders.get())
+        .describedAs(
+            "Updating the counter shouldn't cause the button to re-render (if it does, " +
+                "that invalidates what this test is testing, namely that the EventHandlers update " +
+                "even without a row re-rendering)")
+        .isEqualTo(1)
+    assertThat(clickListener.events)
+        .describedAs("There should be one click of the button")
+        .hasSize(1)
+    assertThat(clickListener.events[0])
+        .describedAs("The latest counter is 1, so the event should have value 1")
+        .isEqualTo(1)
+    clickListener.clear()
+
+    // 3. Update the counter and cause the button to re-render (by changing the textSize prop):
+    // ensure the button re-renders as expected and that the event handler reflects the latest
+    // state again
+    updater.updateCounterSync(2)
+    testLithoView.setRoot(
+        RecyclerCollectionComponent.create(lithoViewRule.context)
+            .section(
+                EventHandlerBindingSection.create(SectionContext(lithoViewRule.context))
+                    .stateUpdater(updater)
+                    .onButtonClickListener(clickListener)
+                    .buttonCreator(
+                        EventHandlerBindingSectionSpec.ButtonCreator {
+                          RecordingButton(numRenders = numButtonRenders, textSize = 20.sp)
+                        }))
+            .build())
+    lithoViewRule.idle()
+    lithoViewRule.act(testLithoView) { clickOnText("Click Me") }
+
+    LithoViewAssert.assertThat(testLithoView.lithoView).hasVisibleText("Counter: 2")
+    assertThat(numButtonRenders.get())
+        .describedAs(
+            "Since we updated the button background, it should re-render (if it does not re-render, " +
+                "that invalidates the premise of the rest of this test)")
+        .isEqualTo(2)
+    assertThat(clickListener.events)
+        .describedAs("There should be one click of the button")
+        .hasSize(1)
+    assertThat(clickListener.events[0])
+        .describedAs("The latest counter is 2, so the event should have value 2")
+        .isEqualTo(2)
+    clickListener.clear()
+
+    // 4. Update the counter again: ensure the button doesn't re-render but the event handler
+    // reflects the latest state. This is the same as (2) but makes sure things still work after the
+    // button re-renders.
+
+    updater.updateCounterSync(3)
+    lithoViewRule.idle()
+    lithoViewRule.act(testLithoView) { clickOnText("Click Me") }
+
+    LithoViewAssert.assertThat(testLithoView.lithoView).hasVisibleText("Counter: 3")
+    assertThat(numButtonRenders.get())
+        .describedAs(
+            "Updating the counter shouldn't cause the button to re-render (if it does, " +
+                "that invalidates what this test is testing, namely that the EventHandlers update " +
+                "even without a row re-rendering)")
+        .isEqualTo(2)
+    assertThat(clickListener.events)
+        .describedAs("There should be one click of the button")
+        .hasSize(1)
+
+    // This is the correct behavior, but it's currently broken!
+    // assertThat(clickListener.events[0])
+    //     .describedAs("The latest counter is 3, so the event should have value 3")
+    //     .isEqualTo(3)
+    assertThat(clickListener.events[0])
+        .describedAs("The latest counter is 3, but the code has a bug so we actually get 2")
+        .isEqualTo(2)
+
+    clickListener.clear()
+  }
+
+  private class RecordingClickListener : EventHandlerBindingSectionSpec.OnButtonClickListener {
+    private val _events: MutableList<Int> = mutableListOf()
+    val events: List<Int>
+      get() = _events.toList()
+
+    override fun onClick(counter: Int) {
+      _events.add(counter)
+    }
+
+    fun clear() {
+      _events.clear()
     }
   }
 }
