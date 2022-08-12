e2f46efc9a366bb4fcc1346e7a6f137b6d98ebbe
Sidharth Guglani <sidharthguglani@fb.com>
2022-08-12 10:03:53 -0700

    move hasCompatibleSizeSpec to MeasureComparisonUtils
    Reviewed By: astreet
    
    Differential Revision: D38656399
    
    fbshipit-source-id: 634cd391ca29c3066fc90c2fff91b0c30db98bbf

--- litho-core/src/main/java/com/facebook/litho/Layout.java
+++ litho-core/src/main/java/com/facebook/litho/Layout.java
@@ -445,7 +445,7 @@ class Layout {
     final Component component = node.getTailComponent();
 
     if (currentLayout != null
-        && hasCompatibleSizeSpec(
+        && MeasureComparisonUtils.hasCompatibleSizeSpec(
             currentLayout.getLastWidthSpec(),
             currentLayout.getLastHeightSpec(),
             widthSpec,
@@ -675,7 +675,7 @@ class Layout {
       final boolean hasValidDirection =
           hasValidLayoutDirectionInNestedTree(holderResult, cachedLayout);
       final boolean hasCompatibleSizeSpec =
-          hasCompatibleSizeSpec(
+          MeasureComparisonUtils.hasCompatibleSizeSpec(
               cachedLayout.getLastWidthSpec(),
               cachedLayout.getLastHeightSpec(),
               widthSpec,
@@ -710,34 +710,6 @@ class Layout {
       NestedTreeHolderResult holder, LithoLayoutResult nestedTree) {
     return nestedTree.getNode().isLayoutDirectionInherit()
         || (nestedTree.getResolvedLayoutDirection() == holder.getResolvedLayoutDirection());
-  }
-
-  /**
-   * Check if a cached nested tree has compatible SizeSpec to be reused as is or if it needs to be
-   * recomputed.
-   *
-   * <p>The conditions to be able to re-use previous measurements are: 1) The measureSpec is the
-   * same 2) The new measureSpec is EXACTLY and the last measured size matches the measureSpec size.
-   * 3) The old measureSpec is UNSPECIFIED, the new one is AT_MOST and the old measured size is
-   * smaller that the maximum size the new measureSpec will allow. 4) Both measure specs are
-   * AT_MOST. The old measure spec allows a bigger size than the new and the old measured size is
-   * smaller than the allowed max size for the new sizeSpec.
-   */
-  public static boolean hasCompatibleSizeSpec(
-      final int oldWidthSpec,
-      final int oldHeightSpec,
-      final int newWidthSpec,
-      final int newHeightSpec,
-      final float oldMeasuredWidth,
-      final float oldMeasuredHeight) {
-    final boolean widthIsCompatible =
-        MeasureComparisonUtils.isMeasureSpecCompatible(
-            oldWidthSpec, newWidthSpec, (int) oldMeasuredWidth);
-
-    final boolean heightIsCompatible =
-        MeasureComparisonUtils.isMeasureSpecCompatible(
-            oldHeightSpec, newHeightSpec, (int) oldMeasuredHeight);
-    return widthIsCompatible && heightIsCompatible;
   }
 
   /**
--- litho-core/src/main/java/com/facebook/litho/MeasureComparisonUtils.java
+++ litho-core/src/main/java/com/facebook/litho/MeasureComparisonUtils.java
@@ -28,6 +28,35 @@ import com.facebook.infer.annotation.Nullsafe;
  */
 @Nullsafe(Nullsafe.Mode.LOCAL)
 public class MeasureComparisonUtils {
+
+  /**
+   * Check if a cached nested tree has compatible SizeSpec to be reused as is or if it needs to be
+   * recomputed.
+   *
+   * <p>The conditions to be able to re-use previous measurements are: 1) The measureSpec is the
+   * same 2) The new measureSpec is EXACTLY and the last measured size matches the measureSpec size.
+   * 3) The old measureSpec is UNSPECIFIED, the new one is AT_MOST and the old measured size is
+   * smaller that the maximum size the new measureSpec will allow. 4) Both measure specs are
+   * AT_MOST. The old measure spec allows a bigger size than the new and the old measured size is
+   * smaller than the allowed max size for the new sizeSpec.
+   */
+  static boolean hasCompatibleSizeSpec(
+      final int oldWidthSpec,
+      final int oldHeightSpec,
+      final int newWidthSpec,
+      final int newHeightSpec,
+      final float oldMeasuredWidth,
+      final float oldMeasuredHeight) {
+    final boolean widthIsCompatible =
+        MeasureComparisonUtils.isMeasureSpecCompatible(
+            oldWidthSpec, newWidthSpec, (int) oldMeasuredWidth);
+
+    final boolean heightIsCompatible =
+        MeasureComparisonUtils.isMeasureSpecCompatible(
+            oldHeightSpec, newHeightSpec, (int) oldMeasuredHeight);
+    return widthIsCompatible && heightIsCompatible;
+  }
+
   private static boolean newSizeIsExactAndMatchesOldMeasuredSize(
       int newSizeSpecMode, int newSizeSpecSize, int oldMeasuredSize) {
     return newSizeSpecMode == EXACTLY && newSizeSpecSize == oldMeasuredSize;
