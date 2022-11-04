effa5a98ba9833be177448937959953c7ae3fa75
Fabio Carballo <fabiocarballo@meta.com>
2022-11-04 14:40:04 -0700

    Add modules to develop testing API
    Summary:
    The goal of this is to provide the needed modules where we will build the testing API. This doesn't necessarily reflect the end structure, but for now
    we will develop in this silo module which should not be visible to other buck modules in the organization
    
    Reviewed By: adityasharat
    
    Differential Revision: D40980698
    
    fbshipit-source-id: 842352223cfd9e53b89778d85fd85e54898045d5

---
+++ litho-testing/src/test/java/com/facebook/litho/testing/api/DummyTest.kt
@@ -0,0 +1,14 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+package com.facebook.litho.testing.api
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class DummyTest {
+
+  @Test
+  fun `this is a dummy test`() {
+    assertEquals(true, true)
+  }
+}
