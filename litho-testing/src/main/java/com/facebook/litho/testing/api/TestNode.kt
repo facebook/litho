29e2acbd0f702119f781d7449dcfb5710090793c
Fabio Carballo <fabiocarballo@meta.com>
2022-11-08 10:20:52 -0800

    Add test node and sample filter
    Summary:
    Adds some of the first concepts that we can work up on for the testing API:
    
    - `TestNode`
    - `TestNodeMatcher`
    
    It also adds an example of a simple `Filter` for the scenario of selecting/matching by component type.
    
    **References**
    - [High level diagram](https://www.internalfb.com/excalidraw/EX28769)
    
    Differential Revision: D41081569
    
    fbshipit-source-id: 02a35cf99bbe27178faffd20e6ae3484d6ade67e

---
+++ litho-testing/src/main/java/com/facebook/litho/testing/api/TestNode.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.testing.api
+
+import com.facebook.litho.Component
+
+/**
+ * A test node is used to hold all the information needed to represent a given component in the
+ * testing tree. This information is be used by testing actions (filter, interaction, assertion) and
+ * also to represent them in a visual way (e.g String representation).
+ */
+class TestNode(private val component: Component) {
+
+  val componentType: Class<*> = component::class.java
+}
