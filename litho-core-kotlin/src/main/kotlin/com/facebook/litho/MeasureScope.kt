b858214767e5ae9e3a9eaaa42524f7f50fc9030a
Michal Karpinski <mkarpinski@meta.com>
2022-11-14 05:56:55 -0800

    Add kdocs for MeasureScope
    Summary: TSIA
    
    Reviewed By: apowolny
    
    Differential Revision: D41262063
    
    fbshipit-source-id: c3f40122f5ef65173ea2ae4fdba331c5cfd035b2

--- litho-core-kotlin/src/main/kotlin/com/facebook/litho/MeasureScope.kt
+++ litho-core-kotlin/src/main/kotlin/com/facebook/litho/MeasureScope.kt
@@ -21,6 +21,10 @@ import com.facebook.litho.utils.MeasureUtils
 import com.facebook.rendercore.LayoutContext
 import com.facebook.rendercore.MeasureResult
 
+/**
+ * The scope for the [SimpleMountable] measure method. Provides access to [androidContext],
+ * [previousLayoutData] and utility methods that help return the appropriate [MeasureResult].
+ */
 class MeasureScope(val layoutContext: LayoutContext<*>, val previousLayoutData: Any?) {
 
   val androidContext: Context
@@ -31,6 +35,17 @@ class MeasureScope(val layoutContext: LayoutContext<*>, val previousLayoutData: 
         androidContext, ResourceCache.getLatest(androidContext.getResources().getConfiguration()))
   }
 
+  /**
+   * Measure according to an aspect ratio an width and height constraints. This version of
+   * measureWithAspectRatio will respect the intrinsic size of the component being measured.
+   *
+   * @param widthSpec A SizeSpec for the width
+   * @param heightSpec A SizeSpec for the height
+   * @param intrinsicWidth A pixel value for the intrinsic width of the measured component
+   * @param intrinsicHeight A pixel value for the intrinsic height of the measured component
+   * @param aspectRatio The aspect ration size against
+   * @param outputSize The output size of this measurement
+   */
   fun withAspectRatio(
       widthSpec: Int,
       heightSpec: Int,
@@ -42,12 +57,30 @@ class MeasureScope(val layoutContext: LayoutContext<*>, val previousLayoutData: 
       MeasureUtils.measureWithAspectRatio(
           widthSpec, heightSpec, intrinsicWidth, intrinsicHeight, aspectRatio, outputSize)
 
+  /**
+   * Returns a {@link MeasureResult} to respect both size specs and try to keep both width and
+   * height equal. This will only not guarantee equal width and height if these specs use modes and
+   * sizes which prevent it.
+   */
   fun withEqualSize(widthSpec: Int, heightSpec: Int): MeasureResult =
       MeasureResult.withEqualDimensions(widthSpec, heightSpec, null)
 
+  /**
+   * Returns a {@link MeasureResult} with sizes set based on the provided {@param widthSpec} and
+   * {@param heightSpec}.
+   *
+   * <p>This method should only be used for Mountable Components which do not measure themselves -
+   * it's the parent that has determined the exact size for this child.
+   *
+   * @throws IllegalArgumentException if the widthSpec or heightSpec is not exact
+   */
   fun fromSpecs(widthSpec: Int, heightSpec: Int): MeasureResult =
       MeasureResult.fromSpecs(widthSpec, heightSpec)
 
+  /**
+   * Returns a {@link MeasureResult} that respects both specs and the desired width and height. The
+   * desired size is usually the necessary pixels to render the inner content.
+   */
   fun withExactSize(
       widthSpec: Int,
       heightSpec: Int,
