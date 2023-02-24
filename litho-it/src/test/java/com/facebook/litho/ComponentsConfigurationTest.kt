f3b73d0b671270b11cf12e79075a3c1d3d30825a
Michal Zielinski <zielinskim@meta.com>
2023-02-24 03:51:27 -0800

    Convert ComponentsConfigurationTest to Kotlin
    Summary: As per title
    
    Reviewed By: fabiocarballo
    
    Differential Revision: D43566618
    
    fbshipit-source-id: 3cf6b5dbf3ad2dacdbefbc90289ce94bb38818d4

---
+++ litho-it/src/test/java/com/facebook/litho/ComponentsConfigurationTest.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.facebook.litho.SizeSpec.makeSizeSpec
+import com.facebook.litho.config.ComponentsConfiguration
+import com.facebook.litho.sections.SectionContext
+import com.facebook.litho.sections.common.SingleComponentSection
+import com.facebook.litho.sections.widget.ListRecyclerConfiguration
+import com.facebook.litho.sections.widget.RecyclerBinderConfiguration
+import com.facebook.litho.sections.widget.RecyclerCollectionComponent
+import com.facebook.litho.testing.LegacyLithoViewRule
+import com.facebook.litho.testing.Whitebox
+import com.facebook.litho.testing.testrunner.LithoTestRunner
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.LooperMode
+
+@LooperMode(LooperMode.Mode.LEGACY)
+@RunWith(LithoTestRunner::class)
+class ComponentsConfigurationTest {
+
+  @JvmField @Rule val legacyLithoViewRule = LegacyLithoViewRule()
+
+  private val componentContext =
+      ComponentContext(ApplicationProvider.getApplicationContext<Context>())
+
+  private val defaultBuilder = ComponentsConfiguration.getDefaultComponentsConfigurationBuilder()
+
+  @Test
+  fun testSetFlagThroughComponentConfigToComponentTree() {
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(
+        ComponentsConfiguration.create().useCancelableLayoutFutures(true))
+    val componentTree =
+        ComponentTree.create(componentContext)
+            .componentsConfiguration(ComponentsConfiguration.getDefaultComponentsConfiguration())
+            .build()
+    val componentsConfiguration =
+        Whitebox.getInternalState<ComponentsConfiguration>(
+            componentTree, "mComponentsConfiguration")
+    assertThat(componentsConfiguration.useCancelableLayoutFutures).isTrue
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(defaultBuilder)
+  }
+
+  @Test
+  fun testSetFlagThroughComponentConfigToComponentTreeWithRecyclerCollectionComponent() {
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(
+        ComponentsConfiguration.create().useCancelableLayoutFutures(true))
+    val recyclerBinderConfiguration =
+        RecyclerBinderConfiguration.create()
+            .componentsConfiguration(ComponentsConfiguration.getDefaultComponentsConfiguration())
+            .build()
+    legacyLithoViewRule
+        .setRoot(
+            RecyclerCollectionComponent.create(componentContext)
+                .recyclerConfiguration(
+                    ListRecyclerConfiguration.create()
+                        .recyclerBinderConfiguration(recyclerBinderConfiguration)
+                        .build())
+                .section(
+                    SingleComponentSection.create(SectionContext(componentContext))
+                        .component(
+                            Row.create(componentContext)
+                                .viewTag("rv_row")
+                                .heightDip(100f)
+                                .widthDip(100f))
+                        .build())
+                .build())
+        .setSizeSpecs(makeSizeSpec(10, SizeSpec.EXACTLY), makeSizeSpec(5, SizeSpec.EXACTLY))
+    legacyLithoViewRule.attachToWindow().measure().layout().setSizeSpecs(10, 10)
+    val childView = legacyLithoViewRule.lithoView.findViewWithTag("rv_row") as LithoView?
+    assertThat(childView).isNotNull
+    val componentsConfiguration =
+        Whitebox.getInternalState<ComponentsConfiguration>(
+            childView?.componentTree, "mComponentsConfiguration")
+    assertThat(componentsConfiguration.useCancelableLayoutFutures).isTrue
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(defaultBuilder)
+  }
+
+  @Test
+  fun testOverrideDefaultBuilder() {
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(
+        ComponentsConfiguration.create()
+            .useCancelableLayoutFutures(true)
+            .ignoreNullLayoutStateError(true))
+    assertThat(
+            ComponentsConfiguration.getDefaultComponentsConfiguration().useCancelableLayoutFutures)
+        .isTrue
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(
+        ComponentsConfiguration.create().useCancelableLayoutFutures(false))
+    assertThat(
+            ComponentsConfiguration.getDefaultComponentsConfiguration().useCancelableLayoutFutures)
+        .isFalse
+    assertThat(
+            ComponentsConfiguration.getDefaultComponentsConfiguration().ignoreNullLayoutStateError)
+        .isTrue
+    ComponentsConfiguration.setDefaultComponentsConfigurationBuilder(defaultBuilder)
+  }
+}
