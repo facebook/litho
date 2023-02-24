5b2a18b7abac3e65d01002433e3a63fe9fcdf22d
Sim Sun <simsun@meta.com>
2023-02-24 10:23:55 -0800

    Bump SoLoader version to 0.10.5
    Summary: Use system linker by default on Android N and above devices.
    
    Reviewed By: pentiumao, adityasharat
    
    Differential Revision: D43575419
    
    fbshipit-source-id: 9eebb27d38c18e17c2020103d18e3891db5bed2b

--- website/versionConfig.js
+++ website/versionConfig.js
@@ -24,6 +24,6 @@
 export const site = {
   lithoVersion: '0.47.0',
   lithoSnapshotVersion: '0.48.0-SNAPSHOT',
-  soloaderVersion: '0.10.4',
+  soloaderVersion: '0.10.5',
   flipperVersion: '0.142.0',
 };
