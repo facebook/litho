1fdafc0e2a0a083f921d52c361b93885bccf3567
Michal Zielinski <zielinskim@meta.com>
2023-02-24 04:12:13 -0800

    Add ExperimentalImage widget
    Summary:
    This diff adds an `ExperimentalImage` widget implemented using Primitive API. It'll be used to run an experiment in FB4A app.
    Primitive API is a composable, Kotlin-first replacement for the MountableComponents API. You can read more about it in the RFC: https://fb.quip.com/G3xXAPKmfTWp
    
    Reviewed By: apowolny
    
    Differential Revision: D43496788
    
    fbshipit-source-id: 58585fa12a23ae34eb4d271a7026f8a818ce9ffd

---
+++ litho-widget-kotlin/src/test/kotlin/com/facebook/litho/widget/ExperimentalImageTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.kotlin.widget
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.facebook.litho.MatrixDrawable
+import com.facebook.litho.Style
+import com.facebook.litho.core.height
+import com.facebook.litho.core.width
+import com.facebook.litho.px
+import com.facebook.litho.testing.LithoViewRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.LooperMode
+
+/** Tests for [ExperimentalImage] */
+@LooperMode(LooperMode.Mode.LEGACY)
+@RunWith(AndroidJUnit4::class)
+class ExperimentalImageTest {
+
+  @Rule @JvmField val lithoViewRule = LithoViewRule()
+
+  @Test
+  fun `ExperimentalImage should render`() {
+    lithoViewRule
+        .render {
+          ExperimentalImage(
+              drawable = ColorDrawable(Color.RED),
+              style = Style.width(100.px).height(100.px),
+          )
+        }
+        .apply {
+          // should find an ExperimentalImage in the tree
+          findComponent(ExperimentalImage::class)
+
+          // should mount an ExperimentalImage
+          assertThat(lithoView.mountItemCount).isEqualTo(1)
+
+          // content of ExperimentalImage should be a MatrixDrawable
+          val content = lithoView.getMountItemAt(0).content as MatrixDrawable<*>
+          assertThat(content.bounds.width()).isEqualTo(100)
+          assertThat(content.bounds.height()).isEqualTo(100)
+
+          // Matrix drawable should host a ColorDrawable
+          val drawable = content.mountedDrawable
+          assertThat(drawable).isInstanceOf(ColorDrawable::class.java)
+        }
+  }
+}
