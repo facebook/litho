b55ddd04f0bdefab6daf02aec5dc90be3537ed0a
Michal Zielinski <zielinskim@meta.com>
2023-02-28 23:01:20 -0800

    Convert ComponentTreeDumpingHelperTest to Kotlin
    Summary: As per title
    
    Reviewed By: pentiumao
    
    Differential Revision: D43654292
    
    fbshipit-source-id: 14b2f084b94f934b6fc136969daf055cc3cf2d9e

---
+++ litho-it/src/test/java/com/facebook/litho/ComponentTreeDumpingHelperTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import android.content.Context
+import android.view.View
+import androidx.test.core.app.ApplicationProvider
+import com.facebook.litho.config.ComponentsConfiguration
+import com.facebook.litho.testing.inlinelayoutspec.InlineLayoutSpec
+import com.facebook.litho.testing.testrunner.LithoTestRunner
+import com.facebook.litho.widget.SimpleMountSpecTester
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assume
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(LithoTestRunner::class)
+class ComponentTreeDumpingHelperTest {
+
+  @Before
+  fun skipIfRelease() {
+    Assume.assumeTrue(
+        "These tests cover debug functionality and can only be run for internal builds.",
+        ComponentsConfiguration.IS_INTERNAL_BUILD)
+  }
+
+  @Test
+  fun testBasicComponentTreeDumping() {
+    val component: Component =
+        object : InlineLayoutSpec() {
+          override fun onCreateLayout(c: ComponentContext): Component? =
+              SimpleMountSpecTester.create(c).widthPx(100).heightPx(100).build()
+        }
+    var componentContext: ComponentContext =
+        ComponentContext(ApplicationProvider.getApplicationContext<Context>())
+    val componentTree = ComponentTree.create(componentContext, component).build()
+    componentContext = ComponentContext.withComponentTree(componentContext, componentTree)
+    val lithoView = LithoView(ApplicationProvider.getApplicationContext<Context>())
+    lithoView.componentTree = componentTree
+    lithoView.measure(
+        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+    val string = ComponentTreeDumpingHelper.dumpContextTree(componentTree)
+    assertThat(string)
+        .containsPattern("InlineLayout\\{V}\n  SimpleMountSpecTester\\{V 100.0x100.0}")
+  }
+}
