a9bff0ae2f3aec4e62b9219263efa9c544a25fe1
Fabio Carballo <fabiocarballo@meta.com>
2023-03-06 13:09:30 -0800

    Add infra-structure for CancellationEvaluator and concrete implementation for Resolve
    Summary:
    This is a series of diffs that are focused in providing support for a first iteration of *Resolve* cancellation. In the schema below you can find a high-level overview of the changes required and the end flow.
    
    This flow will also be possible to be used when we move for specific *Layout* cancellation.
    
    {F886248253}
    
    In this specific diff I implement the `CancellationEvaluator` abstraction and the concrete implementation for *Resolve*.
    
    Whenever we would want to support Layout cancellation we would would have to do a similiar diff to this one (and corresponding integration) - but the core mechanism could be the same.
    
    The logic for *Resolve* cancellation follows these lines:
    
    {F886251887}
    
    Reviewed By: adityasharat
    
    Differential Revision: D43618754
    
    fbshipit-source-id: 8bda6c5636795dbb950b58ab7b5de7c5004fb70a

---
+++ litho-core/src/main/java/com/facebook/litho/cancellation/ResolveCancellationPolicy.kt
@@ -0,0 +1,93 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.cancellation
+
+import com.facebook.litho.LayoutState.CalculateLayoutSource
+import com.facebook.litho.cancellation.CancellationPolicy.Result
+import com.facebook.litho.cancellation.ResolveMetadata.ExecutionMode
+
+/**
+ * This policy is responsible for evaluating the context around the current executions of *Resolve*s
+ * and for the incoming request and act on any possible cancellation.
+ *
+ * This process is started by calling [onResolveRequest]. This method will return a [Result] that
+ * should indicate which action should be taken.
+ */
+interface ResolveCancellationPolicy : CancellationPolicy<ResolveMetadata> {
+
+  override fun evaluate(
+      ongoingRequests: List<ResolveMetadata>,
+      incomingRequest: ResolveMetadata
+  ): Result
+
+  companion object Default : ResolveCancellationPolicy {
+
+    /**
+     * This method identifies which action to take whenever a new Resolve request happens.
+     *
+     * For each of the on-going Resolve's metadata, we will verify if the incoming Resolve is an
+     * equivalent one to decide on the course of action. If both requests are equivalent, but the
+     * incoming one is asynchronous, we can drop it. The reasoning is that a synchronous or
+     * asynchronous one is already running, so there is no need to duplicate that work. If the
+     * incoming one is synchronous, we prefer to attempt to process the request because it will
+     * likely transfer work from the equivalent one.
+     *
+     * When requests are not equivalent, then if the running request runs asynchronously, we can
+     * cancel it because they are redundant, and the new one will effectively replace it.
+     */
+    override fun evaluate(
+        ongoingRequests: List<ResolveMetadata>,
+        incomingRequest: ResolveMetadata
+    ): Result {
+      val cancellableResolves = mutableListOf<ResolveMetadata>()
+
+      for (runningResolve in ongoingRequests) {
+        if (runningResolve.isEquivalentTo(incomingRequest)) {
+          if (incomingRequest.executionMode == ExecutionMode.ASYNC) {
+            return Result.DropIncomingRequest
+          }
+        } else {
+          if (runningResolve.executionMode == ExecutionMode.ASYNC) {
+            cancellableResolves.add(runningResolve)
+          }
+        }
+      }
+
+      return if (cancellableResolves.isNotEmpty()) {
+        Result.CancelRunningRequests(ongoingRequests.map { it.id })
+      } else {
+        Result.ProcessIncomingRequest
+      }
+    }
+  }
+}
+
+private val executionsModesBySource =
+    mapOf(
+        CalculateLayoutSource.SET_ROOT_SYNC to ExecutionMode.SYNC,
+        CalculateLayoutSource.SET_ROOT_ASYNC to ExecutionMode.ASYNC,
+        CalculateLayoutSource.UPDATE_STATE_SYNC to ExecutionMode.SYNC,
+        CalculateLayoutSource.UPDATE_STATE_ASYNC to ExecutionMode.ASYNC,
+        CalculateLayoutSource.SET_SIZE_SPEC_SYNC to ExecutionMode.SYNC,
+        CalculateLayoutSource.SET_SIZE_SPEC_ASYNC to ExecutionMode.ASYNC,
+        CalculateLayoutSource.MEASURE_SET_SIZE_SPEC to ExecutionMode.SYNC,
+        CalculateLayoutSource.MEASURE_SET_SIZE_SPEC_ASYNC to ExecutionMode.ASYNC)
+
+internal fun getResolveExecutionMode(source: Int): ExecutionMode =
+    executionsModesBySource[source] ?: error("Unexpected source for resolve: $source")
---
+++ litho-core/src/main/java/com/facebook/litho/cancellation/ResolveMetadata.kt
@@ -0,0 +1,44 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.cancellation
+
+import com.facebook.litho.TreeProps
+
+/** This metadata encompasses all characteristics associated to a specific `Resolve` request. */
+data class ResolveMetadata(
+    val localVersion: Int,
+    val componentId: Int,
+    val treeProps: TreeProps?,
+    val executionMode: ExecutionMode
+) {
+
+  val id: Int = localVersion
+
+  fun isEquivalentTo(resolveInput: ResolveMetadata): Boolean {
+    if (componentId != resolveInput.componentId) return false
+    if (treeProps != resolveInput.treeProps) return false
+
+    return true
+  }
+
+  enum class ExecutionMode {
+    SYNC,
+    ASYNC
+  }
+}
