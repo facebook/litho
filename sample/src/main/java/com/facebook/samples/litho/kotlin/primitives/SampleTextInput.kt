8cd7c097993845f2272b7b6f3ccaf3d0cbced7fb
Michal Zielinski <zielinskim@meta.com>
2023-03-08 10:51:47 -0800

    Add SampleTextInput example
    Summary: As per title
    
    Differential Revision: D43835120
    
    fbshipit-source-id: 8a17e3f8dd36b3b5c20abf20a09257a650a08d8b

---
+++ sample/src/main/java/com/facebook/samples/litho/kotlin/primitives/SampleTextInput.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.samples.litho.kotlin.primitives
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+import com.facebook.litho.LithoPrimitive
+import com.facebook.litho.PrimitiveComponent
+import com.facebook.litho.PrimitiveComponentScope
+import com.facebook.litho.Size
+import com.facebook.litho.SizeSpec
+import com.facebook.litho.Style
+import com.facebook.litho.utils.MeasureUtils
+import com.facebook.rendercore.primitives.LayoutBehavior
+import com.facebook.rendercore.primitives.LayoutScope
+import com.facebook.rendercore.primitives.PrimitiveLayoutResult
+import com.facebook.rendercore.primitives.ViewAllocator
+import kotlin.math.min
+
+/**
+ * This class is an oversimplified version of a TextInput Component. It was created for the purpose
+ * of showcasing layout() function implemented using creating a View and measuring it.
+ */
+class SampleTextInput(
+    private val initialText: CharSequence = "",
+    private val hint: CharSequence? = "",
+    private val inputBackground: Drawable = ColorDrawable(Color.TRANSPARENT),
+    private val style: Style? = null
+) : PrimitiveComponent() {
+
+  override fun PrimitiveComponentScope.render(): LithoPrimitive {
+    return LithoPrimitive(
+        layoutBehavior = SampleTextInputLayoutBehavior(initialText, hint, inputBackground),
+        mountBehavior =
+            MountBehavior(
+                // start_preallocation_example
+                ViewAllocator(canPreallocate = true, poolSize = 10) { context -> EditText(context) }
+                // end_preallocation_example
+                ) {
+                  hint.bindTo(EditText::setHint, null)
+                  initialText.bindTo(EditText::setText, null)
+                  bind(inputBackground) { content ->
+                    content.background = getBackgroundOrDefault(androidContext, inputBackground)
+                    onUnbind { content.background = null }
+                  }
+                },
+        style = style)
+  }
+}
+
+internal class SampleTextInputLayoutBehavior(
+    private val initialText: CharSequence,
+    private val hintText: CharSequence?,
+    private val inputBackground: Drawable
+) : LayoutBehavior {
+  // start_layout_with_view_measurement
+  override fun LayoutScope.layout(widthSpec: Int, heightSpec: Int): PrimitiveLayoutResult {
+    // The height should be the measured height of EditText with relevant params
+    val editTextForMeasure: EditText =
+        AppCompatEditText(androidContext).apply {
+          setHint(hintText)
+          setText(initialText)
+          background =
+              getBackgroundOrDefault(
+                  androidContext,
+                  if (inputBackground === ColorDrawable(Color.TRANSPARENT)) background
+                  else inputBackground)
+        }
+
+    editTextForMeasure.measure(
+        MeasureUtils.getViewMeasureSpec(widthSpec), MeasureUtils.getViewMeasureSpec(heightSpec))
+
+    val size = Size()
+    size.height = editTextForMeasure.measuredHeight
+
+    // For width we always take all available space, or collapse to 0 if unspecified.
+    if (SizeSpec.getMode(widthSpec) == SizeSpec.UNSPECIFIED) {
+      size.width = 0
+    } else {
+      size.width = min(SizeSpec.getSize(widthSpec), editTextForMeasure.measuredWidth)
+    }
+    return PrimitiveLayoutResult(size.width, size.height)
+  }
+  // end_layout_with_view_measurement
+}
+
+private fun getBackgroundOrDefault(context: Context, specifiedBackground: Drawable): Drawable? {
+  if (specifiedBackground === ColorDrawable(Color.TRANSPARENT)) {
+    val attrs = intArrayOf(android.R.attr.background)
+    val a = context.obtainStyledAttributes(null, attrs, android.R.attr.editTextStyle, 0)
+    val defaultBackground = a.getDrawable(0)
+    a.recycle()
+    return defaultBackground
+  }
+  return specifiedBackground
+}
