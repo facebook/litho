69963951cdcfcde3ea71df46dd838a059f2cd9ea
Fabio Carballo <fabiocarballo@meta.com>
2023-03-10 09:01:04 -0800

    Convert EventDispatchInfo to Kotlin
    Summary: As per title
    
    Reviewed By: zielinskimz
    
    Differential Revision: D43900006
    
    fbshipit-source-id: a9c348b623313f2076d2ba1312c43484ec2d6090

---
+++ litho-core/src/main/java/com/facebook/litho/EventDispatchInfo.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import kotlin.jvm.JvmField
+
+/**
+ * Holds onto the mutable data used to dispatch an event. This data is kept up-to-date with the
+ * latest version of the owning component or section by [EventHandlersController].
+ */
+class EventDispatchInfo(
+    // These should only be null for manually created EventHandlers (i.e. not using
+    // Component.newEventHandler
+    @JvmField var hasEventDispatcher: HasEventDispatcher?,
+    @JvmField var componentContext: ComponentContext?
+)
