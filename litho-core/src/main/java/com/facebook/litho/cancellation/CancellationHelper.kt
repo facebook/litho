c0d10df70392120e684dd0bb76f95fbf860cfc17
Fabio Carballo <fabiocarballo@meta.com>
2023-03-10 08:44:36 -0800

    Improve cancellation mechanisms synchronization
    Summary:
    Applying an improvement in our synchronized guards in the cancellation helper.
    
    The idea is that:
    
    1. Acquire copy of `futures` and iterate over them to extract metadata.
    2. If there are futures to cancel, iterate over the copy and cancel them. At the end return a set of the Futures cancelled.
    3. Remove the set of cancelled futures from the original futures list (in a sync block)
    
    Reviewed By: adityasharat
    
    Differential Revision: D43978088
    
    fbshipit-source-id: db12e578a067f922727924107a61b4eafe7171ff

--- litho-core/src/main/java/com/facebook/litho/cancellation/CancellationHelper.kt
+++ litho-core/src/main/java/com/facebook/litho/cancellation/CancellationHelper.kt
@@ -25,6 +25,7 @@ import com.facebook.litho.TreeFuture
 import com.facebook.litho.TreeFuture.TreeFutureResult
 import com.facebook.litho.cancellation.CancellationPolicy.CancellationExecutionMode
 import com.facebook.litho.stats.LithoStats
+import java.util.LinkedList
 import java.util.concurrent.CancellationException
 
 /**
@@ -44,41 +45,35 @@ internal fun <F, M, T> trackAndRunTreeFutureWithCancellation(
 T : PotentiallyPartialResult,
 F : TreeFuture<T>,
 F : RequestMetadataSupplier<M> {
+  val attribution = treeFuture.description
 
-  val pendingRequestsMetadata = synchronized(mutex) { futures.map { it.getMetadata() } }
+  val pendingFuturesCopy = synchronized(mutex) { LinkedList(futures) }
 
   val result: CancellationPolicy.Result =
       cancellationPolicy.evaluate(
-          ongoingRequests = pendingRequestsMetadata, incomingRequest = treeFuture.getMetadata())
+          ongoingRequests = pendingFuturesCopy.map { it.getMetadata() },
+          incomingRequest = treeFuture.getMetadata())
 
   when (result) {
     is CancellationPolicy.Result.ProcessIncomingRequest -> {
       debugLog { "Processing incoming request." }
     }
     is CancellationPolicy.Result.DropIncomingRequest -> {
-      debugLog { "Dropping incoming request. (${treeFuture.description})" }
+      debugLog { "Dropping incoming request. (${attribution})" }
       incrementCancellationStats(treeFuture.description)
       return TreeFutureResult.cancelled()
     }
     is CancellationPolicy.Result.CancelRunningRequests -> {
-      debugLog {
-        "Will attempt to cancel running requests: ${result.requestIds} (${treeFuture.description})"
-      }
-      result.requestIds.iterator().forEach { version ->
-        val futureToCancel = futures.firstOrNull { it.version == version }
-        if (futureToCancel != null) {
-          incrementCancellationStats(treeFuture.description)
+      debugLog { "Will attempt to cancel running requests: ${result.requestIds} (${attribution})" }
 
-          when (cancellationPolicy.cancellationMode) {
-            CancellationExecutionMode.SHORT_CIRCUIT -> futureToCancel.release()
-            CancellationExecutionMode.INTERRUPT -> futureToCancel.forceCancellation()
-          }
+      val futuresToRemove =
+          cancelFutures<F, M, T>(
+              result.requestIds,
+              pendingFuturesCopy,
+              cancellationPolicy.cancellationMode,
+              attribution)
 
-          synchronized(mutex) { futures.remove(futureToCancel) }
-
-          debugLog { "Cancelled future (${futureToCancel.version}) with success." }
-        }
-      }
+      synchronized(mutex) { futures.removeAll(futuresToRemove) }
     }
   }
 
@@ -92,6 +87,42 @@ F : RequestMetadataSupplier<M> {
   }
 }
 
+/**
+ * This method takes a list of versions of the [TreeFuture] that should be cancelled and returns a
+ * [Set] with all the [TreeFuture] cancelled.
+ *
+ * The [CancellationExecutionMode] decides if a short-circuit or interrupt approach should be taken.
+ */
+private fun <F, M, T> cancelFutures(
+    futuresToCancelVersions: List<Int>,
+    pendingFutures: List<F>,
+    cancellationMode: CancellationExecutionMode,
+    attribution: String
+): Set<TreeFuture<*>> where
+F : TreeFuture<T>,
+F : RequestMetadataSupplier<M>,
+T : PotentiallyPartialResult {
+  val cancelledFutures = mutableSetOf<TreeFuture<*>>()
+
+  futuresToCancelVersions.forEach { version ->
+    val futureToCancel = pendingFutures.firstOrNull { it.version == version }
+    if (futureToCancel != null) {
+      incrementCancellationStats(attribution)
+
+      when (cancellationMode) {
+        CancellationExecutionMode.SHORT_CIRCUIT -> futureToCancel.release()
+        CancellationExecutionMode.INTERRUPT -> futureToCancel.forceCancellation()
+      }
+
+      cancelledFutures.add(futureToCancel)
+
+      debugLog { "Cancelled future (${futureToCancel.version}) with success." }
+    }
+  }
+
+  return cancelledFutures
+}
+
 private fun incrementCancellationStats(attribution: String) {
   when (attribution) {
     "layout" -> LithoStats.incrementCancelledLayout()
@@ -103,7 +134,7 @@ private fun incrementCancellationStats(attribution: String) {
   }
 }
 
-private fun debugLog(msgBuilder: () -> String) {
+private inline fun debugLog(msgBuilder: () -> String) {
   if (isDebugEnabled) {
     Log.d(TAG, msgBuilder())
   }
