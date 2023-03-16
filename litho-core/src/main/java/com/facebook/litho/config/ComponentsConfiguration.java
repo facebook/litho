d403a3de52a11783918a87f17cdd7056e7dd26f8
Michal Zielinski <zielinskim@meta.com>
2023-03-16 10:11:35 -0700

    Rewrite FacecastOrionBackgroundComponent using Primitive API
    Summary:
    ExperimentalFacecastOrionBackgroundComponent is a Component implemented using Primitive API which is a composable, Kotlin-first replacement for the MountableComponents API. You can read more about it in the RFC: https://fb.quip.com/G3xXAPKmfTWp
    
    This experiment is about checking if  Primitive API works correctly and doesn't cause any crashes or perf regressions.
    
    Differential Revision: D43905435
    
    fbshipit-source-id: b8d1c6713c4cde1f7328f6b3131fd0bed196aac3

--- litho-core/src/main/java/com/facebook/litho/config/ComponentsConfiguration.java
+++ litho-core/src/main/java/com/facebook/litho/config/ComponentsConfiguration.java
@@ -122,6 +122,8 @@ public class ComponentsConfiguration {
 
   public static boolean runLooperPrepareForLayoutThreadFactory = true;
 
+  public static boolean enablePrimitiveComponentsInOrion = false;
+
   /**
    * field.getAnnotation() has bugs which is causing java crashes in the App, in addition to this we
    * suspect this might be a reason for few other native crashes as well. Adding this flag to verify
