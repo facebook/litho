3818879af9da05480fd26fd18bd8140d36e8b301
Andrew Wang <wys@meta.com>
2023-03-20 05:21:57 -0700

    Convert RenderState to Kotlin
    Summary: as per title
    
    Reviewed By: fabiocarballo
    
    Differential Revision: D44165275
    
    fbshipit-source-id: 7afe692bd31774737c8d90cb18c5a535aa62554e

---
+++ litho-core/src/main/java/com/facebook/litho/RenderState.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import com.facebook.litho.Component.RenderData
+import java.lang.RuntimeException
+import java.util.HashMap
+import java.util.HashSet
+
+/**
+ * Keeps track of the last mounted @Prop/@State a component was rendered with for components that
+ * care about them (currently, this is just for ComponentSpecs that use [Diff]'s of props in any of
+ * their lifecycle methods).
+ */
+class RenderState {
+
+  private val renderData: MutableMap<String, RenderData> = HashMap()
+  private val seenGlobalKeys: MutableSet<String> = HashSet()
+
+  fun recordRenderData(scopedComponentInfos: List<ScopedComponentInfo>?) {
+    if (scopedComponentInfos == null) {
+      return
+    }
+
+    for (componentInfo in scopedComponentInfos) {
+      recordRenderData(componentInfo)
+    }
+    seenGlobalKeys.clear()
+  }
+
+  fun applyPreviousRenderData(scopedComponentInfos: List<ScopedComponentInfo>?) {
+    if (scopedComponentInfos == null) {
+      return
+    }
+    for (componentInfo in scopedComponentInfos) {
+      applyPreviousRenderData(componentInfo)
+    }
+  }
+
+  private fun recordRenderData(scopedComponentInfo: ScopedComponentInfo) {
+    val component = scopedComponentInfo.component
+    val globalKey = scopedComponentInfo.context.globalKey
+    if (!isPreviousRenderDataSupported(component)) {
+      throw RuntimeException(
+          "Trying to record previous render data for component that doesn't support it")
+    }
+
+    // Sanity check like in StateHandler
+    if (seenGlobalKeys.contains(globalKey)) {
+      // We found two components with the same global key.
+      throw RuntimeException(
+          "Cannot record previous render data for ${component.simpleName}, found another Component with the same key: $globalKey")
+    }
+    seenGlobalKeys.add(globalKey)
+    renderData[globalKey]?.let { info ->
+      val newInfo: RenderData? =
+          (component as SpecGeneratedComponent).recordRenderData(scopedComponentInfo.context, info)
+      newInfo?.let { renderData[globalKey] = it }
+    }
+  }
+
+  private fun applyPreviousRenderData(scopedComponentInfo: ScopedComponentInfo) {
+    val component = scopedComponentInfo.component
+    val globalKey = scopedComponentInfo.context.globalKey
+    if (!isPreviousRenderDataSupported(component)) {
+      throw RuntimeException(
+          "Trying to apply previous render data to component that doesn't support it")
+    }
+    renderData[globalKey]?.let { previousRenderData ->
+      (component as SpecGeneratedComponent).applyPreviousRenderData(previousRenderData)
+    }
+  }
+
+  private fun isPreviousRenderDataSupported(component: Component): Boolean =
+      component is SpecGeneratedComponent && component.needsPreviousRenderData()
+}
