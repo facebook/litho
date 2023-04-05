794f9ac21004334bb21b8e7770e8be0066cd8316
Aditya Sharat <adityasharat@meta.com>
2023-04-05 14:44:52 -0700

    Converts RenderSource to kotlin
    Summary: Converts RenderSource to kotlin
    
    Reviewed By: zielinskimz
    
    Differential Revision: D44705646
    
    fbshipit-source-id: 25debb44cf9e5018a1b1991e2b2af39cc7c47ed6

---
+++ litho-core/src/main/java/com/facebook/litho/RenderSource.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import androidx.annotation.IntDef
+
+@IntDef(
+    RenderSource.TEST,
+    RenderSource.NONE,
+    RenderSource.SET_ROOT_SYNC,
+    RenderSource.SET_ROOT_ASYNC,
+    RenderSource.SET_SIZE_SPEC_SYNC,
+    RenderSource.SET_SIZE_SPEC_ASYNC,
+    RenderSource.UPDATE_STATE_SYNC,
+    RenderSource.UPDATE_STATE_ASYNC,
+    RenderSource.MEASURE_SET_SIZE_SPEC,
+    RenderSource.MEASURE_SET_SIZE_SPEC_ASYNC,
+    RenderSource.RELOAD_PREVIOUS_STATE)
+annotation class RenderSource {
+  companion object {
+    const val TEST = -2
+    const val NONE = -1
+    const val SET_ROOT_SYNC = 0
+    const val SET_ROOT_ASYNC = 1
+    const val SET_SIZE_SPEC_SYNC = 2
+    const val SET_SIZE_SPEC_ASYNC = 3
+    const val UPDATE_STATE_SYNC = 4
+    const val UPDATE_STATE_ASYNC = 5
+    const val MEASURE_SET_SIZE_SPEC = 6
+    const val MEASURE_SET_SIZE_SPEC_ASYNC = 7
+    const val RELOAD_PREVIOUS_STATE = 8
+  }
+}
