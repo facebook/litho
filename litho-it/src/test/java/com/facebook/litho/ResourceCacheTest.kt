40ce8eec472e4e411be165388b0736d940ba0461
Michal Zielinski <zielinskim@meta.com>
2023-04-18 01:12:40 -0700

    Convert ResourceCacheTest to Kotlin
    Summary: As per title
    
    Reviewed By: fabiocarballo
    
    Differential Revision: D44952336
    
    fbshipit-source-id: dc808d19cb78df2bd134deaa4e9f8bd9a3e0b064

---
+++ litho-it/src/test/java/com/facebook/litho/ResourceCacheTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import android.content.Context
+import android.content.res.Configuration
+import androidx.test.core.app.ApplicationProvider
+import com.facebook.litho.testing.testrunner.LithoTestRunner
+import java.util.Locale
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(LithoTestRunner::class)
+class ResourceCacheTest {
+
+  @Test
+  fun testSameConfigurationDoesNotUpdateResourceCache() {
+    val configuration = ApplicationProvider.getApplicationContext<Context>().resources.configuration
+    val cache = ResourceCache.getLatest(configuration)
+    assertThat(cache).isEqualTo(ResourceCache.getLatest(configuration))
+  }
+
+  @Test
+  fun testSameConfigurationNewInstanceDoesNotUpdateResourceCache() {
+    val configuration = ApplicationProvider.getApplicationContext<Context>().resources.configuration
+    val cache = ResourceCache.getLatest(configuration)
+    assertThat(cache).isEqualTo(ResourceCache.getLatest(Configuration(configuration)))
+  }
+
+  @Suppress("AppBundleLocaleChanges")
+  @Test
+  fun testDifferentLocaleUpdatesResourceCache() {
+    val configuration =
+        Configuration(ApplicationProvider.getApplicationContext<Context>().resources.configuration)
+    configuration.setLocale(Locale("en"))
+    val cache = ResourceCache.getLatest(configuration)
+    configuration.setLocale(Locale("it"))
+    assertThat(cache).isNotEqualTo(ResourceCache.getLatest(configuration))
+  }
+}
