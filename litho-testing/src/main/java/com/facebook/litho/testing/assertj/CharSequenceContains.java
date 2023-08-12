1a89e337513b648aefba4e2393d498f53ef609d4
Joshua Selbo <jselbo@meta.com>
2023-08-12 07:24:05 -0700

    Remove usages of @Factory
    Summary:
    > org.hamcrest.Factory has been removed. This was only used in old implementations of the hamcrest build toolchain, and can safely be deleted from client code without any effect.
    
    It's removed in Hamcrest 2.1.
    
    Reviewed By: hick209
    
    Differential Revision: D48281261
    
    fbshipit-source-id: f54ba3a2de4304c1b46b6f1d6308db6173d144a0

--- litho-testing/src/main/java/com/facebook/litho/testing/assertj/CharSequenceContains.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/assertj/CharSequenceContains.java
@@ -16,7 +16,6 @@
 
 package com.facebook.litho.testing.assertj;
 
-import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 
 /** Tests if the argument is a string that contains a substring. */
@@ -36,7 +35,6 @@ public class CharSequenceContains extends SubCharSequenceMatcher {
   }
 
   /** Alias for {@link #containsCharSequence(CharSequence)} for better discoverability. */
-  @Factory
   public static Matcher<CharSequence> containsString(CharSequence substring) {
     return new CharSequenceContains(substring);
   }
@@ -52,7 +50,6 @@ public class CharSequenceContains extends SubCharSequenceMatcher {
    * @param substring the substring that the returned matcher will expect to find within any
    *     examined string
    */
-  @Factory
   public static Matcher<CharSequence> containsCharSequence(CharSequence substring) {
     return new CharSequenceContains(substring);
   }
