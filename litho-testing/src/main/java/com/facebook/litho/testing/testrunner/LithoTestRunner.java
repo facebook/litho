f6198fda22b4badb047f6f32480a6483869a7a30
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2023-10-12 17:04:01 -0700

    Revert D49827367: Multisect successfully blamed "D49766621: [Litho][Layout Caching]Remove legacy code to ship layout caching" for test or build failures
    Summary:
    This diff is reverting D49827367
    D49766621: [Litho][Layout Caching]Remove legacy code to ship layout caching by pentiumao has been identified to be causing the following test or build failures:
    
    Tests affected:
    - [automation_assistant_sdk_sample_debug](https://www.internalfb.com/intern/test/562949987519956/)
    
    Here's the Multisect link:
    https://www.internalfb.com/multisect/3268977
    Here are the tasks that are relevant to this breakage:
    
    We're generating a revert to back out the changes in this diff, please note the backout may land if someone accepts it.
    
    If you believe this diff has been generated in error you may Commandeer and Abandon it.
    
    Reviewed By: pentiumao
    
    Differential Revision: D50237375
    
    fbshipit-source-id: 7369841e9ae192a7e48eadbbb2189eebcf51fe71

--- litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
@@ -74,7 +74,9 @@ public class LithoTestRunner extends RobolectricTestRunner {
    */
   private List<? extends Class<? extends LithoTestRunConfiguration>> getGlobalConfigs() {
     return Arrays.asList(
-        SkipRootCheckingTestRunConfiguration.class, DynamicValuesTestRunConfiguration.class);
+        LayoutCachingTestRunConfiguration.class,
+        SkipRootCheckingTestRunConfiguration.class,
+        DynamicValuesTestRunConfiguration.class);
   }
 
   @Override
