a9e12df2032e05c7561126f42670217032aafab7
Andrew Wang <wys@meta.com>
2023-10-16 03:42:47 -0700

    Create few util methods
    Summary: Introducing few Kotlin util methods that could make code more clarity.
    
    Reviewed By: adityasharat
    
    Differential Revision: D50117635
    
    fbshipit-source-id: f3614221df6f694af77b37e87147873885dc8856

--- litho-core/src/main/java/com/facebook/litho/CommonUtils.kt
+++ litho-core/src/main/java/com/facebook/litho/CommonUtils.kt
@@ -26,3 +26,37 @@ fun <T> mergeLists(a: List<T>?, b: List<T>?): List<T>? {
   if (b.isNullOrEmpty()) return a
   return a + b
 }
+
+/**
+ * Util method that returns the instance if it's not `null` otherwise calls [initBlock] to create a
+ * new instance.
+ */
+inline fun <T> T?.getOrCreate(initBlock: () -> T): T = this ?: initBlock()
+
+/**
+ * Util method that returns the result of [block] if both [var1] and [var2] are not `null`.
+ * Otherwise returns `null`.
+ *
+ * ```
+ * allNotNull(var1, var2) { v1, v2 -> ... }
+ * ```
+ */
+inline fun <A, B, R> allNotNull(var1: A?, var2: B?, block: (A, B) -> R): R? {
+  return if (var1 != null && var2 != null) {
+    block(var1, var2)
+  } else null
+}
+
+/**
+ * Util method that returns the result of [block] if all three [var1], [var2] and [var3] are not
+ * `null`. Otherwise returns `null`.
+ *
+ * ```
+ * allNotNull(var1, var2, var3) { v1, v2, v3 -> ... }
+ * ```
+ */
+inline fun <A, B, C, R> allNotNull(var1: A?, var2: B?, var3: C?, block: (A, B, C) -> R): R? {
+  return if (var1 != null && var2 != null && var3 != null) {
+    block(var1, var2, var3)
+  } else null
+}
