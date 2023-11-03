d3812c93254f360357cdc35c73cb23287bb937cf
Aditya Sharat <adityasharat@meta.com>
2023-11-03 04:46:21 -0700

    Converts LithoConfiguration to Kotlin
    Summary: Converts LithoConfiguration to Kotlin.
    
    Reviewed By: pentiumao
    
    Differential Revision: D50942815
    
    fbshipit-source-id: c20122d55d21e81875f1116e87c7f7f307beaa00

---
+++ litho-core/src/main/java/com/facebook/litho/LithoConfiguration.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import com.facebook.kotlin.compilerplugins.dataclassgenerate.annotation.DataClassGenerate
+import com.facebook.kotlin.compilerplugins.dataclassgenerate.annotation.Mode
+import com.facebook.litho.config.ComponentsConfiguration
+import com.facebook.rendercore.RunnableHandler
+import com.facebook.rendercore.visibility.VisibilityBoundsTransformer
+
+@DataClassGenerate(toString = Mode.OMIT, equalsHashCode = Mode.OMIT)
+data class LithoConfiguration(
+    @JvmField var mComponentsConfiguration: ComponentsConfiguration,
+    @JvmField val areTransitionsEnabled: Boolean,
+    @JvmField val isReconciliationEnabled: Boolean,
+    @JvmField val isVisibilityProcessingEnabled: Boolean,
+    @JvmField val preallocationPerMountContentEnabled: Boolean,
+    @JvmField val mountContentPreallocationHandler: RunnableHandler?,
+    @JvmField val incrementalMountEnabled: Boolean,
+    @JvmField val errorEventHandler: ErrorEventHandler? = DefaultErrorEventHandler.INSTANCE,
+    @JvmField val logTag: String? = null,
+    @JvmField val logger: ComponentsLogger? = null,
+    @JvmField val renderUnitIdGenerator: RenderUnitIdGenerator?,
+    @JvmField val visibilityBoundsTransformer: VisibilityBoundsTransformer?,
+    @JvmField val debugEventListener: ComponentTreeDebugEventListener?
+) {}
