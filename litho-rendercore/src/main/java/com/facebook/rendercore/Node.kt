8fb223ae563712f325865980b98ffff40c737e0c
Aditya Sharat <adityasharat@meta.com>
2023-11-13 07:50:20 -0800

    Adds size constraints based calculate layout API
    Summary: Adds size constraints based calculate layout API, and deprecates the the measure spec based API.
    
    Reviewed By: pentiumao
    
    Differential Revision: D51071717
    
    fbshipit-source-id: b5d37b66ffcc513e2f21d76889845a28e338ecdd

--- litho-rendercore/src/main/java/com/facebook/rendercore/Node.kt
+++ litho-rendercore/src/main/java/com/facebook/rendercore/Node.kt
@@ -30,6 +30,34 @@ interface Node<RenderContext> {
    * * - A Node must call calculateLayout on each child (if it has any) at least once, even if the
    *   Node is going to assign that child an exact size -- this gives a chance for children to lay
    *   out their own children and produce artifacts like text layouts.
+   * * - If a Node calls layout on a child with unspecified or bounded size constraints to get
+   *   sizing information, but ultimately decides to assign that child a different size than the
+   *   child returned, the Node must call calculateLayout again on that child with the exact size
+   *   constraints.
+   *
+   * The default implementation exists for compatibility with existing [Node] implementations until
+   * they are refactored to use this API.
+   *
+   * @param context The LayoutContext associated with this layout calculation [LayoutContext]
+   * @param sizeConstraints The size constraints for this layout pass
+   */
+  @JvmDefault
+  fun calculateLayout(
+      context: LayoutContext<@UnsafeVariance RenderContext>,
+      sizeConstraints: SizeConstraints
+  ): LayoutResult {
+    return calculateLayout(context, sizeConstraints.toWidthSpec(), sizeConstraints.toHeightSpec())
+  }
+
+  /**
+   * Implementations of Node are responsible to calculate a layout based on the width/height
+   * constraints provided. A Node could decide to implement its own layout function or to delegate
+   * to its RenderUnit measure.
+   *
+   * The general contract is:
+   * * - A Node must call calculateLayout on each child (if it has any) at least once, even if the
+   *   Node is going to assign that child an exact size -- this gives a chance for children to lay
+   *   out their own children and produce artifacts like text layouts.
    * * - If a Node calls layout on a child with flexible specs (UNSPECIFIED or AT_MOST) to get
    *   sizing information, but ultimately decides to assign that child a different size than the
    *   child returned, the Node must call calculateLayout again on that child with a mode of EXACTLY
@@ -39,9 +67,12 @@ interface Node<RenderContext> {
    * @param widthSpec a measure spec for the width in the format of [View.MeasureSpec]
    * @param heightSpec a measure spec for the height in the format of [View.MeasureSpec]
    */
+  @Deprecated(message = "Invoke the calculate layout with SizeConstraint API")
   fun calculateLayout(
-      context: LayoutContext<RenderContext>,
+      context: LayoutContext<@UnsafeVariance RenderContext>,
       widthSpec: Int,
       heightSpec: Int,
-  ): LayoutResult
+  ): LayoutResult {
+    throw UnsupportedOperationException("This API must be implemented to be invoked.")
+  }
 }
