67b4e05e1818d458ce7d25d041b147df790f9509
Michal Zielinski <zielinskim@meta.com>
2023-11-16 00:31:48 -0800

    Convert LayoutFuture to Kotlin
    Summary: As per title
    
    Reviewed By: adityasharat
    
    Differential Revision: D51303006
    
    fbshipit-source-id: e76a9e40a9e13e1d8ace07c086f8b22b1c433638

---
+++ litho-rendercore/src/main/java/com/facebook/rendercore/LayoutFuture.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.rendercore
+
+import android.content.Context
+import com.facebook.rendercore.extensions.RenderCoreExtension
+import java.util.concurrent.Callable
+
+class LayoutFuture<State, RenderContext>(
+    context: Context,
+    renderContext: RenderContext?,
+    val tree: Node<RenderContext>,
+    state: State?,
+    val version: Int,
+    previousResult: RenderResult<State, RenderContext>?,
+    extensions: Array<RenderCoreExtension<*, *>>?,
+    val widthSpec: Int,
+    val heightSpec: Int
+) :
+    ThreadInheritingPriorityFuture<RenderResult<State, RenderContext>?>(
+        Callable {
+          if (previousResult != null &&
+              RenderResult.shouldReuseResult(tree, widthSpec, heightSpec, previousResult)) {
+            RenderResult(previousResult.renderTree, tree, previousResult.layoutCacheData, state)
+          } else {
+            RenderResult.layout(
+                RenderResult.createLayoutContext(
+                    previousResult, renderContext, context, version, extensions),
+                tree,
+                state,
+                widthSpec,
+                heightSpec)
+          }
+        },
+        "LayoutFuture")
