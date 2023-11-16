cb386469b7a077938530c8ef3ff6bef894eb18ca
Michal Zielinski <zielinskim@meta.com>
2023-11-16 00:31:48 -0800

    Convert RenderTree to Kotlin
    Summary: As per title
    
    Reviewed By: adityasharat
    
    Differential Revision: D51304732
    
    fbshipit-source-id: 61425d52cc311f0d878b879b09f81ea5ac26801b

---
+++ litho-rendercore/src/main/java/com/facebook/rendercore/RenderTree.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.rendercore
+
+import android.util.LongSparseArray
+import android.util.Pair
+import com.facebook.rendercore.extensions.RenderCoreExtension
+import com.facebook.rendercore.utils.MeasureSpecUtils
+import java.util.Locale
+
+/** TODO add javadoc */
+class RenderTree(
+    val root: RenderTreeNode,
+    private val flatList: Array<RenderTreeNode>,
+    val widthSpec: Int,
+    val heightSpec: Int,
+    val renderStateId: Int,
+    val extensionResults: List<Pair<RenderCoreExtension<*, *>, Any>>?,
+    // This will vary by framework and will be null outside of debug builds
+    val debugData: Any?
+) {
+
+  val width: Int
+    get() = root.bounds.width()
+
+  val height: Int
+    get() = root.bounds.height()
+
+  val mountableOutputCount: Int
+    get() = flatList.size
+
+  private val idToIndexMap = LongSparseArray<Int>()
+
+  init {
+    for (i in 0 until flatList.size) {
+      assertNoDuplicateRenderUnits(i)
+      idToIndexMap.put(flatList[i].renderUnit.id, i)
+    }
+  }
+
+  /**
+   * Throws an exception if this RenderTree already has a RenderUnit with the same ID as the one at
+   * the given index.
+   */
+  private fun assertNoDuplicateRenderUnits(newNodeIndex: Int) {
+    val newNode = flatList[newNodeIndex]
+    if (idToIndexMap[newNode.renderUnit.id] == null) {
+      return
+    }
+    val existingNodeIndex = idToIndexMap[newNode.renderUnit.id]
+    val existingNode = flatList[existingNodeIndex]
+    throw IllegalStateException(
+        String.format(
+            Locale.US,
+            """
+              RenderTrees must not have RenderUnits with the same ID:
+              Attempted to add item with existing ID at index %d: %s
+              Existing item at index %d: %s
+              Full RenderTree: %s
+              """
+                .trimIndent(),
+            newNodeIndex,
+            newNode.generateDebugString(null),
+            existingNodeIndex,
+            existingNode.generateDebugString(null),
+            generateDebugString()))
+  }
+
+  fun getRenderTreeNodeIndex(renderUnitId: Long): Int {
+    return idToIndexMap[renderUnitId, -1]
+  }
+
+  fun getRenderTreeNodeAtIndex(index: Int): RenderTreeNode {
+    return flatList[index]
+  }
+
+  private fun generateDebugString(): String {
+    val l = Locale.US
+    val widthSpecDesc = MeasureSpecUtils.getMeasureSpecDescription(widthSpec)
+    val heightSpecDesc = MeasureSpecUtils.getMeasureSpecDescription(heightSpec)
+
+    return buildString {
+      append("RenderTree details:\n")
+      append(String.format(l, "WidthSpec=%s; HeightSpec=%s\n", widthSpecDesc, heightSpecDesc))
+      append(String.format(l, "Full child list (size = %d):\n", flatList.size))
+      for (node in flatList) {
+        append(String.format(l, "%s\n", node.generateDebugString(this@RenderTree)))
+      }
+    }
+  }
+}
