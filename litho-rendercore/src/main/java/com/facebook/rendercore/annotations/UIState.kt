f5cdb5eb97c9159d4e2f652925e28c8dfa17fdb0
Michal Zielinski <zielinskim@meta.com>
2024-01-12 02:52:40 -0800

    Convert UIState annotation to Kotlin
    Summary: As per title
    
    Reviewed By: pentiumao
    
    Differential Revision: D52693390
    
    fbshipit-source-id: 4cef61056bcbc248c8481cdd3c512230204260a1

--- litho-rendercore/src/main/java/com/facebook/rendercore/annotations/UIState.kt
+++ litho-rendercore/src/main/java/com/facebook/rendercore/annotations/UIState.kt
@@ -14,18 +14,11 @@
  * limitations under the License.
  */
 
-package com.facebook.rendercore.annotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+package com.facebook.rendercore.annotations
 
 /**
  * To be used with a RenderUnit: annotates that this field is mutable state that can be accessed on
  * the UI thread only. This should only be annotated on non-primitive fields because we want to make
  * sure to maintain the same instance when cloning.
  */
-@Target({ElementType.FIELD})
-@Retention(RetentionPolicy.SOURCE)
-public @interface UIState {}
+@Target(AnnotationTarget.FIELD) @Retention(AnnotationRetention.SOURCE) annotation class UIState
