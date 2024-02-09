22abec0e3763260b87b2fcc003a8bc222619bc8c
Aditya Sharat <adityasharat@meta.com>
2024-02-09 02:49:01 -0800

    Creates new value class enum for layout direction
    Summary: Creates new value class enum for layout direction
    
    Reviewed By: pentiumao, zielinskimz
    
    Differential Revision: D53519947
    
    fbshipit-source-id: 3b58cdc7cd41f6d5b124d9f6e563b537f93802f7

---
+++ litho-core/src/main/java/com/facebook/litho/layout/LayoutDirection.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.layout
+
+/**
+ * A class for defining layout directions. A layout direction can be left-to-right (LTR) or
+ * right-to-left (RTL). It can also be inherited (from a parent) or deduced from the default
+ * language script of a locale.
+ */
+@JvmInline
+value class LayoutDirection private constructor(val value: Int) {
+
+  fun getLayoutDirectionForView(): Int {
+    return when (this) {
+      LTR -> android.view.View.LAYOUT_DIRECTION_LTR
+      RTL -> android.view.View.LAYOUT_DIRECTION_RTL
+      INHERIT -> android.view.View.LAYOUT_DIRECTION_INHERIT
+      LOCALE -> android.view.View.LAYOUT_DIRECTION_LOCALE
+      else -> throw IllegalArgumentException("Unknown layout direction $value")
+    }
+  }
+
+  companion object {
+
+    /** Horizontal layout direction is from Left to Right. */
+    val LTR: LayoutDirection = LayoutDirection(0)
+
+    /** Horizontal layout direction is from Right to Left. */
+    val RTL: LayoutDirection = LayoutDirection(1)
+
+    /** Horizontal layout direction is inherited. */
+    val INHERIT: LayoutDirection = LayoutDirection(2)
+
+    /** Horizontal layout direction is deduced from the default language script for the locale. */
+    val LOCALE: LayoutDirection = LayoutDirection(3)
+  }
+}
