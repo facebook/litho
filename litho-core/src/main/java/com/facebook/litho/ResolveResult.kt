dbedc28497aefabb0d15d35b4222794fe099f035
Aditya Sharat <adityasharat@meta.com>
2024-02-12 09:01:29 -0800

    Makes ResolveResult a data class
    Summary: Makes ResolveResult a data class so that copies can be created of it for nested litho tree usages in Bloks for applying state updates
    
    Differential Revision: D53665129
    
    fbshipit-source-id: 81db3961ec859ecb5d1c6431129dd0d826f01fce

--- litho-core/src/main/java/com/facebook/litho/ResolveResult.kt
+++ litho-core/src/main/java/com/facebook/litho/ResolveResult.kt
@@ -17,15 +17,18 @@
 package com.facebook.litho
 
 import android.util.Pair
+import com.facebook.kotlin.compilerplugins.dataclassgenerate.annotation.DataClassGenerate
+import com.facebook.kotlin.compilerplugins.dataclassgenerate.annotation.Mode
 import com.facebook.litho.Resolver.Outputs
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.jvm.JvmField
 
-class ResolveResult(
+@DataClassGenerate(toString = Mode.OMIT, equalsHashCode = Mode.KEEP)
+data class ResolveResult(
     @JvmField val node: LithoNode?,
     @JvmField val context: ComponentContext,
     @JvmField val component: Component,
-    cache: MeasuredResultCache,
+    private val cache: MeasuredResultCache,
     @JvmField val treeState: TreeState,
     override val isPartialResult: Boolean,
     @JvmField val version: Int,
@@ -34,7 +37,7 @@ class ResolveResult(
     @JvmField val contextForResuming: ResolveContext?
 ) : PotentiallyPartialResult {
 
-  private val cache: AtomicReference<MeasuredResultCache> = AtomicReference(cache)
+  private val cacheRef: AtomicReference<MeasuredResultCache> = AtomicReference(cache)
 
-  fun consumeCache(): MeasuredResultCache = cache.getAndSet(MeasuredResultCache.EMPTY)
+  fun consumeCache(): MeasuredResultCache = cacheRef.getAndSet(MeasuredResultCache.EMPTY)
 }
