b1de364adc813b42b4f273d84cc2f8d8c20cfa91
Andrew Wang <wys@meta.com>
2024-02-13 10:08:37 -0800

    Convert CollectionUtils to Kotlin
    Summary: as per title
    
    Differential Revision: D53570253
    
    fbshipit-source-id: c4870da891a0c4623d7f00ced222d9f802f5dfa4

---
+++ litho-core/src/main/java/com/facebook/litho/CollectionsUtils.kt
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho
+
+import android.util.SparseArray
+import androidx.collection.SparseArrayCompat
+import com.facebook.infer.annotation.FalseOnNull
+import com.facebook.infer.annotation.TrueOnNull
+
+/** Provides util methods for common collection patterns. */
+object CollectionsUtils {
+
+  @TrueOnNull
+  @JvmStatic
+  fun isNullOrEmpty(collection: Collection<*>?): Boolean {
+    return collection.isNullOrEmpty()
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isNotNullOrEmpty(collection: Collection<*>?): Boolean {
+    return !isNullOrEmpty(collection)
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isEmpty(collection: Collection<*>?): Boolean {
+    return collection != null && collection.isEmpty()
+  }
+
+  @TrueOnNull
+  @JvmStatic
+  fun isNullOrEmpty(map: Map<*, *>?): Boolean {
+    return map.isNullOrEmpty()
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isNotNullOrEmpty(map: Map<*, *>?): Boolean {
+    return !isNullOrEmpty(map)
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isEmpty(map: Map<*, *>?): Boolean {
+    return map != null && map.isEmpty()
+  }
+
+  @TrueOnNull
+  @JvmStatic
+  fun isNullOrEmpty(sparseArrayCompat: SparseArrayCompat<*>?): Boolean {
+    return sparseArrayCompat == null || sparseArrayCompat.isEmpty
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isNotNullOrEmpty(sparseArrayCompat: SparseArrayCompat<*>?): Boolean {
+    return !isNullOrEmpty(sparseArrayCompat)
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isEmpty(sparseArrayCompat: SparseArrayCompat<*>?): Boolean {
+    return sparseArrayCompat != null && sparseArrayCompat.isEmpty
+  }
+
+  @TrueOnNull
+  @JvmStatic
+  fun isNullOrEmpty(sparseArray: SparseArray<*>?): Boolean {
+    return sparseArray == null || sparseArray.size() == 0
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isNotNullOrEmpty(sparseArray: SparseArray<*>?): Boolean {
+    return !isNullOrEmpty(sparseArray)
+  }
+
+  @FalseOnNull
+  @JvmStatic
+  fun isEmpty(sparseArray: SparseArray<*>?): Boolean {
+    return sparseArray != null && sparseArray.size() == 0
+  }
+}
