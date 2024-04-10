5b250161b8b54098a322d56835a489c125cfb090
Aditya Sharat <adityasharat@meta.com>
2024-04-10 07:53:11 -0700

    Disables broken test runner config
    Summary:
    Disables broken test runner config
    
    Created from CodeHub with https://fburl.com/edit-in-codehub
    
    Reviewed By: passy
    
    Differential Revision: D55966935
    
    fbshipit-source-id: 46cd6c42df425e7d4e69fa99c5449c32272681f0

--- litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/testrunner/LithoTestRunner.java
@@ -73,7 +73,7 @@ public class LithoTestRunner extends RobolectricTestRunner {
    *     configurations.
    */
   private List<? extends Class<? extends LithoTestRunConfiguration>> getGlobalConfigs() {
-    return Arrays.asList(UnmountOnDetachConfiguration.class);
+    return Arrays.asList();
   }
 
   @Override
