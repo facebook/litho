11ffd6221cf2565343e7d1cdb4c92c4ce0de9d5f
Pascal Hartig <realpassy@fb.com>
2024-04-25 17:59:10 +0100

    [sample] Remove Flipper integration temporarily

--- sample/src/main/java/com/facebook/samples/litho/LithoSampleApplication.java
+++ sample/src/main/java/com/facebook/samples/litho/LithoSampleApplication.java
@@ -17,18 +17,9 @@
 package com.facebook.samples.litho;
 
 import android.app.Application;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.flipper.android.AndroidFlipperClient;
-import com.facebook.flipper.android.utils.FlipperUtils;
-import com.facebook.flipper.core.FlipperClient;
-import com.facebook.flipper.plugins.inspector.DescriptorMapping;
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
-import com.facebook.flipper.plugins.sections.SectionsFlipperPlugin;
-import com.facebook.flipper.plugins.uidebugger.UIDebuggerFlipperPlugin;
-import com.facebook.flipper.plugins.uidebugger.core.UIDContext;
-import com.facebook.flipper.plugins.uidebugger.litho.UIDebuggerLithoSupport;
 import com.facebook.fresco.vito.init.FrescoVito;
-import com.facebook.litho.editor.flipper.LithoFlipperDescriptors;
 import com.facebook.rendercore.debug.DebugEventBus;
 import com.facebook.rendercore.debug.DebugEventLogger;
 import com.facebook.soloader.SoLoader;
@@ -44,19 +35,5 @@ public class LithoSampleApplication extends Application {
     Fresco.initialize(this);
     FrescoVito.initialize();
     SoLoader.init(this, false);
-
-    if (FlipperUtils.shouldEnableFlipper(this)) {
-      final FlipperClient client = AndroidFlipperClient.getInstance(this);
-
-      UIDContext uiDebuggerContext = UIDContext.Companion.create(this);
-      UIDebuggerLithoSupport.INSTANCE.enable(uiDebuggerContext);
-      client.addPlugin(new UIDebuggerFlipperPlugin(uiDebuggerContext));
-
-      final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
-      LithoFlipperDescriptors.add(descriptorMapping);
-      client.addPlugin(new InspectorFlipperPlugin(this, descriptorMapping));
-      client.addPlugin(new SectionsFlipperPlugin(true));
-      client.start();
-    }
   }
 }
