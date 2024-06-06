12c77bb41bd4613977b664714872f03e8d33529f
Jett Hsieh <jetthsieh@meta.com>
2024-06-06 06:23:17 -0700

    Convert UnfocusedVisibleEvent to Kotlin
    Summary: Convert UnfocusedVisibleEvent to Kotlin
    
    Reviewed By: pengj
    
    Differential Revision: D58184227
    
    fbshipit-source-id: 9c3522b143b968007b47b0174fefbecd42c8db9c

--- litho-rendercore-visibility/src/main/java/com/facebook/litho/UnfocusedVisibleEvent.kt
+++ litho-rendercore-visibility/src/main/java/com/facebook/litho/UnfocusedVisibleEvent.kt
@@ -14,14 +14,11 @@
  * limitations under the License.
  */
 
-package com.facebook.litho;
-
-import com.facebook.infer.annotation.Nullsafe;
+package com.facebook.litho
 
 /**
  * Event triggered when a Component exits the Focused Range. The Focused Range is defined as at
  * least half of the viewport or, if the Component is smaller than half of the viewport, when the it
  * is fully visible.
  */
-@Nullsafe(Nullsafe.Mode.LOCAL)
-public class UnfocusedVisibleEvent {}
+class UnfocusedVisibleEvent
