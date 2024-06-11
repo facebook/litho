a6d7d5647ae34458fe62cfa49e7789478c35f223
Kingsley Adio <kingsleyadio@meta.com>
2024-06-11 10:14:20 -0700

    //fbandroid/libraries/components/litho-rendercore-transitions/src/main/java/com/facebook/rendercore/transitions/AnimatedRootHost.java
    Reviewed By: pengj
    
    Differential Revision: D58359917
    
    fbshipit-source-id: a8b98c2c144a909d602861b7f63d61f82cfd6f0f

---
+++ litho-rendercore-transitions/src/main/java/com/facebook/rendercore/transitions/AnimatedRootHost.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.rendercore.transitions
+
+import android.graphics.Rect
+
+/**
+ * A root [com.facebook.rendercore.Host] using the transition extension would need to implement this
+ * interface to be able to receive size updates when animating. The size information received in the
+ * callbacks should be used in [android.view.View#onMeasure(int, int)] to override the size
+ * information when animating. This needs to be different from a regular
+ * [com.facebook.rendercore.Host] where we call measure and layout manually.
+ */
+interface AnimatedRootHost {
+
+  /**
+   * Sets the width that the root [com.facebook.rendercore.Host] should take on the next measure
+   * pass and then requests a layout. This should be called from animation-driving code on each
+   * frame to animate the size of the root [com.facebook.rendercore.Host].
+   */
+  fun setAnimatedWidth(width: Int)
+
+  /**
+   * Sets the height that the root [com.facebook.rendercore.Host] should take on the next measure
+   * pass and then requests a layout. This should be called from animation-driving code on each
+   * frame to animate the size of the root [com.facebook.rendercore.Host].
+   */
+  fun setAnimatedHeight(height: Int)
+
+  /**
+   * This allows inner RootHosts to trigger a mount on a custom visibleRect. We need this to force
+   * animated inner root hosts to be mounted for animation.
+   */
+  fun notifyVisibleBoundsChanged(visibleRect: Rect, processVisibilityOutputs: Boolean)
+}
