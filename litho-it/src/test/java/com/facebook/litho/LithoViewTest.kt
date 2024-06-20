42d06fc4bc20c1c856df979ffd011100dd76e5c2
Pascal Hartig <realpassy@meta.com>
2024-06-20 08:47:32 -0700

    Disable broken tests (#992)
    Summary:
    Pull Request resolved: https://github.com/facebook/litho/pull/992
    
    I noticed on GitHub that a bunch of tests are now failing on master. The same is true in Sandcastle:
    
    https://www.internalfb.com/sandcastle/workflow/382805968329783817/artifact/actionlog.382805968392335671.stdout.1
    
    ```
    7 TESTS FAILED
      ✗ fbsource//fbandroid/javatests/instagram/features/clips/viewer/adapter/organic/mediainfo/authorinfo:authorinfo - test ClipsAuthorInfoComponent is rendered for Clips items (instagram.features.clips.viewer.adapter.organic.mediainfo.authorinfo.ClipsAuthorInfoComponentTest)
      ✗ fbsource//fbandroid/libraries/components/litho-it/src/test/java/com/facebook/litho:litho - testIncrementalMountTriggeredAfterUnmountAllWithSameDimensions[IncrementalMountConfiguration:][api=33][looperMode=LEGACY] (com.facebook.litho.LithoViewMountTest)
      ✗ fbsource//fbandroid/libraries/components/litho-it/src/test/java/com/facebook/litho:litho - testSetComponentTwiceWithResetAndAttachRequestsLayout[IncrementalMountConfiguration:][api=33][looperMode=LEGACY] (com.facebook.litho.LithoViewMountTest)
      ✗ fbsource//fbandroid/libraries/components/litho-it/src/test/java/com/facebook/litho:litho - testSetSameSizeComponentAndAttachRequestsLayout[IncrementalMountConfiguration:][api=33][looperMode=LEGACY] (com.facebook.litho.LithoViewMountTest)
      ✗ fbsource//fbandroid/libraries/components/litho-it/src/test/java/com/facebook/litho:litho - testReAttachRequestsLayout[IncrementalMountConfiguration:][api=33][looperMode=LEGACY] (com.facebook.litho.LithoViewMountTest)
      ✗ fbsource//fbandroid/libraries/components/litho-it/src/test/java/com/facebook/litho:litho - measureWithLayoutParams[IncrementalMountConfiguration:][api=33][looperMode=LEGACY] (com.facebook.litho.LithoViewTest)
      ✗ fbsource//fbandroid/javatests/com/oculus/socialplatform/receivers:receivers - main
    ```
    
    Based on adityasharat's recommendation, I first tried to change the LooperMode which sadly didn't change the outcome, and then disabled the tests based on a config flag.
    
    Reviewed By: adityasharat
    
    Differential Revision: D58818690
    
    fbshipit-source-id: dc738bc23f431f74cd34b637571215db575ce686

--- litho-it/src/test/java/com/facebook/litho/LithoViewTest.kt
+++ litho-it/src/test/java/com/facebook/litho/LithoViewTest.kt
@@ -19,6 +19,7 @@ package com.facebook.litho
 import android.content.Context
 import android.view.ViewGroup
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import com.facebook.litho.config.ComponentsConfiguration
 import com.facebook.litho.testing.LithoStatsRule
 import com.facebook.litho.testing.assertj.LithoViewAssert.Companion.assertThat
 import com.facebook.litho.testing.atMost
@@ -29,6 +30,7 @@ import com.facebook.litho.testing.unspecified
 import com.facebook.litho.widget.SimpleMountSpecTester
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assumptions
+import org.junit.Assume.assumeFalse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -117,6 +119,8 @@ class LithoViewTest {
   /** This verifies that the width is 0 with normal layout params. */
   @Test
   fun measureWithLayoutParams() {
+    // TODO(T193117797): Reenable this test.
+    assumeFalse(ComponentsConfiguration.defaultInstance.enableFixForIM)
     val component: Component =
         object : InlineLayoutSpec() {
           override fun onCreateLayout(c: ComponentContext): Component {
