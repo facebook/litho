f0aad93da0a6ce67004264b150bf56c474936399
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-07-25 07:05:08 -0700

    fbandroid/libraries/components/litho-testing/src/main/java/com/facebook/litho/testing/specmodels/MockSpecModel.java
    Reviewed By: pentiumao
    
    Differential Revision: D60224557
    
    fbshipit-source-id: 95e3676177261f2266e170fa7f40eebda2ddb419

--- litho-testing/src/main/java/com/facebook/litho/testing/specmodels/MockSpecModel.java
+++ litho-testing/src/main/java/com/facebook/litho/testing/specmodels/MockSpecModel.java
@@ -819,7 +819,7 @@ public class MockSpecModel implements SpecModel, HasPureRender, HasEnclosedSpecM
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(@Nullable Object o) {
       if (this == o) return true;
       if (o == null || getClass() != o.getClass()) return false;
       final Builder builder = (Builder) o;
