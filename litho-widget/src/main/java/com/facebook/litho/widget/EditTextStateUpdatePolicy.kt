fea0d0bb1e75b9bafeaf40a08de209ce04a08c41
Nichole Clarke <niclarke@meta.com>
2024-08-22 15:58:05 -0700

    fbandroid/libraries/components/litho-widget/src/main/java/com/facebook/litho/widget/EditTextStateUpdatePolicy.java
    Reviewed By: zielinskimz
    
    Differential Revision: D61043949
    
    fbshipit-source-id: 8902acb4880b50b5b6c3e5ef39b135bd176b02e9

--- litho-widget/src/main/java/com/facebook/litho/widget/EditTextStateUpdatePolicy.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/EditTextStateUpdatePolicy.kt
@@ -14,31 +14,35 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
 /**
  * EditTextStateUpdatePolicy specifies when EditText component should update its internal input
  * state
  *
  * @example use UPDATE_ON_LINE_COUNT_CHANGE to create an EditText components that has a static
- *     width, but should dynamically expand in height as user types in (WRAP_CONTENT effect)
+ *   width, but should dynamically expand in height as user types in (WRAP_CONTENT effect)
  */
-public enum EditTextStateUpdatePolicy {
+enum class EditTextStateUpdatePolicy {
+
   /** No updates are performed */
   NO_UPDATES,
+
   /**
-   * {@code lazyUpdateInput()} is called every time text changes, thus allowing to preserve input
-   * state during layout recalculations
+   * `lazyUpdateInput()` is called every time text changes, thus allowing to preserve input state
+   * during layout recalculations
    */
   ONLY_LAZY_UPDATES,
+
   /**
-   * Expands ONLY_LAZY_UPDATES by calling {@code updateInput()} when line count changes, which
-   * triggers re-layout
+   * Expands ONLY_LAZY_UPDATES by calling `updateInput()` when line count changes, which triggers
+   * re-layout
    */
   UPDATE_ON_LINE_COUNT_CHANGE,
+
   /**
-   * {@code updateInput()} is called every time text changes, thus every change leads to
-   * recalculating layout
+   * `updateInput()` is called every time text changes, thus every change leads to recalculating
+   * layout
    */
   UPDATE_ON_TEXT_CHANGE
 }
