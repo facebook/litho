f0387d0e33cdd1bfda7c7e9cef760fcdbc5f5de0
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-09-13 08:12:22 -0700

    fbandroid/libraries/components/litho-espresso/src/main/java/com/facebook/litho/testing/espresso/LithoEspressoTestRunner.java
    Reviewed By: pentiumao
    
    Differential Revision: D62630573
    
    fbshipit-source-id: 0d59a0edcbe3f616a784c600376c0a98f316b005

--- litho-espresso/src/main/java/com/facebook/litho/testing/espresso/LithoEspressoTestRunner.java
+++ litho-espresso/src/main/java/com/facebook/litho/testing/espresso/LithoEspressoTestRunner.java
@@ -19,16 +19,19 @@ package com.facebook.litho.testing.espresso;
 import android.app.Application;
 import android.content.Context;
 import androidx.test.runner.AndroidJUnitRunner;
+import com.facebook.infer.annotation.Nullsafe;
 
 /**
  * An example test runner you can use to test your Litho apps in Espresso and standard integration
  * tests. Since JUnit Test Runners aren't composable, it may be best to copy the initialization
  * logic to your own test runner.
  */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class LithoEspressoTestRunner extends AndroidJUnitRunner {
   @Override
   public Application newApplication(ClassLoader cl, String className, Context context)
       throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    // NULLSAFE_FIXME[Not Vetted Third-Party]
     return super.newApplication(cl, InstrumentationApp.class.getName(), context);
   }
 }
