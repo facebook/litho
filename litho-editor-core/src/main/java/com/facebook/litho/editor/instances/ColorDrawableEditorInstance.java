9292100a02368b7c05152fbd65bbbe1d666e5f2d
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-09-13 08:11:18 -0700

    fbandroid/libraries/components/litho-editor-core/src/main/java/com/facebook/litho/editor/instances/ColorDrawableEditorInstance.java
    Differential Revision: D62629681
    
    fbshipit-source-id: b2c05aebdb618725a54eb25c26f62765e2ecff35

--- litho-editor-core/src/main/java/com/facebook/litho/editor/instances/ColorDrawableEditorInstance.java
+++ litho-editor-core/src/main/java/com/facebook/litho/editor/instances/ColorDrawableEditorInstance.java
@@ -17,12 +17,14 @@
 package com.facebook.litho.editor.instances;
 
 import android.graphics.drawable.ColorDrawable;
+import com.facebook.infer.annotation.Nullsafe;
 import com.facebook.litho.editor.Editor;
 import com.facebook.litho.editor.Reflection;
 import com.facebook.litho.editor.model.EditorNumber;
 import com.facebook.litho.editor.model.EditorValue;
 import java.lang.reflect.Field;
 
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class ColorDrawableEditorInstance implements Editor {
 
   @Override
