dc958a4a347b5e6adefb62dd0a464eed0de0e82b
Anna Powolny <apowolny@meta.com>
2024-09-27 07:13:05 -0700

    Docs + sample
    Summary: $title
    
    Reviewed By: pentiumao
    
    Differential Revision: D63276012
    
    fbshipit-source-id: a7820d088a09a36a23f0a1ff5f18c797595b6d61

---
+++ sample/src/main/java/com/facebook/samples/litho/kotlin/accessibility/FocusOrderComponent.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.samples.litho.kotlin.accessibility
+
+import com.facebook.litho.Column
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentScope
+import com.facebook.litho.KComponent
+import com.facebook.litho.Style
+import com.facebook.litho.accessibility.contentDescription
+import com.facebook.litho.accessibility.focusOrder
+import com.facebook.litho.core.padding
+import com.facebook.litho.kotlin.widget.Text
+import com.facebook.litho.kotlin.widget.TextInput
+import com.facebook.litho.useFocusOrder
+import com.facebook.litho.useState
+import com.facebook.litho.view.onClick
+import com.facebook.rendercore.dp
+
+class FocusOrderComponent : KComponent() {
+
+  override fun ComponentScope.render(): Component {
+    // start_focusorder_a11y_example
+    val hideElements = useState { false }
+    val (first, second, third, fourth, fifth) = useFocusOrder()
+
+    return Column(
+        style = Style.padding(16.dp).contentDescription("Fourth hello!").focusOrder(fourth)) {
+          child(
+              Text(
+                  text = "First hello!",
+                  style =
+                      Style.focusOrder(first).onClick {
+                        hideElements.update { !hideElements.value }
+                      }))
+
+          if (hideElements.value) {
+            child(Text(text = "Fifth hello!", style = Style.focusOrder(fifth)))
+            child(TextInput(initialText = "Second hello!", style = Style.focusOrder(second)))
+          }
+          child(Text(text = "Third hello!", style = Style.focusOrder(third)))
+        }
+    // end_focusorder_a11y_example
+
+  }
+}
