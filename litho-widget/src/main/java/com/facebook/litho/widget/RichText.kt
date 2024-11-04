636c6e7c7821f07a21c75551b11bbb3ce1a763b3
Anna Powolny <apowolny@meta.com>
2024-11-04 05:57:09 -0800

    Move RichText into our widgets
    Summary:
    Im working on deprecating Text Spec.
    So the first milestone of this project is to move RichText into our widgets.
    Next milestone is to add missing apis to TextPrimitive.
    
    Reviewed By: zielinskimz
    
    Differential Revision: D65268501
    
    fbshipit-source-id: c9c2eafea3fe76891cf008d8f19ae0310a5a8baf

---
+++ litho-widget/src/main/java/com/facebook/litho/widget/RichText.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.widget
+
+import com.facebook.litho.LithoPrimitive
+import com.facebook.litho.PrimitiveComponent
+import com.facebook.litho.PrimitiveComponentScope
+import com.facebook.litho.Style
+import com.facebook.litho.useCached
+import com.facebook.rendercore.text.RichTextPrimitive
+import com.facebook.rendercore.text.TextStyle
+
+class RichText(
+    val text: CharSequence,
+    val textStyle: TextStyle? = null,
+    private val style: Style? = null,
+) : PrimitiveComponent() {
+  override fun PrimitiveComponentScope.render(): LithoPrimitive {
+    val resolvedTextStyle =
+        useCached(textStyle) {
+          textStyle ?: TextStyle.createDefaultConfiguredTextStyle(androidContext)
+        }
+    return LithoPrimitive(
+        RichTextPrimitive(createPrimitiveId(), text, resolvedTextStyle), style = style)
+  }
+}
