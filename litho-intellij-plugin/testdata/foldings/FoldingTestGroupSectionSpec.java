de43cd635683a28cd2490344f7e7a3940f41870e
generatedunixname89002005279527 <generatedunixname89002005279527@meta.com>
2025-01-28 03:27:26 -0800

    fbandroid/libraries/components/litho-intellij-plugin/testdata/foldings
    Reviewed By: pasqualeanatriello
    
    Differential Revision: D68761405
    
    fbshipit-source-id: 798b800208a2367e786a20c14c75dc2bdef1c6ff

--- litho-intellij-plugin/testdata/foldings/FoldingTestGroupSectionSpec.java
+++
@@ -1,8 +0,0 @@
-@com.facebook.litho.sections.annotations.GroupSectionSpec
-public class FoldingTestGroupSectionSpec {
-  @com.facebook.litho.annotations.PropDefault
-  final static String withDefault = "Hello";
-
-  private void one(@com.facebook.litho.annotations.Prop String <fold text='withDefault: "Hello"'>withDefault</fold>, 
-                   @com.facebook.litho.annotations.Prop int withoutDefault) {}
-}
--- litho-intellij-plugin/testdata/foldings/FoldingTestLayoutSpec.java
+++
@@ -1,8 +0,0 @@
-@com.facebook.litho.annotations.LayoutSpec
-public class FoldingTestLayoutSpec {
-  @com.facebook.litho.annotations.PropDefault
-  final static String withDefault = "Hello";
-
-  private void one(@com.facebook.litho.annotations.Prop String <fold text='withDefault: "Hello"'>withDefault</fold>, 
-                   @com.facebook.litho.annotations.Prop int withoutDefault) {}
-}
