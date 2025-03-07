72f7d0de4a87c44f253a526b2e32ee81958570e2
Andrew Wang <wys@meta.com>
2025-03-07 12:03:29 -0800

    Introduce CollectionUpdateCallback to split committing changeset
    Summary: Introduce `CollectionUpdateCallback` to generate the changeset, which is mostly derived from `RecyclerBinderUpdateCallback` because I just wanted to avoid changing Sections code(DataDiffSection) and introducing any potential bug.
    
    Reviewed By: pasqualeanatriello, astreet
    
    Differential Revision: D70559719
    
    fbshipit-source-id: 57c0ba5e1fc3fd71d9ec61ae92a19dcb6ac726b0

---
+++ litho-widget-kotlin/src/main/kotlin/com/facebook/litho/widget/collection/CollectionUpdateCallback.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.widget.collection
+
+import androidx.recyclerview.widget.ListUpdateCallback
+import com.facebook.litho.widget.ComponentRenderInfo
+import com.facebook.litho.widget.RenderInfo
+
+/**
+ * This class aims to generate the changeset between two lists of data and create the corresponding
+ * RenderInfo for later use.
+ */
+class CollectionUpdateCallback<T>(
+    val prevData: List<T>?,
+    val nextData: List<T>?,
+    private val renderer: (index: Int, model: T) -> RenderInfo
+) : ListUpdateCallback {
+
+  internal val operations: MutableList<CollectionOperation> = mutableListOf()
+
+  override fun onInserted(position: Int, count: Int) {
+    val renderInfoList = ArrayList<RenderInfo>(count)
+    for (index in position until (position + count)) {
+      val renderInfo =
+          nextData?.get(index)?.let { model -> renderer(index, model) }
+              ?: ComponentRenderInfo.createEmpty()
+      renderInfoList.add(renderInfo)
+    }
+
+    operations.add(
+        CollectionOperation(
+            type = CollectionOperation.Type.INSERT,
+            index = position,
+            count = count,
+            renderInfoList = renderInfoList))
+  }
+
+  override fun onRemoved(position: Int, count: Int) {
+    operations.add(
+        CollectionOperation(
+            type = CollectionOperation.Type.DELETE, index = position, count = count))
+  }
+
+  override fun onMoved(fromPosition: Int, toPosition: Int) {
+    operations.add(
+        CollectionOperation(
+            type = CollectionOperation.Type.MOVE, index = fromPosition, toIndex = toPosition))
+  }
+
+  override fun onChanged(position: Int, count: Int, payload: Any?) {
+    val renderInfoList = ArrayList<RenderInfo>(count)
+
+    for (index in position until (position + count)) {
+      val renderInfo =
+          nextData?.get(index)?.let { model -> renderer(index, model) }
+              ?: ComponentRenderInfo.createEmpty()
+      renderInfoList.add(renderInfo)
+    }
+
+    operations.add(
+        CollectionOperation(
+            type = CollectionOperation.Type.UPDATE,
+            index = position,
+            count = count,
+            renderInfoList = renderInfoList))
+  }
+}
