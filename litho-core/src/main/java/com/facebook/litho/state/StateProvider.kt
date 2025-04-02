ce8a77113484099f34e56b5ef1cdd74668f39d3a
Kingsley Adio <kingsleyadio@meta.com>
2025-04-01 19:13:08 -0700

    Introduce StateProvider
    Summary:
    Introducing StateProvider abstraction for supplying the current value of state in a given scope
    
    [RFC](https://docs.google.com/document/d/1aFOON5LBtuTA9xibwv5WGqPtZi5SU3dquIMso7bw7hY/edit?pli=1&tab=t.0)
    
    Reviewed By: astreet
    
    Differential Revision: D72053552
    
    fbshipit-source-id: 725cdc5fe4dcf20caf5f3be4039cf476367a4a9c

---
+++ litho-core/src/main/java/com/facebook/litho/state/StateProvider.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.state
+
+import com.facebook.litho.State
+import com.facebook.litho.TreeState
+import com.facebook.litho.utils.LithoThreadLocal
+
+/**
+ * StateProvider is an abstraction used to supply the latest (and most accurate) value of a given
+ * [State] in a given scope, when read tracking is enabled.
+ *
+ * This makes it possible for the same [State] object to return different [State.value] in various
+ * scopes, unlocking new possibilities. For example, a state that's read in post-mount will have the
+ * correct value without having to recreate that state object when its value gets updated. This
+ * works even if the previous phases were completely skipped.
+ */
+interface StateProvider {
+  /**
+   * An identifier for the state provider's tree.
+   *
+   * It is especially useful in a setting where state may be read across tree boundaries. In such
+   * case, it may be desirable to adjust the access/tracking logic accordingly.
+   */
+  val treeId: Int
+
+  /**
+   * Registers a [TreeState] that the StateProvider should use to supply the appropriate [State]
+   * value.
+   *
+   * This function should typically be used together with [exitScope] in a try...finally block.
+   *
+   * @param [source] [TreeState] that should be used to supply [State] value
+   */
+  fun enterScope(source: TreeState)
+
+  /**
+   * Unregisters the [TreeState] from the StateProvider.
+   *
+   * @param [source] [TreeState] that is being unregistered from the state provider
+   */
+  fun exitScope(source: TreeState)
+
+  /**
+   * Retrieves the current value of the given [state] from the active scope.
+   *
+   * @param [state] state whose value is being requested
+   */
+  fun <T> getValue(state: State<T>): T
+}
+
+/**
+ * Internal abstraction used to provide the default [TreeState] for use by the [StateProvider].
+ *
+ * The [TreeState] supplied here is typically used whenever there's no active scope in flight.
+ */
+internal interface TreeStateProvider {
+  /** The default [TreeState] needed by the [StateProvider]. */
+  val treeState: TreeState
+}
+
+internal class StateProviderImpl(
+    override val treeId: Int,
+    private val treeStateProvider: TreeStateProvider
+) : StateProvider {
+
+  private val currentSource = LithoThreadLocal<TreeState>()
+
+  override fun enterScope(source: TreeState) {
+    check(currentSource.get() == null)
+    currentSource.set(source)
+  }
+
+  override fun exitScope(source: TreeState) {
+    check(source === currentSource.get())
+    currentSource.set(null)
+  }
+
+  override fun <T> getValue(state: State<T>): T {
+    error("Not yet implemented")
+  }
+}
