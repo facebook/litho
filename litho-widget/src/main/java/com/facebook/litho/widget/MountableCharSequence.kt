5321b782887b5442d21c2ed4c3c1eca84ed8adfa
Michal Zielinski <zielinskim@meta.com>
2025-04-08 04:23:15 -0700

    Convert MountableCharSequence to Kotlin
    Summary: As per title
    
    Reviewed By: apowolny
    
    Differential Revision: D72624404
    
    fbshipit-source-id: 5beacf99c01788d6685f0559d1db1239a8331c6f

--- litho-widget/src/main/java/com/facebook/litho/widget/MountableCharSequence.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/MountableCharSequence.kt
@@ -14,29 +14,28 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
-import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Drawable
 
 /**
- * Mountable {@link CharSequence} that is aware when the Text component using this CharSequence is
- * mounted and unmounted.
+ * Mountable [CharSequence] that is aware when the Text component using this CharSequence is mounted
+ * and unmounted.
  *
  * @see TextSpec
  */
-public interface MountableCharSequence extends CharSequence {
-
+interface MountableCharSequence : CharSequence {
   /**
    * This will be called once the text component using this MountableCharSequence is mounted.
    *
    * @param parent the parent drawable the char sequence is bound to
    */
-  void onMount(Drawable parent);
+  fun onMount(parent: Drawable)
 
   /**
    * This will be called when the text component using MountableCharSequence is unmounted.
    *
    * @param parent the parent drawable the CharSequence was mounted to
    */
-  void onUnmount(Drawable parent);
+  fun onUnmount(parent: Drawable)
 }
