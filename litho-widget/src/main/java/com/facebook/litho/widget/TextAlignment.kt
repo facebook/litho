a08cd2689e7bc4a7388b61f5065fea9e2af39db2
Kingsley Adio <kingsleyadio@meta.com>
2025-04-16 09:17:06 -0700

    Convert TextAlignment to Kotlin
    Summary: $title
    
    Reviewed By: zielinskimz
    
    Differential Revision: D73032179
    
    fbshipit-source-id: 9ebbb320a8a44e66d448477db8fded53954f3720

--- litho-widget/src/main/java/com/facebook/litho/widget/TextAlignment.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextAlignment.kt
@@ -14,41 +14,34 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
 /**
- * Enumeration of text alignment values. These values differ from {@link
- * android.text.Layout.Alignment} and View.TEXT_ALIGNMENT_* because this full list of values is not
- * supported by either implementation.
+ * Enumeration of text alignment values. These values differ from [android.text.Layout.Alignment]
+ * and `View.TEXT_ALIGNMENT_*` because this full list of values is not supported by either
+ * implementation.
  */
-public enum TextAlignment {
+enum class TextAlignment {
 
   /**
-   * Align the text to the start of the paragraph i.e. {@link
-   * android.view.View#TEXT_ALIGNMENT_TEXT_START} or {@link
-   * android.text.Layout.Alignment#ALIGN_NORMAL}.
+   * Align the text to the start of the paragraph i.e. [android.view.View.TEXT_ALIGNMENT_TEXT_START]
+   * or [android.text.Layout.Alignment.ALIGN_NORMAL].
    */
   TEXT_START,
 
   /**
-   * Align the text to the end of the paragraph i.e. {@link
-   * android.view.View#TEXT_ALIGNMENT_TEXT_END} or {@link
-   * android.text.Layout.Alignment#ALIGN_OPPOSITE}/.
+   * Align the text to the end of the paragraph i.e. [android.view.View.TEXT_ALIGNMENT_TEXT_END] or
+   * [android.text.Layout.Alignment.ALIGN_OPPOSITE].
    */
   TEXT_END,
 
   /** Align the text to the center. */
   CENTER,
 
-  /**
-   * Align the text to the start of the view i.e. {@link
-   * android.view.View#TEXT_ALIGNMENT_VIEW_START}.
-   */
+  /** Align the text to the start of the view i.e. [android.view.View.TEXT_ALIGNMENT_VIEW_START]. */
   LAYOUT_START,
 
-  /**
-   * Align the text to the start of the view i.e. {@link android.view.View#TEXT_ALIGNMENT_VIEW_END}.
-   */
+  /** Align the text to the start of the view i.e. [android.view.View.TEXT_ALIGNMENT_VIEW_END]. */
   LAYOUT_END,
 
   /** Align the text to the left, ignoring the text or locale preferences for LTR/RTL. */
