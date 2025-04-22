a0b13918a0d10df55807a17816f8a4e472a98820
Michal Zielinski <zielinskim@meta.com>
2025-04-22 07:47:23 -0700

    Convert RequestFocusEvent to Kotlin
    Summary: As per title
    
    Reviewed By: pentiumao
    
    Differential Revision: D73427928
    
    fbshipit-source-id: dcf604195e741509aaf8460c832a3c31992f2523

--- litho-widget/src/main/java/com/facebook/litho/widget/RequestFocusEvent.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/RequestFocusEvent.kt
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
-import com.facebook.litho.annotations.Event;
+import com.facebook.litho.annotations.Event
 
 /** Event sent to TextEdit to request focus. */
-@Event
-public class RequestFocusEvent {}
+@Event class RequestFocusEvent
