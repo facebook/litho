dad167f12bb36887d5cccff2afa0eddf39e65edb
Anna Powolny <apowolny@meta.com>
2025-04-29 04:28:44 -0700

    Convert TouchableSpanListener
    Summary: $title
    
    Reviewed By: kingsleyadio
    
    Differential Revision: D73189874
    
    fbshipit-source-id: e7a208bb85b7057b93f767cf5a2f3d6ad24a8ec2

--- litho-widget/src/main/java/com/facebook/litho/widget/TouchableSpanListener.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/TouchableSpanListener.kt
@@ -14,26 +14,24 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
-import android.text.style.ClickableSpan;
-import android.view.MotionEvent;
-import android.view.View;
-import javax.annotation.Nullable;
+import android.text.style.ClickableSpan
+import android.view.MotionEvent
+import android.view.View
 
 /**
  * A listener that reacts to touch events on a clickable span. A listener such as this should be
  * used when the touch action needs to access heavyweight objects such as fragments or activities.
- * Using such objects within the spannable can cause memory leaks as spannables are cached in {@link
- * android.text.TextLine#sCached} which is static.
+ * Using such objects within the spannable can cause memory leaks as spannables are cached in
+ * [android.text.TextLine.sCached] which is static.
  */
-public interface TouchableSpanListener {
-
+fun interface TouchableSpanListener {
   /**
    * @param span target span
    * @param motionEvent motion event
    * @param view source view
    * @return true if an action is taken with this touch event
    */
-  boolean onTouch(@Nullable ClickableSpan span, MotionEvent motionEvent, View view);
+  fun onTouch(span: ClickableSpan?, motionEvent: MotionEvent, view: View): Boolean
 }
