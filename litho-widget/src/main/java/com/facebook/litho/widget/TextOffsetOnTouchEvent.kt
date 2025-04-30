e2a57c745f4161537588af117c874251e6a37921
Anna Powolny <apowolny@meta.com>
2025-04-30 06:15:06 -0700

    Convert TextOffsetOnTouchEvent
    Summary: $title
    
    Reviewed By: zielinskimz
    
    Differential Revision: D73190434
    
    fbshipit-source-id: 4a041dcb5a7be03a567839de9074328b7bb94826

--- litho-widget/src/main/java/com/facebook/litho/widget/TextOffsetOnTouchEvent.kt
+++ litho-widget/src/main/java/com/facebook/litho/widget/TextOffsetOnTouchEvent.kt
@@ -14,31 +14,17 @@
  * limitations under the License.
  */
 
-package com.facebook.litho.widget;
+package com.facebook.litho.widget
 
-import com.facebook.litho.annotations.Event;
+import com.facebook.litho.annotations.Event
 
 /**
  * Text component should implement an event of this type in order to receive callback on what was
  * the text offset when text was touched initially. This event is fired only when motion event
  * action is ACTION_DOWN.
- *
- * An example of the correct usage is:
- *
- * <pre>
- * {@code
- *
- * @OnEvent(TextOffsetOnTouchEvent.class)
- * static void textOffsetOnTouchEvent(
- *     ComponentContext c,
- *     @FromEvent CharSequence text,
- *     @FromEvent int textOffset) {
- *     ...
- * }
- * </pre>
  */
 @Event
-public class TextOffsetOnTouchEvent {
-  public CharSequence text;
-  public int textOffset;
+class TextOffsetOnTouchEvent {
+  lateinit var text: CharSequence
+  @JvmField var textOffset: Int = 0
 }
