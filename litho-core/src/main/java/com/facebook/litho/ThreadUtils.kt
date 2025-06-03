5c9e99462bf02602a077e0cf1641728ac4a44e95
Case Beaman <caseb@meta.com>
2025-06-03 11:30:22 -0700

    Remove unused function
    Summary: Function is not used so it can be deleted
    
    Reviewed By: adityasharat
    
    Differential Revision: D75801359
    
    fbshipit-source-id: 6b61aced20c279f42d90960894ff52ecd7606f7b

--- litho-core/src/main/java/com/facebook/litho/ThreadUtils.kt
+++ litho-core/src/main/java/com/facebook/litho/ThreadUtils.kt
@@ -83,15 +83,6 @@ object ThreadUtils {
   }
 
   /**
-   * Try to raise the priority of {@param threadId} to the priority of the calling thread
-   *
-   * @return the original thread priority of the target thread.
-   */
-  @JvmStatic
-  fun tryInheritThreadPriorityFromCurrentThread(threadId: Int): Int =
-      tryRaiseThreadPriority(threadId, Process.getThreadPriority(Process.myTid()))
-
-  /**
    * Try to raise the priority of {@param threadId} to {@param targetThreadPriority}.
    *
    * @return the original thread priority of the target thread.
