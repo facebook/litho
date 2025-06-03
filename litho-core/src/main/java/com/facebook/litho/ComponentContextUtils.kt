b1469a94a503a95b1930fa28b3b7f20a2ac6017a
Aditya Sharat <adityasharat@meta.com>
2025-06-03 03:33:11 -0700

    Adds utility method to get config from component context
    Summary: Adds utility method to get config from component context
    
    Reviewed By: pentiumao
    
    Differential Revision: D75699316
    
    fbshipit-source-id: 0752ff342f5b2aea73b94705baa4f033e79ecd57

--- litho-core/src/main/java/com/facebook/litho/ComponentContextUtils.kt
+++ litho-core/src/main/java/com/facebook/litho/ComponentContextUtils.kt
@@ -32,3 +32,6 @@ object ComponentContextUtils {
         renderUnitIdGenerator = renderUnitIdGenerator)
   }
 }
+
+val ComponentContext.componentsConfig: ComponentsConfiguration
+  get() = lithoConfiguration.componentsConfig
