966c92ef8b73c66d0f606305cc4221d50eadd25a
Andrew Wang <wys@meta.com>
2025-06-05 08:25:44 -0700

    Introduce `LithoCollectionItem`
    Summary: As per title, the default implementation of data holder from Litho side.
    
    Reviewed By: adityasharat
    
    Differential Revision: D75784412
    
    fbshipit-source-id: 0576164c6180087dd7747f6d8574a9355f603fef

---
+++ litho-widget/src/main/java/com/facebook/litho/widget/LithoCollectionItem.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.litho.widget
+
+import com.facebook.litho.LithoRenderTreeView
+import com.facebook.litho.LithoRenderer
+import com.facebook.litho.LithoTree
+import com.facebook.rendercore.SizeConstraints
+
+/** A [CollectionItem] that renders a [LithoRenderTreeView]. */
+class LithoCollectionItem(
+    id: Int = LithoTree.generateComponentTreeId(),
+    viewType: Int,
+    renderInfo: RenderInfo,
+) : CollectionItem<LithoRenderTreeView>(id, viewType, renderInfo) {
+
+  private val renderer: LithoRenderer = LithoRenderer()
+
+  override fun prepare(sizeConstraints: SizeConstraints) {
+    renderer.render(sizeConstraints, null, false)
+  }
+
+  override fun prepareSync(
+      sizeConstraints: SizeConstraints,
+      result: IntArray?,
+      shouldCommit: Boolean
+  ) {
+    renderer.render(sizeConstraints, result, shouldCommit)
+  }
+
+  override fun onBindView(view: LithoRenderTreeView) {
+    renderer.currentLayoutState?.let { layoutState ->
+      view.setLayoutState(layoutState, layoutState.treeState)
+    }
+  }
+
+  override fun onViewRecycled(view: LithoRenderTreeView) {
+    view.clean()
+  }
+
+  override fun unprepare() {
+    // todo
+  }
+}
